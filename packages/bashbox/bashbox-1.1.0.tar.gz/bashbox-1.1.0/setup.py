cfb4a5551edda33486a686c03024aa0898bccd67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package bashbox/1.1.0/bashbox-1.1.0.tar.gz

---
+++ packages/bashbox/bashbox-1.1.0.tar.gz/bashbox-1.1.0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bashbox",
+    version="1.1.0",
+    author="Bash Elliott",
+    author_email="spicethings9@gmail.com",
+    description="Bash's textbox package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bash-elliott/bashbox",
+    project_urls={
+        "Bug Tracker": "https://github.com/bash-elliott/bashbox/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    packages=['bashbox'],
+    package_dir={"bashbox": "src/bashbox"},
+    package_data={'bashbox': ['themes/*']},
+    python_requires=">=3.6",
+
+    include_package_data=True
+)
