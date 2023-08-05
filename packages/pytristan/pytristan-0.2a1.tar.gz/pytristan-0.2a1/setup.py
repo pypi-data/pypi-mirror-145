8cc86e44ca64a23e709f61bd93d6751283c0253e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:09 +0000

    Add package pytristan/0.2a1/pytristan-0.2a1.tar.gz

---
+++ packages/pytristan/pytristan-0.2a1.tar.gz/pytristan-0.2a1/setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from setuptools import setup
+import versioneer
+
+if __name__ == "__main__":
+    setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
