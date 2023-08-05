6257433b0ce5e591735a7c5cbb494a2d81fcc602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:31 +0000

    Add package DE-LogEventos/0.0.8/DE_LogEventos-0.0.8.tar.gz

---
+++ packages/DE-LogEventos/DE_LogEventos-0.0.8.tar.gz/DE_LogEventos-0.0.8/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='DE_LogEventos',
+    version='0.0.8',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Almir J Gomes',
+    author_email='almir.jg@hotmail.com',
+    description='Log de eventos em processos'
+)
+
