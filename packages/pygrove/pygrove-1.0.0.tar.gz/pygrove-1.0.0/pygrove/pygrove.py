60508ee1add475663d6b227bcd2c64e1714288cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:03 +0000

    Add package pygrove/1.0.0/pygrove-1.0.0.tar.gz

---
+++ packages/pygrove/pygrove-1.0.0.tar.gz/pygrove-1.0.0/pygrove/pygrove.py
@@ -0,0 +1,33 @@
+import pyforest
+from pyforest import LazyImport
+
+# HuggingFace
+Trainer = LazyImport("from transformers import Trainer")
+AutoTokenizer = LazyImport("from transformers import AutoTokenizer")
+AutoConfig = LazyImport("from transformers import AutoConfig")
+AutoModel = LazyImport("from transformers import AutoModel")
+AutoModelForSequenceClassification = LazyImport("from transformers import AutoModelForSequenceClassification")
+AutoModelForTokenClassification = LazyImport("from transformers import AutoModelForTokenClassification")
+AutoModelForMultipleChoice = LazyImport("from transformers import AutoModelForMultipleChoice")
+AutoModelForQuestionAnswering = LazyImport("from transformers import AutoModelForQuestionAnswering")
+load_dataset = LazyImport("from datasets import load_dataset")
+Dataset = LazyImport("from datasets import Dataset")
+load_metric = LazyImport("from datasets import load_dataset")
+dataset = LazyImport("import datasets")
+transformers = LazyImport("import Transformers")
+
+
+# Helpers
+ftfy = LazyImport("import ftfy")
+fc = LazyImport("import funcy as fc")
+warnings = LazyImport("import warnings")
+tqdm = LazyImport("from tqdm.auto import tqdm")
+random = LazyImport("import random")
+itertools = LazyImport("import itertools")
+appdirs = LazyImport("import appdirs")
+copy = LazyImport("import copy")
+defaultdict = LazyImport("from collections import defaultdict")
+Counter = LazyImport("from collections import Counter")
+edict = LazyImport("from easydict import EasyDict as edict")
+Xp = LazyImport("from xpflow import Xp")
+millify = LazyImport("from millify import millify")
