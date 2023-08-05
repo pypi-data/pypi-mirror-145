3650c4dafa498659be009f8fd05a90b2b8fe2743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:47 +0000

    Add package dimfred/0.5.0/dimfred-0.5.0.tar.gz

---
+++ packages/dimfred/dimfred-0.5.0.tar.gz/dimfred-0.5.0/dimfred/__init__.py
@@ -0,0 +1,12 @@
+from .worker import Worker, Queue
+from .stopwatch import Stopwatch
+from .config import Config
+from .pipe import fpipe, pipe
+from . import fastapi_utils
+
+from tabulate import tabulate
+from easydict import EasyDict as edict
+from pluck import pluck
+import click
+
+
