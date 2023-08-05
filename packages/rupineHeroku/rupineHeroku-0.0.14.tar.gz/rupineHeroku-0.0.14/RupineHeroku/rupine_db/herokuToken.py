8ecadb2b9dc42d2470ed04997390687355d1ea58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:15 +0000

    Add package rupineHeroku/0.0.14/rupineHeroku-0.0.14.tar.gz

---
+++ packages/rupineHeroku/rupineHeroku-0.0.14.tar.gz/rupineHeroku-0.0.14/RupineHeroku/rupine_db/herokuToken.py
@@ -0,0 +1,28 @@
+import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util import Retry
+import json
+from RupineHeroku.rupine_db import herokuDbAccess
+from psycopg2 import sql
+
+def getToken(connection, schema, chain_id, token_address):
+    
+    query = "SELECT * FROM %s.token WHERE chain_id = %s AND token_address = '%s';"
+    
+    result = herokuDbAccess.fetchDataInDatabase(query, [sql.Identifier(schema),chain_id,token_address], connection)    
+    return result[0][0]
+
+def getTokenWithoutName(connection, schema, chain_id, gteCreatedAt):
+    
+    query = "SELECT * FROM %s.evm_token WHERE chain_id = %s AND name = 'n/a' AND created_at >= %s"
+    
+    result = herokuDbAccess.fetchDataInDatabase(query, [sql.Identifier(schema),chain_id,gteCreatedAt], connection)    
+    return result[0][0]
+
+def getTokenWN(connection, schema, chain_id, gteCreatedAt):
+    
+    query = sql.SQL("SELECT * FROM {}.evm_token WHERE chain_id = %s AND name = 'n/a' AND created_at >= %s").format(sql.Identifier(schema))
+    #query = "SELECT * FROM %s.evm_token WHERE chain_id = %s AND name = 'n/a' AND created_at >= %s"
+    
+    result = herokuDbAccess.fetchDataInDatabase(query, [sql.Identifier(schema),chain_id,gteCreatedAt], connection)    
+    return result[0][0]
