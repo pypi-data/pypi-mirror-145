4ed6424fa1c65bdc942691ca0768e3b4b99053b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:42 +0000

    Add package obj-encrypt/0.7.0/obj-encrypt-0.7.0.tar.gz

---
+++ packages/obj-encrypt/obj-encrypt-0.7.0.tar.gz/obj-encrypt-0.7.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="obj-encrypt",
+    version="0.7.0",
+    author="Cyberbolt",
+    author_email="dtconlyone@gmail.com",
+    description="obj-encrypt is an encryption library based on the AES-256 algorithm. It uses Python objects as the basic unit, which can convert objects into binary ciphertext and support decryption. Objects encrypted with obj-encrypt support TCP communication, database storage, and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",    
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pycryptodome>=3.14.1'
+    ]    
+)
