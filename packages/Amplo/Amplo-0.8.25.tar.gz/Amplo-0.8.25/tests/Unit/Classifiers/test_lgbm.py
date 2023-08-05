fbfff627647692b79723747564cec6997d7f494d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:42 +0000

    Add package Amplo/0.8.25/Amplo-0.8.25.tar.gz

---
+++ packages/Amplo/Amplo-0.8.25.tar.gz/Amplo-0.8.25/tests/Unit/Classifiers/test_lgbm.py
@@ -0,0 +1,14 @@
+import unittest
+import pandas as pd
+from sklearn.datasets import make_classification
+from Amplo.Classifiers import LGBMClassifier
+from .test_classifier import TestClassifier
+
+
+class TestLGBMClassifier(unittest.TestCase, TestClassifier):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.model = LGBMClassifier()
+        x, y = make_classification(n_classes=5, n_informative=15)
+        cls.x, cls.y = pd.DataFrame(x), pd.Series(y)
