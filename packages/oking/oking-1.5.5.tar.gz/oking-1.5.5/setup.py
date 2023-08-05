d49930bdd3cd785b44552d7dee29067dc3e4e3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:57 +0000

    Add package oking/1.5.5/oking-1.5.5.tar.gz

---
+++ packages/oking/oking-1.5.5.tar.gz/oking-1.5.5/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname), encoding='UTF-8').read()
+
+
+def save_version(v: str):
+    f = open('version.txt', 'w')
+    f.write(f'\tOking {v} - Openk Tecnologia')
+    f.close()
+
+
+# Utilizar o padrão x.x.x.xxxx para caso precise subir versão de testes para o respositorio test-pypi
+# Utilizar o padrão x.x.x para subir em produção
+version = '1.5.5'
+save_version(version)
+setup(
+    name="oking",
+    version=version,
+    author="Openk Tecnologia",
+    author_email="<suporte.b2c@openk.com.br>",
+    description='Pacote de integração de produtos, preço, estoque e pedidos com o sistema OkVendas da Openk',
+    long_description_content_type="text/markdown; charset=UTF-8; variant=GFM",
+    long_description=read('README.md'),
+    packages=find_packages(include=['src', 'src.*']),
+    install_requires=['mysql-connector-python',
+                      'schedule',
+                      'requests',
+                      'configparser',
+                      'logger',
+                      'cx-Oracle',
+                      'jsonpickle',
+                      'pyodbc'],
+    keywords=['python', 'oking', 'openk', 'okvendas', 'ok'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        # "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+        'console_scripts': ['oking=src.__main__:main']
+    }
+)
