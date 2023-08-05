4581fae7821c5a069e827674b74781916cfc95fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:59 +0000

    Add package graia-ariadne/0.6.15/graia-ariadne-0.6.15.tar.gz

---
+++ packages/graia-ariadne/graia-ariadne-0.6.15.tar.gz/graia-ariadne-0.6.15/src/graia/ariadne/message/parser/base.py
@@ -0,0 +1,242 @@
+"""Ariadne 基础的 parser, 包括 DetectPrefix 与 DetectSuffix"""
+import abc
+import fnmatch
+import re
+from typing import List, Optional, Type, Union
+
+from graia.broadcast.entities.decorator import Decorator
+from graia.broadcast.exceptions import ExecutionStop
+from graia.broadcast.interfaces.decorator import DecoratorInterface
+from loguru import logger
+
+from ... import get_running
+from ...event.message import GroupMessage
+from ..chain import MessageChain
+from ..element import At, Element, Plain, Quote, Source
+
+
+class Compose(Decorator):
+    """将多个基础 Decorator 串联起来"""
+
+    def __init__(self, *deco: "ChainDecorator") -> None:
+        self.deco: List[ChainDecorator] = list(deco)
+
+    async def target(self, interface: DecoratorInterface):
+        chain = await interface.dispatcher_interface.lookup_param("message_chain", MessageChain, None)
+        for deco in self.deco:
+            chain = await deco.decorate(chain, interface)
+            if chain is None:
+                break
+        if interface.annotation is MessageChain:
+            if chain is None:
+                raise ExecutionStop
+            return chain
+
+
+class ChainDecorator(abc.ABC, Decorator):
+    @abc.abstractmethod
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        ...
+
+    async def target(self, interface: DecoratorInterface):
+        return await self.decorate(
+            await interface.dispatcher_interface.lookup_param("message_chain", MessageChain, None), interface
+        )
+
+
+class DetectPrefix(ChainDecorator):
+    """前缀检测器"""
+
+    pre = True
+
+    def __init__(self, prefix: str) -> None:
+        """初始化前缀检测器.
+
+        Args:
+            prefix (str): 要匹配的前缀
+        """
+        self.prefix = prefix
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        header = chain.include(Quote, Source)
+        rest: MessageChain = chain.exclude(Quote, Source)
+        if not rest.startswith(self.prefix):
+            raise ExecutionStop
+        result = rest.removeprefix(self.prefix).removeprefix(" ")
+        if interface.annotation is MessageChain:
+            return header + result
+
+
+class DetectSuffix(ChainDecorator):
+    """后缀检测器"""
+
+    pre = True
+
+    def __init__(self, suffix: str) -> None:
+        """初始化后缀检测器.
+
+        Args:
+            suffix (str): 要匹配的后缀
+        """
+        self.suffix = suffix
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        header = chain.include(Quote, Source)
+        rest: MessageChain = chain.exclude(Quote, Source)
+        if not rest.endswith(self.suffix):
+            raise ExecutionStop
+        result = rest.removesuffix(self.suffix).removesuffix(" ")
+        if interface.annotation is MessageChain:
+            return header + result
+
+
+class MentionMe(ChainDecorator):
+    """At 账号或者提到账号群昵称"""
+
+    pre = True
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        ariadne = get_running()
+        if isinstance(interface.event, GroupMessage):
+            if not ariadne.account:
+                logger.warning("Unable to detect Ariadne's name because account is not set")
+                raise ExecutionStop
+            name = (await ariadne.getMember(interface.event.sender.group, ariadne.account)).name
+        else:
+            name = (await ariadne.getBotProfile()).nickname
+        header = chain.include(Quote, Source)
+        rest: MessageChain = chain.exclude(Quote, Source)
+        first: Element = rest[0]
+        result: Optional[MessageChain] = None
+        if rest and isinstance(first, Plain) and first.asDisplay().startswith(name):
+            result = header + rest.removeprefix(name).removeprefix(" ")
+        if rest and isinstance(first, At) and first.target == ariadne.account:
+            result = header + MessageChain(rest.__root__[1:], inline=True).removeprefix(" ")
+
+        if result is None:
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return result
+
+
+class Mention(ChainDecorator):
+    """At 或提到指定人"""
+
+    pre = True
+
+    def __init__(self, target: Union[int, str]) -> None:
+        self.person: Union[int, str] = target
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        header = chain.include(Quote, Source)
+        rest: MessageChain = chain.exclude(Quote, Source)
+        first: Element = rest[0]
+        result: Optional[MessageChain] = None
+        if (
+            rest
+            and isinstance(first, Plain)
+            and isinstance(self.person, str)
+            and first.asDisplay().startswith(self.person)
+        ):
+            result = header + rest.removeprefix(self.person).removeprefix(" ")
+        if rest and isinstance(first, At) and isinstance(self.person, int) and first.target == self.person:
+            result = header + MessageChain(rest.__root__[1:], inline=True).removeprefix(" ")
+
+        if result is None:
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return result
+
+
+class ContainKeyword(ChainDecorator):
+    """消息中含有指定关键字"""
+
+    pre = True
+
+    def __init__(self, keyword: str) -> None:
+        self.keyword: str = keyword
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        if self.keyword not in chain:
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return chain
+
+
+class MatchContent(ChainDecorator):
+    """匹配字符串 / 消息链"""
+
+    pre = True
+
+    def __init__(self, content: Union[str, MessageChain]) -> None:
+        self.content: Union[str, MessageChain] = content
+        self.next: Optional[ChainDecorator] = None
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        if isinstance(self.content, str) and chain.asDisplay() != self.content:
+            raise ExecutionStop
+        if isinstance(self.content, MessageChain) and chain != self.content:
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return chain
+
+
+class MatchRegex(ChainDecorator):
+    """匹配正则表达式"""
+
+    pre = True
+
+    def __init__(self, regex: str, flags: re.RegexFlag = re.RegexFlag(0)) -> None:
+        """初始化匹配正则表达式.
+
+        Args:
+            regex (str): 正则表达式
+            flags (re.RegexFlag): 正则表达式标志
+        """
+        self.regex: str = regex
+        self.flags: re.RegexFlag = flags
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        if not re.match(self.regex, chain.asDisplay(), self.flags):
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return chain
+
+
+class MatchTemplate(ChainDecorator):
+    """模板匹配"""
+
+    def __init__(self, template: List[Union[Type[Element], Element]]) -> None:
+        self.template: List[Union[Type[Element], Element, str]] = []
+        for element in template:
+            if isinstance(element, type) and element is not Plain:
+                self.template.append(element)
+            elif isinstance(element, Element) and not isinstance(element, Plain):
+                self.template.append(element)
+            else:
+                element = element.text if isinstance(element, Plain) else "*"
+                if self.template and isinstance(self.template[-1], str):
+                    self.template[-1] = self.template[-1] + element
+                else:
+                    self.template.append(element)
+
+    def match(self, chain: MessageChain):
+        """匹配消息链"""
+        chain = chain.asSendable()
+        if len(self.template) != len(chain):
+            return False
+        for element, template in zip(chain, self.template):
+            if isinstance(template, type) and not isinstance(element, template):
+                return False
+            elif isinstance(template, Element) and element != template:
+                return False
+            elif isinstance(template, str):
+                if not isinstance(element, Plain) or not fnmatch.fnmatch(element.text, template):
+                    return False
+        return True
+
+    async def decorate(self, chain: MessageChain, interface: DecoratorInterface) -> Optional[MessageChain]:
+        if not self.match(chain):
+            raise ExecutionStop
+        if interface.annotation is MessageChain:
+            return chain
---
+++ packages/graia-ariadne/graia-ariadne-0.6.15.tar.gz/graia-ariadne-0.6.15/src/graia/ariadne/message/parser/util.py
@@ -0,0 +1,291 @@
+"""消息链处理器用到的工具函数, 类"""
+import argparse
+import enum
+import inspect
+import re
+from contextvars import ContextVar
+from typing import Any, Dict, List, NoReturn, Tuple, Type, Union
+
+from graia.ariadne.message.element import Element
+
+from ..chain import Element_T, MessageChain
+
+elem_mapping_ctx: ContextVar[Dict[str, Element]] = ContextVar("elem_mapping_ctx")
+
+L_PAREN = ("{", "[")
+R_PAREN = ("}", "]")
+ESCAPE = {
+    "\\": "\x00",
+    "[": "\x01",
+    "]": "\x02",
+    "{": "\x03",
+    "}": "\x04",
+    "|": "\x05",
+}
+R_ESCAPE = {v: k for k, v in ESCAPE.items()}
+
+
+def escape(string: str) -> str:
+    """转义字符串
+
+    Args:
+        string (str): 要转义的字符串
+
+    Returns:
+        str: 转义后的字符串
+    """
+    for k, v in ESCAPE.items():
+        string = string.replace("\\" + k, v)
+    string = string.replace("\\", "")
+    return string
+
+
+def unescape(string: str) -> str:
+    """逆转义字符串, 自动去除空白符
+
+    Args:
+        string (str): 要逆转义的字符串
+
+    Returns:
+        str: 逆转义后的字符串
+    """
+    for k, v in R_ESCAPE.items():
+        string = string.replace(k, v)
+    return string.strip()
+
+
+class CommandToken(enum.Enum):
+    """Command 的 Token."""
+
+    TEXT = "TEXT"
+    CHOICE = "CHOICE"
+    PARAM = "PARAM"
+    ANNOTATED = "ANNOTATED"
+
+
+CommandTokenTuple = Tuple[CommandToken, List[Any]]
+
+
+def tokenize_command(string: str) -> List[CommandTokenTuple]:
+    """将字符串转义化, 并处理为 Text, Choice, Param, AnnotatedParam 四种 token
+
+    Args:
+        string (str): 要处理的字符串
+
+    Returns:
+        List[Tuple[CommandToken, List[int, str]]]: 处理后的 Token
+    """
+
+    string = escape(string)
+
+    paren: str = ""
+    char_stk: List[str] = []
+    token: List[CommandTokenTuple] = []
+
+    for index, char in enumerate(string):
+        if char in L_PAREN + R_PAREN:
+            if char in L_PAREN:
+                if paren:
+                    raise ValueError(
+                        f"""Duplicated parenthesis character "{char}" @ {index} !"""
+                        """Are you sure you've escaped with "\\"?"""
+                    )
+                paren = char
+            elif char in R_PAREN:
+                if paren == "[":  # CHOICE
+                    token.append((CommandToken.CHOICE, list(map(unescape, "".join(char_stk).split("|")))))
+                elif paren == "{":  # PARAM
+                    piece = "".join(char_stk)
+                    match = re.fullmatch(
+                        r"(?P<wildcard>\.\.\.)?"
+                        r"(?P<name>[^:=|]+)"
+                        r"(?P<annotation>:[^=]+)?"
+                        r"(?P<default>=.+)?",
+                        piece,
+                    )
+                    if match and any(s in piece for s in ".:="):
+                        token.append(  # type: List[str]
+                            (
+                                CommandToken.ANNOTATED,
+                                list(
+                                    map(
+                                        lambda x: unescape(x).strip().lstrip(":=").strip() if x else "",
+                                        match.groups(),
+                                    )
+                                ),
+                            )
+                        )
+                    else:
+                        token.append(
+                            (
+                                CommandToken.PARAM,
+                                [
+                                    int(i) if re.match(r"\d+", i) else unescape(i)
+                                    for i in "".join(char_stk).split("|")
+                                ],
+                            )
+                        )
+                else:
+                    raise ValueError(f"No matching parenthesis: {paren} @ {index}")
+                char_stk.clear()
+                paren = ""
+            char_stk.clear()
+        elif char == " " and not paren:
+            if char_stk:
+                token.append((CommandToken.TEXT, ["".join(char_stk)]))
+                char_stk.clear()
+        else:
+            char_stk.append(char)
+
+    if paren:
+        raise ValueError(f"Unclosed parenthesis: {paren}")
+
+    if char_stk:
+        token.append((CommandToken.TEXT, ["".join(char_stk)]))
+        char_stk.clear()
+
+    return token
+
+
+def split(string: str, keep_quote: bool = False) -> List[str]:
+    """尊重引号与转义的字符串切分
+
+    Args:
+        string (str): 要切割的字符串
+        keep_quote (bool): 是否保留引号, 默认 False.
+
+    Returns:
+        List[str]: 切割后的字符串, 可能含有空格
+    """
+    result: List[str] = []
+    quote = ""
+    cache: List[str] = []
+    for index, char in enumerate(string):
+        if char in {"'", '"'}:
+            if not quote:
+                quote = char
+            elif char == quote and index and string[index - 1] != "\\":  # is current quote, not transfigured
+                quote = ""
+            else:
+                cache.append(char)
+                continue
+            if keep_quote:
+                cache.append(char)
+        elif not quote and char == " ":
+            result.append("".join(cache))
+            cache = []
+        elif char != "\\":
+            cache.append(char)
+    if cache:
+        result.append("".join(cache))
+    return result
+
+
+def gen_flags_repr(flags: re.RegexFlag) -> str:
+    """通过 RegexFlag 生成对应的字符串
+
+    Args:
+        flags (re.RegexFlag): 正则表达式的标记
+
+    Returns:
+        str: 对应的标记字符串
+    """
+    flags_list: List[str] = []
+
+    if re.ASCII in flags:
+        flags_list.append("a")
+    if re.IGNORECASE in flags:
+        flags_list.append("i")
+    if re.LOCALE in flags:
+        flags_list.append("L")
+    if re.MULTILINE in flags:
+        flags_list.append("m")
+    if re.DOTALL in flags:
+        flags_list.append("s")
+    if re.UNICODE in flags:
+        flags_list.append("u")
+    if re.VERBOSE in flags:
+        flags_list.append("x")
+
+    return "".join(flags_list)
+
+
+def transform_regex(flag: re.RegexFlag, regex_pattern: str) -> str:
+    """生成嵌套正则表达式字符串来达到至少最外层含有一个捕获组的效果
+
+    Args:
+        flag (re.RegexFlag): 正则表达式标记
+        regex_pattern (str): 正则表达式字符串
+
+    Returns:
+        str: 转换后的正则表达式字符串
+    """
+    if flag:
+        regex_pattern = f"(?{gen_flags_repr(flag)}:({regex_pattern}))"
+    else:
+        regex_pattern = f"({regex_pattern})"
+    return regex_pattern
+
+
+class MessageChainType:
+    """用于标记类型为消息链, 在 ArgumentMatch 上使用"""
+
+    @staticmethod
+    def __call__(string: str) -> MessageChain:
+        return MessageChain._from_mapping_string(string, elem_mapping_ctx.get())
+
+
+class ElementType:
+    """用于标记类型为消息链元素, 在 ArgumentMatch 上使用"""
+
+    def __init__(self, pattern: Type[Element_T]):
+        self.regex = re.compile(f"\x02(\\d+)_{pattern.__fields__['type'].default}\x03")
+
+    def __call__(self, string: str) -> Element:
+        if not self.regex.fullmatch(string):
+            raise ValueError(f"{string} not matching {self.regex.pattern}")
+        return MessageChain._from_mapping_string(string, elem_mapping_ctx.get())[0]
+
+
+class UnmatchedClass:
+    __slots__ = ()
+
+    def __repr__(self) -> str:
+        return "<Unmatched>"
+
+    def __bool__(self) -> bool:
+        return False
+
+
+Unmatched = UnmatchedClass()
+
+
+class TwilightParser(argparse.ArgumentParser):
+    """适于 Twilight 使用的 argparse.ArgumentParser 子类
+    移除了报错时自动退出解释器的行为
+    """
+
+    def error(self, message) -> NoReturn:
+        raise ValueError(message)
+
+    def accept_type(self, action: Union[str, type]) -> bool:
+        """检查一个 action 是否接受 type 参数
+
+        Args:
+            action (Union[str, type]): 检查的 action
+
+        Returns:
+            bool: 是否接受 type 参数
+        """
+        if action is ...:
+            action = "store"
+        if isinstance(action, str):
+            action_cls: Type[argparse.Action] = self._registry_get("action", action, action)
+        elif isinstance(action, type) and issubclass(action, argparse.Action):
+            action_cls = action
+        else:
+            return False
+        action_init_sig = inspect.signature(action_cls.__init__)
+        if "type" not in action_init_sig.parameters:
+            return False
+        return True
