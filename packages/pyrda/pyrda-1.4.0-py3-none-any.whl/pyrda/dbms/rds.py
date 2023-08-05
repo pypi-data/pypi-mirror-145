12aecfc9c25d9d0db6d12eb089ea83662e2e0fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:41 +0000

    Add package pyrda/1.4.0/pyrda-1.4.0-py3-none-any.whl

---
+++ packages/pyrda/pyrda-1.4.0-py3-none-any.whl/pyrda/dbms/rds.py
@@ -0,0 +1,25 @@
+from pyrda.dbms.mssql import MsSqlClient
+#local mode
+#from pyrda.dbms.mssql import MsSqlClient
+#from config import cfg_setting
+#package mode
+from .mssql import MsSqlClient
+from .config import cfg_setting
+class RdClient(MsSqlClient):
+    def __init__(self,token):
+        ip = cfg_setting['host'] + '8'
+        user_name = cfg_setting['user']
+        password = cfg_setting['password'] +'@'
+        db_name = cfg_setting['database'] +'gox'
+        sql = cfg_setting['sql'] + "  where FToken ='" + token+ "'"
+        MsSqlClient.__init__(self,ip=ip,user_name=user_name,password=password,db_name=db_name)
+        login = MsSqlClient.select(self,sql=sql)
+        ncount = len(login)
+        if ncount >0:
+            self.ip = login[0][0]
+            self.user_name = login[0][1]
+            self.password = login[0][2]
+            self.db_name = login[0][3]
+            MsSqlClient.__init__(self, ip=self.ip, user_name=self.user_name, password=self.password, db_name=self.db_name)
+if __name__ == '__main__':
+    pass
