4e246359d56d5fc9a7e9691e8c137fcf059fd0a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:40 +0000

    Add package pyoverkiz/1.3.14/pyoverkiz-1.3.14.tar.gz

---
+++ packages/pyoverkiz/pyoverkiz-1.3.14.tar.gz/pyoverkiz-1.3.14/pyoverkiz/exceptions.py
@@ -0,0 +1,69 @@
+class BadCredentialsException(Exception):
+    pass
+
+
+class InvalidCommandException(Exception):
+    pass
+
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+class TooManyExecutionsException(Exception):
+    pass
+
+
+class TooManyRequestsException(Exception):
+    pass
+
+
+class TooManyConcurrentRequestsException(Exception):
+    pass
+
+
+class MaintenanceException(Exception):
+    pass
+
+
+class InvalidEventListenerIdException(Exception):
+    pass
+
+
+class NoRegisteredEventListenerException(Exception):
+    pass
+
+
+class SessionAndBearerInSameRequestException(Exception):
+    pass
+
+
+class TooManyAttemptsBannedException(Exception):
+    pass
+
+
+# Nexity
+class NexityBadCredentialsException(BadCredentialsException):
+    pass
+
+
+class NexityServiceException(Exception):
+    pass
+
+
+# CozyTouch
+class CozyTouchBadCredentialsException(BadCredentialsException):
+    pass
+
+
+class CozyTouchServiceException(Exception):
+    pass
+
+
+# Somfy
+class SomfyBadCredentialsException(BadCredentialsException):
+    pass
+
+
+class SomfyServiceException(Exception):
+    pass
