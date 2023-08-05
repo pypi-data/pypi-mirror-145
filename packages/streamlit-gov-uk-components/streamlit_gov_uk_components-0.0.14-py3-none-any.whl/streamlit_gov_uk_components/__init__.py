669d12d8fd44153795fbaa201b80f5ffc427f54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:57 +0000

    Add package streamlit-gov-uk-components/0.0.14/streamlit_gov_uk_components-0.0.14-py3-none-any.whl

---
+++ packages/streamlit-gov-uk-components/streamlit_gov_uk_components-0.0.14-py3-none-any.whl/streamlit_gov_uk_components/__init__.py
@@ -0,0 +1,46 @@
+import os
+import streamlit.components.v1 as components
+
+_DEV = os.environ.get('STREAMLIT_GOV_UK_COMPONENTS_DEV')
+
+def _component_options(path):
+    return{
+        "url": "http://localhost:3001",
+    } if _DEV else {
+        "path": os.path.join(os.path.dirname(os.path.abspath(__file__)), path + "/build")
+    }
+
+_gov_uk_checkbox = components.declare_component("gov_uk_checkbox",  **_component_options('frontend_checkbox'))
+
+def gov_uk_checkbox(label, id, key=None, default=False):
+    return _gov_uk_checkbox(label=label, id=id, key=key, default=default)
+
+
+_gov_uk_checkbox_list = components.declare_component("gov_uk_checkbox_list", **_component_options('frontend_checkbox_list'))
+
+def gov_uk_checkbox_list(options, key=None, default=()):
+    return tuple(_gov_uk_checkbox_list(options=options, key=key, default=default))
+
+
+_gov_uk_checkbox_small = components.declare_component("gov_uk_checkbox_small", **_component_options('frontend_checkbox_small'))
+
+def gov_uk_checkbox_small(label, id, key=None, default=False):
+    return _gov_uk_checkbox_small(label=label, id=id, key=key, default=default)
+
+
+_gov_uk_checkbox_small_list = components.declare_component("gov_uk_checkbox_small_list ", **_component_options('frontend_checkbox_small_list'))
+
+def gov_uk_checkbox_small_list(options, key=None, default=()):
+    return tuple(_gov_uk_checkbox_small_list(options=options, key=key, default=default))
+
+
+_gov_uk_radio_list = components.declare_component("gov_uk_radio_list", **_component_options('frontend_radio_list'))
+
+def gov_uk_radio_list(options, id, key=None, default=None):
+    return _gov_uk_radio_list(options=options, id=id, key=key, default=default)
+
+
+_gov_uk_radio_small_list = components.declare_component("gov_uk_radio_small_list", **_component_options('frontend_radio_small_list'))
+
+def gov_uk_radio_small_list(options, id, key=None, default=None):
+    return _gov_uk_radio_small_list(options=options, id=id, key=key, default=default)
