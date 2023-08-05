db1e53a1810f8b56650393ed0345119e3ebaab7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:17 +0000

    Add package d3blocks/0.1.0/d3blocks-0.1.0.tar.gz

---
+++ packages/d3blocks/d3blocks-0.1.0.tar.gz/d3blocks-0.1.0/d3blocks/__init__.py
@@ -0,0 +1,32 @@
+from d3blocks.d3blocks import d3blocks
+
+from d3blocks.d3blocks import (
+    import_example,
+    load_example,
+    )
+
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '0.1.0'
+
+# module level doc-string
+__doc__ = """
+d3blocks
+=====================================================================
+
+Description
+-----------
+d3blocks is for...
+
+Example
+-------
+>>> import d3blocks as d3blocks
+>>> model = d3blocks.fit_transform(X)
+>>> fig,ax = d3blocks.plot(model)
+
+References
+----------
+https://github.com/d3blocks/d3blocks
+
+"""
