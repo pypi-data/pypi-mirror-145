d957539cb7b20d94df6eb58919d92dc6cb1aff47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:19 +0000

    Add package scout-browser/4.51/scout-browser-4.51.tar.gz

---
+++ packages/scout-browser/scout-browser-4.51.tar.gz/scout-browser-4.51/scout/server/blueprints/alignviewers/views.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+import logging
+import os.path
+
+import requests
+from flask import Blueprint, Response, abort, render_template, request, send_file
+
+from . import controllers
+from .partial import send_file_partial
+
+alignviewers_bp = Blueprint(
+    "alignviewers",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    static_url_path="/alignviewers/static",
+)
+
+LOG = logging.getLogger(__name__)
+
+
+@alignviewers_bp.route("/remote/cors/<path:remote_url>", methods=["OPTIONS", "GET"])
+def remote_cors(remote_url):
+    """Proxy a remote URL.
+    Useful to e.g. eliminate CORS issues when the remote site does not
+        communicate CORS headers well, as in cloud tracks on figshare for IGV.js.
+
+    Based on code from answers to this thread:
+        https://stackoverflow.com/questions/6656363/proxying-to-another-web-service-with-flask/
+    """
+    LOG.debug("Got request: %s", request)
+
+    resp = requests.request(
+        method=request.method,
+        url=remote_url,
+        headers={key: value for (key, value) in request.headers if key != "Host"},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=True,
+    )
+
+    excluded_headers = [
+        "content-encoding",
+        "content-length",
+        "transfer-encoding",
+        "connection",
+    ]
+    headers = [
+        (name, value)
+        for (name, value) in resp.raw.headers.items()
+        if name.lower() not in excluded_headers
+    ]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+
+@alignviewers_bp.route("/remote/static", methods=["OPTIONS", "GET"])
+def remote_static():
+    """Stream *large* static files with special requirements."""
+    file_path = request.args.get("file") or ""
+    range_header = request.headers.get("Range", None)
+    if not range_header and (file_path.endswith(".bam") or file_path.endswith(".cram")):
+        return abort(500)
+
+    new_resp = send_file_partial(file_path)
+    return new_resp
+
+
+@alignviewers_bp.route("/remote/static/unindexed", methods=["OPTIONS", "GET"])
+def unindexed_remote_static():
+    file_path = request.args.get("file")
+    base_name = os.path.basename(file_path)
+    resp = send_file(file_path, attachment_filename=base_name)
+    return resp
+
+
+@alignviewers_bp.route(
+    "/igv-splice-junctions/<institute_id>/<case_name>/<variant_id>", methods=["GET"]
+)
+def sashimi_igv(institute_id, case_name, variant_id):
+    """Visualize splice junctions on igv.js sashimi-like viewer for one or more individuals of a case.
+    wiki: https://github.com/igvteam/igv.js/wiki/Splice-Junctions
+    """
+    display_obj = controllers.make_sashimi_tracks(institute_id, case_name, variant_id)
+    return render_template("alignviewers/igv_sashimi_viewer.html", **display_obj)
+
+
+@alignviewers_bp.route("/igv", methods=["POST"])
+def igv():
+    """Visualize BAM alignments using igv.js (https://github.com/igvteam/igv.js)"""
+
+    # Set genome build for displaying alignments:
+    # Genome build is 37 if request.form.get("build") is 37 and chr != MT
+    # Genome build is 38 if request.form.get("build") is 38 or if chrom == MT
+    chromosome_build = request.form.get("build")
+    chrom = request.form.get("contig")
+    if chrom == "MT":
+        chrom = "M"
+    if chromosome_build in ["GRCh38", "38"] or chrom == "M":
+        chromosome_build = "38"
+    else:
+        chromosome_build = "37"
+
+    start = request.form.get("start")
+    stop = request.form.get("stop")
+    locus = "chr{0}:{1}-{2}".format(chrom, start, stop)
+
+    display_obj = {}  # Initialize the dictionary containing all tracks info
+
+    # General tracks (Genes, Clinvar and ClinVar SNVs are shown according to user preferences)
+    controllers.set_common_tracks(display_obj, chromosome_build)
+
+    # Set up bam/cram alignments for case samples:
+    controllers.set_sample_tracks(display_obj, request.form)
+
+    # When chrom != MT, set up case-specific tracks (might be present according to the pipeline)
+    if chrom != "M":
+        controllers.set_case_specific_tracks(display_obj, request.form)
+
+    # Set up custom cloud public tracks, if available
+    controllers.set_cloud_public_tracks(display_obj, chromosome_build)
+
+    display_obj["display_center_guide"] = True
+
+    return render_template("alignviewers/igv_viewer.html", locus=locus, **display_obj)
---
+++ packages/scout-browser/scout-browser-4.51.tar.gz/scout-browser-4.51/tests/server/blueprints/alignviewers/test_alignviewers_controllers.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+import responses
+from flask import url_for
+from flask_login import current_user
+
+from scout.commands.update.individual import individual as ind_cmd
+from scout.server.blueprints.alignviewers import controllers
+from scout.server.extensions import cloud_tracks, store
+
+
+def test_make_sashimi_tracks_variant_38(app, case_obj):
+    """Test function that creates splice junction tracks for a variant with genome build 38"""
+
+    # GIVEN a case with genome build 38
+    store.case_collection.find_one_and_update(
+        {"_id": case_obj["_id"]}, {"$set": {"genome_build": "38"}}
+    )
+
+    # WHEN the gene splice junction track is created for any variant in a gene
+    test_variant = store.variant_collection.find_one({"hgnc_symbols": ["POT1"]})
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for("auto_login"))
+        assert resp.status_code == 200
+
+        # THEN it should return the expected data
+        display_obj = controllers.make_sashimi_tracks(
+            case_obj["owner"], case_obj["display_name"], test_variant["_id"]
+        )
+        assert display_obj["case"] == case_obj["display_name"]
+        assert display_obj["locus"]
+        assert display_obj["tracks"][0]["name"]
+        assert display_obj["tracks"][0]["coverage_wig"]
+        assert display_obj["tracks"][0]["splicej_bed"]
+        assert display_obj["tracks"][0]["splicej_bed_index"]
+        assert display_obj["reference_track"]  # Reference genome track
+        assert display_obj["custom_tracks"]  # Custom tracks include gene track in the right build
+
+
+@responses.activate
+def test_make_sashimi_tracks_variant_37(app, case_obj, ensembl_liftover_response):
+    """Test function that creates splice junction tracks for a variant with genome build 37"""
+
+    # WHEN the gene splice junction track is created for any variant in a gene
+    test_variant = store.variant_collection.find_one({"hgnc_symbols": ["POT1"]})
+
+    # GIVEN a patched response from Ensembl liftover API
+    url = f'http://grch37.rest.ensembl.org/map/human/GRCh37/{test_variant["chromosome"]}:{test_variant["position"]}..{test_variant["end"]}/GRCh38?content-type=application/json'
+    responses.add(
+        responses.GET,
+        url,
+        json=ensembl_liftover_response,
+        status=200,
+    )
+
+    with app.test_client() as client:
+        # GIVEN that the user could be logged in
+        resp = client.get(url_for("auto_login"))
+        assert resp.status_code == 200
+
+        # THEN it should return the expected data
+        display_obj = controllers.make_sashimi_tracks(
+            case_obj["owner"], case_obj["display_name"], test_variant["_id"]
+        )
+        assert display_obj["case"] == case_obj["display_name"]
+        assert display_obj["locus"]
+        assert display_obj["tracks"][0]["name"]
+        assert display_obj["tracks"][0]["coverage_wig"]
+        assert display_obj["tracks"][0]["splicej_bed"]
+        assert display_obj["tracks"][0]["splicej_bed_index"]
+        assert display_obj["reference_track"]  # Reference genome track
+        assert display_obj["custom_tracks"]  # Custom tracks include gene track in the right build
+
+
+def test_set_cloud_public_tracks(app):
+    """Test function that adds cloud public tracks to track display object"""
+
+    # GIVEN an app with public cloud tracks initialized
+    patched_track = {"37": [{"name": "test track"}]}
+    cloud_tracks.public_tracks = patched_track
+
+    display_obj = {}
+    build = "37"
+
+    with app.test_client() as client:
+
+        # GIVEN that the user could be logged in
+        client.get(url_for("auto_login"))
+
+        # And the user has the test track preselected:
+        store.user_collection.find_one_and_update(
+            {"email": current_user.email}, {"$set": {"igv_tracks": ["test track"]}}
+        )
+
+        # Then the set_cloud_public_tracks controller should add the test track to the display object
+        controllers.set_cloud_public_tracks(display_obj, build)
+        assert display_obj["cloud_public_tracks"] == patched_track["37"]
+
+
+def test_make_igv_tracks():
+    """Test function that creates custom track dictionaries"""
+
+    # GIVEN a test track with 2 files
+    file_list = ["sample_1_file", "sample_2_file"]
+    track_list = controllers.make_igv_tracks("test_track", file_list)
+
+    # The function should return a track list with 2 dictionaries
+    assert len(track_list) == 2
+    # Containing the expected info
+    for track in track_list:
+        assert track["name"] == "test_track"
+        assert track["url"] in file_list
+
+
+def test_sample_tracks():
+    """Test the function that creates case individual tracks"""
+
+    # GIVEN a case with 3 samples alignments
+    sample_names = ["sample1", "sample2", "sample3"]
+    sample_bams = ["bam1", "bam2", "bam3"]
+
+    form_data = {
+        "sample": ",".join(sample_names),
+        "align": "bam",
+        "bam": ",".join(sample_bams),
+        "bai": "bai1,bai2,bai3",
+    }
+
+    display_obj = {}
+    # WHEN the set_sample_tracks function is invoked:
+    controllers.set_sample_tracks(display_obj, form_data)
+    # THEN it should return 3 tracks
+    assert len(display_obj["sample_tracks"]) == 3
+    # Containing the expected fields
+    for track in display_obj["sample_tracks"]:
+        assert track["name"] in sample_names
+        assert track["url"] in sample_bams
+
+
+def set_case_specific_tracks():
+    """Test function that creates tracks based on case-specific files:
+    (rhocall files, tiddit coverage files, upd regions and sites files)
+    """
+    # GIVEN a case with a rhocall bed file and a TIDDIT wig file
+    form_data = {
+        "rhocall_bed": "rhocall.bed",
+        "tiddit_coverage_wig": "tiddit_coverage.wig",
+    }
+    # THE case_specific_tracks function should return the expected tracks
+    display_obj = {}
+    controllers.set_case_specific_tracks(display_obj, form_data)
+    assert display_obj["rhocall_bed"]["url"] == form_data["rhocall_bed"]
+    assert display_obj["tiddit_coverage_wig"] == form_data["tiddit_coverage_wig"]
