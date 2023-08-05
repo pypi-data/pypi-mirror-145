3b5cae6aa7dedce833b2815bf98ec9551d713a86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:42 +0000

    Add package MeUtils/2022.4.1.19.29.3/MeUtils-2022.4.1.19.29.3-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2022.4.1.19.29.3-py3-none-any.whl/examples/cache_demo/demo.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : MeUtils.
+# @File         : demo
+# @Time         : 2022/4/1 下午6:25
+# @Author       : yuanjie
+# @WeChat       : 313303303
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
+
+
+def transformers_download(model_name='ckiplab/albert-tiny-chinese', type='torch', out=None):
+    if out is None:
+        out = Path(model_name)
+    else:
+        out = Path(out) / Path(model_name)
+    out.mkdir(parents=True, exist_ok=True)
+
+    from meutils.request_utils.crawler import Crawler
+
+    files = (
+        Crawler(f'https://huggingface.co/{model_name}/tree/main')
+            .xpath("/html/body/div[1]/main/div/section/ul/li[*]/a[1]/span[1]//text()")
+    )
+
+    files = [file for file in files if file.endswith('.txt') or file.endswith('.json')]
+
+    type2name = {
+        'torch': 'pytorch_model.bin',
+        'tensorflow': 'tf_model.h5',
+        'flax': 'flax_model.msgpack'
+
+    }
+    if type == 'all':
+        model_files = list([type2name.values()])
+    else:
+        model_files = [type2name.get(type, 'torch')]
+
+    tqdm_ = tqdm(files + model_files)
+    for file in tqdm_:
+        tqdm_.set_description(file)
+        url = f"https://huggingface.co/{model_name}/resolve/main/{file}"
+        wget.download(url, str(out))
+
+
+if __name__ == '__main__':
+    transformers_download(out='yuanjie')
