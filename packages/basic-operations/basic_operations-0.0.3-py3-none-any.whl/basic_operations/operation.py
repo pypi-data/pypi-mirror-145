4a557adef1f70b15e32aca48294095e17223fa75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:37 +0000

    Add package basic-operations/0.0.3/basic_operations-0.0.3-py3-none-any.whl

---
+++ packages/basic-operations/basic_operations-0.0.3-py3-none-any.whl/basic_operations/operation.py
@@ -0,0 +1,15 @@
+def addition(x,y):
+    print("\n- Addition result: ",x+y)
+    return
+
+def subtraction(x,y):
+    print("\n- Subtraction result: ",x-y)
+    return
+
+def multiplication(x,y):
+    print("\n- Multiplication result: ",x*y)
+    return
+
+def division(x,y):
+    print("\n- Division result: ",x/y)
+    return
