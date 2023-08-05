6c4857dd8b33b1f5b4984a1db114f1be2823b0a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package argclass/0.3.0/argclass-0.3.0.tar.gz

---
+++ packages/argclass/argclass-0.3.0.tar.gz/argclass-0.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+
+setup(
+    name="argclass",
+    version="0.3.0",
+    platforms="all",
+    author="Dmitry Orlov",
+    author_email="me@mosquito.su",
+    maintainer="Dmitry Orlov",
+    maintainer_email="me@mosquito.su",
+    description="Declarative argparse definition",
+    package_dir={"": "src"},
+    packages=[""],
+    license="Apache 2",
+    long_description=open("README.rst").read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+)
