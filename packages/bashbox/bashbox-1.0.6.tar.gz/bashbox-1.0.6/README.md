8db1b8862ba355963420a36eb72c42616aaad9f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:53 +0000

    Add package bashbox/1.0.6/bashbox-1.0.6.tar.gz

---
+++ packages/bashbox/bashbox-1.0.6.tar.gz/bashbox-1.0.6/README.md
@@ -0,0 +1,8 @@
+![bashbox](https://user-images.githubusercontent.com/42397332/161249736-40cb3377-4361-4df1-b06b-83fbf14d3101.png)
+
+<div align="center">
+	<img src="https://img.shields.io/github/workflow/status/bash-elliott/bashbox/Upload%20Python%20Package?label=Package%20Status&style=for-the-badge&logo=python&logoColor=white">
+	<img src="https://img.shields.io/github/v/tag/bash-elliott/bashbox?style=for-the-badge">
+	<img src="https://img.shields.io/github/release-date/bash-elliott/bashbox?style=for-the-badge">
+	<img src="https://img.shields.io/github/commit-activity/w/bash-elliott/bashbox?style=for-the-badge">
+</div>
---
+++ packages/bashbox/bashbox-1.0.6.tar.gz/bashbox-1.0.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bashbox",
+    version="1.0.6",
+    author="Bash Elliott",
+    author_email="spicethings9@gmail.com",
+    description="Bash's textbox package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bash-elliott/bashbox",
+    project_urls={
+        "Bug Tracker": "https://github.com/bash-elliott/bashbox/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+
+    package_data={'': ["src\\bashbox\\themes\\*.bsh", "init.txt"]},
+    include_package_data=True
+)
