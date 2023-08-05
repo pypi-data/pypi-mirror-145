1020fea7ba1153baa0ca0701f0fa95d9b9440bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:43 +0000

    Add package concise-concepts/0.3.6/concise-concepts-0.3.6.tar.gz

---
+++ packages/concise-concepts/concise-concepts-0.3.6.tar.gz/concise-concepts-0.3.6/concise_concepts/test.py
@@ -0,0 +1,9 @@
+
+def test_spacy_embeddings():
+    from .examples import example_spacy
+
+def test_gensim_default():
+    from .examples import example_gensim_default
+
+def test_gensim_custom():
+    from .examples import example_gensim_custom
