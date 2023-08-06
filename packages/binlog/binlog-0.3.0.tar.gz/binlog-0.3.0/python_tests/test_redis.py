11a317865c5e668ca8ae732721c129d1a74d8fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:27 +0000

    Add package binlog/0.3.0/binlog-0.3.0.tar.gz

---
+++ packages/binlog/binlog-0.3.0.tar.gz/binlog-0.3.0/python_tests/test_redis.py
@@ -0,0 +1,16 @@
+import tempfile
+import time
+from binlog import binlog
+
+
+def test_pubsub():
+    store = binlog.RedisStreamStore("redis://localhost:6379", 10)
+    entry = binlog.Entry(1, "pytest_redis_push", [1, 2, 3])
+    sub = store.subscribe("pytest_redis_push")
+    time.sleep(0.1) # give enough time for the thread to start up
+    store.push(entry)
+    sub_entry = next(sub)
+    assert entry.timestamp == sub_entry.timestamp
+    assert entry.name == sub_entry.name
+    assert entry.value == sub_entry.value
+
