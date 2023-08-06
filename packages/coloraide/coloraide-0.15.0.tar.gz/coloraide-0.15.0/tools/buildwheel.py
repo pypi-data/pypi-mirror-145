b80d93ca353669d3ee1868bd461e904f341e0fe7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:26 +0000

    Add package coloraide/0.15.0/coloraide-0.15.0.tar.gz

---
+++ packages/coloraide/coloraide-0.15.0.tar.gz/coloraide-0.15.0/tools/buildwheel.py
@@ -0,0 +1,140 @@
+"""Download necessary wheels and build the `coloraide` wheel."""
+import sys
+import subprocess
+import os
+import urllib.request
+import urllib.error
+import glob
+import shutil
+
+urls = [
+    "https://files.pythonhosted.org/packages/6e/33/1ae0f71395e618d6140fbbc9587cc3156591f748226075e0f7d6f9176522/Markdown-3.3.4-py3-none-any.whl",  # noqa: E501
+    "https://files.pythonhosted.org/packages/9a/9a/36f71797fbaf1f4b6cb8debe1ab6d3ec969e8dbc651181f131a16b794d80/pymdown_extensions-9.0-py3-none-any.whl",  # noqa: E501
+]
+
+
+def get_version():
+    """Get version and version_info without importing the entire module."""
+
+    import importlib.util
+
+    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'coloraide', '__meta__.py')
+    spec = importlib.util.spec_from_file_location("__meta__", path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    vi = module.__version_info__
+    return vi._get_canonical()
+
+
+# Keep a compatible version in the repository so we can always release docs.
+# If we ever have a breaking change, we can patch `coloraide-extras` before
+# the release, locally upload it and then make a release.
+# Afterwards, once `coloraide` is available, we can release the official
+# `coloraide-extras`. This prevents our documentation from ever breaking.
+EXTRA_VERSION = '0.3.0'
+EXTRA_WHEEL = 'coloraide_extras-{}-py3-none-any.whl'.format(EXTRA_VERSION)
+keep = {EXTRA_WHEEL}
+EXTRAS = "https://raw.githubusercontent.com/facelessuser/coloraide/{{}}/docs/src/markdown/playground/{}".format(
+    EXTRA_WHEEL
+)
+output = 'docs/src/markdown/playground/'
+externals = {}
+for url in urls:
+    externals[os.path.join(output, url.split('/')[-1])] = url
+
+
+def build_coloraide():
+    """Build `coloraide` wheel."""
+    cmd = [sys.executable, '-m', 'build', '--wheel', '-o', output]
+
+    if sys.platform.startswith('win'):
+        startupinfo = subprocess.STARTUPINFO()
+        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        process = subprocess.Popen(
+            cmd,
+            startupinfo=startupinfo,
+            shell=False,
+            env=os.environ.copy()
+        )
+    else:
+        process = subprocess.Popen(
+            cmd,
+            shell=False,
+            env=os.environ.copy()
+        )
+    process.communicate()
+
+    return process.returncode
+
+
+def download_wheel(url, dest):
+    """Download a wheel."""
+
+    print('Downloading: {}'.format(url))
+    status = 0
+    try:
+        response = urllib.request.urlopen(url)
+        status = response.status
+        if status == 200:
+            status = 0
+            with open(dest, 'wb') as f:
+                print('Writing: {}'.format(dest))
+                f.write(response.read())
+    except urllib.error.HTTPError as e:
+        status = e.status
+
+    if status:
+        print('Failed to download, recieved status code {}'.format(status))
+
+    return status
+
+
+if __name__ == "__main__":
+    # Clean up all old wheels
+    for file in glob.glob(output + '*.whl'):
+        if file not in externals.keys() and os.path.basename(file) not in keep:
+            os.remove(file)
+
+    # Clean up build directory
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+
+    # Build `coloraide` wheel
+    if build_coloraide():
+        sys.exit(1)
+
+    # Get dependencies
+    for file, url in externals.items():
+        if os.path.exists(file):
+            print('Skipping: {}'.format(file))
+            continue
+        status = download_wheel(url, file)
+        if status:
+            sys.exit(status)
+
+    # Get `coloraide-extras`. If we can't find it in the version's tag,
+    # tag may not be created with this version, particularly during development,
+    # attempt on master
+    extras = EXTRAS.format(get_version())
+    extra_output = os.path.join(output, extras.split('/')[-1])
+    if not os.path.exists(extra_output):
+        status = download_wheel(extras, extra_output)
+        if status:
+            print(
+                (
+                    'Could not find ColorAide Extras at tag {},'
+                    'tag may not have been created yet, try to grab latest on master'
+                ).format(get_version())
+            )
+            extras = EXTRAS.format('master')
+            extra_output = os.path.join(output, extras.split('/')[-1])
+            status = download_wheel(extras, extra_output)
+            if status:
+                print('No available ColorAide Extras on master')
+    else:
+        status = 0
+        print('Skipping: {}'.format(extra_output))
+
+    print("FAILED :(" if status else "SUCCESS :)")
+
+    sys.exit(status)
---
+++ packages/coloraide/coloraide-0.15.0.tar.gz/coloraide-0.15.0/tools/slice_diagram.py
@@ -0,0 +1,253 @@
+"""
+Slice diagrams.
+
+Show a slice of a color space where we plot the relation between two channels holding one constant.
+
+Channels to be plotted are given in the form `name:distance:offset`. For instance `a:1:-0.5` would plot the `a` channel
+from -0.5 to 0.5.
+
+The constant channel is given in the form `name:value` where value is the constant value for the channel.
+"""
+import itertools
+import matplotlib.pyplot as plt
+import argparse
+import sys
+import os
+import math
+
+sys.path.insert(0, os.getcwd())
+
+try:
+    from coloraide_extras import Color
+except ImportError:
+    from coloraide import Color
+from coloraide import NaN  # noqa: E402
+from coloraide.util import fmt_float  # noqa: E402
+from coloraide.spaces import Cylindrical  # noqa: E402
+
+
+def needs_lchuv_workaround(color):
+    """
+    Check if LCHuv has high chroma and no lightness.
+
+    LCHuv will have such values all in gamut and create weird graphs.
+    This is just due to how LCHuv algorithm handles high chroma and zero lightness,
+    it all gets treated as black which is in gamut for almost any color.
+    """
+
+    return color.space().startswith('lchuv') and color.l == 0 and not color.normalize().is_nan('hue')
+
+
+def plot_slice(
+    space,
+    channel0,
+    channel1,
+    channel2,
+    gamut='srgb',
+    resolution=500,
+    dark=False,
+    title="",
+    subtitle='',
+    polar=False
+):
+    """Plot a slice."""
+
+    res = resolution
+    if not dark:
+        plt.style.use('seaborn-darkgrid')
+        default_color = 'black'
+    else:
+        plt.style.use('dark_background')
+        default_color = 'white'
+
+    figure = plt.figure()
+
+    # Create a color object based on the specified space.
+    c = Color(space, [])
+
+    # Parse the channel strings into actual values
+    name0, value = [
+        c if i == 0 else float(c if c != 'none' else 'nan') for i, c in enumerate(channel0.split(":"), 0)
+    ]
+    name1, start1, end1 = [
+        c if i == 0 else float(c) for i, c in enumerate(channel1.split(':'), 0)
+    ]
+    factor1 = abs(start1 - end1) if end1 < 0 else (end1 - start1)
+    offset1 = start1
+
+    name2, start2, end2 = [
+        c if i == 0 else float(c) for i, c in enumerate(channel2.split(':'), 0)
+    ]
+    factor2 = abs(start2 - end2) if end2 < 0 else (end2 - start2)
+    offset2 = start2
+
+    # Get the actual indexes of the specified channels
+    name0 = c._space.CHANNEL_ALIASES.get(name0, name0)
+    index0 = c._space.CHANNEL_NAMES.index(name0)
+    name1 = c._space.CHANNEL_ALIASES.get(name1, name1)
+    index1 = c._space.CHANNEL_NAMES.index(name1)
+    name2 = c._space.CHANNEL_ALIASES.get(name2, name2)
+    index2 = c._space.CHANNEL_NAMES.index(name2)
+    hue_index = -1
+
+    kwargs = {}
+    if polar and isinstance(c._space, Cylindrical):
+        kwargs['projection'] = 'polar'
+        index = c._space.hue_index()
+        if index == index1:
+            hue_index = index
+
+    # Arrays for data points to plot
+    c_map = []
+    xaxis = []
+    yaxis = []
+
+    # Track minimum and maximum values
+    c1_mn = float('inf')
+    c1_mx = float('-inf')
+    c2_mn = float('inf')
+    c2_mx = float('-inf')
+
+    # Track the edge of the graphed shape.
+    edge_map = {}
+
+    # Iterate through the two specified channels
+    for c1, c2 in itertools.product(
+        ((x / res * factor1) + offset1 for x in range(0, res + 1)),
+        ((x / res * factor2) + offset2 for x in range(0, res + 1))
+    ):
+        # Set the appropriate channels and update the color object
+        coords = [NaN] * 3
+        coords[index0] = value
+        coords[index1] = c1
+        coords[index2] = c2
+        c.update(space, coords)
+
+        # Only process colors within the gamut of sRGB.
+        if c.in_gamut(gamut, tolerance=0) and not needs_lchuv_workaround(c):
+            if hue_index != -1:
+                c1 = math.radians(c1)
+
+            # Get the absolute min and max value plotted
+            if c1 < c1_mn:
+                c1_mn = c1
+            if c1 > c1_mx:
+                c1_mx = c1
+
+            if c2 < c2_mn:
+                c2_mn = c2
+            if c2 > c2_mx:
+                c2_mx = c2
+
+            # Create an edge map so we can draw an outline
+            if c1 not in edge_map:
+                mn = mx = c2
+            else:
+                mn, mx = edge_map[c1]
+                if c2 < mn:
+                    mn = c2
+                elif c2 > mx:
+                    mx = c2
+            edge_map[c1] = [mn, mx]
+
+            # Save the points
+            xaxis.append(c1)
+            yaxis.append(c2)
+            c_map.append(c.convert('srgb').to_string(hex=True))
+
+    # Create a border around the data
+    xe = []
+    ye = []
+    xtemp = []
+    ytemp = []
+    for p1, edges in edge_map.items():
+        if hue_index == -1:
+            xe.append(p1)
+            ye.append(edges[0])
+            xtemp.append(p1)
+            ytemp.append(edges[1])
+        else:
+            xe.append(p1)
+            ye.append(edges[1])
+
+    if hue_index == -1:
+        xe.extend(reversed(xtemp))
+        ye.extend(reversed(ytemp))
+        xe.append(xe[0])
+        ye.append(ye[0])
+
+    ax = plt.axes(
+        xlabel='{}: {} - {}'.format(name1, fmt_float(c1_mn, 5), fmt_float(c1_mx, 5)),
+        ylabel='{}: {} - {}'.format(name2, fmt_float(c2_mn, 5), fmt_float(c2_mx, 5)),
+        **kwargs
+    )
+
+    if not title:
+        title = "Plot of {} showing '{}' and '{}' with '{}' at {}".format(
+            space, name1, name2, name0, fmt_float(value, 5)
+        )
+
+    plt.suptitle(title)
+    if subtitle:
+        if hue_index == -1:
+            ax.set_title(subtitle, fontdict={'fontsize': 8})
+        else:
+            ax.set_title(subtitle, fontdict={'fontsize': 8}, pad=2)
+
+    ax.set_aspect('auto' if hue_index == -1 else 'equal')
+    figure.add_axes(ax)
+
+    plt.plot(xe, ye, color=default_color, marker="", linewidth=2, markersize=0, antialiased=True)
+
+    plt.scatter(
+        xaxis,
+        yaxis,
+        marker="o",
+        color=c_map,
+        s=2
+    )
+
+
+def main():
+    """Main."""
+
+    parser = argparse.ArgumentParser(prog='slice_diagrams', description='Plot a slice of a color space.')
+    parser.add_argument('--space', '-s', help='Desired space.')
+    parser.add_argument('--gamut', '-g', default="srgb", help='Gamut to evaluate the color in (default is sRGB).')
+    parser.add_argument('--constant', '-c', help="The channel to hold constant and the value to use 'name:value'.")
+    parser.add_argument('--xaxis', '-x', help="The channel to plot on X axis 'name:min:max'.")
+    parser.add_argument('--yaxis', '-y', help="The channel to plot on Y axis 'name:min:max'.")
+    parser.add_argument('--polar', '-p', action="store_true", help="Graph the cylindrical space in polar coordinates.")
+    parser.add_argument('--resolution', '-r', default="800", help="How densely to render the figure.")
+    parser.add_argument('--title', '-t', default='', help="Provide a title for the diagram.")
+    parser.add_argument(
+        '--sub-title', default='', help="Provide a subtitle, if none is provided, will show contant channel."
+    )
+    parser.add_argument('--dark', action="store_true", help="Use dark theme.")
+    parser.add_argument('--dpi', default=200, type=int, help="DPI of image.")
+    parser.add_argument('--output', '-o', default='', help='Output file.')
+
+    args = parser.parse_args()
+
+    plot_slice(
+        args.space,
+        args.constant,
+        args.xaxis,
+        args.yaxis,
+        gamut=args.gamut,
+        resolution=int(args.resolution),
+        title=args.title,
+        subtitle=args.sub_title,
+        dark=args.dark,
+        polar=args.polar
+    )
+
+    if args.output:
+        plt.savefig(args.output, dpi=args.dpi)
+    else:
+        plt.gcf().set_dpi(args.dpi)
+        plt.show()
+
+
+if __name__ == "__main__":
+    sys.exit(main())
