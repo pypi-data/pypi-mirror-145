e5db3cf5782d1055c3e817219b8ffaaca44ea184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:53 +0000

    Add package oh_my_tools_package/0.0.3b63/oh_my_tools_package-0.0.3beta63.tar.gz

---
+++ packages/oh_my_tools_package/oh_my_tools_package-0.0.3beta63.tar.gz/oh_my_tools_package-0.0.3beta63/my_tools_package/nlp/corrector.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/11/4 1:25 下午
+# @Author  : jeffery
+# @FileName: corrector.py
+# @website : http://www.jeffery.ink/
+# @github  : https://github.com/jeffery0628
+# @Description: 文本纠错
+
+
+
+class Corrector:
+
+    def __init__(self):
+
