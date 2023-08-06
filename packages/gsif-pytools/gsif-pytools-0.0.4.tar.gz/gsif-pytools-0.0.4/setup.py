60874293c275df415893d496b1b0aba3a57f0c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:15 +0000

    Add package gsif-pytools/0.0.4/gsif-pytools-0.0.4.tar.gz

---
+++ packages/gsif-pytools/gsif-pytools-0.0.4.tar.gz/gsif-pytools-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md","r") as readme_file:
+   readme = readme_file.read()
+
+requirements = ["pandas","numpy==1.22.1","requests>=2","openpyxl>=3","pytesseract>=0.3.9","opencv-python>=4","pdf2image>=1"]
+
+setup(
+   name="gsif-pytools",
+   version="0.0.4",
+   author="Mingyin Zhu",
+   author_email="mzhu0114@gmail.com",
+   description="A package with tools to aid Gator Student Investment Fund Portfolio Attribution Specialists.",
+   long_description=readme,
+   long_description_content_type="text/markdown",
+   url="https://github.com/gsifpytools/homepage/",
+   packages=find_packages(),
+   install_requires=requirements,
+   classifiers=[
+      "Programming Language :: Python :: 3.10",
+      "License :: OSI Approved :: MIT License",
+
+   ],
+)
+
+
