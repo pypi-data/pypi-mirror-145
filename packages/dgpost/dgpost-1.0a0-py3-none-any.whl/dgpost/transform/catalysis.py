5ffd4e0684b6d10d82dc79e51baac3712f7b97fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:46 +0000

    Add package dgpost/1.0a0/dgpost-1.0a0-py3-none-any.whl

---
+++ packages/dgpost/dgpost-1.0a0-py3-none-any.whl/dgpost/transform/catalysis.py
@@ -0,0 +1,345 @@
+"""
+Module of transformations relevant to catalysis applications.
+
+For the functions in this module, the inlet and outlet compositions are specified using
+the ``xin`` and ``xout`` arguments, the keys of which are the species names in the 
+inlet / outlet compositions, respectively. These keys are parsed to SMILES, and the
+cross-matching of ``feestock``, ``standard``, and components of ``xin`` and ``xout``
+is performed using these SMILES.
+
+.. note::
+    This module assumes that the provided inlet (``xin``) and outlet (``xout``) 
+    compositions contains all species, and that the atomic balance of the inlet and 
+    outlet is near unity. If multiple inlet/outlet streams are present in the 
+    experiment, they need to be appropriately combined into a single namespace, e.g.
+    using functions in the :mod:`dgpost.transform.rates` before using the functions 
+    in this module.
+
+.. codeauthor:: Peter Kraus <peter.kraus@empa.ch>
+"""
+
+import pint
+
+
+from .helpers import (
+    name_to_chem,
+    columns_to_smiles,
+    default_element,
+    element_from_formula,
+    load_data,
+)
+
+
+@load_data(
+    ("xin", None, dict),
+    ("xout", None, dict),
+)
+def conversion(
+    feedstock: str,
+    xin: dict[str, pint.Quantity],
+    xout: dict[str, pint.Quantity],
+    element: str = None,
+    product: bool = True,
+    standard: str = "N2",
+    output: str = None,
+) -> dict[str, pint.Quantity]:
+    """
+    Calculates the conversion of ``feedstock``, a species present in ``xin``. By
+    default, a product-based carbon conversion (using data from ``xout``) is computed.
+
+    Parameters
+    ----------
+    feedstock
+        Name of the feedstock. Parsed into SMILES and matched against ``xin`` and
+        ``xout``.
+
+    xin
+        A dictionary containing the composition of the inlet mixture with the names
+        of the chemicals as :class:`str` keys.
+
+    xout
+        A dictionary containing the composition of the outlet mixture with the names
+        of the chemicals as :class:`str` keys.
+
+    element
+        Name of the element for determining conversion. If not specified, set to the
+        highest-priority (C > H > O) element in ``feedstock``.
+
+    product
+        Product-based conversion toggle. Default is ``True``.
+
+    standard
+        Internal standard for normalizing the compositions. By default set to "N2".
+
+    output
+        A :class:`str` name of the output variable.
+
+    Returns
+    -------
+    ret
+        A :class:`dict` containing the calculated conversion with ``output`` as its key.
+
+    """
+
+    smiles = columns_to_smiles(xin=xin, xout=xout)
+    fchem = name_to_chem(feedstock)
+    fsmi = fchem.smiles
+    fform = fchem.formula
+    fd = smiles[fsmi]
+    assert fsmi in smiles, f"conversion: Feedstock '{feedstock}' not present."
+    assert "xin" in smiles[fsmi], f"conversion: Feedstock '{feedstock}' not in inlet."
+
+    if element is None:
+        element = default_element(fform)
+
+    # expansion factor
+    sd = smiles[name_to_chem(standard).smiles]
+    exp = xin[sd["xin"]] / xout[sd["xout"]]
+
+    # reactant-based conversion
+    if not product:
+        dfsm = xin[fd["xin"]] - xout[fd["xout"]] * exp
+        Xr = dfsm / xin[fd["xin"]]
+        tag = f"{'Xr' if output is None else output}->{feedstock}"
+        ret = {tag: Xr}
+
+    # product-based conversion
+    else:
+        f_out = xout[fd["xout"]] * element_from_formula(fform, element)
+        nat_out = f_out * 0.0
+        for v in smiles.values():
+            if "xout" in v:
+                formula = v["chem"].formula
+                dnat = xout[v["xout"]] * element_from_formula(formula, element)
+                nat_out += dnat
+        Xp = (nat_out - f_out) / nat_out
+        prefix = f"Xp_{element}" if output is None else output
+        tag = f"{prefix}->{feedstock}"
+        ret = {tag: Xp}
+    return ret
+
+
+@load_data(
+    ("xin", None, dict),
+    ("xout", None, dict),
+)
+def selectivity(
+    feedstock: str,
+    xin: dict[str, pint.Quantity],
+    xout: dict[str, pint.Quantity],
+    element: str = None,
+    output: str = None,
+) -> dict[str, pint.Quantity]:
+    """
+    Calculates product-based elemental selectivities, excluding the feedstock. The sum
+    of selectivities is normalised to unity.
+
+    Parameters
+    ----------
+    feedstock
+        Name of the feedstock. Parsed into SMILES and matched against ``xin`` and
+        ``xout``.
+
+    xin
+        Prefix of the columns determining the inlet composition.
+
+    xout
+        Prefix of the columns determining the outlet composition.
+
+    element
+        The element for determining conversion. If not specified, set from ``feedstock``
+        using :func:`dgpost.transform.chemhelpers.default_element`.
+
+    output
+        A :class:`str` prefix for the output variables.
+
+    Returns
+    -------
+    ret
+        A :class:`dict` containing the calculated selectivities using ``output`` as
+        the prefix for each key.
+
+    """
+    smiles = columns_to_smiles(xin=xin, xout=xout)
+    fchem = name_to_chem(feedstock)
+    fsmi = fchem.smiles
+    fform = fchem.formula
+    assert fsmi in smiles, f"selectivity: Feedstock '{feedstock}' not present."
+
+    if element is None:
+        element = default_element(fform)
+
+    nat_out = None
+    for k, v in smiles.items():
+        if k != fsmi and "xout" in v:
+            formula = v["chem"].formula
+            dnat = xout[v["xout"]] * element_from_formula(formula, element)
+            if nat_out is None:
+                nat_out = dnat
+            else:
+                nat_out += dnat
+    ret = {}
+    for k, v in smiles.items():
+        if k != fsmi and "xout" in v:
+            formula = v["chem"].formula
+            els = element_from_formula(formula, element)
+            if els > 0:
+                Sp = xout[v["xout"]] * els / nat_out
+                pretag = f"Sp_{element}" if output is None else output
+                tag = f"{pretag}->{v['xout']}"
+                ret[tag] = Sp
+    return ret
+
+
+@load_data(
+    ("xin", None, dict),
+    ("xout", None, dict),
+)
+def catalytic_yield(
+    feedstock: str,
+    xin: dict[str, pint.Quantity],
+    xout: dict[str, pint.Quantity],
+    element: str = None,
+    standard: str = "N2",
+    output: str = None,
+) -> None:
+    """
+    Calculates the catalytic yield, defined as the product of conversion and
+    selectivity. Uses product-based conversion of feedstock for consistency with
+    selectivity. The sum of all yields is equal to the conversion. Implicitly runs
+    :func:`conversion` and :func:`selectivity` on the dataframe.
+
+    Parameters
+    ----------
+    feedstock
+        Name of the feedstock. Parsed into SMILES and matched against ``xin`` and
+        ``xout``.
+
+    xin
+        Prefix of the columns determining the inlet composition.
+
+    xout
+        Prefix of the columns determining the outlet composition.
+
+    element
+        The element for determining conversion. If not specified, set from ``feedstock``
+        using :func:`dgpost.transform.chemhelpers.default_element`.
+
+    standard
+        Internal standard for normalizing the compositions. By default set to "N2".
+
+    output
+        A :class:`str` prefix for the output variables.
+
+    Returns
+    -------
+    ret
+        A :class:`dict` containing the calculated yields using ``output`` as
+        the prefix for each key.
+
+    """
+    fchem = name_to_chem(feedstock)
+    if element is None:
+        element = default_element(fchem.formula)
+
+    ret_Xp = conversion(
+        feedstock=feedstock,
+        xin=xin,
+        xout=xout,
+        element=element,
+        product=True,
+        standard=standard,
+    )
+    ret_Sp = selectivity(feedstock=feedstock, xin=xin, xout=xout, element=element)
+
+    Xp = ret_Xp[f"Xp_{element}->{feedstock}"]
+
+    ret = {}
+    for k, v in ret_Sp.items():
+        Yp = v * Xp
+        pretag = f"Yp_{element}" if output is None else output
+        name = k.split("->")[1]
+        tag = f"{pretag}->{name}"
+        ret[tag] = Yp
+    return ret
+
+
+@load_data(
+    ("xin", None, dict),
+    ("xout", None, dict),
+    ("fin", None),
+    ("fout", None),
+)
+def atom_balance(
+    xin: dict[str, pint.Quantity],
+    xout: dict[str, pint.Quantity],
+    fin: pint.Quantity = None,
+    fout: pint.Quantity = None,
+    element: str = "C",
+    standard: str = "N2",
+    output: str = None,
+) -> None:
+    """
+    Calculates atom balance. The total number of atoms of the specified element is
+    compared between between the inlet and outlet, normalized by an internal standard,
+    or optionally weighed by the supplied flows.
+
+    Parameters
+    ----------
+    xin
+        Prefix of the columns determining the inlet composition.
+
+    xout
+        Prefix of the columns determining the outlet composition.
+
+    fin
+        Inlet flow. If not supplied, assumed to be equal to outlet flow.
+
+    fout
+        Outlet flow. If not supplied, assumed to be equal to inlet flow.
+
+    element
+        The element for determining conversion. If not specified, set to ``"C"``.
+
+    standard
+        Internal standard for normalizing flows. By default set to ``"N2"``.
+        Overriden when ``fin`` and ``fout`` are set.
+
+    output
+        A :class:`str` prefix for the output variables.
+
+    Returns
+    -------
+    ret
+        A :class:`dict` containing the calculated atomic balance using ``output`` as
+        the key.
+
+
+    """
+    smiles = columns_to_smiles(xin=xin, xout=xout)
+
+    assert standard is not None or (
+        fin is not None and fout is not None
+    ), f"atom_balance: Neither 'standard' nor 'fin' and 'fout' are defined. "
+
+    if fin is None or fout is None:
+        sd = smiles[name_to_chem(standard).smiles]
+        exp = xin[sd["xin"]] / xout[sd["xout"]]
+    else:
+        exp = fin / fout
+
+    nat_in = None
+    nat_out = None
+    for k, v in smiles.items():
+        formula = v["chem"].formula
+        if "xin" in v:
+            din = xin[v["xin"]] * element_from_formula(formula, element)
+            nat_in = din if nat_in is None else nat_in + din
+        if "xout" in v:
+            dout = exp * xout[v["xout"]] * element_from_formula(formula, element)
+            nat_out = dout if nat_out is None else nat_out + dout
+    dnat = nat_in - nat_out
+    atbal = 1 - dnat / nat_in
+    tag = f"atbal_{element}" if output is None else output
+
+    return {tag: atbal}
---
+++ packages/dgpost/dgpost-1.0a0-py3-none-any.whl/dgpost/transform/electrochemistry.py
@@ -0,0 +1,171 @@
+"""
+Module of transformations relevant to electrochemistry applications.
+
+.. codeauthor:: Peter Kraus <peter.kraus@empa.ch>
+"""
+import pint
+from yadg.dgutils import ureg
+import numpy as np
+from .helpers import load_data, name_to_chem, electrons_from_smiles
+
+
+@load_data(
+    ("Ewe", "V"),
+    ("R", "Ω"),
+    ("I", "A"),
+    ("Eref", "V"),
+    ("T", "K"),
+    ("n", None),
+    ("Q", None),
+    ("pH", None),
+)
+def nernst(
+    Ewe: pint.Quantity,
+    R: pint.Quantity = ureg.Quantity(0.0, "Ω"),
+    I: pint.Quantity = ureg.Quantity(0.0, "A"),
+    Eref: pint.Quantity = ureg.Quantity(0.0, "V"),
+    T: pint.Quantity = ureg.Quantity(298.15, "K"),
+    n: int = None,
+    Q: float = None,
+    pH: float = None,
+    output: str = "Eapp",
+) -> pint.Quantity:
+    """
+    Correct measured voltage to calculate the applied voltage, using corrections
+    for Ohmic drop, reference potential vs RHE, and ionic concentration using 
+    the Nernst equation, either by providing :math:`Q` and :math:`n`, or by 
+    providing the :math:`pH`.
+
+    This function corrects the measured voltage :math:`E_\\text{we}` to applied
+    voltage :math:`E` by implementing the following equation:
+
+    .. math::
+
+        E &= E_\\text{we} + E_\\text{ref} + RI + E_N \\\\
+        E_N &= - \\frac{\\overline{R} T}{nF} ln(Q) = \\frac{\\overline{R} T}{F} ln(10) \\text{pH}
+
+    where :math:`E_\\text{ref}` is the potential of the reference electrode vs RHE,
+    :math:`RI` is the product of cell resistance and applied current, :math:`\\overline{R}`
+    is the molar gas constant, :math:`T` is the temperature, :math:`n` is the number
+    of electrones transferred, :math:`F` is the Faraday constant, :math:`Q` is the 
+    reaction quotient, and :math:`\\text{pH}` is the pH of the electrolyte.
+    
+    Parameters
+    ----------
+    Ewe
+        The measured working potential with respect to the reference electrode.
+        By default in V.
+
+    R
+        The resistance of the cell. By default in Ω. 
+
+    I
+        The applied current. By default in A.
+
+    Eref
+        The potential of the reference electrode with respect to RHE. 
+        By default in V.
+    
+    T
+        Temperature of the working electrode, used in the Nernst equation.
+        By default 298.15 K.
+    
+    n
+        Number of electrons transferred in the process described by the Nernst
+        equation. Must be specified along ``Q``, cannot be specified with ``pH``.
+
+    Q
+        The reaction quotient of the components of the process described by the
+        Nernst equation. Must be specified along ``n``, cannot be specified with
+        ``pH``.
+
+    pH
+        The pH of the solution. This assumes the modelled process is the reduction
+        of :math:`\\text{H}^+ \\rightarrow \\text{H}``, i.e. :math:`n = 1` and
+        :math:`\\text{ln}(Q) = -\\text{ln}(10)\\text{pH}`
+
+    output
+        Name of the output variable. Defaults to ``Eapp``.
+    
+    Returns
+    -------
+    dict(output, E) : dict[str, pint.Quantity]
+        Returns the calculated applied current in V.
+
+    """
+    E = Ewe
+    if Eref is not None:
+        E += Eref
+    if R is not None and I is not None:
+        E += R * I
+    if (pH is not None) or (n is not None and Q is not None):
+        EN = ureg("molar_gas_constant") * T.to("K") / ureg("faraday_constant")
+        if pH is not None:
+            EN = pH * EN * np.log(10)
+        else:
+            EN = -(EN / n) * np.log(Q)
+        E += EN
+    return {output: E}
+
+
+@load_data(
+    ("rate", "mol/s", dict),
+    ("I", "A"),
+)
+def fe(
+    rate: dict[str, pint.Quantity],
+    I: pint.Quantity,
+    charges: dict[str, int],
+    output: str = None,
+) -> dict[str, pint.Quantity]:
+    """
+    Calculate the Faradaic efficiency :math:`\\eta_F` from a set of molar rates
+    :math:`\\dot{n}`` corresponding to a single mixture, and the applied current
+    :math:`I` required to produce that mixture from a source mixture. A set of
+    formal atomic charges corresponding to those in the species comprising the
+    source mixture has to be supplied.
+
+    This function implements the following equation to calculate :math:`\\eta_F`:
+
+    .. math::
+
+        \\eta_F(x) = \\frac{n_\\text{el}(x)\\dot{n}(x)}{I}
+
+    where :math:`x` is a species in the mixture, :math:`n_\\text{el}(x)` is the number
+    of electrons required to produce :math:`x` from the ions specified in the source
+    mixture, and :math:`\\dot{n}(x)` is the molar rate (production or flow) of species
+    :math:`x`.
+
+    Parameters
+    ----------
+    rate
+        A :class:`dict` of molar flow or production rates of species in a mixture.
+        By default in mol/l.
+
+    I
+        The applied current. By default in A.
+
+    charges
+        A :class:`dict` of formal atomic/ionic charges of atoms in the source
+        mixture.
+
+    output
+        Name of the prefix for the species in the output variable. Defaults to ``fe``.
+
+    Returns
+    -------
+    dict(output, E) : dict[str, pint.Quantity]
+        Returns the calculated Faradaic efficiencies.
+
+    """
+    etot = abs(I) / (ureg("elementary_charge") * ureg("avogadro_constant"))
+    pretag = "fe" if output is None else output
+    ret = {}
+    for k, v in rate.items():
+        kchem = name_to_chem(k)
+        n = electrons_from_smiles(kchem.smiles, ions=charges)
+        ek = v * n
+        fek = ek / etot
+        tag = f"{pretag}->{k}"
+        ret[tag] = fek.to_base_units()
+    return ret
