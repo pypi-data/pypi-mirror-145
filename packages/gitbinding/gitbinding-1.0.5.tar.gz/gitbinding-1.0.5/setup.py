32a2575bbfe9e86cc40c42eb56276739b7e43910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:31:33 +0000

    Add package gitbinding/1.0.5/gitbinding-1.0.5.tar.gz

---
+++ packages/gitbinding/gitbinding-1.0.5.tar.gz/gitbinding-1.0.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+requirements = ['anybinding']
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='gitbinding',
+    version='v1.0.5',
+    packages=['gitbinding', ],
+    license='MIT',
+    author="guangrei",
+    author_email="myawn@pm.me",
+    description="python binding git command.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    keywords="git",
+    url="https://github.com/guangrei/Gitpybinding",
+)
