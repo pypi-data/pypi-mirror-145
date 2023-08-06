1ef4368aa1241330d05aa1ccf31f81291790b6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:19 +0000

    Add package django-labportalen/0.0.27/django-labportalen-0.0.27.tar.gz

---
+++ packages/django-labportalen/django-labportalen-0.0.27.tar.gz/django-labportalen-0.0.27/src/labportalen/api/views.py
@@ -0,0 +1,45 @@
+# Python import
+
+# Django import
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from rest_framework.permissions import IsAuthenticated
+
+# Self import
+from labportalen.models import LabportalenReport
+from labportalen.api.serializers import (
+    CreateRemissSerializer,
+    LabportalenReportSerializer
+)
+from labportalen.api.services import LabportalenApiServices
+
+class CreateRemissApiView(APIView):
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        CreateRemissSerializer(data=data).is_valid(raise_exception=True)
+        response = LabportalenApiServices().create_remiss(data, request.user)
+        return Response(response, status=HTTP_200_OK)
+    
+
+class FetchReportForTestEnvApiView(APIView):
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        fetched = LabportalenApiServices().fetch_test_env_report(data)
+        return Response({'Fetched': fetched}, status=HTTP_200_OK)
+
+class LabportalenReportModelViewset(ModelViewSet):
+    model = LabportalenReport
+    serializer_class = LabportalenReportSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post', 'retrieve']
+
+    def get_queryset(self, *args, **kwargs):
+        return self.model.objects.filter(**kwargs)
