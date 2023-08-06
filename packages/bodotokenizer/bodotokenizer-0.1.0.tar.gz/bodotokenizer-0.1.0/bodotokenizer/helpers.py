e9e0af429e89e05a045b7e302461214537260774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:34 +0000

    Add package bodotokenizer/0.1.0/bodotokenizer-0.1.0.tar.gz

---
+++ packages/bodotokenizer/bodotokenizer-0.1.0.tar.gz/bodotokenizer-0.1.0/bodotokenizer/helpers.py
@@ -0,0 +1,31 @@
+"""
+Contains helpers for normalizations
+"""
+
+SINGLE_QUOTE = {
+    "‘",
+    "‛",
+    "’",
+    "❛", 
+    "❜", 
+    "`", 
+    "´", 
+    "‘", 
+    "’"
+}
+
+DOUBLE_QUOTE = {
+    "''",
+    "„",
+    "“",
+    "‟",
+    "”",
+    "❝",
+    "❞",
+    "``",
+    "´´",
+}
+
+DARI_VARIATIONS = {
+    "\|", # escape | for re compile
+}
