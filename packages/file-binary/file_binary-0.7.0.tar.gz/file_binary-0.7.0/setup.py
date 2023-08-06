f25312a27541899b9fc3deef667c45e18c42aa01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:32 +0000

    Add package file-binary/0.7.0/file_binary-0.7.0.tar.gz

---
+++ packages/file-binary/file_binary-0.7.0.tar.gz/file_binary-0.7.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['file_binary']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'file-binary',
+    'version': '0.7.0',
+    'description': 'File Binary is a package with some tools to help you with binary files',
+    'long_description': None,
+    'author': 'matheus',
+    'author_email': 'geof.matheus@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MatheusRamo/file_binary',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
