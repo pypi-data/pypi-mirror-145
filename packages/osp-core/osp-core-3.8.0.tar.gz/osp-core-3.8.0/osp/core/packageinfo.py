87e0ed59ca5806a258d7b156e49fe97cf8c4a8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:14 +0000

    Add package osp-core/3.8.0/osp-core-3.8.0.tar.gz

---
+++ packages/osp-core/osp-core-3.8.0.tar.gz/osp-core-3.8.0/osp/core/packageinfo.py
@@ -0,0 +1,14 @@
+"""Information about the package."""
+
+NAME = "osp-core"
+VERSION = "3.8.0"
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
+# DO NOT MODIFY. Modify the file in the repository's root.
