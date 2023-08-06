8b68a20c1619cbb3fe95389ee75b77a3ef240fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:08 +0000

    Add package ffuzzer/0.1.0/ffuzzer-0.1.0.tar.gz

---
+++ packages/ffuzzer/ffuzzer-0.1.0.tar.gz/ffuzzer-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ffuzzer',
+    version='0.1.0',
+    license='MIT',
+    author='samuzora',
+    author_email='lucastanyj@gmail.com',
+    packages=find_packages('src'),
+    py_modules=['ffuzzer'],
+    keywords='formatstring pwn',
+    url='https://github.com/samuzora/ffuzzer',
+    install_requires=[
+        'Click',
+        'pwntools'
+    ],
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': [
+            'ffuzzer = ffuzzer:cli',
+        ],
+    },
+)
