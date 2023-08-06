b18cfb1def5b05cafec9bf4602f5b3d00d46cf95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:54 +0000

    Add package sqlraw/0.1.0/sqlraw-0.1.0.tar.gz

---
+++ packages/sqlraw/sqlraw-0.1.0.tar.gz/sqlraw-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['sqlraw']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy>=1.4.35,<2.0.0', 'psycopg2>=2.9.3,<3.0.0', 'redis>=4.2.2,<5.0.0']
+
+setup_kwargs = {
+    'name': 'sqlraw',
+    'version': '0.1.0',
+    'description': 'Path base SQL Query',
+    'long_description': None,
+    'author': 'Uygun Bodur',
+    'author_email': 'uygun@dop.com.tr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
