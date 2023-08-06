ce12cf4c9947b3689df0cd5c7fcc64e15c0e9bfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:17 +0000

    Add package artistools/2022.4.6.165022/artistools-2022.4.6.165022-py3-none-any.whl

---
+++ packages/artistools/artistools-2022.4.6.165022-py3-none-any.whl/artistools/inputmodel/__init__.py
@@ -0,0 +1,7 @@
+import artistools
+import artistools.inputmodel.botyanski2017
+import artistools.inputmodel.describeinputmodel
+import artistools.inputmodel.makeartismodel
+import artistools.inputmodel.rprocess_from_trajectory
+
+from .inputmodel import *
