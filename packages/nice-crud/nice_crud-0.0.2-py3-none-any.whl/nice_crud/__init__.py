0275e94197d67e14d6a4405526e0245ffe3e7d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:12 +0000

    Add package nice-crud/0.0.2/nice_crud-0.0.2-py3-none-any.whl

---
+++ packages/nice-crud/nice_crud-0.0.2-py3-none-any.whl/nice_crud/__init__.py
@@ -0,0 +1,7 @@
+from .psql import PostgresCrud
+
+
+__all__ = ['PostgresCrud']
+
+__name__ = 'nice_crud'
+__version__ = '0.0.2'
---
+++ packages/nice-crud/nice_crud-0.0.2-py3-none-any.whl/nice_crud/constants.py
@@ -0,0 +1,11 @@
+class IndexTypes:
+    """
+    This class contains the constants for the index types.
+    """
+
+    BTree = "btree"
+    Hash = "hash"
+    Gist = "gist"
+    Gin = "gin"
+    SpGist = "spgist"
+    BRin = "brin"
