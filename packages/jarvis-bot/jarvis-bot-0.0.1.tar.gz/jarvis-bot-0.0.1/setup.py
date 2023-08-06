a55418760d1563e46b0145e9945deb59d721de39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:37 +0000

    Add package jarvis-bot/0.0.1/jarvis-bot-0.0.1.tar.gz

---
+++ packages/jarvis-bot/jarvis-bot-0.0.1.tar.gz/jarvis-bot-0.0.1/setup.py
@@ -0,0 +1,51 @@
+import os
+
+from setuptools import setup
+
+from version import version_info
+
+base_url = 'https://github.com/thevickypedia/Jarvis'
+
+# https://pypi.org/classifiers/
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Information Technology',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Multimedia :: Sound/Audio :: Speech',
+    'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+    'Topic :: Scientific/Engineering :: Image Recognition',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Multimedia :: Sound/Audio :: Conversion',
+    'Natural Language :: English'
+]
+
+
+setup(
+    name='jarvis-bot',
+    version='.'.join(str(c) for c in version_info),
+    description="IronMan's Jarvis with python.",
+    long_description="PLACE HOLDER FOR SOMETHING GREAT",
+    long_description_content_type='text/markdown',
+    author='Vignesh Sivanandha Rao',
+    author_email='svignesh1793@gmail.com',
+    License='MIT',
+    url=base_url,
+    python_requires=">=3.8",
+    install_requires=[],
+    classifiers=classifiers,
+    keywords='python, natural-language-processing, text-to-speech, speech-recognition, jarvis, hotword-detection,'
+             'virtual-assistant, multiprocessing, threadpool',
+    download_url=f'{base_url}/archive/master.zip',
+    project_urls={
+        'Source': base_url,
+        'Docs': 'https://thevickypedia.github.io/Jarvis',
+        'Demo': 'https://vigneshrao.com/Jarvis/Jarvis_Demo.mp4',
+        'Bug Tracker': f'{base_url}/issues'
+    },
+    zip_safe=True
+)
