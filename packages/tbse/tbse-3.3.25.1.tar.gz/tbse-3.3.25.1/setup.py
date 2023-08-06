2a0a72e9753b68d7e10c0a046df3f726e0434244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:29:19 +0000

    Add package tbse/3.3.25.1/tbse-3.3.25.1.tar.gz

---
+++ packages/tbse/tbse-3.3.25.1.tar.gz/tbse-3.3.25.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools #导入setuptools打包工具
+ 
+with open("src\\README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+ 
+setuptools.setup(
+    name="tbse", # 用自己的名替换其中的YOUR_USERNAME_
+    packages=['tbse'],
+    package_dir={'tbse':'src'},
+    provides=['tbutils','psmlc'],
+    platforms=['nt','posix'],
+    version=open('ver.txt').read(),    #包版本号，便于维护版本
+    author="laman29",    #作者，可以写自己的姓名
+    author_email="fzwxr@hotmail.com",    #作者联系方式，可写自己的邮箱地址
+    description="tbutil simple edition",#包的简述
+    long_description=long_description,    #包的详细介绍，一般在README.md文件内
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/laman29/toolbox",    #自己项目地址，比如github的项目地址
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',    #对python的最低版本要求
+)
