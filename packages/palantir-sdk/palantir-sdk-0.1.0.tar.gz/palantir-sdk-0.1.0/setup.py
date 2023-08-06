53dbfa29d64d59f611375d1b6c27e32d9c27de40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:49 +0000

    Add package palantir-sdk/0.1.0/palantir-sdk-0.1.0.tar.gz

---
+++ packages/palantir-sdk/palantir-sdk-0.1.0.tar.gz/palantir-sdk-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['palantir', 'palantir.core', 'palantir.datasets', 'palantir.datasets.rpc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['conjure-python-client>=2.0.0,<3.0.0',
+ 'pytest>=7.1.0,<8.0.0',
+ 'requests>=2.27.1,<3.0.0',
+ 'tomli>=2.0.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'palantir-sdk',
+    'version': '0.1.0',
+    'description': 'Palantir Python SDK',
+    'long_description': None,
+    'author': 'Andrew Higgins',
+    'author_email': 'ahiggins@palantir.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
