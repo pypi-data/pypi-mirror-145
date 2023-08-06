e0edb2df4421c866a92411e5c94b20d52e20a95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:25 +0000

    Add package terminal-graphics/0.15.0/terminal_graphics-0.15.0.tar.gz

---
+++ packages/terminal-graphics/terminal_graphics-0.15.0.tar.gz/terminal_graphics-0.15.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def find_version():
+    return re.search(r"^__version__ = '(.*)'$",
+                     open('terminal_graphics/version.py', 'r').read(),
+                     re.MULTILINE).group(1)
+
+setup(name='terminal_graphics',
+      version=find_version(),
+      description=('Images in the terminal.'),
+      long_description=open('README.rst', 'r').read(),
+      author='Erik Moqvist',
+      author_email='erik.moqvist@gmail.com',
+      license='MIT',
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+      ],
+      keywords=['terminal', 'image'],
+      url='https://github.com/eerimoq/terminal_graphics',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'Pillow',
+          'numpy'
+      ],
+      python_requires='>=3.6',
+      test_suite="tests",
+      entry_points = {
+          'console_scripts': ['terminal_graphics=terminal_graphics:_main']
+      })
---
+++ packages/terminal-graphics/terminal_graphics-0.15.0.tar.gz/terminal_graphics-0.15.0/terminal_graphics/__init__.py
@@ -0,0 +1,143 @@
+import argparse
+import sys
+
+from PIL import Image
+from PIL.ImageOps import scale as scale_image
+
+from . import kitty
+from . import sixel
+from .terminal import get_graphics_info
+from .terminal import get_preferred_graphics_protocol
+from .terminal import get_size as get_terminal_size
+from .utils import pad_ratio
+
+
+def _do_show(args):
+    if args.size is not None:
+        size = (args.columns, args.rows)
+    else:
+        size = None
+
+    for file in args.files:
+        write_file(file,
+                   sys.stdout.buffer,
+                   args.scale,
+                   size,
+                   args.fill,
+                   True,
+                   args.protocol)
+        sys.stdout.buffer.write(b'\n')
+
+
+def _yes_or_no(value):
+    if value:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def _do_info(args):
+    size = get_terminal_size()
+    print(f'Rows:       {size.cells[1]}')
+    print(f'Columns:    {size.cells[0]}')
+
+    if size.pixels is not None:
+        print(f'Width:      {size.pixels[0]}')
+        print(f'Height:     {size.pixels[1]}')
+
+    if size.cell_pixels is not None:
+        print(f'CellWidth:  {size.cell_pixels[0]}')
+        print(f'CellHeight: {size.cell_pixels[1]}')
+
+    graphics_info = get_graphics_info()
+    sixel = graphics_info.sixel
+    print(f'SixelSupport: {_yes_or_no(sixel.is_supported)}')
+    kitty = graphics_info.kitty
+    print(f'KittySupport: {_yes_or_no(kitty.is_supported)}')
+
+    if kitty.is_supported:
+        transmission_mediums = ', '.join(kitty.transmission_mediums)
+        print(f'KittyTransmissionMediums: {transmission_mediums}')
+
+    iterm = graphics_info.iterm
+    print(f'ITermSupport: {_yes_or_no(iterm.is_supported)}')
+
+
+def _main():
+    parser = argparse.ArgumentParser()
+
+    # Workaround to make the subparser required in Python 3.
+    subparsers = parser.add_subparsers(title='subcommands',
+                                       dest='subcommand')
+    subparsers.required = True
+
+    subparser = subparsers.add_parser('show')
+    subparser.add_argument('--scale',
+                           type=float,
+                           help='Image size scale factor.')
+    subparser.add_argument('--size', help='Width and height in cells.')
+    subparser.add_argument('--fill',
+                           action='store_true',
+                           help='Stretch or squish to fill given size.')
+    subparser.add_argument('--protocol',
+                           help='Protocol to use.')
+    subparser.add_argument('files', nargs="+")
+    subparser.set_defaults(func=_do_show)
+
+    subparser = subparsers.add_parser('info')
+    subparser.set_defaults(func=_do_info)
+
+    args = parser.parse_args()
+
+    try:
+        args.func(args)
+    except BaseException as e:
+        sys.exit('error: ' + str(e))
+
+
+def write(image,
+          fout=None,
+          scale=None,
+          size=None,
+          fill=None,
+          move_cursor=True,
+          protocol=None):
+    """Write given image to the terinal.
+
+    Size is a tuple of width and height in cells.
+
+    Fill is only used if size is given.
+
+    Protocol is the terminal protocol to use, for example 'kitty'.
+
+    """
+
+    if protocol is None:
+        protocol = get_preferred_graphics_protocol()
+
+    if fout is None:
+        fout = sys.stdout.buffer
+
+    if scale is not None:
+        image = scale_image(image, scale)
+
+    if size is not None and not fill:
+        terminal_size = get_terminal_size()
+        image = pad_ratio(image, size, terminal_size.cell_pixels)
+
+    if protocol == 'kitty':
+        kitty.write(image, fout, size, move_cursor)
+    elif protocol == 'sixel':
+        sixel.write(image, fout, size)
+    else:
+        raise Exception(f"Unsupported graphics protocol '{protocol}'.")
+
+
+def write_file(path,
+               fout=None,
+               scale=None,
+               size=None,
+               fill=None,
+               move_cursor=True,
+               protocol=None):
+    write(Image.open(path), fout, scale, size, fill, move_cursor, protocol)
