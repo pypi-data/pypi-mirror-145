ae75f7b1b86ff556948052a6d1148fdddff0287f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package vcorelib/0.2.0/vcorelib-0.2.0.tar.gz

---
+++ packages/vcorelib/vcorelib-0.2.0.tar.gz/vcorelib-0.2.0/README.md
@@ -0,0 +1,15 @@
+<!--
+    =====================================
+    generator=datazen
+    version=1.14.0
+    hash=d4efbc26789da738591a5a5f0e9b39fc
+    =====================================
+-->
+
+# vcorelib ([0.2.0](https://pypi.org/project/vcorelib/))
+
+[![python](https://img.shields.io/pypi/pyversions/vcorelib.svg)](https://pypi.org/project/vcorelib/)
+![Build Status](https://github.com/vkottler/vcorelib/workflows/Python%20package/badge.svg)
+[![codecov](https://codecov.io/gh/vkottler/vcorelib/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/github/vkottler/vcorelib)
+
+*A collection of core Python utilities.*
---
+++ packages/vcorelib/vcorelib-0.2.0.tar.gz/vcorelib-0.2.0/setup.py
@@ -0,0 +1,40 @@
+# =====================================
+# generator=datazen
+# version=1.14.0
+# hash=1d4b1b8b95f6a72ff93ce27d3fbd8bdb
+# =====================================
+
+"""
+vcorelib - Package definition for distribution.
+"""
+
+# third-party
+try:
+    from vmklib.setup import setup
+except (ImportError, ModuleNotFoundError):
+    from vcorelib_bootstrap.setup import setup  # type: ignore
+
+# internal
+from vcorelib import DESCRIPTION, PKG_NAME, VERSION
+
+author_info = {
+    "name": "Vaughn Kottler",
+    "email": "vaughnkottler@gmail.com",
+    "username": "vkottler",
+}
+pkg_info = {
+    "name": PKG_NAME,
+    "slug": PKG_NAME.replace("-", "_"),
+    "version": VERSION,
+    "description": DESCRIPTION,
+    "versions": [
+        "3.7",
+        "3.8",
+        "3.9",
+        "3.10",
+    ],
+}
+setup(
+    pkg_info,
+    author_info,
+)
