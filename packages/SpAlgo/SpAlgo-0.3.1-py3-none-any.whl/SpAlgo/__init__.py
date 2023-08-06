82565b495de9f0d28e0ea20d80ce0541d0663f05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:46 +0000

    Add package SpAlgo/0.3.1/SpAlgo-0.3.1-py3-none-any.whl

---
+++ packages/SpAlgo/SpAlgo-0.3.1-py3-none-any.whl/SpAlgo/__init__.py
@@ -0,0 +1,14 @@
+"""
+The python source code.
+"""
+
+from SpAlgo._peak._array import Array
+from SpAlgo._peak._matrix import Matrix
+from SpAlgo._sequences._sequence import MaxSubseq
+from SpAlgo._knapsack._knapsack import Knapsack
+from SpAlgo._closest_pair._closest import ClosestPair
+from SpAlgo._closest_pair._point import Point
+
+__version__ = "0.3.1"
+__all__ = ['Matrix', 'MaxSubseq', 'Array', 'Knapsack', 'ClosestPair',
+           'Point']
