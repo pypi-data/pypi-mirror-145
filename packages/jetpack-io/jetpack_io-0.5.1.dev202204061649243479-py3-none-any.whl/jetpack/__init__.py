7068950e09e8fd8f80d1bc2a16eea7581b7073c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:14 +0000

    Add package jetpack-io/0.5.1.dev202204061649243479/jetpack_io-0.5.1.dev202204061649243479-py3-none-any.whl

---
+++ packages/jetpack-io/jetpack_io-0.5.1.dev202204061649243479-py3-none-any.whl/jetpack/__init__.py
@@ -0,0 +1,34 @@
+# __version__ is placeholder
+# It gets set in the build/publish process (publish_with_credentials.sh)
+__version__ = "0.5.1-dev202204061649243479"
+
+from typing import Dict
+
+from jetpack.cmd import _root
+from jetpack.core._interface import function, jet, jetroutine, schedule, workflow
+from jetpack.runtime._client import init
+
+__pdoc__: Dict[str, bool] = {}
+
+# Exclude Internal Submodules
+_exclude_list = [
+    "cmd",
+    "cli",
+    "console",
+    "config",
+    "proto",
+    "_utils",
+    "models",
+]
+for key in _exclude_list:
+    __pdoc__[key] = False
+# Include _task Submodule
+__pdoc__["_task"] = True
+
+__all__ = ["function", "jet", "jetroutine", "schedule", "workflow"]
+
+
+def run() -> None:
+    # options can be passed in as env variables with JETPACK prefix
+    # e.g. JETPACK_ENTRYPOINT
+    _root.cli(auto_envvar_prefix="JETPACK")
