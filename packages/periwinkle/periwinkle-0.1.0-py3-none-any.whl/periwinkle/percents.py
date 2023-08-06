c9cbf77b9bfb56c14d29c95daa8525cd5698081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:36:39 +0000

    Add package periwinkle/0.1.0/periwinkle-0.1.0-py3-none-any.whl

---
+++ packages/periwinkle/periwinkle-0.1.0-py3-none-any.whl/periwinkle/percents.py
@@ -0,0 +1,17 @@
+#Laksh Patel ©2022
+
+
+def Solve_PercentWhole(part,whole):
+    answer = part/whole
+    return answer
+
+def Solve_PercentChange(point1,point2):
+    answer = point1 + point2
+    answer = answer/2
+    return answer
+
+
+def Solve_PercentPart(percent,whole):
+    answer = percent/100
+    answer = answer * whole
+    return answer
