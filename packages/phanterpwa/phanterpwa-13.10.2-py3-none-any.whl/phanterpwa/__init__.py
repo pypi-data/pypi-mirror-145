4aa948ce6c4c8b0694cc19f92ec09f6d9ac4c6a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:45 +0000

    Add package phanterpwa/13.10.2/phanterpwa-13.10.2-py3-none-any.whl

---
+++ packages/phanterpwa/phanterpwa-13.10.2-py3-none-any.whl/phanterpwa/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# author: PhanterJR<junior.conex@gmail.com>
+# license: MIT
+
+__author__ = "PhanterJR<junior.conex@gmail.com>"
+__version__ = "13.10.2"
+__install_requeriments__ = [
+    "psutil",
+    "tornado",
+    "libsass",
+    "transcrypt",
+    "pillow",
+    "itsdangerous",
+    "requests-oauthlib",
+    "pydal>=19.04",
+    "passlib",
+    "appdirs>=1.4.3"
+]
