79b859ea2735ecfcb1bea61f33dafacdc84ed4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:56 +0000

    Add package stytch/4.13.0/stytch-4.13.0.tar.gz

---
+++ packages/stytch/stytch-4.13.0.tar.gz/stytch-4.13.0/stytch/api/oauth.py
@@ -0,0 +1,43 @@
+from typing import Any, Dict, Optional
+from enum import Enum
+
+from .base import _validate_attributes, Base
+
+class SessionManagementType(Enum):
+    IDP = "idp"
+    STYTCH = "stytch"
+    NONE = "none"
+
+class OAuth(Base):
+    def __init__(self, client):
+        super().__init__(client)
+
+    @property
+    def oauth_url(self):
+        return self.get_url("oauth")
+
+    def authenticate(
+        self,
+        token: str,
+        session_management_type: Optional[SessionManagementType] = None,
+        session_token: Optional[str] = None,
+        session_jwt: Optional[str] = None,
+        session_duration_minutes: Optional[int] = None,
+    ):
+
+        data: Dict[str, Any] = {
+            "token": token,
+        }
+        if session_management_type:
+            data["session_management_type"] = session_management_type
+        if session_token:
+            data["session_token"] = session_token
+        if session_jwt:
+            data["session_jwt"] = session_jwt
+        if session_duration_minutes:
+            data["session_duration_minutes"] = session_duration_minutes
+
+        return self._post(
+            "{0}/authenticate".format(self.oauth_url),
+            data=data,
+        )
