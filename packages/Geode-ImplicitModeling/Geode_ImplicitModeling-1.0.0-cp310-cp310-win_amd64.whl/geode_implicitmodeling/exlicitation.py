1c5efeb17b6e330e7f90cecf45eaa6259320218b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:40 +0000

    Add package Geode-ImplicitModeling/1.0.0/Geode_ImplicitModeling-1.0.0-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.0.0-cp310-cp310-win_amd64.whl/geode_implicitmodeling/exlicitation.py
@@ -0,0 +1,11 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+from geode_conversion import model
+from geode_backgroundmesh import bmesh_2d
+
+from .geode_implicitmodeling_py_explicitation import *
+
