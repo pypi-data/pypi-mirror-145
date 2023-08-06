6f71f9a8017dc968389d64ceff8f4f73a5db370e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:32 +0000

    Add package breogann/0.2.1/breogann-0.2.1.tar.gz

---
+++ packages/breogann/breogann-0.2.1.tar.gz/breogann-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='breogann',
+    version='0.2.1',
+    license='MIT',
+    author="Fernando Costa",
+    author_email='fernandocostapradillo@gmail.com',
+    packages=find_packages(),
+    url='https://github.com/breogann/pypi',
+    keywords='breogann'
+)
