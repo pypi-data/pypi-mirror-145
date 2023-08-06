8ba6410e8d734081d05e1b70b09628ddcc94dab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:23 +0000

    Add package ktuhope/0.0.5/ktuhope-0.0.5.tar.gz

---
+++ packages/ktuhope/ktuhope-0.0.5.tar.gz/ktuhope-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: POSIX :: Linux',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.8'
+]
+
+setup(
+    name='ktuhope',
+    version='0.0.5',
+    description='Programs on Data Sceince & Machine Learning',
+    long_description=open('README.txt').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    author='Zhongli',
+    author_email='',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='DataScience KTU DSLab',
+    packages=find_packages(),
+    install_requires=['']
+)
