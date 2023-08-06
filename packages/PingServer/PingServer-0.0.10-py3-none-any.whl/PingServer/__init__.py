90594a9647e865902d61baadbf4acd8c78b074a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:57 +0000

    Add package PingServer/0.0.10/PingServer-0.0.10-py3-none-any.whl

---
+++ packages/PingServer/PingServer-0.0.10-py3-none-any.whl/PingServer/__init__.py
@@ -0,0 +1,27 @@
+from tracemalloc import start
+
+
+def start_server(**message):
+  from flask import Flask
+  from flask import Flask, render_template, redirect
+  if message == {}:
+    message = ":)"
+  app = Flask('app')
+  @app.route('/')
+  def Start():
+    return message
+  app.run(host='0.0.0.0', port=6969)
+
+
+def run():
+  from flask import Flask
+  from flask import Flask, render_template, redirect
+  app = Flask('app')
+  app.run(host='0.0.0.0', port=6969)
+
+def thread():
+    from threading import Thread
+    start_server()
+    t = Thread(target=run)
+    t.start()
+    return True
