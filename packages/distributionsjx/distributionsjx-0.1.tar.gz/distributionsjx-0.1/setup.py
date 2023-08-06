60123bf6e0affe872ad0366f2664b09c16172e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:27 +0000

    Add package distributionsjx/0.1/distributionsjx-0.1.tar.gz

---
+++ packages/distributionsjx/distributionsjx-0.1.tar.gz/distributionsjx-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsjx',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsjx'],
+      zip_safe=False)
