0dc82d4cab09172e66c93c8ea88825a35d4a0d53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:04 +0000

    Add package mathematic-package-reza/1.2.0/mathematic-package-reza-1.2.0.tar.gz

---
+++ packages/mathematic-package-reza/mathematic-package-reza-1.2.0.tar.gz/mathematic-package-reza-1.2.0/src/mathematic_reza/aritmatika.py
@@ -0,0 +1,25 @@
+def penjumlahan (a, b):
+    try:
+        return a + b
+    except :
+        return None
+
+
+
+def pengurangan (a, b):
+    try:
+        return a - b
+    except:
+        return None
+
+def perkalian (a, b):
+    try:
+        return a * b
+    except:
+        return None
+
+def pembagian (a, b):
+    try:
+        return a / b
+    except:
+        return None
