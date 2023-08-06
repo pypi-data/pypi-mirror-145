e7732bffd87b3d1c023eb4cd279fcd7836a2c35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:02 +0000

    Add package streamlit-on-Hover-tabs/0.0.2/streamlit_on_Hover_tabs-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-on-Hover-tabs/streamlit_on_Hover_tabs-0.0.2-py3-none-any.whl/on_hover_tabs/frontend/build/precache-manifest.77a933acd9b9462b964bb70542b81ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a570dbf9f46e07b61ec130a843e3e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9abf9925f883d461f39",
+    "url": "./static/css/main.4eabfa2c.chunk.css"
+  },
+  {
+    "revision": "c6c17612b4ac7cffb2d8",
+    "url": "./static/js/2.1558b6fd.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.1558b6fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9abf9925f883d461f39",
+    "url": "./static/js/main.77b8ab10.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
