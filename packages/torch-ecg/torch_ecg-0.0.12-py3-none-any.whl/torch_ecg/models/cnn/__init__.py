1d338bada84c8046dafe11ab4b421d5d93cb4483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:42 +0000

    Add package torch-ecg/0.0.12/torch_ecg-0.0.12-py3-none-any.whl

---
+++ packages/torch-ecg/torch_ecg-0.0.12-py3-none-any.whl/torch_ecg/models/cnn/__init__.py
@@ -0,0 +1,9 @@
+"""
+"""
+
+from . import mobilenet, multi_scopic, resnet, vgg, xception  # noqa: F401
+
+# NOT implemented yet
+# from . import darknet
+# from . import efficientnet
+# from . import ho_resnet
