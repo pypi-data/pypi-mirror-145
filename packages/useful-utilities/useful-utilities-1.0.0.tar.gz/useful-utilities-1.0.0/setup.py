83441f37f5678ab0e19b315b7fbb9f0631be394c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:23 +0000

    Add package useful-utilities/1.0.0/useful-utilities-1.0.0.tar.gz

---
+++ packages/useful-utilities/useful-utilities-1.0.0.tar.gz/useful-utilities-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['useful_utilities']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'useful-utilities',
+    'version': '1.0.0',
+    'description': "Various useful utilities that I created when I didn't find an alternative.",
+    'long_description': None,
+    'author': 'lzrdblzzrd',
+    'author_email': 'lzrdblzzrd@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/lzrdblzzrd/useful-utilities',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
