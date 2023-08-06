8190638543e1bd9f4b554140bd213d4466cb1e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:49:39 +0000

    Add package mrsimulator/0.7.0rc2/mrsimulator-0.7.0rc2-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/mrsimulator/mrsimulator-0.7.0rc2-cp39-cp39-macosx_10_9_x86_64.whl/mrsimulator/utils/error.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+class NamedMethodError(Exception):
+    """Exception raised when NamedClassMethods are renamed.
+
+    Attributes:
+        name: renamed name.
+        cls_name: class name.
+        message: explanation of the error.
+    """
+
+    def __init__(self, name, cls_name, message=None):
+        message = message or (
+            f"`name={name} != classname={cls_name}`. Use the class with the same name "
+            "as the attribute name or use the generic `Method` class."
+        )
+        super().__init__(message)
+
+
+class ImmutableEventError(Exception):
+    """Exception raised for Events are changed in a Named Method object.
+
+    Attributes:
+        cls_name: class name.
+        message: explanation of the error.
+    """
+
+    def __init__(self, cls_name, message=None):
+        message = message or (
+            f"Event objects are immutable for {cls_name} class. Remove all event "
+            "objects or use the generic `Method` class."
+        )
+        super().__init__(message)
+
+
+class MixedSpectralDimensionTypeError(Exception):
+    """Exception raised when SpectralDimension objects and dictionaries are used in the
+    same spectral_dimension list
+
+    Attributes:
+        message: explanation of the error.
+    """
+
+    def __init__(self, message=None):
+        message = message or (
+            "Both dict and SpectralDimension objects found in spectral dimension list. "
+            "Use either dict representation or SpectralDimension objects."
+        )
+        super().__init__(message)
+
+
+class MissingSpectralDimensionError(Exception):
+    """Exception raised when a generic Method object is missing the spectral_dimension
+    argument or is passed no spectral dimensions.
+
+    Attributes:
+        message: explanation of the error.
+    """
+
+    def __init__(self, message=None):
+        message = message or (
+            "Method requires at least one SpectralDimension, none found."
+        )
+        super().__init__(message)
+
+
+class MissingSpectralEventError(Exception):
+    """Exception raised for missing SpectralEvent from SpectralDimension.
+
+    Attributes:
+        message: explanation of the error.
+    """
+
+    def __init__(self, message=None):
+        message = message or (
+            "SpectralDimension requires at least one SpectralEvent, none found."
+        )
+        super().__init__(message)
