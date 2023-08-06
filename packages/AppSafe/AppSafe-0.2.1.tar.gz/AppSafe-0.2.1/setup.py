fa40f058314d6f5f3287259462651eceed70bd97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:17 +0000

    Add package AppSafe/0.2.1/AppSafe-0.2.1.tar.gz

---
+++ packages/AppSafe/AppSafe-0.2.1.tar.gz/AppSafe-0.2.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.1'
+DESCRIPTION = 'A Simple python AppLocker and FileLocker'
+LONG_DESCRIPTION = 'A Simple python AppLocker and FileLocker'
+
+# Setting up
+setup(
+    name="AppSafe",
+    version=VERSION,
+    author="Xeroxxhah (Muhammad Nauman Azeem)",
+    author_email="xeroxxhah@pm.me",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['easygui', 'pyAesCrypt'],
+    keywords=['python', 'python3', 'FileLock', 'AppLock'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
