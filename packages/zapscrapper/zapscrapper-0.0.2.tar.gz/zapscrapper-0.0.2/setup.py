32bfb585ae31190736483684f5b45079465aa856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:37 +0000

    Add package zapscrapper/0.0.2/zapscrapper-0.0.2.tar.gz

---
+++ packages/zapscrapper/zapscrapper-0.0.2.tar.gz/zapscrapper-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license = f.read()
+
+setup(
+    name="zapscrapper",
+    version="0.0.2",
+    url="",
+    license="MIT License",
+    author="Eduardo M. de Morais",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="emdemor415@gmail.com",
+    keywords=["webscrapping", "imoveis"],
+    description="This is a package to make webscrappng on zapimoveis.com.br",
+    packages=["zapscrapper"],
+    install_requires=[
+        "BeautifulSoup4==4.10.0",
+        "boto3==1.21.32",
+        "sqlalchemy==1.4.34",
+        "tqdm==4.63.1",
+        "pandas==1.4.2",
+    ],
+)
