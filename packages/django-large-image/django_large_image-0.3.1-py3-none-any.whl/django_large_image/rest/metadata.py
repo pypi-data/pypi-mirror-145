29305a1f67b3f6e138305f65a6716c6707f3eeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:38 +0000

    Add package django-large-image/0.3.1/django_large_image-0.3.1-py3-none-any.whl

---
+++ packages/django-large-image/django_large_image-0.3.1-py3-none-any.whl/django_large_image/rest/metadata.py
@@ -0,0 +1,60 @@
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from django_large_image import tilesource
+from django_large_image.rest import params
+from django_large_image.rest.base import LargeImageViewMixinBase
+
+
+class MetaDataMixin(LargeImageViewMixinBase):
+    @swagger_auto_schema(
+        method='GET',
+        operation_summary='Returns tile metadata.',
+        manual_parameters=[params.projection],
+    )
+    @action(detail=True)
+    def metadata(self, request: Request, pk: int) -> Response:
+        source = self.get_tile_source(request, pk)
+        metadata = tilesource.get_metadata(source)
+        return Response(metadata)
+
+    """Returns additional known metadata about the tile source."""
+
+    @swagger_auto_schema(
+        method='GET',
+        operation_summary='Returns additional known metadata about the tile source.',
+        manual_parameters=[params.projection],
+    )
+    @action(detail=True)
+    def internal_metadata(self, request: Request, pk: int) -> Response:
+        source = self.get_tile_source(request, pk)
+        metadata = tilesource.get_internal_metadata(source)
+        return Response(metadata)
+
+    @swagger_auto_schema(
+        method='GET',
+        operation_summary='Returns bands information.',
+        manual_parameters=[params.projection],
+    )
+    @action(detail=True)
+    def bands(self, request: Request, pk: int) -> Response:
+        source = self.get_tile_source(request, pk)
+        metadata = source.getBandInformation()
+        return Response(metadata)
+
+    @swagger_auto_schema(
+        method='GET',
+        operation_summary='Returns single band information.',
+        manual_parameters=[
+            params.projection,
+            params.band,
+        ],
+    )
+    @action(detail=True)
+    def band(self, request: Request, pk: int) -> Response:
+        band = int(request.query_params.get('band', 1))
+        source = self.get_tile_source(request, pk)
+        metadata = source.getOneBandInformation(band)
+        return Response(metadata)
