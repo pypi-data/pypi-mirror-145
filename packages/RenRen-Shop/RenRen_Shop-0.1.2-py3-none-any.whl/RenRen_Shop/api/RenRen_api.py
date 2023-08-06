09e7ad5955ef735a4ee1fc2b9ccf8dda80eac70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:43 +0000

    Add package RenRen-Shop/0.1.2/RenRen_Shop-0.1.2-py3-none-any.whl

---
+++ packages/RenRen-Shop/RenRen_Shop-0.1.2-py3-none-any.whl/RenRen_Shop/api/RenRen_api.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2022/3/30 14:27 
+@Author : YarnBlue 
+@description : 
+@File : RenRen_api.py
+"""
+
+from RenRen_Shop.api.url.url import URL
+
+
+class RenRenApi:
+    def __init__(self, session, **kwargs):
+        self.session = session
+        self.URL = URL()
+        self.kwargs = kwargs
