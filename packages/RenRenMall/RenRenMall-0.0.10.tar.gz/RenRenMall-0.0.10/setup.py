bab8450c0c0516e56a43c4ddda80f3b145b79469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:23 +0000

    Add package RenRenMall/0.0.10/RenRenMall-0.0.10.tar.gz

---
+++ packages/RenRenMall/RenRenMall-0.0.10.tar.gz/RenRenMall-0.0.10/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RenRenMall',  # 应用名
+    version='0.0.10',  # 版本号
+    author='YarnBlue',
+    author_email='zqb090325@126.com',
+    description='人人商城后台管理',
+    packages=find_packages(),
+    include_package_data=True
+)
