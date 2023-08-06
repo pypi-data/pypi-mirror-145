5566fde597cdc41061c83c500a285b6dcf92879d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:25 +0000

    Add package cioppino.twothumbs/2.2.4/cioppino.twothumbs-2.2.4-py2.py3-none-any.whl

---
+++ packages/cioppino.twothumbs/cioppino.twothumbs-2.2.4-py2.py3-none-any.whl/cioppino/twothumbs/indexers.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from cioppino.twothumbs.interfaces import ILoveThumbsDontYou
+from plone.indexer.decorator import indexer
+
+import cioppino.twothumbs.rate as rate
+
+
+@indexer(ILoveThumbsDontYou)
+def positive_ratings(obj, **kw):
+    return rate.getTotalPositiveRatings(obj)
---
+++ packages/cioppino.twothumbs/cioppino.twothumbs-2.2.4-py2.py3-none-any.whl/cioppino/twothumbs/interfaces.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from zope.interface import Interface
+
+
+class ILoveThumbsDontYou(Interface):
+    """ Marker to show the thumbs up and thumbs down viewlet
+    """
