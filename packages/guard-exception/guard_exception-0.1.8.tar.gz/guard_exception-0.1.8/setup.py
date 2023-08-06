e0dde085a8b60e2a05519ce9b9791028d3482a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:54 +0000

    Add package guard-exception/0.1.8/guard_exception-0.1.8.tar.gz

---
+++ packages/guard-exception/guard_exception-0.1.8.tar.gz/guard_exception-0.1.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['guard_exception']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'guard-exception',
+    'version': '0.1.8',
+    'description': 'Catch fatal errors or any type of error, used to easily normalize executions in swift calls with PythonKit',
+    'long_description': '# guard_exception\n    - guard all exception for swift run python',
+    'author': 'Rafael Garcia Sagastume',
+    'author_email': 'rafael.garcia@ciberc.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
