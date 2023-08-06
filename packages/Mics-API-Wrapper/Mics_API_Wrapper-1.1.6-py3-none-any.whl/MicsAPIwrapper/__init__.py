25c13c815611bd7ebc7d061ab6ba7c357dc48767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:12 +0000

    Add package Mics-API-Wrapper/1.1.6/Mics_API_Wrapper-1.1.6-py3-none-any.whl

---
+++ packages/Mics-API-Wrapper/Mics_API_Wrapper-1.1.6-py3-none-any.whl/MicsAPIwrapper/__init__.py
@@ -0,0 +1,9 @@
+
+__title__ = "MicsAPIWrapper"
+__license__ = "MIT"
+__author__ = "im_the_tea_drinker"
+__version__ = "1.1.6"
+
+from .wallpapers import moviewallpaperimage, wallpapersimage, filmimagelink , Wallpapersimagelink
+from .textmaker import MctextMaker , Pokemontextmaker , theavengerstextmaker
+from .drinks import teaimage, coffeeimage
