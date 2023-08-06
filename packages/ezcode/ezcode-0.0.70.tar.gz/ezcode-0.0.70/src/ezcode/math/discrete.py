33347087fb7378bad94c82255ff8d49540a2f752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:18 +0000

    Add package ezcode/0.0.70/ezcode-0.0.70.tar.gz

---
+++ packages/ezcode/ezcode-0.0.70.tar.gz/ezcode-0.0.70/src/ezcode/math/discrete.py
@@ -0,0 +1,189 @@
+from ezcode.array.utils import swap, reverse
+from ezcode.array.search import binary_search_subarray_exclusive_boundery
+
+
+def permutation_size(total_size: int, selection_size: int) -> int:
+    """ Not for duplicate items: P(N,r) = N!/(N-r)! """
+    if selection_size > total_size:
+        raise ValueError(f"selection_size:{selection_size} cannot be greater than total_size:{total_size}")
+    if selection_size < 0:
+        raise ValueError(f"selection_size:{selection_size} cannot be negative")
+    result = 1
+    for i in range(total_size - selection_size + 1, total_size + 1):
+        result *= i
+    return result
+
+
+def permutations(selection_size: int, items: list) -> list:
+    def _permutations(selection_size: int, items: list, selected_indices: set(), permutation: list, result: list):
+        if len(permutation) == selection_size:
+            result.append(permutation.copy())
+            return
+        selected_items = set()
+        for i in range(len(items)):
+            if i not in selected_indices and items[i] not in selected_items:  # check for duplicate indices and duplicate items
+                selected_indices.add(i)
+                selected_items.add(items[i])
+                permutation.append(items[i])
+                _permutations(selection_size, items, selected_indices, permutation, result)
+                permutation.pop()
+                selected_indices.remove(i)
+
+    if items is None:
+        return None
+    if selection_size > len(items):
+        raise ValueError(f"selection_size:{selection_size} cannot be greater than len(items):{len(items)}")
+    if selection_size < 0:
+        raise ValueError(f"selection_size:{selection_size} cannot be negative")
+    result = list()
+    _permutations(selection_size, items, set(), list(), result)
+    return result
+
+
+def next_lexicographic_permutation(items: list, copy=True) -> list:
+    if copy:
+        items = items.copy()
+    begin = -1
+    for i in range(len(items) - 2, -1, -1):
+        if items[i] < items[i + 1]:
+            begin = i
+            break
+    if begin < 0:
+        reverse(items)
+    else:
+        first_greater_from_end = binary_search_subarray_exclusive_boundery(
+            items, begin=begin + 1, end=len(items) - 1, target=items[begin], is_ascending=False, is_smaller=False)
+        swap(items, begin, first_greater_from_end)
+        reverse(items, begin + 1, len(items) - 1)
+    return items
+
+
+def all_items_permutations(items: list) -> list:
+    """ Equals to permutations(len(items, items)) """
+    def _permutations_with_all_items(items: list, current_index, result: list):
+        if current_index == len(items) - 1:
+            result.append(items.copy())
+            return
+        selected_items = set()
+        for next_index in range(current_index, len(items)):
+            if items[next_index] not in selected_items:  # check for duplicate items
+                selected_items.add(items[next_index])
+                swap(items, current_index, next_index)
+                _permutations_with_all_items(items, current_index + 1, result)
+                swap(items, current_index, next_index)
+
+    if items is None:
+        return None
+    if len(items) == 0:
+        return [[]]
+    result = list()
+    _permutations_with_all_items(items, 0, result)
+    return result
+
+
+def combination_size(total_size: int, selection_size: int) -> int:
+    """ C(N,r) = C(N,N-r) = A(N,r)/r! = N!/[r! * (N-r)!] """
+    if selection_size > total_size:
+        raise ValueError(f"selection_size:{selection_size} cannot be greater than total_size:{total_size}")
+    if selection_size < 0:
+        raise ValueError(f"selection_size:{selection_size} cannot be negative")
+    result = permutation_size(total_size, selection_size)
+    for i in range(1, selection_size + 1):
+        result //= i
+    return result
+
+
+def combinations(selection_size: int, items: list):
+    def _combinations(selection_size: int, items: list, start_index: int, combination: list, result: list):
+        if len(combination) == selection_size:
+            result.append(combination.copy())
+            return
+        selected_items = set()
+        end_index = len(items) - (selection_size - len(combination))  # not enough indices left, terminate recursion early
+        for i in range(start_index, end_index + 1):
+            if items[i] not in selected_items:  # check for duplicate items
+                selected_items.add(items[i])
+                combination.append(items[i])
+                _combinations(selection_size, items, i + 1, combination, result)
+                combination.pop()
+
+    if items is None:
+        return None
+    if selection_size > len(items):
+        raise ValueError(f"selection_size:{selection_size} cannot be greater than len(items):{len(items)}")
+    if selection_size < 0:
+        raise ValueError(f"selection_size:{selection_size} cannot be negative")
+    result = list()
+    items.sort()  # for duplicate items
+    _combinations(selection_size, items, 0, list(), result)
+    return result
+
+
+def all_subsets(items: list, has_duplicate=True):
+    """ Equals to combinations(i, items) for i in range(len(items) + 1) """
+    def _all_subsets_recursion(items: list, start_index: int, subset: list, result: list):
+        if start_index > len(items):
+            return
+        result.append(subset.copy())
+        selected_items = set()
+        for i in range(start_index, len(items)):
+            if items[i] not in selected_items:  # check for duplicate items
+                selected_items.add(items[i])
+                subset.append(items[i])
+                _all_subsets_recursion(items, i + 1, subset, result)
+                subset.pop()
+
+    def _all_subsets_iteration(items: list, result: list):
+        """ 2^N subsets, Not for duplicate items """
+        number_range = 1 << len(items)
+        for i in range(number_range):
+            subset = list()
+            for shift in range(len(items)):
+                if ((i >> shift) & 1) == 1:
+                    subset.append(items[shift])
+            result.append(subset.copy())
+
+    if items is None:
+        return None
+    result = list()
+    if has_duplicate:
+        items.sort()  # for duplicate items
+        _all_subsets_recursion(items, 0, list(), result)
+    else:
+        _all_subsets_iteration(items, result)
+    result.sort(key=lambda x: len(x))  # nice to have
+    return result
+
+
+def partitions(string: str) -> list:
+    """
+        '1234' -> [
+            ['1', '234'],
+            ['1', '2', '34'],
+            ['1', '2', '3', '4'],
+            ['1', '23', '4'],
+            ['12', '34'],
+            ['12', '3', '4'],
+            ['123', '4']
+        ]
+    """
+    return list(partition_generator(string))
+
+
+def partition_generator(string: str):
+    yield [string]
+    for i in range(1, len(string)):
+        for parts in partition_generator(string[i:]):
+            yield [string[0:i]] + parts
+
+
+
+
+
+
+
+
+
+
+
+
