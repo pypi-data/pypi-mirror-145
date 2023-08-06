516189bfc250d11b8bfe0fb02c988639f751427f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:59 +0000

    Add package test-example-package/0.0.2/test_example_package-0.0.2.tar.gz

---
+++ packages/test-example-package/test_example_package-0.0.2.tar.gz/test_example_package-0.0.2/src/test_example_package/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+test_example_package: Test package to use it as template for building other packages
+"""
+__author__  = "Robert Rijnbeek"
+__email__   = "robert270384@gmail.com"
+__version__ = "0.0.2"
+
+
+def helloWorld():
+    print("helo world!!")
+    
+
+def helloAuthor():
+    print(f"hello: {__author__}")
