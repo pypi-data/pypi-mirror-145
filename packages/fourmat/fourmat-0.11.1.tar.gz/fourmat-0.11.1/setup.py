7a1864e4a20a8bc7b9b9bfaa15d8c30a9b41a23b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:02 +0000

    Add package fourmat/0.11.1/fourmat-0.11.1.tar.gz

---
+++ packages/fourmat/fourmat-0.11.1.tar.gz/fourmat-0.11.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="fourmat",
+    version="0.11.1",
+    description="A library for batteries-included linting and autoformatting",
+    url="https://github.com/4Catalyzer/fourmat",
+    author="Giacomo Tagliabue",
+    author_email="giacomo@gmail.com",
+    license="MIT",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords="lint autoformat black flake8 isort",
+    packages=find_packages(),
+    package_data={"fourmat": ("assets/*.*", "assets/.*")},
+    install_requires=(
+        "click>=7",
+        "black==22.3.0",
+        "flake8-bugbear>=21,<22",
+        "flake8>=3,<4",
+        "isort>=5,<6",
+    ),
+    entry_points={"console_scripts": ("fourmat = fourmat:cli",)},
+)
