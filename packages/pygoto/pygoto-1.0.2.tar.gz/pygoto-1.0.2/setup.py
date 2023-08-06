0d000d5ddccc88dad7e6c1a8dc78cac1a123dccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:35 +0000

    Add package pygoto/1.0.2/pygoto-1.0.2.tar.gz

---
+++ packages/pygoto/pygoto-1.0.2.tar.gz/pygoto-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+setup(
+    name='pygoto',
+    version='1.0.2',
+    license='MIT',
+    author='Elisha Hollander',
+    author_email='just4now666666@gmail.com',
+    description="Use goto in Python",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/donno2048/pygoto',
+    project_urls={
+        'Documentation': 'https://github.com/donno2048/pygoto#readme',
+        'Bug Reports': 'https://github.com/donno2048/pygoto/issues',
+        'Source Code': 'https://github.com/donno2048/pygoto',
+    },
+    python_requires='>=3.0',
+    packages=find_packages(),
+    classifiers=['Programming Language :: Python :: 3']
+)
