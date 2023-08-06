c9100ad255ecd9a616302c81126d581d131362d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:28 +0000

    Add package tdmclient/0.1.15/tdmclient-0.1.15.tar.gz

---
+++ packages/tdmclient/tdmclient-0.1.15.tar.gz/tdmclient-0.1.15/tdmclient/__init__.py
@@ -0,0 +1,26 @@
+# This file is part of tdmclient.
+# Copyright 2021 ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
+# Miniature Mobile Robots group, Switzerland
+# Author: Yves Piguet
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Communication with TDM.
+"""
+
+from tdmclient.fb import FlatBuffer, Table, Union
+from tdmclient.zeroconf import TDMZeroconfBrowser
+from tdmclient.tcp import TDMConnection
+from tdmclient.thymio import ThymioFB, Node
+from tdmclient.client import Client
+from tdmclient.clientasync import ClientAsync
+from tdmclient.clientnode import ClientNode
+from tdmclient.clientasyncnode import ClientAsyncNode
+from tdmclient.clientasynccachenode import ClientAsyncCacheNode, ArrayCache
+from tdmclient.repl import TDMConsole
+
+from tdmclient.server import Server, ServerNode, ServerRawTDMHandler
+
+# shortcut
+aw = ClientAsync.aw
