f0448298a91bb717ec40ce845c63dcf0079d7483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:36 +0000

    Add package PingServer/0.0.8/PingServer-0.0.8.tar.gz

---
+++ packages/PingServer/PingServer-0.0.8.tar.gz/PingServer-0.0.8/src/PingServer/__init__.py
@@ -0,0 +1,15 @@
+def start_server(**message):
+  from flask import Flask
+  from flask import Flask, render_template, redirect
+  from threading import Thread
+  if message == {}:
+    message = ":)"
+  app = Flask('app')
+  @app.route('/')
+  def Start():
+    return message
+  app.run(host='0.0.0.0', port=6969)
+
+
+def run():
+  app.run(host='0.0.0.0', port=6969)
