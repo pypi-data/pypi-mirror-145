65955692d4b12853c4754edd53885ec21e3e73d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:35 +0000

    Add package coilcraft/1.0/coilcraft-1.0.tar.gz

---
+++ packages/coilcraft/coilcraft-1.0.tar.gz/coilcraft-1.0/setup.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from distutils.core import setup
+setup(name='coilcraft',  #打包后的包文件名
+      version='1.0',
+      description='coilcraft_loss', #说明
+      author='felix',
+      author_email='qiyu_sjtu@163.com',
+      url='',
+      py_modules=['coilcraft.coilcraft_loss'],   #你要打包的文件
+)
