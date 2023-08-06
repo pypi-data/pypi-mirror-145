55295cd08ce3547c2a8e80585ce1326925a02866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:18 +0000

    Add package adbutil/0.0.4/adbutil-0.0.4.tar.gz

---
+++ packages/adbutil/adbutil-0.0.4.tar.gz/adbutil-0.0.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2022/4/6 10:31
+# @Author : w
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='adbutil',
+    version='0.0.4',
+    author='jayden',
+    author_email='282669595@qq.com',
+    url='https://blog.csdn.net/Jonder_wu?type=blog',
+    description=u'android flash update da1',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            # 'flash=flash:pp_flash',
+            'jujube=flash:jujube',
+        ]
+    }
+)
