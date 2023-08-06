dbb1901be6ff2a688bd755a455e40587959732f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:41 +0000

    Add package iothub-device-client/0.0.1/iothub-device-client-0.0.1.tar.gz

---
+++ packages/iothub-device-client/iothub-device-client-0.0.1.tar.gz/iothub-device-client-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='iothub-device-client',
+    version='0.0.1',
+    description='Welcome to the `azure-iothub-device-client` package',
+    author='David Doran',
+    author_email='daviddoran20161@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    license='AGPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Development Status :: 3 - Alpha'
+    ]
+)
