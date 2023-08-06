e22584f463f5ae9725b36ae4a6245b520db8116d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:17 +0000

    Add package pysyd/4.8.1/pysyd-4.8.1.tar.gz

---
+++ packages/pysyd/pysyd-4.8.1.tar.gz/pysyd-4.8.1/pysyd/__init__.py
@@ -0,0 +1,31 @@
+"""
+``pySYD``
+
+"""
+
+import os
+import sys
+
+__all__ = ['cli', 'pipeline', 'models', 'target', 'plots', 'utils']
+__author__ = 'ashley <achontos@hawaii.edu>'
+__version__ = '4.8.1'
+
+_ROOT = os.path.abspath(os.getcwd())
+INFDIR = os.path.join(_ROOT, 'info')
+INPDIR = os.path.join(_ROOT, 'data')
+OUTDIR = os.path.join(_ROOT, 'results')
+
+import sys
+
+# enforce python version
+# (same as check at beginning of setup.py)
+
+__minimum_python_version__ = "3.6"
+
+class PythonNotSupportedError(Exception):
+    pass
+
+
+if sys.version_info < tuple((int(val) for val in __minimum_python_version__.split('.'))):
+    raise PythonNotSupportedError(
+        f"{__package__} does not support Python < {__minimum_python_version__}")
