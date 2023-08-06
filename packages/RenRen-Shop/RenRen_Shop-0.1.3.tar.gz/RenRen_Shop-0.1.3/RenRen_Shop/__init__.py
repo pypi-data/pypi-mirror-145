c14d3e936836ab0779d24c6ceedffae1e73b15fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:53 +0000

    Add package RenRen-Shop/0.1.3/RenRen_Shop-0.1.3.tar.gz

---
+++ packages/RenRen-Shop/RenRen_Shop-0.1.3.tar.gz/RenRen_Shop-0.1.3/RenRen_Shop/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/4/5 18:26
+# @Author  : YarnBlue
+# @desc    : 
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ packages/RenRen-Shop/RenRen_Shop-0.1.3.tar.gz/RenRen_Shop-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='RenRen_Shop',  # 应用名
+    version='0.1.3',  # 版本号
+    author='YarnBlue',
+    author_email='zqb090325@126.com',
+    description='人人商城后台管理',
+    py_modules=['RenRen_Shop'],
+    include_package_data=True
+)
