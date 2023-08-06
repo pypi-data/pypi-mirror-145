7610c75940a5d0e9c43ded407558c81318de0655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package aisquared/0.1.3/aisquared-0.1.3-py3-none-any.whl

---
+++ packages/aisquared/aisquared-0.1.3-py3-none-any.whl/aisquared/config/feedback/__init__.py
@@ -0,0 +1,6 @@
+from .SimpleFeedback import SimpleFeedback
+from .BinaryFeedback import BinaryFeedback
+from .MulticlassFeedback import MulticlassFeedback
+from .RegressionFeedback import RegressionFeedback
+from .ModelFeedback import ModelFeedback
+from .QualitativeFeedback import QualitativeFeedback
