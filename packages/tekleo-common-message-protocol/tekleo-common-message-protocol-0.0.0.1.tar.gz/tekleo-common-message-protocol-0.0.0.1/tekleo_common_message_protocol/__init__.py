ccdf446745afcfa9fa7ca2d1f38dc77e897f9768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:57 +0000

    Add package tekleo-common-message-protocol/0.0.0.1/tekleo-common-message-protocol-0.0.0.1.tar.gz

---
+++ packages/tekleo-common-message-protocol/tekleo-common-message-protocol-0.0.0.1.tar.gz/tekleo-common-message-protocol-0.0.0.1/tekleo_common_message_protocol/__init__.py
@@ -0,0 +1,9 @@
+from .image_base64_input import ImageBase64Input
+from .image_url_input import ImageUrlInput
+from .ping_output import PingOutput
+
+__all__ = [
+    ImageBase64Input,
+    ImageUrlInput,
+    PingOutput,
+]
