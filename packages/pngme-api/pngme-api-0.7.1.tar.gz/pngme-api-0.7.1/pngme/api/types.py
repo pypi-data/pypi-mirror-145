9add0a38e790a30ee4a875a9b375dfeae6fafe8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:36 +0000

    Add package pngme-api/0.7.1/pngme-api-0.7.1.tar.gz

---
+++ packages/pngme-api/pngme-api-0.7.1.tar.gz/pngme-api-0.7.1/pngme/api/types.py
@@ -0,0 +1,6 @@
+from typing import Literal
+
+AccountType = Literal["depository", "loan", "mobilemoney"]
+Category = Literal["LOANS_AND_CREDIT_CARDS", "BETTING_AND_LOTTERY", "SALARY", "INCOME"]
+Currency = Literal["AED", "GHS", "KES", "NGN", "USD", "unknown"]
+Impact = Literal["CREDIT", "DEBIT"]
