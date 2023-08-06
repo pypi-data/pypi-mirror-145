ba74682479c24dd5cd54b3167920e4c141947f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:12 +0000

    Add package isic-cli/2.0.0/isic_cli-2.0.0-py3-none-any.whl

---
+++ packages/isic-cli/isic_cli-2.0.0-py3-none-any.whl/isic_cli/session.py
@@ -0,0 +1,44 @@
+import logging
+import time
+from typing import Optional
+
+from retryable_requests import RetryableSession
+
+logger = logging.getLogger('isic_cli')
+
+
+class IsicCliSession(RetryableSession):
+    def __init__(self, *args, **kwargs) -> None:
+        from isic_cli.utils.version import get_version
+
+        super().__init__(*args, **kwargs)
+
+        self.headers.update(
+            {
+                'Accept': 'application/json',
+                'User-agent': f'isic-cli/{get_version()}',
+            }
+        )
+
+    def request(self, *args, **kwargs):
+        start = time.time()
+        r = super().request(*args, **kwargs)
+        end = time.time()
+
+        # TODO: this is a little confusing because retries are included in
+        # these times - maybe break it out.
+        logger.debug(f'timing: {end - start}')
+
+        if not r.ok:
+            logger.debug(f'bad response: {r.text}')
+
+        return r
+
+
+def get_session(
+    base_url: str = 'https://api.isic-archive.com/api/v2/', headers: Optional[dict] = None
+) -> IsicCliSession:
+    session = IsicCliSession(base_url)
+    if headers:
+        session.headers.update(headers)
+    return session
