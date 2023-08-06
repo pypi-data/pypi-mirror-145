732605b45e13fc7a5e158631469be3d07fac3faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:03 +0000

    Add package logger-bro/0.0.2/logger_bro-0.0.2.tar.gz

---
+++ packages/logger-bro/logger_bro-0.0.2.tar.gz/logger_bro-0.0.2/logger_bro/settings.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+from functools import lru_cache
+import os
+
+
+class Settings(BaseSettings):
+    APP_NAME: str
+    ENV: str
+    LOKI: str
+
+    class Config:
+        env_file = os.getenv("ENV_FILE") or ".env.dev"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
