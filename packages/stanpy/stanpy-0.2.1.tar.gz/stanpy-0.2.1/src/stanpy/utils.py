fae1eb2d6587b6dface6d9df03960f74ce06094e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:19 +0000

    Add package stanpy/0.2.1/stanpy-0.2.1.tar.gz

---
+++ packages/stanpy/stanpy-0.2.1.tar.gz/stanpy-0.2.1/src/stanpy/utils.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def signif(x, p, round=10):
+    x = np.asarray(x)
+    x_positive = np.where(np.isfinite(x) & (x != 0), np.abs(x), 10 ** (p - 1))
+    mags = 10 ** (p - 1 - np.floor(np.log10(x_positive)))
+    return np.round(np.round(x * mags) / mags, 10)
