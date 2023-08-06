a906c87e484337fa465f952860cbafc1536df805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package django-import-export-celery/1.1.8/django_import_export_celery-1.1.8-py3-none-any.whl

---
+++ packages/django-import-export-celery/django_import_export_celery-1.1.8-py3-none-any.whl/import_export_celery/models/exportjob.py
@@ -0,0 +1,143 @@
+# Copyright (C) 2019 o.s. Auto*Mat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+from django.utils import timezone
+import json
+
+from author.decorators import with_author
+
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.db import transaction
+from django.dispatch import receiver
+
+from django.db.models.signals import post_save
+from django.utils.translation import ugettext_lazy as _
+
+from import_export.formats.base_formats import DEFAULT_FORMATS
+
+from ..tasks import run_export_job
+
+
+@with_author
+class ExportJob(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._content_type = None
+
+    file = models.FileField(
+        verbose_name=_("exported file"),
+        upload_to="django-import-export-celery-export-jobs",
+        blank=False,
+        null=False,
+        max_length=255,
+    )
+
+    processing_initiated = models.DateTimeField(
+        verbose_name=_("Have we started processing the file? If so when?"),
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+    job_status = models.CharField(
+        verbose_name=_("Status of the job"),
+        max_length=160,
+        blank=True,
+    )
+
+    format = models.CharField(
+        verbose_name=_("Format of file to be exported"),
+        max_length=255,
+        blank=False,
+        null=True,
+    )
+
+    app_label = models.CharField(
+        verbose_name=_("App label of model to export from"),
+        max_length=160,
+    )
+
+    model = models.CharField(
+        verbose_name=_("Name of model to export from"),
+        max_length=160,
+    )
+
+    resource = models.CharField(
+        verbose_name=_("Resource to use when exporting"),
+        max_length=255,
+        default="",
+    )
+
+    queryset = models.TextField(
+        verbose_name=_("JSON list of pks to export"),
+        null=False,
+    )
+
+    email_on_completion = models.BooleanField(
+        verbose_name=_("Send me an email when this export job is complete"),
+        default=True,
+    )
+
+    site_of_origin = models.TextField(
+        max_length=255,
+        default="",
+    )
+
+    def get_resource_class(self):
+        if self.resource:
+            return (
+                self.get_content_type()
+                .model_class()
+                .export_resource_classes()[self.resource][1]
+            )
+
+    def get_content_type(self):
+        if not self._content_type:
+            self._content_type = ContentType.objects.get(
+                app_label=self.app_label,
+                model=self.model,
+            )
+        return self._content_type
+
+    def get_queryset(self):
+        pks = json.loads(self.queryset)
+        return self.get_content_type().model_class().objects.filter(pk__in=pks)
+
+    def get_resource_choices(self):
+        return [
+            (k, v[0])
+            for k, v in self.get_content_type()
+            .model_class()
+            .export_resource_classes()
+            .items()
+        ]
+
+    @staticmethod
+    def get_format_choices():
+        """returns choices of available export formats"""
+        return [
+            (f.CONTENT_TYPE, f().get_title())
+            for f in DEFAULT_FORMATS
+            if f().can_export()
+        ]
+
+
+@receiver(post_save, sender=ExportJob)
+def exportjob_post_save(sender, instance, **kwargs):
+    if instance.resource and not instance.processing_initiated:
+        instance.processing_initiated = timezone.now()
+        instance.save()
+        transaction.on_commit(lambda: run_export_job.delay(instance.pk))
