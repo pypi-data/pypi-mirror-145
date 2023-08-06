61dc87d68b8b0d874234a1b3c8c97e0bb58d5346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package pdf-crop/0.0.15/pdf_crop-0.0.15.tar.gz

---
+++ packages/pdf-crop/pdf_crop-0.0.15.tar.gz/pdf_crop-0.0.15/pdf_crop/__init__.py
@@ -0,0 +1,26 @@
+from rich.console import Console
+from rich.table import Table
+from pdf_crop.crop_pymupdf import PymuPDFCrop
+from pdf_crop.helper import rect_str
+
+
+def crop(input_file, page_option: list):
+    table = Table(title="PDF Crop Summary", leading=0)
+    table.add_column("Page No.", justify="right", style="cyan", no_wrap=True)
+    table.add_column("MediaBox", justify="right", style="magenta")
+    table.add_column("CropBox", justify="right", style="magenta")
+    table.add_column("Output", justify="right", style="green")
+
+    space, threshold = (5, 0.008)
+    for option in page_option:
+        page_no, output_file = option[:2]
+        if len(option) > 2:
+            space = option[2]
+        if len(option) > 3:
+            threshold = option[3]
+        pdf_crop = PymuPDFCrop(input_file=input_file, space=space, threshold=threshold)
+        output_file, media_box, crop_box = pdf_crop.crop(page_no, output_file=output_file, crop_box=None)
+        table.add_row(str(option[0]), rect_str(media_box), rect_str(crop_box), output_file)
+    # Show summary
+    console = Console()
+    console.print(table)
