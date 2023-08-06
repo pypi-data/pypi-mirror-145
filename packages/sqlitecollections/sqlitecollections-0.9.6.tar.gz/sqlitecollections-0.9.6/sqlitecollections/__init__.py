09088a577f86ace47055ebb8367661a9780be308
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:40 +0000

    Add package sqlitecollections/0.9.6/sqlitecollections-0.9.6.tar.gz

---
+++ packages/sqlitecollections/sqlitecollections-0.9.6.tar.gz/sqlitecollections-0.9.6/sqlitecollections/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.9.6"
+__author__ = "osoken"
+__description__ = "Python collections that are backended by sqlite3 DB and are compatible with the built-in collections"
+__email__ = "osoken.devel@outlook.jp"
+__package_name__ = "sqlitecollections"
+
+
+from .base import RebuildStrategy
+from .dict import Dict
+from .list import List
+from .set import Set
+
+__all__ = ["Dict", "List", "Set", "RebuildStrategy"]
