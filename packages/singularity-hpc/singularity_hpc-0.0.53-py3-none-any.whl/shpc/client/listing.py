f7c291df1153ec38e2ecca18eac5f61ccc3681ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:34 +0000

    Add package singularity-hpc/0.0.53/singularity_hpc-0.0.53-py3-none-any.whl

---
+++ packages/singularity-hpc/singularity_hpc-0.0.53-py3-none-any.whl/shpc/client/listing.py
@@ -0,0 +1,17 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2021-2022, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+
+def main(args, parser, extra, subparser):
+
+    from shpc.main import get_client
+
+    cli = get_client(
+        quiet=args.quiet,
+        settings_file=args.settings_file,
+        module=args.module,
+        container_tech=args.container_tech,
+    )
+
+    cli.list(args.pattern, args.names_only, short=args.short)
