681e197f4c5615dbcdeddefc03076f337def5377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:48 +0000

    Add package STPN/0.0.4/STPN-0.0.4.tar.gz

---
+++ packages/STPN/STPN-0.0.4.tar.gz/STPN-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="STPN", # Replace with your own username
+    version="0.0.4",
+    author="Sin Yong Tan",
+    author_email="tsyong98@gmail.com",
+    description="Library for Spatiotemporal Pattern Network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tsyong98/STPN",
+    project_urls={
+        "Bug Tracker": "https://github.com/tsyong98/STPN/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires='>=3.6',
+)
