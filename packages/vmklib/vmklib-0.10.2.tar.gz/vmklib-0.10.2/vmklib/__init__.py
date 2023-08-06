254064195d3dcbb152450ff71263a69baf67b6da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:37 +0000

    Add package vmklib/0.10.2/vmklib-0.10.2.tar.gz

---
+++ packages/vmklib/vmklib-0.10.2.tar.gz/vmklib-0.10.2/vmklib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.14.0
+# hash=7eea0690abfecb0ea6c23b70ba7d7e4e
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
+PKG_NAME = "vmklib"
+VERSION = "0.10.2"
