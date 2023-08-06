f26610d60f5111d8eecbaadb913e1d635b1f0775
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:03 +0000

    Add package ABSQL/0.1.5/ABSQL-0.1.5-py3-none-any.whl

---
+++ packages/ABSQL/ABSQL-0.1.5-py3-none-any.whl/tests/test_render.py
@@ -0,0 +1,41 @@
+import pytest
+import mock
+import os
+from absql import Runner
+
+
+@pytest.fixture(autouse=True)
+def mock_settings_env_vars():
+    with mock.patch.dict(os.environ, {"name": "Bob"}):
+        yield
+
+
+@pytest.fixture()
+def runner():
+    return Runner(greeting="Hello")
+
+
+@pytest.fixture
+def simple_sql_path():
+    return "tests/files/simple.sql"
+
+
+@pytest.fixture
+def no_frontmatter_path():
+    return "tests/files/no_frontmatter.sql"
+
+
+def test_render_simple_sql(runner, simple_sql_path):
+    sql = runner.render(simple_sql_path)
+    assert sql == "SELECT * FROM my_table"
+
+
+def test_render_no_frontmatter(runner, no_frontmatter_path):
+    sql = runner.render(no_frontmatter_path)
+    assert sql == "SELECT * FROM Hello"
+
+
+def test_render_text_only(runner):
+    got = runner.render("{{greeting}}, {{env_var('name')}}!")
+    want = "Hello, Bob!"
+    assert got == want
