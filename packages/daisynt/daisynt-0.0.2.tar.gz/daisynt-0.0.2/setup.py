808f3ee5c054ca571c1c4b32bd4ba81f4fe01fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:04 +0000

    Add package daisynt/0.0.2/daisynt-0.0.2.tar.gz

---
+++ packages/daisynt/daisynt-0.0.2.tar.gz/daisynt-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="daisynt",
+    version="0.0.2",
+    author="Giacomo Graffi",
+    author_email="giacomo.graffi@gmail.com",
+    description="Test Suite to enable Synthetic Data adoption",
+    long_description="Visit the demo notebook on https://github.com/GiacomoGraffi/DAISYnt/blob/main/SDV_generative_models_comparison.ipynb",
+    python_requires='>=3.6'
+)
