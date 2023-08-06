8d375d1e579a5b079acc2df23d322742f5ea78c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:15 +0000

    Add package drugstone/0.0.2/drugstone-0.0.2.tar.gz

---
+++ packages/drugstone/drugstone-0.0.2.tar.gz/drugstone-0.0.2/src/drugstone/scripts/constants/url.py
@@ -0,0 +1,17 @@
+"""
+drugstone.scripts.constants.url
+
+This module implements the class Url.
+
+:copyright: 2022 Institute for Computational Systems Biology by Prof. Dr. Jan Baumbach
+:author: Ugur Turhan
+"""
+
+
+class Url:
+    BASE = "https://drugst.one/drugstone_api/"
+    MAP_NODES = BASE + "map_nodes/"
+    TASK = BASE + "task/"
+    TASKS = BASE + "tasks/"
+    TASK_RESULTS = BASE + "task_result/"
+    FETCH_EDGES = BASE + "fetch_edges/"
