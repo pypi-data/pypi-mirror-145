bcd08ebd15440478893ef4d5029f1387bc20d439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:41 +0000

    Add package dbt-greenplum/0.19.3/dbt-greenplum-0.19.3.tar.gz

---
+++ packages/dbt-greenplum/dbt-greenplum-0.19.3.tar.gz/dbt-greenplum-0.19.3/dbt/adapters/greenplum/__init__.py
@@ -0,0 +1,15 @@
+from dbt.adapters.greenplum.connections import GreenplumConnectionManager
+from dbt.adapters.greenplum.connections import GreenplumCredentials
+from dbt.adapters.greenplum.relation import GreenplumColumn
+from dbt.adapters.greenplum.relation import GreenplumRelation
+from dbt.adapters.greenplum.impl import GreenplumAdapter
+
+
+from dbt.adapters.base import AdapterPlugin
+from dbt.include import greenplum
+
+
+Plugin = AdapterPlugin(
+    adapter=GreenplumAdapter,
+    credentials=GreenplumCredentials,
+    include_path=greenplum.PACKAGE_PATH)
