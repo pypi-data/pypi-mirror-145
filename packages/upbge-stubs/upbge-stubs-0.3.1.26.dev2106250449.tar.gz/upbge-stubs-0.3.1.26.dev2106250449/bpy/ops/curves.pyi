e5c8723bc2c8044a36fa4d8b5580fa78e41ade63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:15 +0000

    Add package upbge-stubs/0.3.1.26.dev2106250449/upbge-stubs-0.3.1.26.dev2106250449.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.26.dev2106250449.tar.gz/upbge-stubs-0.3.1.26.dev2106250449/bpy/ops/curves.pyi
@@ -0,0 +1,33 @@
+"""
+
+
+Curves Operators
+****************
+
+:func:`convert_to_particle_system`
+
+:func:`sculptmode_toggle`
+
+"""
+
+import typing
+
+def convert_to_particle_system() -> None:
+
+  """
+
+  Add a new or update an existing hair particle system on the surface object
+
+  """
+
+  ...
+
+def sculptmode_toggle() -> None:
+
+  """
+
+  Enter/Exit sculpt mode for curves
+
+  """
+
+  ...
