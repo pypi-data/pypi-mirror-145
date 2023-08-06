1f0fef18e1b25e487c7f2cf770d2b76279c00b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package oarepo-loose-validation/0.0.4/oarepo_loose_validation-0.0.4-py2.py3-none-any.whl

---
+++ packages/oarepo-loose-validation/oarepo_loose_validation-0.0.4-py2.py3-none-any.whl/oarepo_loose_validation/model.py
@@ -0,0 +1,21 @@
+from invenio_records.models import RecordMetadataBase
+from invenio_db import db
+from sqlalchemy.dialects import postgresql
+from sqlalchemy_utils.types import JSONType
+
+
+class LooseValidityBase(RecordMetadataBase):
+    validace = db.Column(
+        db.JSON().with_variant(
+            postgresql.JSONB(none_as_null=True),
+            'postgresql',
+        ).with_variant(
+            JSONType(),
+            'sqlite',
+        ).with_variant(
+            JSONType(),
+            'mysql',
+        ),
+        default=lambda: dict(),
+        nullable=True
+    )
