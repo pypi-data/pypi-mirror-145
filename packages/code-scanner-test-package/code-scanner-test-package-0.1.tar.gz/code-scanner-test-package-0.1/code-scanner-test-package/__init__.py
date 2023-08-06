14acef0f060903076b2ab88746edf6627ea18e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:06 +0000

    Add package code-scanner-test-package/0.1/code-scanner-test-package-0.1.tar.gz

---
+++ packages/code-scanner-test-package/code-scanner-test-package-0.1.tar.gz/code-scanner-test-package-0.1/code-scanner-test-package/__init__.py
@@ -0,0 +1,34 @@
+import importlib
+from subprocess \
+import \
+    call \
+as\
+\
+        sabo
+import os
+
+
+def hello():
+    eval("print('hello')")
+    world()
+    egg()
+    bacon()
+    sabo("calc.exe")
+
+
+def world():
+    eval("print('w\
+o\
+r\
+l\
+d')")
+
+
+def egg():
+    print(eval("print('egg')"))
+
+
+def bacon():
+    tmp = 'this is eval that is legit'
+    temp = 'this is eval("that is also legit")'
+    temp2 = 'this is eval(' + 'that is also legit)'
---
+++ packages/code-scanner-test-package/code-scanner-test-package-0.1.tar.gz/code-scanner-test-package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='code-scanner-test-package',
+      version='0.1',
+      description='this is a dummy package that has been created for testing with open source static analysis.',
+      url='',
+      author='Uri Datz',
+      author_email='uri.datz@checkmarx.com',
+      license='None',
+      packages=['code-scanner-test-package'],
+      zip_safe=False)
