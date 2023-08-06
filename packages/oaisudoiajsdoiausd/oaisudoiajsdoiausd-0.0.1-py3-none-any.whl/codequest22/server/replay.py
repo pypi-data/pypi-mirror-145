ca3557c5f910afbe92de1ba58556118629190bf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:47 +0000

    Add package oaisudoiajsdoiausd/0.0.1/oaisudoiajsdoiausd-0.0.1-py3-none-any.whl

---
+++ packages/oaisudoiajsdoiausd/oaisudoiajsdoiausd-0.0.1-py3-none-any.whl/codequest22/server/replay.py
@@ -0,0 +1,26 @@
+class ReplayManager:
+
+    @classmethod
+    def set_output(cls, path):
+        cls.file = open(path, "w")
+
+    @classmethod
+    def set_input(cls, path):
+        cls.file = open(path, "r")
+        cls.lines = list(cls.file.readlines())
+        cls.line_index = 0
+
+    @classmethod
+    def write_line(cls, string):
+        assert "\n" not in string, "Replay stores should be single lines."
+        cls.file.write(string + "\n")
+
+    @classmethod
+    def read_line(cls) -> str:
+        line = cls.lines[cls.line_index]
+        cls.line_index += 1
+        return line
+
+    @classmethod
+    def close(cls):
+        cls.file.close()
