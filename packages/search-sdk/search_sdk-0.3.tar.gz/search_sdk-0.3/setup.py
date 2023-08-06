50fc39d4dad7d45ee686804e3cfd79f11945e1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:18:40 +0000

    Add package search-sdk/0.3/search_sdk-0.3.tar.gz

---
+++ packages/search-sdk/search_sdk-0.3.tar.gz/search_sdk-0.3/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import setuptools
+
+setuptools.setup(name='',
+                 version='0.3',
+                 description='',
+                 author="ggyupen",
+                 author_email='ggyupen@gmail.com',
+                 url='',
+                 packages=setuptools.find_packages(),
+                 )
