c2c07e66dd923b12757efd50a4f7e8ad4963ac35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:30 +0000

    Add package cookie-helper/1.0/cookie_helper-1.0.tar.gz

---
+++ packages/cookie-helper/cookie_helper-1.0.tar.gz/cookie_helper-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cookie_helper',
+    version='1.0',    
+    description='the easy way to save and load cookies while using selenium.',
+    author='Christoph Schmalfuß',
+    author_email='post@christoph-schmalfuss.de',
+    license='MIT',
+    url='https://github.com/chrischma/selenium_cookie_helper'
+)
