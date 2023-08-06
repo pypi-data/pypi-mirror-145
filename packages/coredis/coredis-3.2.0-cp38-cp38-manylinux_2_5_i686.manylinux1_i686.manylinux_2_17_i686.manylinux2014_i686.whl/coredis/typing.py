d2a73d606e0d38f21b8e6224183a1e9a9ab4eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:28 +0000

    Add package coredis/3.2.0/coredis-3.2.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/coredis/coredis-3.2.0-cp38-cp38-manylinux_2_5_i686.manylinux1_i686.manylinux_2_17_i686.manylinux2014_i686.whl/coredis/typing.py
@@ -0,0 +1,177 @@
+from __future__ import annotations
+
+import os
+import warnings
+from abc import ABC, abstractmethod
+from numbers import Number
+from typing import (
+    TYPE_CHECKING,
+    AbstractSet,
+    Any,
+    AnyStr,
+    AsyncGenerator,
+    Awaitable,
+    Callable,
+    Coroutine,
+    Dict,
+    Generic,
+    Iterable,
+    Iterator,
+    List,
+    Literal,
+    Mapping,
+    NamedTuple,
+    Optional,
+    Protocol,
+    Sequence,
+    Set,
+    Tuple,
+    Type,
+    TypeVar,
+    Union,
+)
+
+from typing_extensions import (
+    Deque,
+    OrderedDict,
+    ParamSpec,
+    TypeAlias,
+    TypedDict,
+    runtime_checkable,
+)
+
+RUNTIME_TYPECHECKS = False
+
+if os.environ.get("COREDIS_RUNTIME_CHECKS", "").lower() in ["1", "true", "t"]:
+    try:
+        import beartype
+
+        if not TYPE_CHECKING:
+            from beartype.typing import (  # noqa: F811
+                Deque,
+                Dict,
+                Iterable,
+                Iterator,
+                List,
+                Mapping,
+                OrderedDict,
+                Sequence,
+                Set,
+                Tuple,
+                TypedDict,
+            )
+
+        RUNTIME_TYPECHECKS = True
+    except ImportError:  # noqa
+        warnings.warn("Runtime checks were requested but could not import beartype")
+
+CommandArgList = List[Union[str, bytes, float, Number]]
+
+
+P = ParamSpec("P")
+R = TypeVar("R")
+
+KeyT: TypeAlias = Union[str, bytes]
+ValueT: TypeAlias = Union[str, bytes, int, float]
+StringT: TypeAlias = KeyT
+
+
+def add_runtime_checks(func: Callable[P, R]) -> Callable[P, R]:
+    if RUNTIME_TYPECHECKS:
+        return beartype.beartype(func)
+
+    return func
+
+
+@runtime_checkable
+class SupportsWatch(Protocol):  # noqa
+    async def __aenter__(self) -> "SupportsWatch":
+        ...
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        ...
+
+    async def watch(self, *keys: KeyT) -> bool:
+        ...
+
+    async def execute(self, raise_on_error=True) -> Any:
+        ...
+
+
+@runtime_checkable
+class SupportsScript(Protocol):  # noqa
+    async def evalsha(
+        self,
+        sha1: StringT,
+        keys: Optional[Iterable[KeyT]] = None,
+        args: Optional[Iterable[ValueT]] = None,
+    ) -> Any:
+        ...
+
+    async def evalsha_ro(
+        self,
+        sha1: StringT,
+        keys: Optional[Iterable[KeyT]] = None,
+        args: Optional[Iterable[ValueT]] = None,
+    ) -> Any:
+        ...
+
+    async def script_load(self, script: StringT) -> AnyStr:
+        ...
+
+
+@runtime_checkable
+class SupportsPipeline(Protocol):  # noqa
+    async def pipeline(
+        self,
+        transaction: Optional[bool] = True,
+        watches: Optional[Iterable[StringT]] = None,
+    ) -> SupportsWatch:
+        ...
+
+
+class AbstractExecutor(ABC, Generic[AnyStr]):
+    @abstractmethod
+    async def execute_command(self, command: bytes, *args: Any, **options: Any) -> Any:
+        pass
+
+
+__all__ = [
+    "AbstractExecutor",
+    "AbstractSet",
+    "Any",
+    "AnyStr",
+    "AsyncGenerator",
+    "Awaitable",
+    "Callable",
+    "CommandArgList",
+    "Coroutine",
+    "Deque",
+    "Dict",
+    "Generic",
+    "KeyT",
+    "Iterable",
+    "Iterator",
+    "List",
+    "Literal",
+    "Mapping",
+    "NamedTuple",
+    "OrderedDict",
+    "Optional",
+    "ParamSpec",
+    "Protocol",
+    "Sequence",
+    "Set",
+    "SupportsWatch",
+    "SupportsScript",
+    "SupportsPipeline",
+    "StringT",
+    "Tuple",
+    "Type",
+    "TypedDict",
+    "TypeVar",
+    "Union",
+    "ValueT",
+    "TYPE_CHECKING",
+    "RUNTIME_TYPECHECKS",
+]
