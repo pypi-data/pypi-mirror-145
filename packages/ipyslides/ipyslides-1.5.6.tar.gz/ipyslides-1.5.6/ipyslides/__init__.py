8212fe7decb27e857bcbfb102ed842d5cf798e85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:43 +0000

    Add package ipyslides/1.5.6/ipyslides-1.5.6.tar.gz

---
+++ packages/ipyslides/ipyslides-1.5.6.tar.gz/ipyslides-1.5.6/ipyslides/__init__.py
@@ -0,0 +1,12 @@
+from .__version__ import __version__
+
+version = __version__
+
+from .core import LiveSlides 
+from . import parsers
+
+__all__ = ['LiveSlides', 'parsers']
+        
+if __name__ == '__main__':
+    print('Use this package in Jupyter notebook!')
+    
