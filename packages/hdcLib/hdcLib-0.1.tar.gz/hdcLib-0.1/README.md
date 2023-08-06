14ecc265104a8bae132cec993f7535d109545e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:27 +0000

    Add package hdcLib/0.1/hdcLib-0.1.tar.gz

---
+++ packages/hdcLib/hdcLib-0.1.tar.gz/hdcLib-0.1/README.md
@@ -0,0 +1,3 @@
+# Hyperdimensional Computing Library
+
+  The hdc_lib library is a comprehensive package for all hyperdimensional computing related tasks.
---
+++ packages/hdcLib/hdcLib-0.1.tar.gz/hdcLib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="hdcLib",
+      version="0.1",
+      description='A python library for hyperdimensional computing', 
+      url='https://github.com/mikeheddes/hdc-lib', 
+      license='MIT', 
+      packages=['hdcLib'], 
+      zip_safe=False)
