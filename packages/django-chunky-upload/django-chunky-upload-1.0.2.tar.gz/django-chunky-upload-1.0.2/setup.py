e7721699d5516179178b7b15fcdcf72ae7e9fdfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:49 +0000

    Add package django-chunky-upload/1.0.2/django-chunky-upload-1.0.2.tar.gz

---
+++ packages/django-chunky-upload/django-chunky-upload-1.0.2.tar.gz/django-chunky-upload-1.0.2/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open("VERSION.txt", "r") as v:
+    version = v.read().strip()
+
+CURRENT_DIR = Path(__file__).parent
+
+
+def get_long_description() -> str:
+    return (CURRENT_DIR / "README.md").read_text(encoding="utf8")
+
+
+download_url = "https://github.com/Vader19695/django-chunky-upload/tarball/%s"
+
+setup(
+    name="django-chunky-upload",
+    packages=[
+        "chunky_upload",
+        "chunky_upload.migrations",
+        "chunky_upload.management",
+    ],
+    version=version,
+    description=(
+        "Upload large files to Django in multiple chunks, with the "
+        "ability to resume if the upload is interrupted."
+    ),
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Jaryd Rester",
+    author_email="pypi@jarydrester.com",
+    url="https://github.com/Vader19695/django-chunky-upload",
+    download_url=download_url % version,
+    python_requires=">=3.7",
+    install_requires=[],
+    license="MIT-Zero",
+)
