348d2263ae61331a3c02731711c75109b91c01c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:42 +0000

    Add package package-simple-dio/0.0.1/package_simple_dio-0.0.1.tar.gz

---
+++ packages/package-simple-dio/package_simple_dio-0.0.1.tar.gz/package_simple_dio-0.0.1/README.md
@@ -0,0 +1,25 @@
+# Package_simple
+
+<b/>Description: The package package_simple is used to an example of package creation.<b/>
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_simple
+
+```bash
+pip install package_simple
+```
+
+## Usage
+
+```python
+from package_simple import file1_name
+
+file1_name.my_function()
+```
+
+## Author
+Carla Edila
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/package-simple-dio/package_simple_dio-0.0.1.tar.gz/package_simple_dio-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_simple_dio",
+    version="0.0.1",
+    author="Carla Edila",
+    author_email="rosa.carla@pucpr.edu.br",
+    description="Example of package creation",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rosacarla/package_simple_project",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
