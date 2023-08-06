c6c5a9fa5c17651c62907510bfc1aff1692812e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:10 +0000

    Add package pyraf/2.2.1/pyraf-2.2.1.tar.gz

---
+++ packages/pyraf/pyraf-2.2.1.tar.gz/pyraf-2.2.1/pyraf/tests/utils.py
@@ -0,0 +1,21 @@
+import os
+import sys
+
+from distutils.spawn import find_executable
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
+
+try:
+    from pyraf import iraf
+    iraf.imhead("dev$pix", Stdout=os.devnull, Stderr=os.devnull)
+except (iraf.IrafError, AttributeError):
+    HAS_IRAF = False
+    HAS_STSDAS = False
+else:
+    HAS_IRAF = True
+    try:
+        iraf.stsdas(_doprint=0)
+    except (iraf.IrafError, AttributeError):
+        HAS_STSDAS = False
+    else:
+        HAS_STSDAS = True
