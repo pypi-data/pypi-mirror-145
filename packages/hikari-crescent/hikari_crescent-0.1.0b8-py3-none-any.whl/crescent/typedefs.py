2702f4a5f14c45c4c5b2defc9b79743fa5a2a104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:05 +0000

    Add package hikari-crescent/0.1.0b8/hikari_crescent-0.1.0b8-py3-none-any.whl

---
+++ packages/hikari-crescent/hikari_crescent-0.1.0b8-py3-none-any.whl/crescent/typedefs.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Awaitable,
+    Callable,
+    Dict,
+    Optional,
+    Protocol,
+    Sequence,
+    TypeVar,
+    Union,
+)
+
+from hikari import Message, PartialChannel, Role, User
+
+if TYPE_CHECKING:
+    from crescent.commands.hooks import HookResult
+    from crescent.context import Context
+    from crescent.mentionable import Mentionable
+
+__all__: Sequence[str] = (
+    "CommandCallbackT",
+    "OptionTypesT",
+    "UserCommandCallbackT",
+    "MessageCommandCallbackT",
+    "CommandOptionsT",
+    "ClassCommandProto",
+    "HookCallbackT",
+    "ErrorHandlerCallbackT",
+)
+
+CommandCallbackT = Callable[..., Awaitable[Any]]
+UserCommandCallbackT = Callable[["Context", User], Awaitable[None]]
+MessageCommandCallbackT = Callable[["Context", Message], Awaitable[None]]
+
+OptionTypesT = Union[str, bool, int, float, PartialChannel, Role, User, "Mentionable"]
+CommandOptionsT = Dict[str, Union[OptionTypesT, User, Message]]
+HookCallbackT = Callable[["Context"], Awaitable[Optional["HookResult"]]]
+
+
+class ClassCommandProto(Protocol):
+    async def callback(self, ctx: Context) -> Any:
+        ...
+
+
+ERROR = TypeVar("ERROR", bound=Exception, contravariant=True)
+
+ErrorHandlerCallbackT = Callable[[ERROR, "Context"], Awaitable[None]]
