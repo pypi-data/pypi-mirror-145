b156cff66e5dd383e1a3f6f3bcf7c96cfa12f68c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:35 +0000

    Add package tile-renderer/2.1/tile-renderer-2.1.tar.gz

---
+++ packages/tile-renderer/tile-renderer-2.1.tar.gz/tile-renderer-2.1/renderer/__init__.py
@@ -0,0 +1,9 @@
+from renderer.base import merge_tiles, render
+import renderer.mathtools
+import renderer.validate
+import renderer.types
+from renderer.objects.nodes import *
+from renderer.objects.components import *
+from renderer.objects.skin import *
+
+__version__ = '2.1'
