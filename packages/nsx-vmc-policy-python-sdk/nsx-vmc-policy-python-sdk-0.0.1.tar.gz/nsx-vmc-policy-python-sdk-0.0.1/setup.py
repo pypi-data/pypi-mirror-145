54386c3291cb89bafa2836da00b87e1132dd5b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:59 +0000

    Add package nsx-vmc-policy-python-sdk/0.0.1/nsx-vmc-policy-python-sdk-0.0.1.tar.gz

---
+++ packages/nsx-vmc-policy-python-sdk/nsx-vmc-policy-python-sdk-0.0.1.tar.gz/nsx-vmc-policy-python-sdk-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fn:
+        return fn.read()
+
+setup(
+    name='nsx-vmc-policy-python-sdk',
+    version='0.0.1',
+    description='nsx-vmc-policy-python-sdk',
+    long_description=read('README.rst'),
+    license=read('LICENSE.txt'),
+    author='VMware, Inc.',
+    url='https://github.com/vmware/vsphere-automation-sdk-python',
+)
