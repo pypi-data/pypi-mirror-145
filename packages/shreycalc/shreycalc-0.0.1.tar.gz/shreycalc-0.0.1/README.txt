ecabd2c54182ad645c5f32da4ddda4c28d35422d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:38 +0000

    Add package shreycalc/0.0.1/shreycalc-0.0.1.tar.gz

---
+++ packages/shreycalc/shreycalc-0.0.1.tar.gz/shreycalc-0.0.1/README.txt
@@ -0,0 +1,2 @@
+this is a very basic calculator that takes two number and perform addition, subtraction, multiplication and division operation in it
+
---
+++ packages/shreycalc/shreycalc-0.0.1.tar.gz/shreycalc-0.0.1/shreycalc/__init__.py
@@ -0,0 +1,18 @@
+from abc import abstractclassmethod
+
+
+def addnumbers(num1, num2):
+    return num1+num2
+
+def subtractnumbers(num1, num2):
+    return num1-num2
+
+def multiplynumbers(num1, num2):
+    return num1*num2
+
+def dividennumbers(num1, num2):
+    return num1/num2
+
+
+print(addnumbers(3,5))
+
