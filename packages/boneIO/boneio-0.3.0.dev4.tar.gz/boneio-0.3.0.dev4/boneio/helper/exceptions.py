86225eb67d24d199c9b13984e20ae63cec3cef84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:14:04 +0000

    Add package boneIO/0.3.0.dev4/boneio-0.3.0.dev4.tar.gz

---
+++ packages/boneIO/boneio-0.3.0.dev4.tar.gz/boneio-0.3.0.dev4/boneio/helper/exceptions.py
@@ -0,0 +1,17 @@
+"""BoneIO Errors"""
+
+
+class BoneIOException(Exception):
+    """BoneIO standard exception."""
+
+
+class GPIOInputException(BoneIOException):
+    """GPIOInput Exception."""
+
+
+class I2CError(BoneIOException):
+    """I2C Exception."""
+
+
+class ConfigurationException(BoneIOException):
+    """Configuration yaml exception."""
