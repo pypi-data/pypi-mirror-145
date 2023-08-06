d9a080a3963df3cb7adc87a314024f2fec425243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:14 +0000

    Add package oneai/0.1.7/oneai-0.1.7.tar.gz

---
+++ packages/oneai/oneai-0.1.7.tar.gz/oneai-0.1.7/src/oneai/__init__.py
@@ -0,0 +1,9 @@
+import oneai.skills as skills
+from oneai.classes import *
+from oneai.pipeline import Pipeline
+
+URL = 'https://api.oneai.com/api/v0/pipeline'
+
+api_key = ''
+
+PRINT_PROGRESS = True
