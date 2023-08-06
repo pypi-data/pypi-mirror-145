4526b36ddf357ea05861f86ad037d4f4623b4793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:56 +0000

    Add package mathstropy/2.2.4/mathstropy-2.2.4.tar.gz

---
+++ packages/mathstropy/mathstropy-2.2.4.tar.gz/mathstropy-2.2.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mathstropy", # Replace with your own username
+    version="2.2.4",
+    author="Richard Hamilton",
+    author_email="richard.ha@mathstronauts.ca",
+    description="Python library with functions to make learning more efficient",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathstronauts/mathstropy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
