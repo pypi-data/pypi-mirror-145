c9deb5ea3ca3bd0ecd7109a6352df95cc9fc5aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:21 +0000

    Add package fieldedge-utilities/0.9.3/fieldedge_utilities-0.9.3-py3-none-any.whl

---
+++ packages/fieldedge-utilities/fieldedge_utilities-0.9.3-py3-none-any.whl/fieldedge_utilities/__init__.py
@@ -0,0 +1,10 @@
+"""
+# FieldEdge Utilities
+
+This Python library contains common functionality used by many of the Inmarsat
+**FieldEdge** embedded microservices.
+
+"""
+__docformat__ = 'google'
+
+__version__ = '0.4.0'
