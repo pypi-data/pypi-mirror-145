fcaeccc11ff42192c226bbe883868a220723b8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:59 +0000

    Add package pystassh/1.2.1/pystassh-1.2.1.tar.gz

---
+++ packages/pystassh/pystassh-1.2.1.tar.gz/pystassh-1.2.1/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import find_packages, setup
+
+current_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name="pystassh",
+    version="1.2.1",
+    author="Julien Chaumont",
+    author_email="pystassh@julienc.io",
+    description="An easy to use libssh wrapper to execute commands on a remote server via SSH with Python",
+    license="MIT",
+    url="https://julienc91.github.io/pystassh/",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["cffi"],
+)
