2cb8ade7ae654ac146cfaac36f5a31871ad11cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:16 +0000

    Add package ccxtools/0.0.6/ccxtools-0.0.6.tar.gz

---
+++ packages/ccxtools/ccxtools-0.0.6.tar.gz/ccxtools-0.0.6/src/ccxtools/bybit.py
@@ -0,0 +1,26 @@
+from decimal import Decimal
+import ccxt
+from ccxtools import exchange_interface
+
+
+class Bybit(exchange_interface.ExchangeInterface):
+
+    def __init__(self, who, config):
+        self.ccxt_inst = ccxt.bybit({
+            'apiKey': config(f'BYBIT_API_KEY{who}'),
+            'secret': config(f'BYBIT_SECRET_KEY{who}')
+        })
+        self.max_trading_qtys = self.get_max_trading_qtys()
+
+    def get_max_trading_qtys(self):
+        markets = self.ccxt_inst.fetch_markets()
+
+        result = {}
+        for market in markets:
+            if not market['linear']:
+                continue
+
+            ticker = market['base']
+            result[ticker] = Decimal(market['info']['lot_size_filter']['max_trading_qty'])
+
+        return result
