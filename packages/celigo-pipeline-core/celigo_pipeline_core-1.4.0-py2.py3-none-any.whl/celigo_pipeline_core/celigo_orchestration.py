626c4c698697e75e5808d3626880d9432bf6b373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:38:09 +0000

    Add package celigo-pipeline-core/1.4.0/celigo_pipeline_core-1.4.0-py2.py3-none-any.whl

---
+++ packages/celigo-pipeline-core/celigo_pipeline_core-1.4.0-py2.py3-none-any.whl/celigo_pipeline_core/celigo_orchestration.py
@@ -0,0 +1,92 @@
+import pathlib
+import shutil
+import subprocess
+import time
+import pathlib
+import os
+
+from .celigo_single_image import (
+    CeligoSingleImageCore,
+)
+
+
+def run_all(raw_image_path: pathlib.Path):
+    upload_location = pathlib.Path('/allen/aics/microscopy/PRODUCTION/Celigo_Metric_Output')
+    image = CeligoSingleImageCore(raw_image_path)
+
+    job_ID, output_file = image.downsample()
+    job_complete_check(job_ID, output_file, "downsample")
+    job_ID, output_file = image.run_ilastik()
+    job_complete_check(job_ID, output_file, "ilastik")
+    job_ID, output_file = image.run_cellprofiler()
+    job_complete_check(job_ID, output_file, "cell profiler")
+    # job_ID, output_dir = image.upload_metrics()
+    # job_complete_check(job_ID, output_dir, "cell profiler")
+
+    # Temporary until upload location is specified.
+    shutil.copytree(output_file.parent, upload_location / output_file.with_suffix("").name)
+    # Upload raw image with low priority
+    image.cleanup()
+    print("Complete")
+
+
+# This function runs in between bash script calls to determine when the correct files are output
+# and when to go onto the next process.
+def job_complete_check(
+    job_ID: int,
+    endfile: pathlib.Path,
+    name: str,
+):
+
+    job_status = "waiting"  # Status code for bash script process
+    count = 0  # number of time through loop each is about 3 sec. This is to determine when to break out and say the process failed
+
+    # Loop goes while the output file isnt there and the job isnt complete
+    while (not endfile.exists()) and job_status != "complete":
+        time.sleep(
+            3
+        )  # wait in between each loop to not get incorrect codes or too much printed info to terminal
+
+        # initial check to see if job was ever added to queue. Sometimes this can take a bit
+        if (not (job_in_queue_check(job_ID))) and (job_status == "waiting"):
+            job_status = "waiting"
+            print("waiting")
+
+        # If the job is in the queue (running) prints "Job; <Number> <Name> is running"
+        elif job_in_queue_check(job_ID):
+            job_status = "running"
+            print(f"Job: {job_ID} {name} is running")
+
+            """ 
+                Once job is in the queue the loop will continue printing running until 
+                the job is no longer in the queue. Then the next logic statements come
+                into play to determine if the run was sucessful 
+
+            """
+        # This logic is only reached if the process ran and is no longer in the queue
+        # Counts to 600 to wait and see if the output file gets created. If it doesnt then
+        # prints that the job has failed and breaks out of the loop.
+        elif not endfile.exists() and count > 200:
+            job_status = "failed"
+            print(f"Job: {job_ID} {name} has failed!")
+            break
+
+        # The final statement confirming if the process was sucessful.
+        elif endfile.exists():
+            job_status = "complete"
+            print(f"Job: {job_ID} {name} is complete!")
+
+        count = count + 1  # Internal clock adding one to times through while loop.
+
+
+# Function that checks if a current job ID is in the squeue. Returns True if it is and False if it isnt.
+def job_in_queue_check(job_ID: int):
+
+    output = subprocess.run(
+        ["squeue", "-j", f"{job_ID}"], check=True, capture_output=True
+    )
+
+    # The output of subprocess is an array turned into a string so in order to
+    # count the number of entries we count the frequency of "\n" to show if the
+    # array was not empty, indicating the job is in the queue.
+    return output.stdout.decode("utf-8").count("\n") >= 2
---
+++ packages/celigo-pipeline-core/celigo_pipeline_core-1.4.0-py2.py3-none-any.whl/celigo_pipeline_core/celigo_single_image/celigo_single_image_core.py
@@ -0,0 +1,221 @@
+import importlib.resources as pkg_resources
+import os
+from pathlib import Path
+import pwd
+import shutil
+import subprocess
+
+from jinja2 import Environment, PackageLoader
+import pandas as pd
+
+from .. import pipelines
+
+
+class CeligoSingleImageCore:
+
+    """
+    This Class provides utility functions for the Celigo
+    pipeline to prepare single images for:
+
+    1) Ilastik Processing
+
+    2) Cell Profiler Processing
+
+    Given its large processing needs it is set up to run on slurm
+
+    """
+
+    def __init__(self, raw_image_path):
+
+        # Specific name of experiment
+        self.tempdirname = Path(raw_image_path).with_suffix("").name
+
+        # Working Directory
+        if not os.path.exists(
+            f"/home/{pwd.getpwuid(os.getuid())[0]}/{self.tempdirname}"
+        ):
+            os.mkdir(f"/home/{pwd.getpwuid(os.getuid())[0]}/{self.tempdirname}")
+        self.working_dir = Path(
+            f"/home/{pwd.getpwuid(os.getuid())[0]}/{self.tempdirname}"
+        )
+
+        # Image Paths
+        self.raw_image_path = Path(raw_image_path)
+        shutil.copyfile(
+            self.raw_image_path, f"{self.working_dir}/{self.raw_image_path.name}"
+        )
+        self.image_path = Path(f"{self.working_dir}/{self.raw_image_path.name}")
+
+        # Future resource paths
+        self.filelist_path = Path()
+        self.resize_filelist_path = Path()
+        self.cell_profiler_output_path = Path()
+
+        self.downsample_job_ID = int()
+        self.ilastik_job_ID = int()
+        self.cellprofiler_job_ID = int()
+
+        # Pipeline paths for templates
+        with pkg_resources.path(pipelines, "rescale_pipeline.cppipe") as p:
+            self.rescale_pipeline_path = p
+        with pkg_resources.path(pipelines, "colony_morphology.model") as p:
+            self.classification_model_path = p
+
+        script_config = {
+            "classifier_path": str(self.classification_model_path.parent),
+        }
+
+        jinja_env = Environment(
+            loader=PackageLoader(
+                package_name="celigo_pipeline_core", package_path="templates"
+            )
+        )
+        script_body = jinja_env.get_template("96_well_pipeline_v1.0_tempalate.j2").render(
+            script_config
+        )
+
+        # Creates .cppipe script locally.
+        with open(self.working_dir / "96_well_colony_pipeline_v_0.1.cppipe", "w+") as rsh:
+            rsh.write(script_body)
+
+        self.cellprofiler_pipeline_path = self.working_dir / "96_well_colony_pipeline_v_0.1.cppipe"
+
+    def downsample(self):
+        # Generates a filelist
+        with open(self.working_dir / "resize_filelist.txt", "w+") as rfl:
+            rfl.write(str(self.image_path) + "\n")
+
+        self.resize_filelist_path = self.working_dir / "resize_filelist.txt"
+
+        # Defines variables for bash script
+        script_config = {
+            "filelist_path": str(self.resize_filelist_path),
+            "output_path": str(self.working_dir),
+            "pipeline_path": str(self.rescale_pipeline_path),
+        }
+
+        # Generates script_body from existing templates.
+        jinja_env = Environment(
+            loader=PackageLoader(
+                package_name="celigo_pipeline_core", package_path="templates"
+            )
+        )
+        script_body = jinja_env.get_template("resize_cellprofiler_template.j2").render(
+            script_config
+        )
+
+        # Creates bash script locally.
+        with open(self.working_dir / "resize.sh", "w+") as rsh:
+            rsh.write(script_body)
+
+        # Runs resize on slurm
+        output = subprocess.run(
+            ["sbatch", f"{str(self.working_dir)}/resize.sh"],
+            check=True,
+            capture_output=True,
+        )
+
+        # Sets path to resized image to image path for future use
+        self.image_path = (
+            self.image_path.parent
+            / f"{self.image_path.with_suffix('').name}_rescale.tiff"
+        )
+
+        job_ID = int(output.stdout.decode("utf-8").split(" ")[-1][:-1])
+        return job_ID, self.image_path
+
+    def run_ilastik(self):
+
+        # Parameters to input to bash script template
+        script_config = {
+            "image_path": f"'{str( self.image_path)}'",
+            "output_path": f"'{str(self.image_path.with_suffix(''))}_probabilities.tiff'",
+        }
+
+        # Generates script_body from existing templates.
+        jinja_env = Environment(
+            loader=PackageLoader(
+                package_name="celigo_pipeline_core", package_path="templates"
+            )
+        )
+
+        script_body = jinja_env.get_template("ilastik_template.j2").render(
+            script_config
+        )
+
+        # Creates bash script locally.
+        with open(self.working_dir / "ilastik.sh", "w+") as rsh:
+            rsh.write(script_body)
+
+        # Runs ilastik on slurm
+        output = subprocess.run(
+            ["sbatch", f"{str(self.working_dir)}/ilastik.sh"],
+            check=True,
+            capture_output=True,
+        )
+
+        # Creates filelist.txt
+        with open(self.working_dir / "filelist.txt", "w+") as rfl:
+            rfl.write(str(self.image_path) + "\n")
+            rfl.write(str(self.image_path.with_suffix("")) + "_probabilities.tiff")
+
+        self.filelist_path = self.working_dir / "filelist.txt"
+        job_ID = int(output.stdout.decode("utf-8").split(" ")[-1][:-1])
+        return job_ID, Path(f"{self.image_path.with_suffix('')}_probabilities.tiff")
+
+    def run_cellprofiler(self):
+
+        # Parameters to input to bash script template.
+        script_config = {
+            "filelist_path": str(self.filelist_path),
+            "output_dir": str(self.working_dir / "cell_profiler_outputs"),
+            "pipeline_path": str(self.cellprofiler_pipeline_path),
+        }
+
+        # Generates script_body from existing templates.
+        jinja_env = Environment(
+            loader=PackageLoader(
+                package_name="celigo_pipeline_core", package_path="templates"
+            )
+        )
+        script_body = jinja_env.get_template("cellprofiler_template.j2").render(
+            script_config
+        )
+
+        # Creates bash script locally.
+        with open(self.working_dir / "cellprofiler.sh", "w+") as rsh:
+            rsh.write(script_body)
+
+        # Runs cellprofiler on slurm
+        output = subprocess.run(
+            ["sbatch", f"{str(self.working_dir)}/cellprofiler.sh"],
+            check=True,
+            capture_output=True,
+        )
+
+        # Returns path to directory of cellprofiler outputs
+        self.cell_profiler_output_path = self.working_dir / "cell_profiler_outputs"
+        job_ID = int(output.stdout.decode("utf-8").split(" ")[-1][:-1])
+        return (
+            job_ID,
+            Path(f"{script_config['output_dir']}/{self.image_path.with_suffix('').name}_outlines.png"),
+        )  # TODO change this to the last output
+
+    def upload_metrics(self):
+        # combine output metrics and send to database
+
+        BallCraterDATA = pd.read_csv(
+            self.cell_profiler_output_path / "BallCraterDATA.csv"
+        )
+        ColonyDATA = pd.read_csv(self.cell_profiler_output_path / "ColonyDATA.csv")
+        ImageDATA = pd.read_csv(self.cell_profiler_output_path / "ImageDATA.csv")
+        ExperimentDATA = pd.read_csv(
+            self.cell_profiler_output_path / "ExperimentDATA.csv"
+        )
+
+        # combine metrics
+
+        # Send to DB
+
+    def cleanup(self):
+        shutil.rmtree(self.working_dir)
