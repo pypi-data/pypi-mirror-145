5b313e3fa7cacd9c8253fd56545cb7b4ba2f1b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:31 +0000

    Add package eggdriver/0.0.2b0/eggdriver-0.0.2b0.tar.gz

---
+++ packages/eggdriver/eggdriver-0.0.2b0.tar.gz/eggdriver-0.0.2b0/README.md
@@ -0,0 +1,20 @@
+# eggdriver
+
+Your proyect trusted driver.
+
+## Quick start
+
+### 1. Install eggdriver
+
+    pip install eggdriver
+
+### 2. (Optional) Create the file **eggconfig.py** and then write in it the following lines:
+
+    username, password = "{your user}", "{your password}"
+
+    pypi = {<br>
+        "user" : "{your PyPI user or "\__token__"}",
+        "password" : "{your PyPI password or token}"
+    }
+
+
---
+++ packages/eggdriver/eggdriver-0.0.2b0.tar.gz/eggdriver-0.0.2b0/manual_test.py
@@ -0,0 +1,30 @@
+#############################################
+wannaBuildRelease = False                  ##     ** Build panel **
+#############################################     Set
+import eggdriver as ed                     ##         wannaBuildRelease = True 
+if wannaBuildRelease:                      ##     to build a new release!
+    ed.buildEggdriver(True, '0.0.2')      ##
+#############################################
+
+# ed.header("uwu")
+
+theta = ed.pi/3
+print(ed.sin(theta), ed.cos(theta), ed.tan(theta))
+
+p = ed.sin_serie
+print(p.eval(theta))
+ed.derive(p)
+print(p.eval(theta))
+
+
+c = ed.Matrix("""
+| 1 1 2 3 4 |
+| 0 1 2 3 4 |
+| 1 1 2 3 4 |
+| 1 1 2 3 4 |
+| 1 1 2 3 4 |
+""")
+c.display()
+
+a = ed.Vector("[ 1 2 3 4 5 6 30 0 9]")
+a.display()
