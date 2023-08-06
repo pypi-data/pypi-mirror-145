c6a5d83a67a0581d66cb395c91b3a72ba836de89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:28 +0000

    Add package pls/3.3.2/pls-3.3.2.tar.gz

---
+++ packages/pls/pls-3.3.2.tar.gz/pls-3.3.2/src/pls/parser/args/info.py
@@ -0,0 +1,45 @@
+import argparse
+
+from pls.enums.unit_system import UnitSystem
+from pls.output.detail_columns import detail_columns
+
+
+detail_choices = list(detail_columns.keys()) + [
+    "def",  # means the default set of details
+    "all",  # means all details
+    "none",  # means no details
+]
+
+
+def add_args(parser: argparse.ArgumentParser):
+    """
+    Add arguments for showing and customising details to the given parser.
+
+    :param parser: the parser to which to add the arguments
+    """
+
+    info = parser.add_argument_group(
+        title="info",
+        description="arguments for toggling and customising details for nodes",
+    )
+    info.add_argument(
+        *["-d", "--details"],
+        metavar="FIELD",
+        action="append",
+        nargs=argparse.OPTIONAL,
+        help="the data points to show for each node in the output",
+        const="def",  # when there is a --details flag without value
+        choices=detail_choices,
+    )
+    info.add_argument(
+        *["-u", "--units"],
+        metavar="SYSTEM",
+        type=UnitSystem,
+        choices=list(UnitSystem),
+        help="the units to use when listing the size of files",
+    )
+    info.add_argument(
+        *["-t", "--time-fmt"],
+        metavar="FMT",
+        help="the template for formatting the timestamps on the file",
+    )
