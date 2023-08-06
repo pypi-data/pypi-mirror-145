3ae3623c6a7a915aa17353a4ee84de944aa7331a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:06 +0000

    Add package test-example-package/0.0.1/test_example_package-0.0.1-py3-none-any.whl

---
+++ packages/test-example-package/test_example_package-0.0.1-py3-none-any.whl/test_example_package/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+test_example_package: Test package to use it as template for building other packages
+"""
+__author__  = "Robert Rijnbeek"
+__email__   = "robert270384@gmail.com"
+__version__ = "0.0.1"
+
+
+def helloWorld():
+    print("helo world!!")
+
+def helloAuthor():
+    print(f"hello: {__author__}")
