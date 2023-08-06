a32565eee0ce0b626c50c065876895c12e9e7802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:58 +0000

    Add package oarepo-loose-validation/0.0.4/oarepo-loose-validation-0.0.4.tar.gz

---
+++ packages/oarepo-loose-validation/oarepo-loose-validation-0.0.4.tar.gz/oarepo-loose-validation-0.0.4/oarepo_loose_validation/components.py
@@ -0,0 +1,12 @@
+#todo: invenio records resources
+
+# class LooseValidationComponent(ServiceComponent):
+#     """Service component for metadata."""
+#
+#     def create(self, identity, data=None, record=None, errors=None, **kwargs):
+#         """Inject parsed metadata to the record."""
+#         record.validace = data.get('validace', {})
+#
+#     def update(self, identity, data=None, record=None, **kwargs):
+#         """Inject parsed metadata to the record."""
+#         record.validace = data.get('validace', {})
