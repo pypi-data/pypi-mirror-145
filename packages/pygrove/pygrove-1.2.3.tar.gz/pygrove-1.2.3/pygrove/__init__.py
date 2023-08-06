141162a364035e97831a17dc43a9b2ee93e66e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:30 +0000

    Add package pygrove/1.2.3/pygrove-1.2.3.tar.gz

---
+++ packages/pygrove/pygrove-1.2.3.tar.gz/pygrove-1.2.3/pygrove/__init__.py
@@ -0,0 +1,11 @@
+from pyforest.utils import get_user_symbols
+
+from .pygrove import *
+
+user_symbols = get_user_symbols()
+pyforest_imports = globals().copy().keys()
+
+for import_symbol in pyforest_imports:
+    # don't overwrite symbols of the user
+    if import_symbol not in user_symbols.keys():
+        user_symbols[import_symbol] = eval(import_symbol)
