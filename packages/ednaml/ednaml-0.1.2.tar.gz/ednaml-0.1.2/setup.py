3bd2133f861f7d917258679f1070c1a21bced6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:52 +0000

    Add package ednaml/0.1.2/ednaml-0.1.2.tar.gz

---
+++ packages/ednaml/ednaml-0.1.2.tar.gz/ednaml-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+
+requires = [
+        "kaptan>=0.5.12",
+        "scikit-learn>=1.0.2",
+        "torch>=1.10.*",
+        "torchinfo>=1.6.5",
+        "torchvision>=0.11.*",
+        "Pillow>=9.0.*",
+        "tqdm>=4.63.*",
+    ]
+
+
+# Create full install that includes all extra dependencies
+
+setup(
+    name="ednaml",
+    install_requires=requires,
+    keywords=["deep learning", "pytorch", "AI"],
+)
