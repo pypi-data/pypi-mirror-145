ca1506cc9724e50b98393103658357e66897be14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:50 +0000

    Add package cassandra-mmm/1.0.2/cassandra_mmm-1.0.2.tar.gz

---
+++ packages/cassandra-mmm/cassandra_mmm-1.0.2.tar.gz/cassandra_mmm-1.0.2/src/cassandra/model/mlpRegressor.py
@@ -0,0 +1,63 @@
+from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
+from cassandra.data.trasformations.trasformations import create_model
+from cassandra.model.modelEvaluation.plot import show_nrmse, show_mape, show_rsquared
+
+
+def mlpRegressor(df, X_columns, target, name_model, random_state=1, max_iter=500, medias=[], organic=[],
+                 metric=['rsq', 'nrmse', 'mape'], return_metric=False, size=0.2, positive=False, force_coeffs=False, coeffs=[], intercept=0):
+    metrics_values = {}
+    X = df[X_columns]
+    y = df[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=size, random_state=random_state)
+
+    if medias or organic:
+        model = create_model(medias, organic, MLPRegressor(random_state=random_state, max_iter=max_iter))
+    else:
+        model = MLPRegressor(random_state=random_state, max_iter=max_iter)
+
+    model.fit(X_train, y_train)
+
+    if force_coeffs and coeffs:
+        model.coef_ = coeffs
+        model.intercept_ = intercept
+
+    # Ask the model to predict on X_test without having Y_test
+    # This will give you exact predicted values
+
+    # We can use our NRMSE and MAPE functions as well
+
+    # Create new DF not to edit the original one
+    result = df
+
+    # Create a new column with predicted values
+    if medias or organic:
+        result['prediction'] = model.predict(result)
+    else:
+        result['prediction'] = model.predict(X)
+
+    # Score returns the accuracy of the above prediction or R^2
+    if 'rsq' in metric:
+        rsq = show_rsquared(result[target], result['prediction'])
+        if return_metric:
+            metrics_values[name_model + '_rsq'] = rsq
+        print(name_model, 'RSQ: ', rsq)
+
+    # Get the NRMSE & MAPE values
+    if 'nrmse' in metric:
+        nrmse_val = show_nrmse(result[target], result['prediction'])
+        if return_metric:
+            metrics_values[name_model + '_nrmse'] = nrmse_val
+        print(name_model, 'NRMSE: ', nrmse_val)
+
+    if 'mape' in metric:
+        mape_val = show_mape(result[target], result['prediction'])
+        if return_metric:
+            metrics_values[name_model + '_mape'] = mape_val
+        print(name_model, 'MAPE: ', mape_val)
+
+    if metrics_values:
+        return result, model, metrics_values
+    else:
+        return result, model
---
+++ packages/cassandra-mmm/cassandra_mmm-1.0.2.tar.gz/cassandra_mmm-1.0.2/src/cassandra/model/nevergrad/nevergrad.py
@@ -0,0 +1,63 @@
+# TODO
+import nevergrad as ng
+from cassandra.model.nevergrad.utils import instrum_transformations_nevergrad, create_df_transformations_nevergrad, choose_optimizer_algoritm, optimize_metric
+from cassandra.model.utils import choose_model
+
+
+def nevergrad_model(df, hyperparameters_dict, medias, organic, target, use_intercept=True, use_adstock=True, use_saturation=True,
+                                adstock_type='weibull', model_regression='linear', X_transformations_columns=[],
+                     model_regression_log_log='linear', ridge_number=0, metric=['rsq', 'nrmse', 'mape'],
+                     return_metric=True, size=0.2, positive=False, random_state=42, metric_return='mape',
+              force_coeffs=False, coeffs=[], intercept=0, optimizer_algoritm='TwoPointsDE', budget=1000):
+
+    value_trasf = instrum_transformations_nevergrad(hyperparameters_dict)
+    instrum = ng.p.Instrumentation(**value_trasf)
+    optimizer = choose_optimizer_algoritm(optimizer_algoritm, budget, instrum)
+
+    metric_array = []
+    df_transformations = df.copy()
+    metrics_values = {}
+    all_hyper = list(hyperparameters_dict.keys())
+
+    def build_model(**all_hyper):
+        df_transf = create_df_transformations_nevergrad(all_hyper, df, df_transformations, medias, use_adstock, use_saturation, adstock_type)
+        if return_metric:
+            result, model, metrics_values = choose_model(df_transf, medias + organic, target, model_regression, X_transformations_columns,
+                     model_regression_log_log, ridge_number, metric,
+                     return_metric, size, positive, random_state, force_coeffs, coeffs, intercept)
+        else:
+            result, model = choose_model(df_transf, medias + organic, target, model_regression, X_transformations_columns,
+                     model_regression_log_log, ridge_number, metric,
+                     return_metric, size, positive, random_state, force_coeffs, coeffs, intercept)
+
+        metric_to_optimize = optimize_metric(metric_return, metrics_values)
+        metric_array.append(metric_to_optimize)
+
+        return metric_to_optimize
+
+    recommendation = optimizer.minimize(build_model)
+
+    def build_model_result(**all_hyper):
+
+        df_transf = create_df_transformations_nevergrad(all_hyper, df, df_transformations, medias, use_adstock, use_saturation, adstock_type)
+
+        if return_metric:
+            result, model, metrics_values = choose_model(df_transf, medias + organic, target, model_regression, X_transformations_columns,
+                     model_regression_log_log, ridge_number, metric,
+                     return_metric, size, positive, random_state, force_coeffs, coeffs, intercept)
+        else:
+            result, model = choose_model(df_transf, medias + organic, target, model_regression, X_transformations_columns,
+                     model_regression_log_log, ridge_number, metric,
+                     return_metric, size, positive, random_state, force_coeffs, coeffs, intercept)
+
+        metric_to_optimize = optimize_metric(metric_return, metrics_values)
+        metric_array.append(metric_to_optimize)
+
+        return metric_to_optimize, result, model
+
+    metric, result, model = build_model_result(**recommendation.value[1])
+    return metric, result, model, recommendation.value[1], metric_array
+
+
+
+
