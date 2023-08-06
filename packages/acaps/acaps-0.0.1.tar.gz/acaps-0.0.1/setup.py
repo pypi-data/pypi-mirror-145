af834b62afd1d23e3ec2ebf1406402dced503bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:46:14 +0000

    Add package acaps/0.0.1/acaps-0.0.1.tar.gz

---
+++ packages/acaps/acaps-0.0.1.tar.gz/acaps-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='acaps',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.acaps.org/',
+    author='Chiara Rizzi',
+    author_email='chiara.rizzi@acaps.org',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
