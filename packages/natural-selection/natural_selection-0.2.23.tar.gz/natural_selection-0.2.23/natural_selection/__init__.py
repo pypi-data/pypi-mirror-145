e8f06ebc251566f2ad6cec6e2fd235840abb0b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:44 +0000

    Add package natural-selection/0.2.23/natural_selection-0.2.23.tar.gz

---
+++ packages/natural-selection/natural_selection-0.2.23.tar.gz/natural_selection-0.2.23/natural_selection/__init__.py
@@ -0,0 +1,20 @@
+__version__ = '0.2.23'
+__date__ = "2022-04-05"
+
+def get_random_string(length : int = 8, include_numeric=False) -> str:
+    """
+    Generate a random string with a given length. Used mainly for password generation.
+    Args:
+        length (int): Length to generate.
+        include_numeric (bool): Include numbers?
+
+    Returns:
+        str: Random character string.
+    """
+    import random
+    import string
+
+    letters = string.ascii_letters
+    if include_numeric:
+        letters = f'{letters}{string.digits}'
+    return ''.join(random.choice(letters) for i in range(length))
