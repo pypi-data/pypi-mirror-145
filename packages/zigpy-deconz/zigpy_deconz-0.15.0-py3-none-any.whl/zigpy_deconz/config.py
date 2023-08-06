8cdcfabf83ac257a5d8292eecdb9b9a29944cb2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:17 +0000

    Add package zigpy-deconz/0.15.0/zigpy_deconz-0.15.0-py3-none-any.whl

---
+++ packages/zigpy-deconz/zigpy_deconz-0.15.0-py3-none-any.whl/zigpy_deconz/config.py
@@ -0,0 +1,30 @@
+"""Default configuration values."""
+
+import voluptuous as vol
+from zigpy.config import (  # noqa: F401 pylint: disable=unused-import
+    CONF_DEVICE,
+    CONF_DEVICE_PATH,
+    CONF_NWK,
+    CONF_NWK_CHANNEL,
+    CONF_NWK_CHANNELS,
+    CONF_NWK_EXTENDED_PAN_ID,
+    CONF_NWK_KEY,
+    CONF_NWK_PAN_ID,
+    CONF_NWK_TC_ADDRESS,
+    CONF_NWK_TC_LINK_KEY,
+    CONF_NWK_UPDATE_ID,
+    CONFIG_SCHEMA,
+    SCHEMA_DEVICE,
+    cv_boolean,
+)
+
+CONF_WATCHDOG_TTL = "watchdog_ttl"
+CONF_WATCHDOG_TTL_DEFAULT = 600
+
+CONFIG_SCHEMA = CONFIG_SCHEMA.extend(
+    {
+        vol.Optional(CONF_WATCHDOG_TTL, default=CONF_WATCHDOG_TTL_DEFAULT): vol.All(
+            int, vol.Range(min=180)
+        )
+    }
+)
