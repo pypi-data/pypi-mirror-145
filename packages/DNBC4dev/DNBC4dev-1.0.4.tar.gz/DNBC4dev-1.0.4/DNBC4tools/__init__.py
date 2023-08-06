a8ad6be82adaec4a147c7bcaa1fee64caa985330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:52 +0000

    Add package DNBC4dev/1.0.4/DNBC4dev-1.0.4.tar.gz

---
+++ packages/DNBC4dev/DNBC4dev-1.0.4.tar.gz/DNBC4dev-1.0.4/DNBC4tools/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+_version = "1.0.4"
+
+_pipelist = [
+    "data",
+    'count',
+    'analysis',
+    'report',
+    'clean',
+    'mkref',
+    'run',
+    'list',
+]
+_root_dir = os.path.abspath(os.path.dirname(__file__))
