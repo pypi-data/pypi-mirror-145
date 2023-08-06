c45d0300b15ed6273c0c896042dcc4542f7adb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:08 +0000

    Add package pygsuite/0.0.18/pygsuite-0.0.18.tar.gz

---
+++ packages/pygsuite/pygsuite-0.0.18.tar.gz/pygsuite-0.0.18/pygsuite/__init__.py
@@ -0,0 +1,28 @@
+from pygsuite.auth.authorization import Clients
+from pygsuite.common.style import DefaultFonts, TextStyle, Color
+from pygsuite.docs import Document
+from pygsuite.sheets import Spreadsheet
+from pygsuite.slides import Presentation
+from pygsuite.images import ImageUploader
+from pygsuite.drive.drive import Drive
+from pygsuite.drive.file import File
+from pygsuite.drive.folder import Folder
+from pygsuite.enums import UserType, PermissionType
+
+__version__ = "0.0.18"
+__author__ = "Ethan Dickinson <ethan.dickinson@gmail.com>"
+__all__ = [
+    "Clients",
+    "Color",
+    "DefaultFonts",
+    "Document",
+    "Drive",
+    "File",
+    "Folder",
+    "PermissionType",
+    "Presentation",
+    "Spreadsheet",
+    "TextStyle",
+    "ImageUploader",
+    "UserType",
+]  # type: ignore
