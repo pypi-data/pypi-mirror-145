27685aca46b79d15310a9b7c1dea4b96b86146ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:03 +0000

    Add package savepagenow/1.2.2/savepagenow-1.2.2.tar.gz

---
+++ packages/savepagenow/savepagenow-1.2.2.tar.gz/savepagenow-1.2.2/savepagenow/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from .api import BlockedByRobots, CachedPage, capture, capture_or_cache
+
+__version__ = "1.0.1"
+
+__all__ = ("BlockedByRobots", "capture", "capture_or_cache", "CachedPage")
