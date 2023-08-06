d41b3c1d04d9d8cc2a2cabfedb11f6fbd3573a8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:55 +0000

    Add package tbutil/3.3.25/tbutil-3.3.25.tar.gz

---
+++ packages/tbutil/tbutil-3.3.25.tar.gz/tbutil-3.3.25/src/__init__.py
@@ -0,0 +1,6 @@
+try:
+    print('import starting')
+    from tb import checkfile,crawler,exe,getch,make_class,prt2,sfz,std,str033,tbsocket
+    print('tb3 import successfully!')
+except:
+    raise ImportError('failed import tb3.0')
