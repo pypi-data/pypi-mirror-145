2119441e8add6c03d90436712de02af4a0080263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:37:09 +0000

    Add package dash-charlotte/0.2.0/dash_charlotte-0.2.0-py3-none-any.whl

---
+++ packages/dash-charlotte/dash_charlotte-0.2.0-py3-none-any.whl/dash_charlotte/themes/themes.py
@@ -0,0 +1,11 @@
+"""CDN Links for Dash Charlotte Themes.
+
+"""
+
+
+
+ROOT = r'https://gusfurtado.github.io/dash-charlotte/css_themes/'
+
+CHARLOTTE_DARK = ROOT + 'charlotte_dark.css'
+CHARLOTTE_LIGHT = ROOT + 'charlotte_light.css'
+DRACULA = ROOT + 'dracula.css'
