b98521e9bf340bdcbe4cb05e83e0f33cbe22e9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:53 +0000

    Add package novels/0.0.57/novels-0.0.57.tar.gz

---
+++ packages/novels/novels-0.0.57.tar.gz/novels-0.0.57/setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup,find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="novels",
+    version="0.0.57",
+    description="free novel retriever",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/everwalker/free-novels/src/master/",
+    author="Yonghang Wang",
+    author_email="wyhang@gmail.com",
+    license="Apache 2",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[ "beautifulsoup4" ],
+    keywords=[ "novel"],
+    entry_points={ "console_scripts": 
+        [ 
+            "novels=novels:novels_main", 
+        ] 
+    },
+)
