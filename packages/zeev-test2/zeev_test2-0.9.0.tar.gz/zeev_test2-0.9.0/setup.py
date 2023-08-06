c5e4bcf4f00318e35d111bfb19626d82badd1f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:48:36 +0000

    Add package zeev-test2/0.9.0/zeev_test2-0.9.0.tar.gz

---
+++ packages/zeev-test2/zeev_test2-0.9.0.tar.gz/zeev_test2-0.9.0/setup.py
@@ -0,0 +1,30 @@
+"""Setup for the cs.eyrie package."""
+
+import setuptools
+import os
+
+
+setuptools.setup(
+    author="sihish",
+    author_email="hace@gmail.com",
+    name='zeev_test2',
+    license="MIT",
+    description='cs.eyrie is a amazing package for data handling',
+    version='v0.9.0',
+    long_description="no long decsription given",
+    url='https://github.com/aaaditisinghhhh/',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
