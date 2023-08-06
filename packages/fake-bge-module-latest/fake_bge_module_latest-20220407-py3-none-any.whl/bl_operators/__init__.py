9a3bdc2961a661b03836d3440fc00ff51bbf0083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:03 +0000

    Add package fake-bge-module-latest/20220407/fake_bge_module_latest-20220407-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220407-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import vertexpaint_dirt
+from . import image
+from . import geometry_nodes
+from . import object_randomize_transform
+from . import console
+from . import node
+from . import file
+from . import constraint
+from . import uvcalc_follow_active
+from . import uvcalc_lightmap
+from . import userpref
+from . import spreadsheet
+from . import freestyle
+from . import mesh
+from . import add_mesh_torus
+from . import bmesh
+from . import rigidbody
+from . import assets
+from . import object
+from . import presets
+from . import view3d
+from . import object_align
+from . import clip
+from . import anim
+from . import sequencer
+from . import object_quick_effects
+from . import screen_play_rendered_anim
+from . import wm
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220407-py3-none-any.whl/bpy/ops/curves.py
@@ -0,0 +1,18 @@
+import sys
+import typing
+
+
+def convert_to_particle_system():
+    ''' Add a new or update an existing hair particle system on the surface object
+
+    '''
+
+    pass
+
+
+def sculptmode_toggle():
+    ''' Enter/Exit sculpt mode for curves
+
+    '''
+
+    pass
