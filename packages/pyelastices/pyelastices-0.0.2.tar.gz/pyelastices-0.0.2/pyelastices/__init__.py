7ccb696761556cbbadfff05d6cc9b6e708ec1d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:28 +0000

    Add package pyelastices/0.0.2/pyelastices-0.0.2.tar.gz

---
+++ packages/pyelastices/pyelastices-0.0.2.tar.gz/pyelastices-0.0.2/pyelastices/__init__.py
@@ -0,0 +1,9 @@
+# *_*coding:utf-8 *_*
+"""
+Author：SZJ
+"""
+
+from .__version__ import version, __version__
+
+from pyelastices.api import Client as Elasticsearch
+from pyelastices.api import helpers
