b5ed5d182476411d8f16ed2ca06e1a382b6321c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:51 +0000

    Add package docarray/0.11.4.dev2/docarray-0.11.4.dev2.tar.gz

---
+++ packages/docarray/docarray-0.11.4.dev2.tar.gz/docarray-0.11.4.dev2/docarray/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '0.11.4.dev2'
+
+import os
+
+from .document import Document
+from .array import DocumentArray
+from .dataclasses import dataclass, Image, Text, Audio, JSON, field
+
+
+if 'DA_NO_RICH_HANDLER' not in os.environ:
+    from rich.traceback import install
+
+    install()
