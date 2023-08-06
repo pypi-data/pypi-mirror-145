4364ee1d55a6d8a949b7fd397bf23c51db89b3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:52:43 +0000

    Add package maxipago-sdk-iesde/1.3.3/maxipago-sdk-iesde-1.3.3.tar.gz

---
+++ packages/maxipago-sdk-iesde/maxipago-sdk-iesde-1.3.3.tar.gz/maxipago-sdk-iesde-1.3.3/README.md
@@ -0,0 +1,42 @@
+pyOpenSSL - necessario para a biblioteca Requests.
+
+# Historico de versoes:
+
+1.3.3: # BUG FIX
+
+1.3.2: # correção para futuro warning
+
+1.3.1: # BUG FIX
+
+1.3.0: Atualização das bibliotecas request, lxml e pyOpenSSL. 
+Foi reescrito o módulo de recorrências para acrescentar campos 
+de identificação do cliente.
+
+1.2.3: Adicionado resource para editar recorrências. 
+Foi removido por falta de implantação da maxipago - erro interno.
+
+1.2.2: # BUG FIX
+
+1.2.1: Adicionado resource para consulta de transactions.
+
+1.2.2: #BUG FIX
+
+1.2.1: Adicionado recurso para alteração de recorrencia
+
+1.2.0: Criacao e adicao de resource para cancelamento de recorrencia.
+
+1.1.9: Foi retirado o caracter com acento (ascii invalido) deste documento.
+
+1.1.8: #BUG FIX
+
+1.1.7: #BUG FIX
+
+1.1.6: Adicao do certificado "pyOpenSSL" para a biblioteca Requests
+
+1.1.5: Adicionado um resource para o retorno da recorrencia.
+
+1.1.4: Atualizacao da biblioteca Requests
+
+1.1.3: #BUG FIX
+
+1.1.2: Adicao de desconto, multa e juros para geracao de boletos.
---
+++ packages/maxipago-sdk-iesde/maxipago-sdk-iesde-1.3.3.tar.gz/maxipago-sdk-iesde-1.3.3/maxipago/resources/recurring.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+from io import BytesIO
+from maxipago.utils import etree
+from maxipago.resources.base import Resource
+from maxipago.exceptions import PaymentException
+
+
+class CancelResource(Resource):
+
+    def process(self):
+    
+        tree = etree.parse(BytesIO(self.data))
+        #print(etree.tostring(tree, pretty_print=True))
+        error_code = tree.find('errorCode')
+        if error_code is not None and error_code.text != '0':
+            error_message = tree.find('errorMessage')
+            if error_message is None:
+                error_message = tree.find('errorMsg')
+
+            if error_message is not None:
+                error_message = error_message.text
+            
+            raise PaymentException(message=error_message)
+
+        fields = [
+            ('errorCode', 'processor_code'),
+            ('errorMessage', 'error_message'),
+            ('command', 'acao'),
+            ('time', 'transaction_timestamp'),
+            ('result', 'processor_message')
+        ]
+
+        for f_name, f_translated in fields:
+            field = tree.find(f_name)
+            if field is not None:
+                setattr(self, f_translated, field.text)
+
+
+
+class EditResource(Resource):
+
+    def process(self):
+    
+        tree = etree.parse(BytesIO(self.data))
+        error_code = tree.find('errorCode')
+
+        fields = [
+            ('errorCode', 'processor_code'),
+            ('errorMessage', 'error_message'),
+            ('time', 'transaction_timestamp'),
+            ('result', 'processor_message')
+        ]
+
+        for f_name, f_translated in fields:
+            field = tree.find(f_name)
+            if field is not None:
+                setattr(self, f_translated, field.text)
