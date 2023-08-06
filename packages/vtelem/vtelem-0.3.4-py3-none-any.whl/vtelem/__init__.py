1c474961d1e46f56ce1a6f9b1754c50471abdd05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:46 +0000

    Add package vtelem/0.3.4/vtelem-0.3.4-py3-none-any.whl

---
+++ packages/vtelem/vtelem-0.3.4-py3-none-any.whl/vtelem/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=2.1.1
+# hash=abf20f7c7c8bd78d2f4180862aae111e
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A real-time telemetry library."
+PKG_NAME = "vtelem"
+VERSION = "0.3.4"
