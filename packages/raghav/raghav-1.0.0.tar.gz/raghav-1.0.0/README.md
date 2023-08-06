3836e192fe30ba3c6a52ee7609a5b53a4695078b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:01 +0000

    Add package raghav/1.0.0/raghav-1.0.0.tar.gz

---
+++ packages/raghav/raghav-1.0.0.tar.gz/raghav-1.0.0/README.md
@@ -0,0 +1,1 @@
+A Program that takes in a string and returns it in reverse
---
+++ packages/raghav/raghav-1.0.0.tar.gz/raghav-1.0.0/raghav/__init__.py
@@ -0,0 +1,15 @@
+class StringFunc:
+    def __init_(self):
+        pass
+
+    @staticmethod
+    def first_char(str):
+        return str[:1]
+
+    @staticmethod
+    def last_char(str):
+        return str[-1:]
+
+    @staticmethod
+    def reverse(str):
+        return str[::-1]
