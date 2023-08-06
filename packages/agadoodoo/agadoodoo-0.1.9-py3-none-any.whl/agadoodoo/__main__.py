1ca9c1bab85032b14162344d442a777f8e7b2956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:03 +0000

    Add package agadoodoo/0.1.9/agadoodoo-0.1.9-py3-none-any.whl

---
+++ packages/agadoodoo/agadoodoo-0.1.9-py3-none-any.whl/agadoodoo/__main__.py
@@ -0,0 +1,40 @@
+import os, sys, subprocess
+
+
+__BASE_DIR__ = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_cli_args():
+    return sys.argv
+
+
+def get_execution_for(path):
+    cmd = [path]
+
+    if sys.platform in ['win32', 'cygwin']:
+        cmd.insert(0, 'python')
+    return cmd
+
+
+def main():
+    args = get_cli_args()
+    script_path = os.path.join(__BASE_DIR__, 'scripts', 'foo.py')
+
+    print("sys.argv:", args)
+
+    try:
+        child = subprocess.Popen(
+            get_execution_for(script_path)
+        )
+    except Exception as e:
+        print(type(e), ":", str(e))
+        return -1
+    code = child.wait()
+
+    print("execution finished")
+    print(f"({script_path}): {code}")
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
