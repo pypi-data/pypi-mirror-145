8ad35731f5d8476fa5f85e34e6c26089b484c593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:17 +0000

    Add package twixfact/0.0.2/twixfact-0.0.2.tar.gz

---
+++ packages/twixfact/twixfact-0.0.2.tar.gz/twixfact-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Factorial Package'
+LONG_DESCRIPTION = 'You can calculate factorial using this package'
+
+# Setting up
+setup(
+    name="twixfact",
+    version=VERSION,
+    author="twixour (Raushan Kashyap)",
+    author_email="<twixour@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'factorial'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
