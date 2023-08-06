a005c3f60201d92ec4cb242fe8078759bb7266d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:34 +0000

    Add package vcorelib/0.2.0/vcorelib-0.2.0-py3-none-any.whl

---
+++ packages/vcorelib/vcorelib-0.2.0-py3-none-any.whl/vcorelib/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.14.0
+# hash=43c10539f25ac147cbade36d485c6b93
+# =====================================
+
+"""
+vcorelib - Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A collection of core Python utilities."
+PKG_NAME = "vcorelib"
+VERSION = "0.2.0"
---
+++ packages/vcorelib/vcorelib-0.2.0-py3-none-any.whl/vcorelib_bootstrap/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=1.14.0
+# hash=f5f5fa5b37eebf9860ccc3bb170836c0
+# =====================================
+
+"""
+vmklib - Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
+PKG_NAME = "vmklib"
+VERSION = "0.9.0"
