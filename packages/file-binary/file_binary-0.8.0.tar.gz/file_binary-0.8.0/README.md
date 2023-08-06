02348d5312d2c8ca66f4b1f2005df1496b5dc9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:33 +0000

    Add package file-binary/0.8.0/file_binary-0.8.0.tar.gz

---
+++ packages/file-binary/file_binary-0.8.0.tar.gz/file_binary-0.8.0/README.md
@@ -0,0 +1,20 @@
+<div align="center">
+  <h1>File Binary</h1>
+</div>
+
+[![PyPI Latest Release](https://img.shields.io/pypi/v/pandas.svg)](https://pypi.org/project/file-binary)
+
+---
+### What is it ?
+
+**File Binary** is a package with some tools to help you with binary files
+
+### How to install
+```py
+pip install file-binary
+```
+### How to use
+
+```py
+from binary_file import fb
+```
---
+++ packages/file-binary/file_binary-0.8.0.tar.gz/file_binary-0.8.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['file_binary']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'file-binary',
+    'version': '0.8.0',
+    'description': 'File Binary is a package with some tools to help you with binary files',
+    'long_description': '<div align="center">\n  <h1>File Binary</h1>\n</div>\n\n[![PyPI Latest Release](https://img.shields.io/pypi/v/pandas.svg)](https://pypi.org/project/file-binary)\n\n---\n### What is it ?\n\n**File Binary** is a package with some tools to help you with binary files\n\n### How to install\n```py\npip install file-binary\n```\n### How to use\n\n```py\nfrom binary_file import fb\n```',
+    'author': 'matheus',
+    'author_email': 'geof.matheus@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MatheusRamo/file_binary',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
