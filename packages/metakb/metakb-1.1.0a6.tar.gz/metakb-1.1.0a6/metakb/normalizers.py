79f9d399af46560e9cc432c929a3f60865e551a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:53 +0000

    Add package metakb/1.1.0a6/metakb-1.1.0a6.tar.gz

---
+++ packages/metakb/metakb-1.1.0a6.tar.gz/metakb-1.1.0a6/metakb/normalizers.py
@@ -0,0 +1,135 @@
+"""Module for VICC normalizers."""
+from typing import Optional, Tuple
+
+from ga4gh.vrsatile.pydantic.vrs_models import VRSTypes
+from ga4gh.vrsatile.pydantic.vrsatile_models import VariationDescriptor
+from variation.query import QueryHandler as VariationQueryHandler
+from therapy.query import QueryHandler as TherapyQueryHandler
+from therapy.schemas import NormalizationService as NormalizedTherapy
+from disease.query import QueryHandler as DiseaseQueryHandler
+from disease.schemas import NormalizationService as NormalizedDisease
+from gene.query import QueryHandler as GeneQueryHandler
+from gene.schemas import NormalizeService as NormalizedGene
+import logging
+
+logger = logging.getLogger('metakb.normalizers')
+logger.setLevel(logging.DEBUG)
+
+
+class VICCNormalizers:
+    """A class for normalizing terms using VICC normalizers."""
+
+    def __init__(self):
+        """Initialize the VICC Normalizers."""
+        self.gene_query_handler = GeneQueryHandler()
+        self.variation_normalizer = VariationQueryHandler()
+        self.disease_query_handler = DiseaseQueryHandler()
+        self.therapy_query_handler = TherapyQueryHandler()
+
+    async def normalize_variation(self,
+                                  queries) -> Optional[VariationDescriptor]:
+        """Normalize variation queries.
+
+        :param list queries: Possible query strings to try to normalize
+            which are used in the event that a MANE transcript cannot be found
+        :return: A normalized variation
+        """
+        for query in queries:
+            if not query:
+                continue
+
+            try:
+                variation_norm_resp = \
+                    await self.variation_normalizer.normalize(query)
+                if variation_norm_resp:
+                    if variation_norm_resp.variation.type != VRSTypes.TEXT:
+                        return variation_norm_resp
+            except Exception as e:  # noqa: E722
+                logger.warning(f"Variation Normalizer raised an exception "
+                               f"using query {query}: {e}")
+        return None
+
+    def normalize_gene(self, queries)\
+            -> Tuple[Optional[NormalizedGene], Optional[str]]:
+        """Normalize gene queries
+
+        :param list queries: Gene queries to normalize
+        :return: The highest matched gene's normalized response and ID
+        """
+        gene_norm_resp = None
+        normalized_gene_id = None
+        highest_match = 0
+        for query_str in queries:
+            if not query_str:
+                continue
+
+            try:
+                gene_norm_resp = self.gene_query_handler.normalize(query_str)
+            except Exception as e:
+                logger.warning(f"Gene Normalizer raised an exception using "
+                               f"query {query_str}: {e}")
+            else:
+                if gene_norm_resp.match_type > highest_match:
+                    highest_match = gene_norm_resp.match_type
+                    normalized_gene_id = \
+                        gene_norm_resp.gene_descriptor.gene_id
+                    if highest_match == 100:
+                        break
+        return gene_norm_resp, normalized_gene_id
+
+    def normalize_disease(self, queries)\
+            -> Tuple[Optional[NormalizedDisease], Optional[str]]:
+        """Normalize disease queries
+
+        :param list queries: Disease queries to normalize
+        :return: The highest matched disease's normalized response and ID
+        """
+        highest_match = 0
+        normalized_disease_id = None
+        disease_norm_resp = None
+
+        for query in queries:
+            if not query:
+                continue
+
+            try:
+                disease_norm_resp = self.disease_query_handler.normalize(query)
+            except Exception as e:
+                logger.warning(f"Disease Normalizer raised an exception using "
+                               f"query {query}: {e}")
+            else:
+                if disease_norm_resp.match_type > highest_match:
+                    highest_match = disease_norm_resp.match_type
+                    normalized_disease_id = \
+                        disease_norm_resp.disease_descriptor.disease_id
+                    if highest_match == 100:
+                        break
+        return disease_norm_resp, normalized_disease_id
+
+    def normalize_therapy(self, queries)\
+            -> Tuple[Optional[NormalizedTherapy], Optional[str]]:
+        """Normalize therapy queries
+
+        :param list queries: Therapy queries to normalize
+        :return: The highest matched therapy's normalized response and ID
+        """
+        highest_match = 0
+        normalized_therapy_id = None
+        therapy_norm_resp = None
+
+        for query in queries:
+            if not query:
+                continue
+
+            try:
+                therapy_norm_resp = self.therapy_query_handler.normalize(query)
+            except Exception as e:
+                logger.warning(f"Therapy Normalizer raised an exception using "
+                               f"query {query}: {e}")
+            else:
+                if therapy_norm_resp.match_type > highest_match:
+                    highest_match = therapy_norm_resp.match_type
+                    normalized_therapy_id = therapy_norm_resp.therapy_descriptor.therapy_id  # noqa: E501
+                    if highest_match == 100:
+                        break
+        return therapy_norm_resp, normalized_therapy_id
