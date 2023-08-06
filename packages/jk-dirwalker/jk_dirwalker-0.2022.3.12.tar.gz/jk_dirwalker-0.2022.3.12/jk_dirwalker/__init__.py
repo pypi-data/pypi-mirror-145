2d94cc6911d60d50a4494f3b015654c17b66590c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:50 +0000

    Add package jk-dirwalker/0.2022.3.12/jk_dirwalker-0.2022.3.12.tar.gz

---
+++ packages/jk-dirwalker/jk_dirwalker-0.2022.3.12.tar.gz/jk_dirwalker-0.2022.3.12/jk_dirwalker/__init__.py
@@ -0,0 +1,42 @@
+﻿
+
+
+__version__ = "0.2022.3.12"
+
+
+
+from .DirEntryX import DirEntryX
+from .DescendFilter import DescendFilter
+from .EmitFilter import EmitFilter
+from .StdEmitFilter import StdEmitFilter
+
+from .DirWalker import DirWalker
+
+
+
+def scandir(
+		dirPath:str,
+		*,
+		emitFilter:EmitFilter = None,
+		descendFilter:DescendFilter = None,
+	):
+	yield from DirWalker(
+		emitFilter=emitFilter,
+		descendFilter=descendFilter,
+	).scandir(dirPath)
+#
+
+def listdir(
+		dirPath:str,
+		*,
+		emitFilter:EmitFilter = None,
+		descendFilter:DescendFilter = None,
+	):
+	yield from DirWalker(
+		emitFilter=emitFilter,
+		descendFilter=descendFilter,
+	).listdir(dirPath)
+#
+
+
+
