5139180ce66363a2b3358005f2c9a08fb2059978
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:53 +0000

    Add package algovera/0.1.3/algovera-0.1.3.tar.gz

---
+++ packages/algovera/algovera-0.1.3.tar.gz/algovera-0.1.3/src/widget.ts
@@ -0,0 +1,24 @@
+// Copyright (c) ipylab contributors
+// Distributed under the terms of the Modified BSD License.
+
+import { CommandRegistryModel } from "./widgets/commands";
+import { CommandPaletteModel } from "./widgets/palette";
+import { SessionManagerModel } from "./widgets/sessions";
+import { JupyterFrontEndModel } from "./widgets/frontend";
+import { PanelModel } from "./widgets/panel";
+import { ShellModel } from "./widgets/shell";
+import { SplitPanelModel, SplitPanelView } from "./widgets/split_panel";
+import { TitleModel } from "./widgets/title";
+//import { EstuaryWidget } from "./widgets/react";
+
+export {
+  CommandRegistryModel,
+  CommandPaletteModel,
+  JupyterFrontEndModel,
+  PanelModel,
+  ShellModel,
+  SplitPanelModel,
+  SplitPanelView,
+  TitleModel,
+  SessionManagerModel
+};
