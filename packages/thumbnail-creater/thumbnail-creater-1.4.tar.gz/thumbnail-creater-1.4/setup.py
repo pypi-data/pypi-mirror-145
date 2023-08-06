801ce0d2e07bc67ef9301e465799ba68657fc225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package thumbnail-creater/1.4/thumbnail-creater-1.4.tar.gz

---
+++ packages/thumbnail-creater/thumbnail-creater-1.4.tar.gz/thumbnail-creater-1.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='thumbnail-creater',
+    version=1.4,
+    keywords='project',
+    scripts=['bin/thumbnail-creater'],
+    install_requires=[
+          'glob2',
+      ],
+
+)
