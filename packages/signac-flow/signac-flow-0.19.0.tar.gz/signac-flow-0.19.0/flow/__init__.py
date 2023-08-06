2cf43a7d68cfb6e165b2fbbc82f5d59666b64a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:34 +0000

    Add package signac-flow/0.19.0/signac-flow-0.19.0.tar.gz

---
+++ packages/signac-flow/signac-flow-0.19.0.tar.gz/signac-flow-0.19.0/flow/__init__.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2017 The Regents of the University of Michigan
+# All rights reserved.
+# This software is licensed under the BSD 3-Clause License.
+"""Workflow management based on the signac framework.
+
+The signac-flow package provides the basic infrastructure to easily
+configure and implement a workflow to operate on a signac_ data space.
+
+.. _signac: https://signac.io/
+"""
+from . import environment, errors, hooks, scheduling, testing
+from .aggregates import aggregator, get_aggregate_id
+from .environment import get_environment
+from .operations import cmd, directives, with_job
+from .project import FlowProject, IgnoreConditions, classlabel, label, staticlabel
+from .template import init
+
+# Import packaged environments unless disabled in config:
+from .util.config import _FLOW_CONFIG_DEFAULTS
+from .util.config import get_config_value as _get_config_value
+from .util.misc import redirect_log
+from .version import __version__
+
+__all__ = [
+    "environment",
+    "errors",
+    "hooks",
+    "scheduling",
+    "testing",
+    "aggregator",
+    "get_aggregate_id",
+    "get_environment",
+    "cmd",
+    "directives",
+    "with_job",
+    "FlowProject",
+    "IgnoreConditions",
+    "classlabel",
+    "label",
+    "staticlabel",
+    "init",
+    "redirect_log",
+    "__version__",
+]
+
+
+if _get_config_value(
+    "import_packaged_environments",
+    default=_FLOW_CONFIG_DEFAULTS["import_packaged_environments"],
+):
+    from . import environments  # noqa: F401
+
+    __all__.append("environments")
