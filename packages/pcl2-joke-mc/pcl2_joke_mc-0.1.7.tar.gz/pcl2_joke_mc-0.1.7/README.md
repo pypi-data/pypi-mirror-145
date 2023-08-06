d79e0e416dbff027bf9b4a42e2ff842a993ef95c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:56 +0000

    Add package pcl2-joke-mc/0.1.7/pcl2_joke_mc-0.1.7.tar.gz

---
+++ packages/pcl2-joke-mc/pcl2_joke_mc-0.1.7.tar.gz/pcl2_joke_mc-0.1.7/README.md
@@ -0,0 +1,11 @@
+# CLI Package
+
+Authors: 
+Cui Ding(olatname: cding)
+Matriculation Numbers: 21-718-945
+Mia Tatjana Egli (olatname: miaegl)
+Matriculation Numbers: 21-700-406
+
+This is a CLI package, using CLI to processing jokes. 
+
+This is the exercise 3.
---
+++ packages/pcl2-joke-mc/pcl2_joke_mc-0.1.7.tar.gz/pcl2_joke_mc-0.1.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme_content = fh.read().strip()
+
+setuptools.setup(
+    name="pcl2_joke_mc",
+    version="0.1.7",
+    author="cui, mia",
+    author_email="cui.ding@uzh.com",
+    description="creating CLI for processing joke.",
+    long_description=readme_content,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+	packages=['clipkg'],
+	
+    include_package_data=True,
+    
+    package_data={'clipkg': ['data/*']},
+    
+    entry_points={
+        'console_scripts': [
+            'jg = clipkg.CLI:main'
+        ]
+    },
+    python_requires=">=3.6",
+)
