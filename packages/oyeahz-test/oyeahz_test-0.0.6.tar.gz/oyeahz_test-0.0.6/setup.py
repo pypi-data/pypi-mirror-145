aab2359a384abd6dee61bbb81baffd83757cb83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:07 +0000

    Add package oyeahz-test/0.0.6/oyeahz_test-0.0.6.tar.gz

---
+++ packages/oyeahz-test/oyeahz_test-0.0.6.tar.gz/oyeahz_test-0.0.6/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oyeahz_test",
+    version="0.0.6",
+    author="linan890107",
+    author_email="linan890107@126.com",
+    description="Oyeahz组件测试包",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Linan0107/oyeahz_test.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/Linan0107/oyeahz_test/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['oyeahz_test', 'oyeahz_test.test1'],
+    python_requires=">=3.6",
+)
