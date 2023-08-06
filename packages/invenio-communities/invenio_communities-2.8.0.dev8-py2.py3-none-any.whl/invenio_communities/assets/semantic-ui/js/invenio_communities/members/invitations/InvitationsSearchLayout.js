0f94738513cce7b07565d8a672ed6ddf4b19ae3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:28 +0000

    Add package invenio-communities/2.8.0.dev8/invenio_communities-2.8.0.dev8-py2.py3-none-any.whl

---
+++ packages/invenio-communities/invenio_communities-2.8.0.dev8-py2.py3-none-any.whl/invenio_communities/assets/semantic-ui/js/invenio_communities/members/invitations/InvitationsSearchLayout.js
@@ -0,0 +1,35 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2022 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or modify it
+ * under the terms of the MIT License; see LICENSE file for more details.
+ */
+
+import React from "react";
+import { Button } from "semantic-ui-react";
+import { SearchAppResultsPane } from "@js/invenio_search_ui/components";
+import { SearchBarWithFiltersWithState } from "../components/SearchBarWithFilters";
+import { i18next } from "@translations/invenio_communities/i18next";
+
+export const InvitationsSearchLayout = ({ config, updateQueryState }) => {
+  const sortOptions = config.sortOptions;
+
+  return (
+    <>
+      <SearchBarWithFiltersWithState
+        sortOptions={sortOptions}
+        updateQueryState={updateQueryState}
+        customCmp={
+          <Button
+            content={i18next.t("Invite members")}
+            positive
+            size="medium"
+            className="ml-5"
+          />
+        }
+      />
+      <SearchAppResultsPane layoutOptions={config.layoutOptions} />
+    </>
+  );
+};
