13fdd13cfa03fbeb29b32e84f05d933f498e38eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:04 +0000

    Add package requests-mock-flask/2022.4.3/requests-mock-flask-2022.4.3.tar.gz

---
+++ packages/requests-mock-flask/requests-mock-flask-2022.4.3.tar.gz/requests-mock-flask-2022.4.3/lint.mk
@@ -0,0 +1,81 @@
+# Make commands for linting
+
+SHELL := /bin/bash -euxo pipefail
+
+.PHONY: black
+black:
+	black --check .
+
+.PHONY: fix-black
+fix-black:
+	black .
+
+.PHONY: mypy
+mypy:
+	mypy .
+
+.PHONY: check-manifest
+check-manifest:
+	check-manifest .
+
+.PHONY: doc8
+doc8:
+	doc8 .
+
+.PHONY: flake8
+flake8:
+	flake8 .
+
+.PHONY: isort
+isort:
+	isort --check-only .
+
+.PHONY: fix-isort
+fix-isort:
+	isort .
+
+.PHONY: pip-extra-reqs
+pip-extra-reqs:
+	pip-extra-reqs --requirements-file=requirements/requirements.txt src/
+
+.PHONY: pip-missing-reqs
+pip-missing-reqs:
+	pip-missing-reqs \
+		--requirements-file=requirements/requirements.txt \
+		--ignore-file=src/requests_mock_flask/_type_check_imports/__init__.py \
+		src/
+
+.PHONY: pylint
+pylint:
+	pylint *.py src/ tests/ docs/
+
+.PHONY: pyroma
+pyroma:
+	pyroma --min 10 .
+
+.PHONY: vulture
+vulture:
+	vulture --min-confidence 100 --exclude _vendor --exclude .eggs .
+
+.PHONY: linkcheck
+linkcheck:
+	$(MAKE) -C docs/ linkcheck SPHINXOPTS=$(SPHINXOPTS)
+
+.PHONY: spelling
+spelling:
+	$(MAKE) -C docs/ spelling SPHINXOPTS=$(SPHINXOPTS)
+
+.PHONY: autoflake
+autoflake:
+	autoflake \
+	    --in-place \
+	    --recursive \
+	    --remove-all-unused-imports \
+	    --remove-unused-variables \
+	    --expand-star-imports \
+	    --exclude _vendor,release \
+	    .
+
+.PHONY: pydocstyle
+pydocstyle:
+	pydocstyle
