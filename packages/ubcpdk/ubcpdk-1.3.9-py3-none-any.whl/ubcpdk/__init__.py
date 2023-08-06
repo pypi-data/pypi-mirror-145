4dcef9b1b1144664a648338cf085328d3725ee7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:56 +0000

    Add package ubcpdk/1.3.9/ubcpdk-1.3.9-py3-none-any.whl

---
+++ packages/ubcpdk/ubcpdk-1.3.9-py3-none-any.whl/ubcpdk/__init__.py
@@ -0,0 +1,32 @@
+"""UBC Siepic Ebeam PDK from edx course"""
+import gdsfactory as gf
+from gdsfactory.config import logger
+from ubcpdk.config import CONFIG, PATH, module
+from ubcpdk.tech import LAYER, strip
+from ubcpdk import components
+from ubcpdk import tech
+from ubcpdk import data
+
+from ubcpdk.components import component_factory
+from ubcpdk.tech import cross_section_factory
+
+
+gf.asserts.version(">=4.2.1")
+lys = gf.layers.load_lyp(PATH.lyp)
+__version__ = "1.3.9"
+
+__all__ = [
+    "CONFIG",
+    "data",
+    "PATH",
+    "components",
+    "tech",
+    "strip",
+    "LAYER",
+    "__version__",
+    "component_factory",
+    "cross_section_factory",
+]
+
+
+logger.info(f"Found UBCpdk {__version__!r} installed at {module!r}")
