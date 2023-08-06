228ab7a77951984c715e52e0193541d3f024a0e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:54 +0000

    Add package pythonagent/0.0.1/pythonagent-0.0.1.tar.gz

---
+++ packages/pythonagent/pythonagent-0.0.1.tar.gz/pythonagent-0.0.1/pythonagent/agent/probes/sql/__init__.py
@@ -0,0 +1,12 @@
+
+
+
+"""Interceptors for SQL databases.
+
+"""
+
+from __future__ import unicode_literals
+
+from . import mysql_connector, mysqldb, psycopg2, pymysql, dynamodb
+
+__all__ = ['pymysql','mysql_connector', 'mysqldb', 'psycopg2', 'dynamodb']
