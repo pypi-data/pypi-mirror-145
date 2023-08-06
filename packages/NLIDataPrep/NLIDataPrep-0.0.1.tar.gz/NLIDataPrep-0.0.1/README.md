68c03578f587b187ed0882ed63c4d238e64a4385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:41 +0000

    Add package NLIDataPrep/0.0.1/NLIDataPrep-0.0.1.tar.gz

---
+++ packages/NLIDataPrep/NLIDataPrep-0.0.1.tar.gz/NLIDataPrep-0.0.1/README.md
@@ -0,0 +1,26 @@
+# NLIDataPrep
+Package for data preprocessing and preparation.
+
+This package contains tools and utilities to work with messy data and prepare tabular data to MARC mapping and conversion.
+
+## Installation
+
+Run the following to install
+```python
+pip install NLIDataPrep
+```
+
+## Usage
+
+```python
+from NLIDataPrep as NDP import *
+```
+
+## Developing NLIDataPrep
+
+To install NLIDataPrep, along with the tools you need to develop and run tests, run the following
+ command in your virtualenv:
+
+ ```bash
+ $ pip install -e .[dev]
+ ```
---
+++ packages/NLIDataPrep/NLIDataPrep-0.0.1.tar.gz/NLIDataPrep-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='NLIDataPrep',
+    url='https://github.com/ygherman/NLIDataPrep',
+    author="Yael Gherman",
+    author_email="gh.gherman@gmail.com",
+    version='0.0.1',
+    description='Python Package for data preprocessing and preparation for REI prject metadata ingest',
+    py_modules = [""],
+    package_dir={'':'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_require = [
+        "pandas ~= 1.4",
+    ],
+    extras_require = {
+        "dev": [
+            "pytest>=3.8",
+            "mock>=4.0.0"
+        ],
+    }
+)
+
