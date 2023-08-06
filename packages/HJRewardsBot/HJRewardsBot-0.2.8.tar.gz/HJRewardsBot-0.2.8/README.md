9f2454d0f573614c7eca5d960958562fa753a03d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:50 +0000

    Add package HJRewardsBot/0.2.8/HJRewardsBot-0.2.8.tar.gz

---
+++ packages/HJRewardsBot/HJRewardsBot-0.2.8.tar.gz/HJRewardsBot-0.2.8/README.md
@@ -0,0 +1,11 @@
+# HJRewardsBot
+
+Under construction! Not ready for use yet! Currently experimenting and planning!
+
+Developed by Hassan jlilati (c) 2021
+
+## Examples of How To Use (Buggy Alpha Version)
+
+Creating A Bot
+ASk hassan :)
+ 
---
+++ packages/HJRewardsBot/HJRewardsBot-0.2.8.tar.gz/HJRewardsBot-0.2.8/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.8'
+DESCRIPTION = 'Bing Rewards Bot by Hassan'
+LONG_DESCRIPTION = 'A package that allows to complete bing bot rewards'
+
+# Setting up
+setup(
+    name="HJRewardsBot",
+    version=VERSION,
+    author="Hassan jlilati",
+    author_email="<hassanjlilati10000@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['webdriver_manager', 'selenium', 'requests'],
+    keywords=['python', 'rewards'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
