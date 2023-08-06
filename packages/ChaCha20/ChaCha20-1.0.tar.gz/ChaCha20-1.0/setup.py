8eb6a6097fc7d9967d55e3539686c6fd67a2bb88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:27 +0000

    Add package ChaCha20/1.0/ChaCha20-1.0.tar.gz

---
+++ packages/ChaCha20/ChaCha20-1.0.tar.gz/ChaCha20-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ChaCha20',
+    version='1.0',
+    license='GPL-3.0 License',
+    author="Ellis Harlan",
+    author_email='EllisCHarlan@gmail.com',
+    packages=find_packages('ChaCha20'),
+    package_dir={'': 'ChaCha20'},
+    url='https://github.com/ech2901/ChaCha',
+    keywords='ChaCha Stream Cipher',
+    install_requires=[],
+
+)
