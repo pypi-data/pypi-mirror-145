1706b5adae84e14e9841dcd2795c8f32fdde88ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:48 +0000

    Add package graphdb-module/0.3.4/graphdb-module-0.3.4.tar.gz

---
+++ packages/graphdb-module/graphdb-module-0.3.4.tar.gz/graphdb-module-0.3.4/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="graphdb-module",
+    version="0.3.4",
+    description="Wrapper for graphdb module",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://mnc-repo.mncdigital.com/ai-team/vision_plus/graph_db_module",
+    author="AI Teams",
+    author_email="ferdina.kusumah@mncgroup.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["pydantic==1.9.0", "pandas==1.4.2", "gremlinpython==3.5.2"],
+)
