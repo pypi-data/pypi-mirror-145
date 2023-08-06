2a0378dc6e346fca4cbbcb258e1cdd7a79865a2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:05 +0000

    Add package hikari-crescent/0.1.0b8/hikari-crescent-0.1.0b8.tar.gz

---
+++ packages/hikari-crescent/hikari-crescent-0.1.0b8.tar.gz/hikari-crescent-0.1.0b8/crescent/commands/args.py
@@ -0,0 +1,71 @@
+from abc import ABC
+from typing import Any, Sequence
+
+from attr import define
+from hikari import ChannelType, CommandChoice
+
+__all__: Sequence[str] = ("Description", "Name", "Choices", "ChannelTypes", "MaxValue", "MinValue")
+
+
+class Arg(ABC):
+    @property
+    def payload(self) -> Any:
+        """Returns the data for this object"""
+        ...
+
+    def __hash__(self) -> int:
+        return super().__hash__() ^ hash(self.payload)
+
+
+@define(hash=True)
+class Description(Arg):
+    description: str
+
+    @property
+    def payload(self) -> str:
+        return self.description
+
+
+@define(hash=True)
+class Name(Arg):
+    name: str
+
+    @property
+    def payload(self) -> str:
+        return self.name
+
+
+class Choices(Arg):
+    def __init__(self, *choices: CommandChoice) -> None:
+        self.choices = choices
+
+    @property
+    def payload(self) -> Sequence[CommandChoice]:
+        return self.choices
+
+
+class ChannelTypes(Arg):
+    def __init__(self, *channel_types: ChannelType) -> None:
+        self.channel_types = channel_types
+
+    @property
+    def payload(self) -> Sequence[ChannelType]:
+        return self.channel_types
+
+
+@define(hash=True)
+class MaxValue(Arg):
+    max_value: int
+
+    @property
+    def payload(self) -> int:
+        return self.max_value
+
+
+@define(hash=True)
+class MinValue(Arg):
+    min_value: int
+
+    @property
+    def payload(self) -> int:
+        return self.min_value
