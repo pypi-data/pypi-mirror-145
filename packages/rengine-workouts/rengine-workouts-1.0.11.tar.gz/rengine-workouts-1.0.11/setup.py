1e6264f4692de41af5dfe5147caf8eee8a38a32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:41 +0000

    Add package rengine-workouts/1.0.11/rengine-workouts-1.0.11.tar.gz

---
+++ packages/rengine-workouts/rengine-workouts-1.0.11.tar.gz/rengine-workouts-1.0.11/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+
+# This call to setup() does all the work
+setup(
+    name="rengine-workouts",
+    version="1.0.11",
+    description="Tools to generate workouts",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/noahsolomon0518/rengine",
+    author="noahsolomon0518",
+    author_email="noahsolomon0518@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License"
+    ],
+    install_requires=["pandas", "numpy", "dataframe_image"],
+    packages=["rengine", "rengine.scripts"],
+    include_package_data=True,
+    package_data={"rengine.data":["*"]},
+    entry_points={
+        "console_scripts": [
+            "generate-exercise=rengine.scripts.generate_exercise:run",
+            "generate-plan=rengine.scripts.generate_workout_plan:run",
+        ]
+    }
+)
