3af5c30ee1181e831b52c040d8d79db1da54ad80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:56 +0000

    Add package syncari-sdk/1.1.7/syncari-sdk-1.1.7.tar.gz

---
+++ packages/syncari-sdk/syncari-sdk-1.1.7.tar.gz/syncari-sdk-1.1.7/syncari/synapse/abstract_synapse.py
@@ -0,0 +1,204 @@
+from abc import ABC, abstractmethod
+from asyncio.log import logger
+import json
+from requests import Response
+from syncari.models.request import (DescribeRequest, ErrorResponse, ReadResponse,
+    SyncRequest, WebhookRequest, RequestType, Request)
+from syncari.models.core import Connection, EventData, Record, Result, SynapseInfo
+from syncari.models.schema import Schema
+from syncari.rest.client import SyncariException
+from ..logger import SyncariLogger
+
+# pylint: disable=missing-function-docstring
+class Synapse(ABC):
+    """
+        The abstract synapse class to enforce synapse implementations
+    """
+    raw_request = None
+    connection = None
+    request = None
+    request_type = None
+
+    def __init__(self, raw_request: Request) -> None:
+        self.raw_request = raw_request
+        self.request = Request.parse_raw(self.raw_request)
+        self.connection = self.request.connection
+        self.request_type = self.request.type
+
+    def execute(self):
+        """
+            The route method that looks for the type of the synapse request and invokes
+            appropriate synapse supported method.
+        """
+        self.logger.info(self.raw_request)
+        response = None
+        try:
+            if self.request_type == RequestType.CONNECT:
+                response = self.connect()
+                if not isinstance(response, Connection):
+                    raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.SYNAPSE_INFO:
+                response = self.synapse_info()
+                if not isinstance(response, SynapseInfo):
+                    raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.DESCRIBE:
+                self.request = DescribeRequest.parse_obj(self.request.body)
+                response = self.describe()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, Schema):
+                        raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.READ:
+                self.request = SyncRequest.parse_obj(self.request.body)
+                response = self.read()
+                if not isinstance(response, ReadResponse):
+                    raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.GET:
+                self.request = SyncRequest.parse_obj(self.request.body)
+                response = self.get()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, Record):
+                        raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.CREATE:
+                self.request = SyncRequest.parse_obj(self.request.body)
+                response = self.create()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, Result):
+                        raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.UPDATE:
+                self.request = SyncRequest.parse_obj(self.request.body)
+                response = self.update()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, Result):
+                        raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.DELETE:
+                self.request = SyncRequest.parse_obj(self.request.body)
+                response = self.delete()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, Result):
+                        raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.EXTRACT_WEBHOOK_IDENTIFIER:
+                self.request = WebhookRequest.parse_obj(self.request.body)
+                response = self.extract_webhook_identifier()
+                if not isinstance(response, str):
+                    raise self.__prep_invalid_response_exception(response)
+
+            elif self.request_type == RequestType.PROCESS_WEBHOOK:
+                self.request = WebhookRequest.parse_obj(self.request.body)
+                response = self.process_webhook()
+                if not isinstance(response, list):
+                    raise self.__prep_invalid_response_exception(response)
+                for val in response:
+                    if not isinstance(val, EventData):
+                        raise self.__prep_invalid_response_exception(response)
+
+            else:
+                self.logger.error(self.request)
+                raise Exception('Invalid synapse request {}'.format(self.request_type))
+
+        except SyncariException as e:
+            response = e.error_response
+            
+        except Exception as e:
+            err_msg = 'Failed to execute request {}'.format(self.request_type)
+            err = Response()
+            err.status_code = 400
+            err.reason = str(e)
+            err.raw = str(e)
+            err.url = self.connection.endpoint
+            response = ErrorResponse(message=err_msg, response=err)
+
+        if (isinstance(response, list)):
+            json_vals = []
+            for v in response:
+                json_vals.append(v.json())
+            return json.dumps(json_vals)
+
+        try:
+            json_resp = response.json()
+            return json_resp
+        except:
+            self.logger.warn('Response was not serializable: {}'.format(response))
+            return response
+
+    def __prep_invalid_response_exception(self, response):
+        err_msg = 'Invalid response type for request {}'.format(self.request_type)
+        err = Response()
+        err.status_code = 400
+        err.reason = err_msg
+        err.raw = response
+        err.url = self.connection.endpoint
+        raise SyncariException(error_response=ErrorResponse(message=err_msg, response=err))
+        
+    @property
+    def name(self) -> str:
+        """
+            Synapse name.
+        """
+        return self.__class__.__name__
+
+    def print(self, funcname, request):
+        self.logger.info(funcname)
+        self.logger.info(request)
+        print()
+
+    @property
+    def logger(self):
+        return SyncariLogger.get_logger(f"{self.name}")
+
+    @abstractmethod
+    def synapse_info(self):
+        pass
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def describe(self):
+        pass
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def get(self):
+        pass
+
+    @abstractmethod
+    def create(self):
+        pass
+
+    @abstractmethod
+    def update(self):
+        pass
+
+    @abstractmethod
+    def delete(self):
+        pass
+
+    @abstractmethod
+    def extract_webhook_identifier(self):
+        pass
+
+    @abstractmethod
+    def process_webhook(self):
+        pass
