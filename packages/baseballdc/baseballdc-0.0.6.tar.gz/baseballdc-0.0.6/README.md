e618e239b60ab2a435c5808137da48f099bef173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:28 +0000

    Add package baseballdc/0.0.6/baseballdc-0.0.6.tar.gz

---
+++ packages/baseballdc/baseballdc-0.0.6.tar.gz/baseballdc-0.0.6/README.md
@@ -0,0 +1,13 @@
+# baseballdc
+Baseball Data Center
+
+example request: 
+baseballdc_request_player = {
+	'data_source': 'BASEBALL_REFERENCE',
+	'query_params': {
+        'scope': 'INDIVIDUAL_PLAYER',
+        'table': 'Standard Batting',
+        'first_name': 'Miguel',
+        'last_name': "Cabrera"
+	}
+}
---
+++ packages/baseballdc/baseballdc-0.0.6.tar.gz/baseballdc-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+ 
+setup(
+  name='baseballdc',
+  version='0.0.6',
+  description='Baseball Data Center',
+  author='Joe Smith',
+  author_email='joesmi9.sde@gmail.com',
+  url='https://github.com/joesmi9/baseballdc',
+  license='MIT', 
+  keywords=['baseball', 'baseball analytics', 'baseball reference'],
+  packages=['baseballdc'],
+  install_requires=[''],
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Operating System :: Unix',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+  ]
+)
