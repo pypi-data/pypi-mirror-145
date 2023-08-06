3e9bd290076934714c46e1a4cf485abf1c9e0302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:35 +0000

    Add package lyhapi/0.0.10/lyhapi-0.0.10.tar.gz

---
+++ packages/lyhapi/lyhapi-0.0.10.tar.gz/lyhapi-0.0.10/setup.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+"""
+作者：vissy@zhu
+"""
+from setuptools import setup, find_packages
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
+setup(
+    name="lyhapi",  # Replace with your own username
+    version="0.0.10",
+    author="vissyzhu",
+    author_email="1209354095@qq.com",
+    description="lyhcc information retriver",
+    url="https://github.com/vissyzhu/lyhapi.git",
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
