a0040438c8ae1394ba42cfd217f3c400e238ac86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:21 +0000

    Add package ivanov/0.2.0/ivanov-0.2.0.tar.gz

---
+++ packages/ivanov/ivanov-0.2.0.tar.gz/ivanov-0.2.0/src/ivanov/__init__.py
@@ -0,0 +1,3 @@
+from .core import get_objects_from_spk
+from .sasma import SASMA
+from .sascs import SASCS
---
+++ packages/ivanov/ivanov-0.2.0.tar.gz/ivanov-0.2.0/src/ivanov/globals.py
@@ -0,0 +1,18 @@
+
+# Default Java
+java = '/usr/bin/java'
+
+# SAS globals...
+all_obj_types = ['DeployedFlow', 'DeployedJob', 'ExternalFile', 'Folder',
+                     'Library', 'Role', 'Server', 'StoredProcess', 'Table',
+                     'User', 'Job']
+
+
+# Default platfrom settings
+# SASMA
+sasma_sasobjsp_host = 'vs246.imb.ru'
+sasma_sasobjsp_port = '8591'
+
+# SAS Scroring
+sascs_sasobjsp_host = 'vs2458.imb.ru'
+sascs_sasobjsp_port = '8591'
