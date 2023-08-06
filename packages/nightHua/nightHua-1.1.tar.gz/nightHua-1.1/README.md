8a2067a747de45fbf3a240b6be832835719ec443
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:10 +0000

    Add package nightHua/1.1/nightHua-1.1.tar.gz

---
+++ packages/nightHua/nightHua-1.1.tar.gz/nightHua-1.1/README.md
@@ -0,0 +1,17 @@
+### 自用模块nightHua
+ 
+#### 安装
+ 
+> pip install nightHua
+ 
+#### 使用
+
+```python
+from nightHua import Usual
+from nightHua import Config
+```
+#### 版本更新内容
+>##### 1.1:
+>> 添加了Config类
+>> Usual类中添加了getTimeStr,StrOfSize函数
+
---
+++ packages/nightHua/nightHua-1.1.tar.gz/nightHua-1.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author     : ZhangYafei
+Description: timer
+"""
+
+import setuptools
+
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='nightHua',  # 模块名称
+    version="1.1",  # 当前版本
+    author="nightHua",  # 作者
+    author_email="lirui001002@163.com",  # 作者邮箱
+    description="自用模块",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/zhangyafeii/timer",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    # install_requires=[],
+    python_requires='>=3.6',
+)
