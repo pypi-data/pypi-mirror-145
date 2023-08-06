85dac72dbf7a22afad7b6da69d66f28d73d6767f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:01 +0000

    Add package deepspeed-moee/0.12/deepspeed_moee-0.12.tar.gz

---
+++ packages/deepspeed-moee/deepspeed_moee-0.12.tar.gz/deepspeed_moee-0.12/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.6.2+5be7e16c'
+git_hash='5be7e16c'
+git_branch='master'
+installed_ops={'cpu_adam': False, 'cpu_adagrad': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'async_io': False, 'utils': False, 'quantizer': False, 'transformer_inference': False}
+compatible_ops={'cpu_adam': True, 'cpu_adagrad': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'async_io': False, 'utils': True, 'quantizer': True, 'transformer_inference': True}
+torch_info={'version': '1.8', 'cuda_version': '0.0', 'hip_version': '0.0'}
