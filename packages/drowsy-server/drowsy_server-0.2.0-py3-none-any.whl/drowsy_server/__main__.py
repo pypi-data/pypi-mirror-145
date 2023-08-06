4c05e0a90f0fbd43e61243bf13f0776e5e02b4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:02 +0000

    Add package drowsy-server/0.2.0/drowsy_server-0.2.0-py3-none-any.whl

---
+++ packages/drowsy-server/drowsy_server-0.2.0-py3-none-any.whl/drowsy_server/__main__.py
@@ -0,0 +1,67 @@
+# type: ignore[attr-defined]
+from typing import Optional
+
+from enum import Enum
+from random import choice
+
+import typer
+from rich.console import Console
+
+from drowsy_server import version
+from drowsy_server.example import hello
+
+
+class Color(str, Enum):
+    white = "white"
+    red = "red"
+    cyan = "cyan"
+    magenta = "magenta"
+    yellow = "yellow"
+    green = "green"
+
+
+app = typer.Typer(
+    name="drowsy_server",
+    help="Drowsy Server",
+    add_completion=False,
+)
+console = Console()
+
+
+def version_callback(print_version: bool) -> None:
+    """Print the version of the package."""
+    if print_version:
+        console.print(f"[red]drowsy_server[/] version: [bold blue]{version}[/]")
+        raise typer.Exit()
+
+
+@app.command(name="")
+def main(
+    name: str = typer.Option(..., help="Person to greet."),
+    color: Optional[Color] = typer.Option(
+        None,
+        "-c",
+        "--color",
+        "--colour",
+        case_sensitive=False,
+        help="Color for print. If not specified then choice will be random.",
+    ),
+    print_version: bool = typer.Option(
+        None,
+        "-v",
+        "--version",
+        callback=version_callback,
+        is_eager=True,
+        help="Prints the version of the drowsy_server package.",
+    ),
+) -> None:
+    """Print a greeting with a giving name."""
+    if color is None:
+        color = choice(list(Color))
+
+    greeting: str = hello(name)
+    console.print(f"[bold {color}]{greeting}[/]")
+
+
+if __name__ == "__main__":
+    app()
