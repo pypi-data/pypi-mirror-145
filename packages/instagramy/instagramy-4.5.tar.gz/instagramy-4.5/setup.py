507c60e33415a8011bfd6372eece6338a1fb7e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:23 +0000

    Add package instagramy/4.5/instagramy-4.5.tar.gz

---
+++ packages/instagramy/instagramy-4.5.tar.gz/instagramy-4.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="instagramy",
+    version="4.5",
+    license='MIT',
+    author="Yogeshwaran R",
+    author_email="yogeshin247@gmail.com",
+    description="Python Package for Instagram User, Posts, Hashtags and Locations data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yogeshwaran01/instagramy/",
+    packages=setuptools.find_packages(),
+    download_url="https://github.com/yogeshwaran01/instagramy/archive/master.zip",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
