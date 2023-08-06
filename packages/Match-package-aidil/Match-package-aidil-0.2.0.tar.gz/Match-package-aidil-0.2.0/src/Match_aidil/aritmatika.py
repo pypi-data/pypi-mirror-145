6d736e69e2a25a690db20a84fe7af9d3ea8cf0ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:55 +0000

    Add package Match-package-aidil/0.2.0/Match-package-aidil-0.2.0.tar.gz

---
+++ packages/Match-package-aidil/Match-package-aidil-0.2.0.tar.gz/Match-package-aidil-0.2.0/src/Match_aidil/aritmatika.py
@@ -0,0 +1,25 @@
+def penjumalah(a, b):
+    try
+        return a + b
+    except
+        return None
+
+def pengurangan(a, b):
+    try
+        return a - b
+    except
+        return None
+
+def perkalian(a, b):
+    try
+        return a *
+    except
+        return None
+
+def pembagian(a, b)
+    try
+        return a / b
+    except ValueError:
+        return None
+    except ZeroDivisionError
+        return 0
