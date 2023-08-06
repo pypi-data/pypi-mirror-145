4c7381bac6574f690d333ffde03b133abd02c605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:33 +0000

    Add package scipp/0.13.1/scipp-0.13.1-cp310-cp310-win_amd64.whl

---
+++ packages/scipp/scipp-0.13.1-cp310-cp310-win_amd64.whl/scipp/include/scipp/core/eigen.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+
+#include "scipp/core/dtype.h"
+#include "scipp/core/spatial_transforms.h"
+
+namespace scipp::core {
+
+template <> inline constexpr DType dtype<Eigen::Vector3d>{4000};
+// 4001-4004 defined in spatial_transforms.h
+template <>
+inline constexpr DType dtype<scipp::span<const Eigen::Vector3d>>{4100};
+template <> inline constexpr DType dtype<scipp::span<Eigen::Vector3d>>{4200};
+
+} // namespace scipp::core
