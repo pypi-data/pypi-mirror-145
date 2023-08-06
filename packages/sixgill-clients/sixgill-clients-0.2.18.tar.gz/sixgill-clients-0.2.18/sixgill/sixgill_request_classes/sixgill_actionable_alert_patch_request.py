29c9a9964b70c84b436cc5dacb993217949f0827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:30 +0000

    Add package sixgill-clients/0.2.18/sixgill-clients-0.2.18.tar.gz

---
+++ packages/sixgill-clients/sixgill-clients-0.2.18.tar.gz/sixgill-clients-0.2.18/sixgill/sixgill_request_classes/sixgill_actionable_alert_patch_request.py
@@ -0,0 +1,15 @@
+from sixgill.sixgill_request_classes.sixgill_base_post_auth_request import SixgillBasePostAuthRequest
+
+
+class SixgillActionableAlertPatchRequest(SixgillBasePostAuthRequest):
+    end_point = None
+    method = 'PATCH'
+
+    def __init__(self, channel_id, access_token, actionable_alert_id, organization_id, json_body):
+        super(SixgillActionableAlertPatchRequest, self).__init__(channel_id, access_token)
+
+        self.end_point = 'alerts/actionable_alert/{}'.format(actionable_alert_id)
+        self.request.params['consumer'] = channel_id
+        if organization_id:
+            self.request.params['organization_id'] = organization_id
+        self.request.json = json_body
