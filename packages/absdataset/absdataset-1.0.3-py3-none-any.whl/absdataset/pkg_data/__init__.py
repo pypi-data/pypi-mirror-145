6f145ae8bced23bca2644b1cd075304471e1d27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:37:24 +0000

    Add package absdataset/1.0.3/absdataset-1.0.3-py3-none-any.whl

---
+++ packages/absdataset/absdataset-1.0.3-py3-none-any.whl/absdataset/pkg_data/__init__.py
@@ -0,0 +1,10 @@
+import os as __os
+try:
+    import pandas as __pd
+except:
+    raise SystemExit("No package found")
+
+__here = __os.path.abspath(__os.path.dirname(__file__))
+
+GGPLAY = __pd.read_csv(f"{__here}/googleplaystore.csv")
+HEARTDATA = __pd.read_csv(f"{__here}/heart_2020_cleaned.csv")
