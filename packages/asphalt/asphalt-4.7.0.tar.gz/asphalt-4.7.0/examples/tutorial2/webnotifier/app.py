93dd954e02cc0c428579910e3647adda568e7c44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:05 +0000

    Add package asphalt/4.7.0/asphalt-4.7.0.tar.gz

---
+++ packages/asphalt/asphalt-4.7.0.tar.gz/asphalt-4.7.0/examples/tutorial2/webnotifier/app.py
@@ -0,0 +1,31 @@
+"""This is the root component for the Asphalt webnotifier tutorial."""
+# isort: off
+import logging
+from difflib import HtmlDiff
+
+from async_generator import aclosing
+from asphalt.core import CLIApplicationComponent
+
+from webnotifier.detector import ChangeDetectorComponent
+
+logger = logging.getLogger(__name__)
+
+
+class ApplicationComponent(CLIApplicationComponent):
+    async def start(self, ctx):
+        self.add_component("detector", ChangeDetectorComponent)
+        self.add_component("mailer", backend="smtp")
+        await super().start(ctx)
+
+    async def run(self, ctx):
+        diff = HtmlDiff()
+        async with aclosing(ctx.detector.changed.stream_events()) as stream:
+            async for event in stream:
+                difference = diff.make_file(
+                    event.old_lines, event.new_lines, context=True
+                )
+                await ctx.mailer.create_and_deliver(
+                    subject=f"Change detected in {event.source.url}",
+                    html_body=difference,
+                )
+                logger.info("Sent notification email")
