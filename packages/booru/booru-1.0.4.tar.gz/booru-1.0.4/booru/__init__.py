428721ae80bd782a5a1e351324425516cd9a84b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:48 +0000

    Add package booru/1.0.4/booru-1.0.4.tar.gz

---
+++ packages/booru/booru-1.0.4.tar.gz/booru-1.0.4/booru/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "1.0.4"
+from booru.gelbooru import Gelbooru
+from booru.rule34 import Rule34
+from booru.tbib import Tbib
+from booru.safebooru import Safebooru
+from booru.xbooru import Xbooru
+from booru.structures.realbooru import Realbooru
+from booru.utils import *
