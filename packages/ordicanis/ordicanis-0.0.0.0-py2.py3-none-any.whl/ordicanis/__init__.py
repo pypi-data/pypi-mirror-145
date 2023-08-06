8788c035fdf8d2db9d2d814eb5d795000c2f8261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:31 +0000

    Add package ordicanis/0.0.0.0/ordicanis-0.0.0.0-py2.py3-none-any.whl

---
+++ packages/ordicanis/ordicanis-0.0.0.0-py2.py3-none-any.whl/ordicanis/__init__.py
@@ -0,0 +1,6 @@
+
+'''
+👨‍💻🛠️⌛🏃✨Python, javascript, & ... To easy up, speed coding in the latest & trending frameworks,..🔥
+'''
+__version__ = '0.0.0.0'
+#from markdown_code_exec._markdown_code_exec import parse_file, parse_text
