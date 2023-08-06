8743f0f16122054233d3167698ad679962fcd1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:13 +0000

    Add package plywood-gallery/0.0.2/plywood_gallery-0.0.2-py3-none-any.whl

---
+++ packages/plywood-gallery/plywood_gallery-0.0.2-py3-none-any.whl/plywood_gallery/__init__.py
@@ -0,0 +1,14 @@
+from .plywood_tools import PlywoodGalleryMagic, ChapterManager
+from .quickstart import quickstart
+from .generate_html import generate_html_from_jinja2_and_yaml
+from IPython import get_ipython  # register cell magic
+import pkg_resources
+
+__version__: str = pkg_resources.get_distribution(__name__).version
+
+
+try:
+    ipy = get_ipython()
+    ipy.register_magics(PlywoodGalleryMagic)
+except:
+    pass
