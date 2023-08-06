49cfbdb99146d6da91870be739b32fedce2cbf22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:31 +0000

    Add package httpaste-victorykit/1.0.2a0/httpaste-victorykit-1.0.2a0.tar.gz

---
+++ packages/httpaste-victorykit/httpaste-victorykit-1.0.2a0.tar.gz/httpaste-victorykit-1.0.2a0/src/httpaste/backend/sqlite/paste.py
@@ -0,0 +1,81 @@
+"""SQlite backend paste model interface
+"""
+from os import path
+from sqlite3 import Connection
+from time import time
+
+
+def load(proto: object, connection: Connection, model_class: type):
+    """load a paste
+    """
+
+    cur = connection.cursor()
+
+    cur.execute(
+        'SELECT pid, data, data_hash, sub, expiration, encoding FROM pastes WHERE pid=?',
+        (proto.pid,
+         ))
+
+    result = cur.fetchone()
+
+    if result:
+
+        return model_class(
+            result['pid'],
+            result['sub'],
+            result['data'],
+            result['data_hash'],
+            result['expiration'],
+            result['encoding'])
+
+    return None
+
+
+def dump(model: object, connection: Connection):
+    """dump a paste
+    """
+
+    cur = connection.cursor()
+
+    cur.execute(
+        '''INSERT INTO pastes (pid, data, data_hash, sub, expiration, encoding)
+                   VALUES (?,?,?,?,?,?)''',
+        (model.pid,
+         model.data,
+         model.data_hash,
+         model.sub,
+         model.expiration,
+         model.encoding))
+
+    connection.commit()
+
+
+def delete(proto: object, connection: Connection) -> bool:
+
+    cur = connection.cursor()
+
+    cur.execute('''DELETE FROM pastes WHERE pid=?''', (proto.pid,))
+
+    connection.commit()
+
+
+def init(connection: Connection):
+
+    cur = connection.cursor()
+
+    with open(path.join(path.dirname(__file__), 'paste.sql'), 'r') as fh:
+
+        cur.execute(fh.read())
+
+    connection.commit()
+
+
+def sanitize(connection: Connection, model_class: type) -> bool:
+
+    cur = connection.cursor()
+
+    cur.execute('''SELECT pid FROM pastes WHERE expiration < ? AND expiration > 0''', (int(time()),))
+
+    for row in cur.fetchall():
+
+        delete(model_class(row['pid']))
