6be490d17ac961ef13017350321a028e702286bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:13 +0000

    Add package alpaca-historical-extract/1.2.0/alpaca-historical-extract-1.2.0.tar.gz

---
+++ packages/alpaca-historical-extract/alpaca-historical-extract-1.2.0.tar.gz/alpaca-historical-extract-1.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='alpaca-historical-extract',
+    version='1.2.0',
+    license='MIT',
+    description='README.md',
+    long_description=open('README.md').read(),
+    author="Cole Dylewski",
+    author_email='cole.dylewski@gmail.com',
+    url='https://github.com/Cole-Dylewski/Alpaca-Stock-Algo',
+    keywords='Alpaca Stock Extraction',
+    include_package_data=False,
+    packages=['alpaca-historical-extract'],
+    package_dir={'alpaca-historical-extract':'src/alpaca-historical-extract'},
+    install_requires=[
+        'pytz',
+        'tzdata',
+        'pandas-market-calendars',
+        'pandas',
+        'requests',
+        'numpy',
+        'alpaca-trade-api',
+        'yahoo-fin',
+        'yfinance',
+      ],
+
+)
