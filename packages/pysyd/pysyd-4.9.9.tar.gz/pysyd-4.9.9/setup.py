a93d8fa76b568e9c854eccacd9125812832c80d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:02 +0000

    Add package pysyd/4.9.9/pysyd-4.9.9.tar.gz

---
+++ packages/pysyd/pysyd-4.9.9.tar.gz/pysyd-4.9.9/setup.py
@@ -0,0 +1,41 @@
+import re
+import setuptools
+
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project+'/__init__.py').read())
+    return result.group(1)
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+reqs = []
+for line in open('requirements.txt', 'r').readlines():
+    reqs.append(line)
+
+setuptools.setup(
+    name="pysyd",
+    version=get_property('__version__', 'pysyd'),
+    license="MIT",
+    author="Ashley Chontos",
+    author_email="achontos@hawaii.edu",
+    description="Automated measurements of global asteroseismic parameters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ashleychontos/pysyd",
+    project_urls={
+        "Documentation": "https://pysyd.readthedocs.io",
+        "Source": "https://github.com/ashleychontos/pySYD",
+        "Bug Tracker": "https://github.com/ashleychontos/pySYD/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=reqs,
+    packages=setuptools.find_packages(),
+    package_data={"": ["dicts/*.dict"]},
+    entry_points={'console_scripts':['pysyd=pysyd.cli:main']},
+    python_requires=">=3.6",
+)
