cf2d976b1d5c8a95fd6a30553796aa0d3aa247a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:44 +0000

    Add package lsx/0.1.1/lsx-0.1.1.tar.gz

---
+++ packages/lsx/lsx-0.1.1.tar.gz/lsx-0.1.1/lsx/__init__.py
@@ -0,0 +1,30 @@
+_help = help
+_print = print
+
+__keep__ = list(globals()) # Don't use word `__all__`, its will rewrite many times below.
+
+# base on built-in library
+from .cls import *
+from .line import *
+from .refact import *
+from .useful import *
+from .windll import *
+from .decorator import *
+
+# 3rd-library needed
+from .np import *
+from .doc import *
+from .gui import *
+from .auto import *
+
+
+def help():
+    for k in __all__:
+        if not k.startswith('_'):
+            _help(k)
+
+
+# __all__ = [k for k in globals() if k not in __keep__]
+__all__ = [k for k, v in globals().items() if k not in __keep__ and callable(v)]
+
+_print('functions:', len(__all__))
