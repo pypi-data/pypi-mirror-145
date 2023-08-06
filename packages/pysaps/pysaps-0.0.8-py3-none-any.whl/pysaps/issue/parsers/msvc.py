ec2969387bf27117ea59b94512068b170212e885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:00 +0000

    Add package pysaps/0.0.8/pysaps-0.0.8-py3-none-any.whl

---
+++ packages/pysaps/pysaps-0.0.8-py3-none-any.whl/pysaps/issue/parsers/msvc.py
@@ -0,0 +1,25 @@
+import re
+from typing import List
+
+from pysaps.issue import Issue
+from pysaps.issue.parsers import IssueParser
+from pysaps.issue.severity import Severity
+
+
+class MSVC(IssueParser):
+    pattern = "^(?P<filename>.*)\((?P<line_start>\d+)\)\s*:\swarning\s(?P<category>C\d+):\s*(?P<message>.*)$"
+
+    @classmethod
+    def parse(cls, content: str) -> List[Issue]:
+        issues = []
+        for line in content.splitlines():
+            try:
+                match: dict = next(re.finditer(cls.pattern, line)).groupdict()
+            except StopIteration:
+                continue
+            match['line_start'] = int(match['line_start'])
+            match['severity'] = Severity.WARNING_NORMAL
+            # match['column_start'] = 0
+            issues.append(Issue(**match))
+
+        return list(dict.fromkeys(issues))
