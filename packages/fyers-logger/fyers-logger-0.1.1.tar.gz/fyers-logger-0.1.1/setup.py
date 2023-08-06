b5e266ffe243f836ebea58532b8ac44925bf842e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:42 +0000

    Add package fyers-logger/0.1.1/fyers-logger-0.1.1.tar.gz

---
+++ packages/fyers-logger/fyers-logger-0.1.1.tar.gz/fyers-logger-0.1.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='fyers-logger',  
+     version='0.1.1',
+     author="Fyers-Tech",
+     author_email="support@fyers.in",
+     description="Fyers Internal Logging Library",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/FyersDev/fyers-logger",
+     packages=setuptools.find_packages(),
+     install_requires=[
+                'aws-lambda-powertools==1.25.5',
+          ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
