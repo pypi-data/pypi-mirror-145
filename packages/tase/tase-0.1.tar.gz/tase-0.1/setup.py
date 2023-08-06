bb9985507e5ef0eb6bc4042ea4ba942dcacd9408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:00 +0000

    Add package tase/0.1/tase-0.1.tar.gz

---
+++ packages/tase/tase-0.1.tar.gz/tase-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tase',
+    version='0.1',
+    license='MIT',
+    author=["Soran Ghaderi", "Taleb Zarhesh"],
+    author_email='soran.gdr.cs@gmail.com',
+    packages=find_packages('tase'),
+    package_dir={'': 'tase'},
+    url='https://github.com/soran/tase',
+    keywords='dump',
+    install_requires=[
+      ],
+
+)
