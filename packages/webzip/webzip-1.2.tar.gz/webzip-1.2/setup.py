d48ebaf89e663c48e3ac5e2d512ab95847e0b074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:35:45 +0000

    Add package webzip/1.2/webzip-1.2.tar.gz

---
+++ packages/webzip/webzip-1.2.tar.gz/webzip-1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webzip',
+    version='1.2',
+    description='Install python packages from any source on the web',
+    author='Maximilian Lange',
+    author_email='maxhlange@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'Click',
+        "requests",
+        "python-slugify",
+    ],
+    entry_points={
+        'console_scripts': [
+            'webzip = webzip.installer:action',
+        ],
+    },
+)
