3217de9b6820806f19dc9e78ecf066eb055998ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:25 +0000

    Add package ananimlib/0.0.1.1/ananimlib-0.0.1.1-py3-none-any.whl

---
+++ packages/ananimlib/ananimlib-0.0.1.1-py3-none-any.whl/ananimlib/ananim.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 24 21:42:30 2019
+
+@author: gtruch
+"""
+import ananimlib as al
+import numpy as np
+
+class AnEngine():
+    """The central animation engine.
+
+    Contains and the central animation loop and maintains references to the 
+    *Scene* (the set of AnObjects currently being 
+    animated), the *Camera* (generates image frames the *Scene*),  and a 
+    *Backend* (stores a sequence of frames from the camera)
+    
+    """
+
+    def __init__(self):
+
+        # Write down some critical parameters
+        self.width       = al._defaults['width']
+        self.ar          = al._defaults['ar']
+        self.frame_rate  = al._defaults['frame_rate']
+        self.DPI         = al._defaults['DPI']
+
+        self._render=True
+
+        # reset_scene rebuilds the camra, the backend, and the scene
+        self.reset_scene()
+
+        # Render frames by default (Rather than just executing the instructions)
+        self.render=True
+
+    def run(self, *instructions):
+        """Execute the instructions and render the results
+
+        This is where the main animation loop lives
+
+        Parameters
+        ----------
+        *instructions :tuple of Instruction
+            The set of instructions to run. 
+            
+        """
+        instruction_tree = al.RunSequential(*instructions)
+        frame_rate = self.scene.camera.frame_rate
+        pw,ph = self.scene.camera.pixelsPerFrame
+
+
+        dt  = 1.0/frame_rate
+        self.backend.start()               # Tell the backend to get ready
+        instruction_tree.start(self.scene)  # Tell the instruction tree to get 
+                                           # ready
+
+        # Perform a dt=0 update of the scene to execute leading
+        # instructions that use no animation time prior to the first render
+        instruction_tree.update(self.scene,0.0)
+
+        ###########################
+        # The main animation loop #
+        ###########################
+        while not instruction_tree.finished:
+
+            if self.render:
+                # Render the scene
+                self.scene.render()
+
+                # Feed the rendered frame into the backend
+                self.backend.addFrame(self.scene.frame)
+
+            # Update the scene for the next frame
+            instruction_tree.update(self.scene,dt)
+
+        # Tell the Backend that no more frames are coming
+        if self.render:
+            self.backend.end()
+        return self
+            
+    @property
+    def render(self):
+        """Set to True to render frames"""
+        return self._render
+
+    @render.setter
+    def render(self,value):
+        """True when rendering frames
+
+        Setting render to False causes the animation sequencer to rapidly
+        execute instructions and update the scene without rendering frames.
+
+        Useful for testing and debugging long animation sequences.
+        """
+        if value:
+            self._render = True
+            self.config_camera(self.width,self.ar,self.frame_rate,self.DPI)
+        else:
+            self._render = False
+            self.config_camera(self.width,self.ar,1,self.DPI)
+
+    def config_camera(self,width, ar, frame_rate, DPI):
+        """Reconfigure the video settings.
+
+        Parameters
+        ----------
+        width : float
+            The width of the camera frame in Scene Units
+
+        ar  : float
+            The aspect ratio of a frame width/height
+
+        frame_rate : int
+            The frame rate in frames per second
+
+        DPI : int
+            Dots per Inch. But really, Pixels per Scene Unit
+        """
+
+        if self.render:
+            self.width      = width
+            self.ar         = ar
+            self.frame_rate = frame_rate
+            self.DPI        = DPI
+
+        # Calculate height based on width and aspect ratio
+        height = width/ar
+
+        # Set up the camera
+        pixel_height = DPI*height
+        pixel_width  = DPI*width
+        frame_height = height
+        frame_width  = width
+        frame_rate   = frame_rate
+
+        # MP4 backend requires an even number of pixels
+        if pixel_height%2 > 0:
+            pixel_height+=1
+        if pixel_width%2 > 0:
+           pixel_width+=1
+
+        self.scene.camera = al.Camera(pixel_width,pixel_height,
+                                       frame_height,frame_rate,
+                                       frame_width)
+
+        # We also need to change the backend
+        if self.render:
+            self.backend.frameSize  = np.array([pixel_width,pixel_height],
+                                               dtype=int)
+            self.backend.frame_rate = frame_rate
+
+    def reset_scene(self):
+        self.scene = al.Scene(None)
+        self._config_backend(self.width*self.DPI, self.width/self.ar*self.DPI,
+                            self.frame_rate)
+        self.config_camera(self.width,self.ar,self.frame_rate,self.DPI)
+
+    def play_movie(self, repeat=-1):
+        """Have the current backend play the movie"""
+        self.backend.play_movie(repeat)
+
+    def _config_backend(self,pw,ph,frame_rate):
+        """Set up the backend so that it can receive frames
+
+        Parameters
+        ----------
+        pw, ph : int
+            Dimensions of the frames in pixels
+
+        frame_rate : float
+            The frame rate in frames per second
+        """
+        self.backend = al.Backend(pw,ph,frame_rate)
---
+++ packages/ananimlib/ananimlib-0.0.1.1-py3-none-any.whl/ananimlib/common_anobs.py
@@ -0,0 +1,522 @@
+# -*- coding: utf-8 -*-
+"""
+A small collection of basic AnObjects
+
+Created on Wed Nov 20 15:23:10 2019
+@author: gtruch
+
+"""
+
+import ananimlib as al
+
+import copy as cp
+import numpy as np
+
+
+class Circle(al.BezierAnObject):
+    """A Circle
+
+    Parameters
+    ----------
+    radius : float
+        The radius of hte circle
+
+    coordinates : Coordinates, optional
+        An instance of a Coordinates object containing the initial position
+        and orientation the circle.
+
+    pen : Pen
+        An instance of a Pen object with rendering instructions
+
+    """
+
+    def __init__(self,radius,e=0.0,pen=None,coordinates=None):
+        """Create the circle"""
+
+        super().__init__(pen=pen)
+        self.e=e
+        self.radius=radius
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self,radius):
+        self._radius = radius
+        self._build_circle()
+
+    def _build_circle(self):
+        angle = np.arange(-45,360+90,45)*np.pi/180
+
+        a = self.radius
+        b = a*np.sqrt(1-self.e**2)
+        path = al.PolyBezier()
+
+        path.connect_smooth(np.array([
+            a*np.cos(angle),
+            b*np.sin(angle)]
+        ).transpose())
+
+        path._data = path._data[1:-1,:,:]
+
+        self.data = path
+
+
+class CrossHair(al.BezierAnObject):
+    """A crosshair"""
+
+    def __init__(self,size=0.25,pen=None,coordinates=None):
+
+        if pen is None:
+            pen = al.Pen(stroke_width=0.75)
+
+        try:
+            iter(size)
+        except:
+            size = [size,size]
+
+
+        # Initialize the path
+        path = al.PolyBezier()
+        path.connect_linear([al.Vector(-size[0]/2,0),al.Vector(size[0]/2,0)])
+        path.connect_linear([al.Vector(0,-size[1]/2),al.Vector(0,size[1]/2)])
+
+
+        super().__init__(path, pen)
+
+
+
+class Arc(Circle):
+    """A circular arc
+
+    Parameters
+    ----------
+    radius : float
+        Radius of curvature
+
+    start_angle : float
+        The angle in radians wrt the x-axis where the arc starts
+
+    stop_angle : float
+        The angle in radians wrt the x-axis where the arc stops
+    """
+
+    def __init__(self,radius,start_angle, stop_angle, e=0.0, pen=None):
+        super().__init__(radius,e=e,pen=pen)
+        
+        self._start_angle=start_angle
+        self._stop_angle=stop_angle
+        
+        # Save the original circle
+        self.circle = self.data
+        
+        self._build()
+        
+
+    @property
+    def start_angle(self):
+        return self._start_angle
+    
+    @property
+    def stop_angle(self):
+        return self._stop_angle
+    
+    @start_angle.setter
+    def start_angle(self,val):
+        self._start_angle = val
+        self._build()
+        
+    @stop_angle.setter
+    def stop_angle(self,val):
+        self._stop_angle = val
+        self._build()
+
+    def _build(self):
+
+        _,new = self.circle.split(self.start_angle/(2*np.pi))
+
+        new,_ = new.split((self.stop_angle-
+                           self.start_angle)/(2*np.pi-self.start_angle))
+
+        self.data = new
+
+class Arrow(al.CompositeAnObject):
+    """A basic arrow consisting of a line with a pointy tip.
+
+    Provides several positioning helpers.
+
+    Attributes
+    ----------
+    start, end : Vector
+        The start and end points of the arrow.
+        Start is the tail, end is the head.
+
+
+    pen, head_pen : optional Pen
+        The pen used to draw the arrow
+    """
+
+    def __init__(self,tail_pos,head_pos,head_size=1.0,
+                 pen=None,
+                 head_pen=None):
+
+        super().__init__()
+
+        tpos = al.Vector(tail_pos)
+        hpos = al.Vector(head_pos)
+        self.head_size=head_size
+
+
+        # Find magnitude and angle of the arrow
+        self._magnitude     = (hpos-tpos).r
+        self.rotation_angle = (hpos-tpos).theta
+
+        if pen is None:
+            pen = al.Pen()
+
+        if head_pen is None:
+            head_pen=al.Pen(stroke_opacity=0.0,
+                               stroke_width=pen.stroke_width,
+                               fill_color=pen.stroke_color,
+                               fill_opacity=pen.stroke_opacity)
+
+        self.head_pen = head_pen
+        self.pen = pen
+
+        # Create the arrow components
+        head = ArrowHead(pen=head_pen,size=head_size)
+        tail = al.BezierAnObject(pen=pen)
+
+        self.add_anobject(tail,'tail')
+        self.add_anobject(head,'head')
+
+        self._build_arrow()
+        self.about_point = [0.0,0.0]
+        self.position = tpos
+
+    def about_tail(self):
+        """Position changes and rotations are about the tail of the arrow."""
+        self.about_point = [0,0]
+
+    def about_head(self):
+        """Position changes and rotations are about the head of the arrow."""
+        self.about_point = [self.magnitude,0]
+
+    @property
+    def head_pos(self):
+        """Position of the head of the arrow in Scene Cooordinates."""
+        return self.internal2external(al.Vector(self.magnitude,0))
+
+    @property
+    def tail_pos(self):
+        """Position of the tail of the arrow in Scene Cooordinates."""
+        return self.internal2external(al.Vector(0,0))
+
+    @property
+    def magnitude(self):
+        return self._magnitude
+
+    @magnitude.setter
+    def magnitude(self,mag):
+        self._magnitude = mag
+        self._build_arrow()
+
+
+    @head_pos.setter
+    def head_pos(self,head_pos):
+        """Move the head of the arrow leaving the tail in place.
+
+        Parameters
+        ----------
+        pos : iterable of floats
+            The Scene Space cartesian coordinates of the end of the arrow
+        """
+        head_pos = al.Vector(head_pos)
+        diff = head_pos-self.tail_pos
+        self.magnitude      = diff.r
+        self.rotation_angle = diff.theta
+        self._build_arrow()
+        self.about_head()
+        self.position       = head_pos
+
+    @tail_pos.setter
+    def tail_pos(self,tail_pos):
+        """Move the tail of the arrow leaving the head in place"""
+
+        tail_pos = al.Vector(tail_pos)
+        head_pos = cp.copy(self.head_pos)
+
+        diff = head_pos - tail_pos
+
+        self.magnitude     = diff.r
+        self.rotation_angle = diff.theta
+        self._build_arrow()
+        self.about_head()
+        self.position = head_pos
+
+    @property
+    def arrow_width(self):
+        return self.get_anobject('tail').stroke_width
+
+    @arrow_width.setter
+    def arrow_width(self,width):
+        self.get_anobject('tail').stroke_width=width
+
+
+    @property
+    def opacity(self):
+        return self.get_anobject('tail').stroke_opacity
+
+    @opacity.setter
+    def opacity(self,value):
+        self.get_anobject("tail").stroke_opacity=value
+        self.get_anobject("head").fill_opacity=value
+
+    @property
+    def color(self):
+        return self.get_anobject("tail").stroke_color
+
+    @color.setter
+    def color(self,value):
+        self.get_anobject("tail").stroke_color=value
+        self.get_anobject("head").fill_color=value
+
+
+    def _build_arrow(self):
+
+        head = self.get_anobject('head')
+        tail = self.get_anobject('tail')
+
+        # Lay the arrow natively on the x-axis
+        # with its tail at the origin
+        tail.data = al.PolyBezier()
+        tail.data.connect_linear(
+                    [[                         0, 0],
+                     [self.magnitude-head.length, 0]])
+
+        # Put the head at the front end of the tail
+        head.about_right()
+        head.position      = [self.magnitude,0]
+
+class ArrowHead(al.BezierAnObject):
+
+    def __init__(self,length=0.2,width=0.15,back=0.05,size=1.0,pen=None):
+        super().__init__(pen=pen)
+
+        self.size=size
+        self.o_length =length
+        self.o_width  = width
+        self.o_back   = back
+
+        self._build_head()
+
+
+    def set_head_size(self,size):
+        self.size = size
+        self._build_head()
+
+    def _build_head(self):
+        self.length = self.o_length*self.stroke_width*self.size
+        self.width  = self.o_width*self.stroke_width*self.size
+        self.back   = self.o_back*self.stroke_width*self.size
+
+        tl,tw,tb = np.array((self.length,self.width,self.back))
+
+        # Build the arrowhead
+        path = al.PolyBezier()
+        path.connect_smooth([[-tb,-tw/2], [0, 0],[-tb, tw/2]])
+        path.connect_linear([[-tb, tw/2,],[tl,0],[-tb,-tw/2]])
+
+        self.data = path
+
+class Dot(Circle):
+    """A solid dot (filled in circle)
+    
+    Parameters
+    ----------
+    radius : optional float
+        The radius of the dot.
+        default = 0.1
+        
+    pen : optional Pen
+        The pen used to draw the dot
+        default = A pleasing (to me) color
+    """
+    
+    def __init__(self,radius=0.1,pen=None):
+        if pen is None:
+            pen = al.Pen(fill_color="#cda448", fill_opacity=1.0)
+            super().__init__(radius,pen=pen)
+
+class DoubleArrow(Arrow):
+
+    def _build_arrow(self):
+
+        if not 'head2' in self.data.keys:
+            head2 = ArrowHead(pen=self.head_pen,size=self.head_size)
+            self.add_anobject(head2,'head2')
+        else:
+            head2 = self.get_anobject('head2')
+
+        head = self.get_anobject('head')
+        tail = self.get_anobject('tail')
+
+        # Lay the arrow natively on the x-axis
+        # with its tail at the origin
+        tail.data = al.PolyBezier()
+        tail.data.connect_linear(
+                    [[              head2.length, 0],
+                     [self.magnitude-head.length, 0]])
+
+        # Put the head at the front end of the tail
+        head.about_right()
+        head.position      = [self.magnitude,0]
+
+        head2.about_right()
+        head2.rotation_angle = np.pi
+        head2.position=[0,0]
+
+class Grid(al.BezierAnObject):
+    """A grid of lines.
+
+    Attributes
+    ----------
+    size : array of float
+        The size of the grid: [sx, sy]
+
+    spacing : array of float
+        The x and y grid spacing
+
+    """
+
+    def __init__(self,size,spacing,offset=None,pen=None):
+
+        if len(size) == 1:
+            size = al.Vector([size,size])
+        else: 
+            size = al.Vector(size)
+            
+        if offset is None:
+            offset = al.Vector([0,0])
+        else:
+            offset = al.Vector(offset)
+
+        path = al.PolyBezier()
+        
+        start = -np.floor((size/2+offset)/spacing)*spacing + offset
+        end = start + size
+
+        # Horizontal lines
+        for ypos in np.arange (start[1],end[1],spacing[1]):
+            path.connect_linear([[-size[0]/2, ypos],
+                                 [ size[0]/2, ypos]])
+            
+        # Vertical lines
+        for xpos in np.arange(start[0],end[0],spacing[0]):
+            path.connect_linear([[xpos, -size[1]/2],
+                                 [xpos, size[1]/2]])
+
+
+        super().__init__(path,pen)
+
+class Line(al.BezierAnObject):
+    """A straight line.
+
+    Parameters
+    ----------
+    point 1 : Vector
+        The coordinates of the first end point
+
+    point 2 : Vector
+        The coordinates of the second end point
+    """
+    def __init__(self,point1,point2,pen=None):
+        super().__init__(pen=pen)
+
+        self._point1 = al.Vector(point1)
+        self._point2 = al.Vector(point2)
+
+        self._build()
+        
+
+
+    @property
+    def point1(self):
+        return self._point1
+
+    @point1.setter
+    def point1(self, newpoint):
+        self._point1 = al.Vector(newpoint)
+        self._build()
+
+    @property
+    def point2(self):
+        return self._point2
+
+    @point2.setter
+    def point2(self, newpoint):
+        self._point2 = al.Vector(newpoint)
+        self._build()
+
+    def _build(self):
+        path = al.PolyBezier()
+        path.connect_linear([self._point1,self._point2])
+
+        self.data=path
+
+
+
+class Rectangle(al.BezierAnObject):
+    """A rectangle."""
+
+    def __init__(self,size, pen=None, coordinates=None):
+        """Initialize the rectangle
+
+        Parameters
+        ----------
+        size : iterable of floats
+            The size of the rectangle as [dx,dy,dz]
+
+        position : optional iterable of floats
+            The position of the center of the rectangle
+            default: [0.0,0.0]
+        """
+
+        # Initialize the path
+        self.data = al.PolyBezier()
+        self.size = al.Vector(size)
+
+        super().__init__(self.data, pen)
+
+        if coordinates is None:
+            self.about_center()
+            self.position = [0.0,0.0]
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self,value):
+        self._size = al.Vector(value)
+        self._build_rectangle()
+
+    def  _build_rectangle(self):
+        
+        # Center the rectangle on [0,0]
+        ll = self.size*[-0.5,-0.5,0.0]
+        ul = self.size*[-0.5,0.5,0.0]
+        ur = self.size*[0.5,0.5,0.0]
+        lr = self.size*[0.5,-0.5,0.0]
+        
+        self.data = al.PolyBezier()
+        self.data.connect_linear([ll,ul,ur,lr,ll])
+            # [ml.Vector(          0, 0     ),      # ll
+            #                      ml.Vector(           0, self._size.y), # ul
+            #                      ml.Vector(self._size.x, self._size.y), # ur
+            #                      ml.Vector(self._size.x, 0     )],      # lr
+            #                      close=True)
+
+
+
