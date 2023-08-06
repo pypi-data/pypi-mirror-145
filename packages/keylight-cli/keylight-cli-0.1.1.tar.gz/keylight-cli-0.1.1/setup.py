31ac559d5155db939eaf24807de631ab3fb1dd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:41 +0000

    Add package keylight-cli/0.1.1/keylight-cli-0.1.1.tar.gz

---
+++ packages/keylight-cli/keylight-cli-0.1.1.tar.gz/keylight-cli-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['keylight_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.2,<9.0.0', 'leglight>=0.2.0,<0.3.0']
+
+entry_points = \
+{'console_scripts': ['keylight = keylight_cli.keylight_cli:start']}
+
+setup_kwargs = {
+    'name': 'keylight-cli',
+    'version': '0.1.1',
+    'description': 'A Python based CLI to control Elgato Keylights',
+    'long_description': None,
+    'author': 'Ben Lyall',
+    'author_email': 'ben@lyall.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/benlyall/keylight-cli',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
