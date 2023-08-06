e13248cc1db40a80a6c7f196c9ec5e86292315d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:28 +0000

    Add package coredis/3.2.0/coredis-3.2.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/coredis/coredis-3.2.0-cp38-cp38-macosx_10_9_x86_64.whl/coredis/response/callbacks/connection.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from coredis.response.callbacks import ResponseCallback
+from coredis.typing import Any, AnyStr, Dict, List, Set, Union
+from coredis.utils import EncodingInsensitiveDict, flat_pairs_to_dict
+
+
+class ClientTrackingInfoCallback(ResponseCallback):
+    def transform_3(
+        self, response: Any, **options: Any
+    ) -> Dict[AnyStr, Union[AnyStr, Set[AnyStr], List[AnyStr]]]:
+        return response
+
+    def transform(
+        self, response: Any, **options: Any
+    ) -> Dict[AnyStr, Union[AnyStr, Set[AnyStr], List[AnyStr]]]:
+        response = EncodingInsensitiveDict(flat_pairs_to_dict(response))
+        response["flags"] = set(response["flags"])
+        return dict(response)
