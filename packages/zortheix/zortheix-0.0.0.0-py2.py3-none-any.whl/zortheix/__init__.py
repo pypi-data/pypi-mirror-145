d7ab28797d7d4b6b787eb6e31ea78f9c562fc9ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:28 +0000

    Add package zortheix/0.0.0.0/zortheix-0.0.0.0-py2.py3-none-any.whl

---
+++ packages/zortheix/zortheix-0.0.0.0-py2.py3-none-any.whl/zortheix/__init__.py
@@ -0,0 +1,6 @@
+
+'''
+👨‍💻🛠️⌛📦Zortheix, when diverse cross technologies meet to offer the best !🔥...🏃
+'''
+__version__ = '0.0.0.0'
+#from markdown_code_exec._markdown_code_exec import parse_file, parse_text
