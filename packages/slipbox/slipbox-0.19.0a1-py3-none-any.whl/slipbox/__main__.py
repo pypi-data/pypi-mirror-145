1e59c19f8ddbffd93fad5acf0cabe5789981aa5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:55 +0000

    Add package slipbox/0.19.0a1/slipbox-0.19.0a1-py3-none-any.whl

---
+++ packages/slipbox/slipbox-0.19.0a1-py3-none-any.whl/slipbox/__main__.py
@@ -0,0 +1,48 @@
+"""Static site generator for Zettelkasten notes."""
+
+import sys
+import typing as t
+
+from . import commands
+from .app import error, RootlessApp, startup
+from .build import build
+from .dependencies import check_requirements
+from .cli import parse_args
+from .tools.new import new_note
+
+
+Command = t.Callable[[RootlessApp], None]
+
+
+handlers: t.Dict[str, Command] = {
+    "build": build,
+    "check": commands.check_notes,
+    "formats": commands.list_supported_formats,
+    "info": commands.show_info,
+    "init": commands.init,
+    "new": new_note,
+}
+
+
+def main() -> None:
+    """Entrypoint."""
+    app = startup(parse_args())
+    if not check_requirements(app):
+        error("pandoc not found")
+
+    if app.args.get("version"):
+        print("""slipbox 0.18.0
+Copyright (C) 2020-2022 Levi Gruspe
+https://github.com/lggruspe/slipbox""")
+        sys.exit()
+
+    command = app.args["(command)"]
+    if command is not None:
+        handler = handlers[command]
+        if handler is not None:
+            handler(app)
+    app.cleanup()
+
+
+if __name__ == "__main__":
+    main()
