817c44f16fa1b8fb3e4ba6a2e06cfc7f455eba1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:43 +0000

    Add package streamlit-globe/0.2.1/streamlit_globe-0.2.1-py3-none-any.whl

---
+++ packages/streamlit-globe/streamlit_globe-0.2.1-py3-none-any.whl/streamlit_globe/frontend/build/precache-manifest.a7428282e9436bfe7c980b13fc84917a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058f9cf2c2ed92c5fcaec0c33204fec4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac73608e1915f8b9ffc3",
+    "url": "./static/js/2.70ab07e1.chunk.js"
+  },
+  {
+    "revision": "164933573ecce9c5fa8212c1a75cc591",
+    "url": "./static/js/2.70ab07e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4346a0653029d3b05d",
+    "url": "./static/js/main.b270d566.chunk.js"
+  },
+  {
+    "revision": "32e0ff1e41637f764f70",
+    "url": "./static/js/runtime-main.b2da421e.js"
+  },
+  {
+    "revision": "8ec81bb437b68fdb4bd46604ff698736",
+    "url": "./static/media/earth-day2.8ec81bb4.jpg"
+  },
+  {
+    "revision": "5350543dfb8e809a3bcc0724df2dc443",
+    "url": "./static/media/earth-night.5350543d.jpg"
+  },
+  {
+    "revision": "1701d6355eb0713db98c2734e162c169",
+    "url": "./static/media/night-sky.1701d635.png"
+  }
+]);
