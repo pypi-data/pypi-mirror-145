4ad97a92708c6abb73af559522c1ada700df8b1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:28 +0000

    Add package opensqli/0.1.2/opensqli-0.1.2.tar.gz

---
+++ packages/opensqli/opensqli-0.1.2.tar.gz/opensqli-0.1.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['opensqli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'aiodns>=3.0.0,<4.0.0',
+ 'aiohttp>=3.8.1,<4.0.0',
+ 'aiolimiter>=1.0.0,<2.0.0',
+ 'cchardet>=2.1.7,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['opensqli = opensqli.cli:main']}
+
+setup_kwargs = {
+    'name': 'opensqli',
+    'version': '0.1.2',
+    'description': 'OpenAPI SQLi Scanner',
+    'long_description': None,
+    'author': 'Sergey M',
+    'author_email': 'tz4678@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tz4678/openapi-sqli-scanner',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
