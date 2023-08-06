bc49d4e4d1979cba151f67b349243fe5bd7131b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:19 +0000

    Add package categorize-cli/0.5.0/Categorize-CLI-0.5.0.tar.gz

---
+++ packages/categorize-cli/Categorize-CLI-0.5.0.tar.gz/Categorize-CLI-0.5.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+
+    name='Categorize-CLI',
+    version='0.5.0',
+    description='Categorize-CLI is a command-line-tool made using python to organize files in a given directory',
+    url="https://github.com/Rohith-JN/Categorize-CLI",
+    author="Rohith Nambiar",
+    packages=find_packages(exclude="tests"),
+    include_package_data=True,
+    author_email='rohithnambiar04@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        "License :: OSI Approved :: MIT License",
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: Microsoft :: Windows'
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/Rohith-JN/Categorize-CLI/issues"
+    },
+    install_requires=['Click', 'colorama', 'progress'],
+
+    entry_points=""" 
+        [console_scripts] 
+        Categorize=src.Categorize_CLI.main:main
+        """
+)
