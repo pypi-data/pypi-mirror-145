2f448914dd99f446e8f218c0aabd3a4850998f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:17 +0000

    Add package viadot/0.4.2/viadot-0.4.2.tar.gz

---
+++ packages/viadot/viadot-0.4.2.tar.gz/viadot-0.4.2/viadot/tasks/__init__.py
@@ -0,0 +1,39 @@
+from .azure_blob_storage import BlobFromCSV
+from .azure_data_lake import (
+    AzureDataLakeCopy,
+    AzureDataLakeDownload,
+    AzureDataLakeList,
+    AzureDataLakeToDF,
+    AzureDataLakeUpload,
+)
+from .azure_key_vault import (
+    AzureKeyVaultSecret,
+    CreateAzureKeyVaultSecret,
+    DeleteAzureKeyVaultSecret,
+)
+from .azure_sql import (
+    AzureSQLBulkInsert,
+    AzureSQLCreateTable,
+    AzureSQLDBQuery,
+    CreateTableFromBlob,
+    AzureSQLToDF,
+    CheckColumnOrder,
+    AzureSQLUpsert,
+)
+from .bcp import BCPTask
+from .github import DownloadGitHubFile
+from .great_expectations import RunGreatExpectationsValidation
+from .sqlite import SQLiteInsert, SQLiteSQLtoDF, SQLiteQuery
+from .supermetrics import SupermetricsToCSV, SupermetricsToDF
+from .sharepoint import SharepointToDF
+from .cloud_for_customers import C4CReportToDF, C4CToDF
+from .prefect_date_range import GetFlowNewDateRange
+from .aselite import ASELiteToDF
+
+try:
+    from .sap_rfc import SAPRFCToDF
+except ImportError:
+    pass
+
+from .duckdb import DuckDBCreateTableFromParquet, DuckDBQuery, DuckDBToDF
+from .sql_server import SQLServerCreateTable
