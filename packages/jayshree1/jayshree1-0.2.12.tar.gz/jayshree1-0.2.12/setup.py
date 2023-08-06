57ecc1da880ec5dede1be453ea9f4a95c2889ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:13:53 +0000

    Add package jayshree1/0.2.12/jayshree1-0.2.12.tar.gz

---
+++ packages/jayshree1/jayshree1-0.2.12.tar.gz/jayshree1-0.2.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='jayshree1',
+    version='0.2.12',
+    description = 'A small example package',
+    long_description = open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    License= 'MIT',
+    py_modules=['first'],
+    python_requires='>=3.8, <4',
+    install_requires=[
+        'Click','pyDrive','requests','python-decouple','setuptools','pybtex','python-dotenv',
+    ],
+    entry_points={
+        'console_scripts': [
+            'init = first:cli', 'run = first:run', 'seed = first:seed',
+        ],
+    },
+)
