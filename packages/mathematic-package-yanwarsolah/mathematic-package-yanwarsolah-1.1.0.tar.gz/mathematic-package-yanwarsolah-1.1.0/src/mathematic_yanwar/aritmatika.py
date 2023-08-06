0ff79c116564b9e61c55f86c37d4b70b0dd1317e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:00 +0000

    Add package mathematic-package-yanwarsolah/1.1.0/mathematic-package-yanwarsolah-1.1.0.tar.gz

---
+++ packages/mathematic-package-yanwarsolah/mathematic-package-yanwarsolah-1.1.0.tar.gz/mathematic-package-yanwarsolah-1.1.0/src/mathematic_yanwar/aritmatika.py
@@ -0,0 +1,13 @@
+def penjumlahan(a, b):
+    return a + b
+
+
+def pengurangan(a, b):
+    return a - b
+
+
+def perkalian(a, b):
+    return a * b
+
+def pembagian(a, b):
+    return a / b
---
+++ packages/mathematic-package-yanwarsolah/mathematic-package-yanwarsolah-1.1.0.tar.gz/mathematic-package-yanwarsolah-1.1.0/src/mathematic_yanwar/bangun.py
@@ -0,0 +1,6 @@
+def luas_persegi_panjang(p, l):
+    return p * l
+
+def keliling_persegi(s):
+    return s + s + s + s
+
