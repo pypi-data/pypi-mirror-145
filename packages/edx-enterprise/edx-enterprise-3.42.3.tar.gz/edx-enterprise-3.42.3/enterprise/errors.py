8ddf4cf3eef819b85079801349a7b729eb229a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:09 +0000

    Add package edx-enterprise/3.42.3/edx-enterprise-3.42.3.tar.gz

---
+++ packages/edx-enterprise/edx-enterprise-3.42.3.tar.gz/edx-enterprise-3.42.3/enterprise/errors.py
@@ -0,0 +1,21 @@
+"""
+Errors thrown by the APIs in the Enterprise application.
+"""
+
+
+class CodesAPIRequestError(Exception):
+    """There was a problem with a request to the Codes application's APIs."""
+
+
+class EnrollmentModificationException(Exception):
+    """
+    An exception that represents an error when modifying the state
+    of an enrollment via the EnrollmentApiClient.
+    """
+
+
+class AdminNotificationAPIRequestError(Exception):
+    """
+    An exception that represents an error when creating admin notification
+    read status via the NotificationReadApiClient.
+    """
