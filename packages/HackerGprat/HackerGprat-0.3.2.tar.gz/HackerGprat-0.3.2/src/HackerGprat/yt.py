4a069dfd3072d5c309f606d760eb11ecd6197088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:49 +0000

    Add package HackerGprat/0.3.2/HackerGprat-0.3.2.tar.gz

---
+++ packages/HackerGprat/HackerGprat-0.3.2.tar.gz/HackerGprat-0.3.2/src/HackerGprat/yt.py
@@ -0,0 +1,42 @@
+from HackerGprat import clear
+from pytube import YouTube
+import os
+
+def yt(videourl, path="./Output"):
+
+    clear()
+    
+    print("Connecting...")
+    
+    yt = YouTube(videourl)
+    yt = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+    
+    if not os.path.exists(path):
+        print("Creating Folder...")
+        os.makedirs(path)
+
+    print("Downloading...")
+    yt.download(path)
+    print("SucessFully Downloaded...")
+
+
+
+# url = 'https://www.youtube.com/watch?v=r2giUilvkBQ&list=PLC3y8-rFHvwg2-q6Kvw3Tl_4xhxtIaNlY&index=45'
+# yt_download( url )
+
+# urls = []
+
+
+def yt_download( url ):
+    
+    if type( url ) == str:
+        yt( url )
+        
+    elif type( url ) == list:
+        for link in url:
+            yt( link )
+    else:
+        print("See The Docs & Try Again")
+
+
+
