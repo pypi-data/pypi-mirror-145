88ce77b020aaa43845183657aa4439364c0dbcde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:04 +0000

    Add package gase/1.0/gase-1.0.tar.gz

---
+++ packages/gase/gase-1.0.tar.gz/gase-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='gase',
+	version='1.0',
+	author='Kaver',
+	author_email='aroslavsik2@gmail.com',
+	description='-',
+	packages=['Coder GA-SE'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
