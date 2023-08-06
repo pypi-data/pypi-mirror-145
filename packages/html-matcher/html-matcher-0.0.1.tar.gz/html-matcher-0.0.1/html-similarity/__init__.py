195076e16f623b3dca8b41a7ab0f499e3f46c0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:34 +0000

    Add package html-matcher/0.0.1/html-matcher-0.0.1.tar.gz

---
+++ packages/html-matcher/html-matcher-0.0.1.tar.gz/html-matcher-0.0.1/html-similarity/__init__.py
@@ -0,0 +1,7 @@
+from export import ExcelExporter
+from lcs import LongestCommonSequence, LongestCommonSequenceOptimized
+from similarity import Similarity, MixedSimilarity
+from style import StyleSimilarity
+from ted import AllPathTreeEditDistance, AllPathTreeEditDistanceOptimized, CustomConfig
+from utils import get_files, canonical, node, get_results, html_to_json, html_to_json_improved, json_to_array, \
+    json_count_elements, filter_unwanted_tags_canonical, filter_unwanted_tags
