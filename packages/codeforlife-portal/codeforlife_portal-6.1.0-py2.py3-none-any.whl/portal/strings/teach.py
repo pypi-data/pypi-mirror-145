07c216d0adf9a511e04c5899d91becaadb025b5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:48 +0000

    Add package codeforlife-portal/6.1.0/codeforlife_portal-6.1.0-py2.py3-none-any.whl

---
+++ packages/codeforlife-portal/codeforlife_portal-6.1.0-py2.py3-none-any.whl/portal/strings/teach.py
@@ -0,0 +1,7 @@
+TEACH_BANNER = {
+    "title": "Educate",
+    "subtitle": "Get ready to teach the next generation of computer scientists",
+    "text": "",
+    "image_class": "banner--picture--educate",
+    "image_description": "teacher and student at laptop, girl pointing at screen",
+}
