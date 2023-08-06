824c51f0102e04055a6b12b40ac33e5f4def328d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:46 +0000

    Add package marfa-bi-connection/0.1/marfa_bi_connection-0.1.tar.gz

---
+++ packages/marfa-bi-connection/marfa_bi_connection-0.1.tar.gz/marfa_bi_connection-0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='marfa_bi_connection',
+    version='0.1',
+    license='MIT',
+    author="Ruslan Galimov",
+    author_email='rgalimov@marfa-tech.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/NorddyM/MarfaBI',
+    keywords='Marfa BI connections',
+    install_requires=[
+        'pymysql',
+        'PyYAML',
+        'sshtunnel',
+        'pandas',
+        'python-telegram-bot',
+        'SQLAlchemy',
+        'clickhouse-sqlalchemy',
+        'clickhouse-driver',
+        'google-cloud-bigquery',
+        'slack-sdk',
+        'google'
+      ],
+
+)
