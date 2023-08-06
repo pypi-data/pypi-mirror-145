6a284c28a7035c525bc2598418c272e158cdf071
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:41 +0000

    Add package robotpy-wpimath/2022.4.1.3/robotpy_wpimath-2022.4.1.3-cp39-cp39-manylinux_2_27_x86_64.whl

---
+++ packages/robotpy-wpimath/robotpy_wpimath-2022.4.1.3-cp39-cp39-manylinux_2_27_x86_64.whl/wpimath/_wpimath/__init__.pyi
@@ -0,0 +1,14 @@
+from __future__ import annotations
+import wpimath._wpimath
+import typing
+
+__all__ = [
+    "angleModulus",
+    "applyDeadband"
+]
+
+
+def angleModulus(angle: radians) -> radians:
+    pass
+def applyDeadband(value: float, deadband: float) -> float:
+    pass
