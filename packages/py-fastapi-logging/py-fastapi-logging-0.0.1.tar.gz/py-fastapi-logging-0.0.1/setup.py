7cbf8438093718b59d8c3443d06c2103b4b9f7ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:34 +0000

    Add package py-fastapi-logging/0.0.1/py-fastapi-logging-0.0.1.tar.gz

---
+++ packages/py-fastapi-logging/py-fastapi-logging-0.0.1.tar.gz/py-fastapi-logging-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-fastapi-logging',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests']),
+    url='',
+    license='',
+    author='Dmitry Akhnazarov',
+    author_email='DAkhnazarov@croc.ru',
+    description='FastAPI logging requests with VTB Standard'
+)
