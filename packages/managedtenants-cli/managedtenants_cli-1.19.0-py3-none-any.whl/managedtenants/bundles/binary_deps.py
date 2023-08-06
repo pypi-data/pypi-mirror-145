0554c82290b8841aef999c395cc8c01f2e1a3969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:42 +0000

    Add package managedtenants-cli/1.19.0/managedtenants_cli-1.19.0-py3-none-any.whl

---
+++ packages/managedtenants-cli/managedtenants_cli-1.19.0-py3-none-any.whl/managedtenants/bundles/binary_deps.py
@@ -0,0 +1,4 @@
+from sretoolbox.binaries import Mtcli, Opm
+
+OPM = Opm(version="1.15.1", download_path="/tmp")
+MTCLI = Mtcli(version="0.4.0", download_path="/tmp")
---
+++ packages/managedtenants-cli/managedtenants_cli-1.19.0-py3-none-any.whl/managedtenants/data/paths.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+DATA_DIR = (Path(__file__) / "..").resolve()
+
+SCHEMAS_DIR = (Path(__file__) / ".." / ".." / "schemas").resolve()
