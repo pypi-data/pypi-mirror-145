017ef83349fa697f63b3bbd7a5a58a376c3bade5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:49:28 +0000

    Add package text-cleaning/0.0.5/text_cleaning-0.0.5.tar.gz

---
+++ packages/text-cleaning/text_cleaning-0.0.5.tar.gz/text_cleaning-0.0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.5'
+DESCRIPTION = 'Text Cleaning'
+LONG_DESCRIPTION = 'A package that allows to automatic and custom text cleaning.'
+
+# Setting up
+setup(
+    name="text_cleaning",
+    version=VERSION,
+    author="Piyush Mittal",
+    author_email="<piyushmittal2192@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['more_itertools', 'sklearn', 'numpy', 'levenshtein']
+    
+)
