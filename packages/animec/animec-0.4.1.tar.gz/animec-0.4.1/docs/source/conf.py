64b4ba4da58d33bd3ad0b02bb3f711406e00177e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:28 +0000

    Add package animec/0.4.1/animec-0.4.1.tar.gz

---
+++ packages/animec/animec-0.4.1.tar.gz/animec-0.4.1/docs/source/conf.py
@@ -0,0 +1,82 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../"))
+
+# -- Project information -----------------------------------------------------
+
+project = "animec"
+copyright = "2021, Drift Asimov"
+author = "Drift Asimov"
+
+# The full version, including alpha/beta/rc tags
+setup_file = (
+    open("../../setup.py").read().split("version")[1].split('"')[1].split('"')[0]
+)
+release = str(setup_file)
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks",
+]
+
+autodoc_member_order = "bysource"
+
+intersphinx_mapping = {
+    "py": ("https://docs.python.org/3", None),
+}
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+# The master toctree document.
+master_doc = "index"
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+html_context = {
+    "discord_invite": "https://discord.gg/x3qAZV3",
+    "issues": "https://github.com/DriftAsimov/animec/issues",
+}
+
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "navigation_with_keys": True
+}
+
+html_favicon = "../assets/animec.png"
+html_logo = "../assets/animec.png"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
