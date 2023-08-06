6619d4dc3fd82be6e2f1bc4185fcb120f11ed738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:40 +0000

    Add package lrslib/1.0.2/lrslib-1.0.2-py3.7.egg

---
+++ packages/lrslib/lrslib-1.0.2-py3.7.egg/lrslib/__init__.py
@@ -0,0 +1,18 @@
+import .tkin
+import .tools
+
+def by():
+    print('LrsLib作者：刘镕硕\n刘镕硕版权所有')
+def func(fun='all'):
+    function = {'tkin.Window':'仿tk窗口，用Button时，Tk()为 .win',
+                'tools.pront':'一个一个地打印文本',
+                'tools.show_image':'显示图片在pygame窗口中',
+                'all':'''全部函数：
+'tkin.Window':'仿tk窗口，用Button时，Tk()为 .win',
+'tools.pront':'一个一个地打印文本',
+'tools.show_image':'显示图片在pygame窗口中'
+'by':'关于'
+'func':'帮助'
+'''
+                }
+    print(function[fun])
