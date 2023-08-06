1fcb92f6f1794941626e5d2ee5d87427e0d44a69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:44 +0000

    Add package griffe/0.15.0/griffe-0.15.0.tar.gz

---
+++ packages/griffe/griffe-0.15.0.tar.gz/griffe-0.15.0/tests/test_cli.py
@@ -0,0 +1,23 @@
+"""Tests for the `cli` module."""
+
+import pytest
+
+from griffe import cli
+
+
+def test_main():
+    """Basic CLI test."""
+    assert cli.main(["griffe", "-s", "src", "-o/dev/null"]) == 0
+
+
+def test_show_help(capsys):
+    """
+    Show help.
+
+    Parameters:
+        capsys: Pytest fixture to capture output.
+    """
+    with pytest.raises(SystemExit):
+        cli.main(["-h"])
+    captured = capsys.readouterr()
+    assert "griffe" in captured.out
