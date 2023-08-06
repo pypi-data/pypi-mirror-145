ee89ec36c2e99c7e61cd33dbdb34546d2ae16516
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:14 +0000

    Add package atila/0.16.10.1/atila-0.16.10.1-py3-none-any.whl

---
+++ packages/atila/atila-0.16.10.1-py3-none-any.whl/atila/executors/ws_executor.py
@@ -0,0 +1,34 @@
+from . import wsgi_executor
+from skitai.protocols.sock.impl.http import respcodes
+from skitai.backbone.http_response import catch
+import asyncio
+from ..coroutine import utils
+import threading
+
+
+class Executor (wsgi_executor.Executor):
+    def respond_async (self, was, task):
+        try:
+            content = task.fetch ()
+            if type (content) is not tuple:
+                content = (content,)
+            was.websocket.send (*content)
+        except:
+            was.traceback ()
+            was.websocket.channel and was.websocket.channel.close ()
+        finally:
+            was.request = None
+            was.websocket = None
+            was.env = None
+            was.async_executor.done ()
+
+    def __call__ (self):
+        self.was = self.env ["skitai.was"]
+        current_app, wsfunc = self.env.get ("websocket.handler")
+        content = wsfunc (self.was, **self.env.get ("websocket.params", {}))
+        if not asyncio.iscoroutine (content):
+            return content
+
+        assert hasattr (self.was, "async_executor"), "async is not enabled"
+        self.add_async_task (content)
+
