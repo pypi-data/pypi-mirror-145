c7e16b4372d6987ee589e257192106ec1fa9a567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:26 +0000

    Add package ilsmc-rivasiker/0.0.4/ilsmc_rivasiker-0.0.4-py3-none-any.whl

---
+++ packages/ilsmc-rivasiker/ilsmc_rivasiker-0.0.4-py3-none-any.whl/ilsmc/cutpoints_AB.py
@@ -0,0 +1,26 @@
+import numpy as np
+from scipy.stats import truncexpon
+
+def cutpoints_AB(n_int_AB, t_AB, coal_AB):
+    """
+    This function returns a the cutpoints for the
+    intervals for the two-sequence CTMC. The cutpoints
+    will be defined by the quantiles of a truncated
+    exponential distribution. 
+    
+    Parameters
+    ----------
+    n_int_AB : integer
+        Number of intervals in the two-sequence CTMC.
+    t_AB : float
+        Total time interval of the two-sequence CTMC
+    coal_AB : float
+        coalescent rate of the two-sequence CTMC.
+    """
+    # Define probabilities for quantiles
+    quantiles_AB = np.array(list(range(n_int_AB+1)))/n_int_AB
+    # Define truncexpon shape parameters
+    lower, upper, scale = 0, t_AB, 1/coal_AB
+    # Get quantiles
+    cut_AB = truncexpon.ppf(quantiles_AB, b=(upper-lower)/scale, loc=lower, scale=scale)
+    return cut_AB 
---
+++ packages/ilsmc-rivasiker/ilsmc_rivasiker-0.0.4-py3-none-any.whl/ilsmc/load_trans_mat.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+def load_trans_mat(n_seq):
+    """
+    This functions returns a string matrix for the CTMC.
+    
+    Parameters
+    ----------
+    n_seq : integer
+        Number of sequences of the CTMC
+    """
+    
+    # Read string data frame for the CTMC
+    df = pd.read_csv('../trans_mat/trans_mat_full_'+str(n_seq)+'.csv')
+    
+    # Create dictionary with all CTMC state names and their index
+    df_2 = {
+        'names': pd.concat([df['from_str'], df['to_str']]),
+        'values': pd.concat([df['from'], df['to']])
+    }
+    # Convert names to data frame, drop duplicated states and sort by index
+    df_2 = pd.DataFrame(data=df_2).drop_duplicates().sort_values(by=['values'])
+    
+    # Pivot data frame to matrix, and fill missing values with '0'
+    df_1 = df[['value', 'from', 'to']].pivot(index='from',columns='to',values='value').fillna('0')
+    # Reset column names
+    df_1.columns.name = None
+    df_1 = df_1.reset_index().iloc[:, 1:]
+    
+    return np.array(df_1), list(df_2['names'])
