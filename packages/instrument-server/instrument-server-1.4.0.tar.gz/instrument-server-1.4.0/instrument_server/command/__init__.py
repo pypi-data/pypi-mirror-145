3e1c0ede9ef6689a68b709e4a0233665e0087ac8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:48 +0000

    Add package instrument-server/1.4.0/instrument-server-1.4.0.tar.gz

---
+++ packages/instrument-server/instrument-server-1.4.0.tar.gz/instrument-server-1.4.0/instrument_server/command/__init__.py
@@ -0,0 +1,4 @@
+from .base           import Base
+from .command        import is_plugin, plugins, register_plugin, remove_plugin
+from .parsed_command import ParsedCommand
+from .translation    import Translation
