512a25102f7afdf65f6fdf6c12ac4dd827495a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:10 +0000

    Add package pyevr/0.5.2/pyevr-0.5.2.tar.gz

---
+++ packages/pyevr/pyevr-0.5.2.tar.gz/pyevr-0.5.2/pyevr/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyevr."""
+
+__author__ = """Thorgate"""
+__email__ = 'code@thorgate.eu'
+__version__ = '0.5.2'
+
+
+from pyevr.client import EVRClient  # noqa: F401
