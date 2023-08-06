2a9e40403464f535e072fa6ebd636128bfc91b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:55 +0000

    Add package datasetcrux/0.0.32/datasetcrux-0.0.32.tar.gz

---
+++ packages/datasetcrux/datasetcrux-0.0.32.tar.gz/datasetcrux-0.0.32/README.md
@@ -0,0 +1,24 @@
+# datasetcrux
+
+Documentation will be added over here.
+
+0.0.1
+blank
+
+0.0.2
+```
+random_string(length)
+```
+
+can return random string
+
+0.0.3
+OS classifiers added
+
+0.0.31
+python minimum 3.6
+
+0.0.32
+csv_printer, csv_reader, csv_reader_dict
+csv_writer, csv_writer_append, csv_writer_append_dict, csv_writer_append_dict_header, csv_writer_append_dict_header_if_not_exist
+csv functions  added
---
+++ packages/datasetcrux/datasetcrux-0.0.32.tar.gz/datasetcrux-0.0.32/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="datasetcrux",
+    version="0.0.32",
+    author="Vinay Panchal",
+    author_email="vinay.npanchal@gmail.com",
+    description="Dataset creation, management and analysis tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/datasetcrux/datasetcrux",
+    project_urls={
+        "Bug Tracker": "https://github.com/datasetcrux/datasetcrux/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows ",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Operating System :: OS Independent",
+       
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
