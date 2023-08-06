49e036acdb948fd3f2281bef3cf4b759da09b97a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:58 +0000

    Add package kaldifeat/1.13/kaldifeat-1.13.tar.gz

---
+++ packages/kaldifeat/kaldifeat-1.13.tar.gz/kaldifeat-1.13/kaldifeat/python/kaldifeat/__init__.py
@@ -0,0 +1,15 @@
+import torch
+from _kaldifeat import (
+    FbankOptions,
+    FrameExtractionOptions,
+    MelBanksOptions,
+    MfccOptions,
+    PlpOptions,
+    SpectrogramOptions,
+)
+
+from .fbank import Fbank, OnlineFbank
+from .mfcc import Mfcc, OnlineMfcc
+from .plp import OnlinePlp, Plp
+from .spectrogram import Spectrogram
+__version__ = '1.13'
