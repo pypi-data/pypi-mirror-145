8a9ef1abc40eb2705f1e099ffdaecb9d20df3628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:01 +0000

    Add package RenRenMall/0.0.8/RenRenMall-0.0.8.tar.gz

---
+++ packages/RenRenMall/RenRenMall-0.0.8.tar.gz/RenRenMall-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RenRenMall',  # 应用名
+    version='0.0.8',  # 版本号
+    author='YarnBlue',
+    author_email='zqb090325@126.com',
+    description='人人商城后台管理',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True
+)
