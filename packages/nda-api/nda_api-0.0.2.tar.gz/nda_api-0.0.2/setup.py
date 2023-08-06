bdfb375642a07dfd2fa8a158934cd0c6e96f1438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:07 +0000

    Add package nda-api/0.0.2/nda_api-0.0.2.tar.gz

---
+++ packages/nda-api/nda_api-0.0.2.tar.gz/nda_api-0.0.2/setup.py
@@ -0,0 +1,37 @@
+"""To push to pypit
+
+python setup.py sdist
+twine upload dist/*
+"""
+import setuptools
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nda_api",
+    version="0.0.2",
+    author="Oscar Lundberg",
+    author_email="lundberg.oscar@gmail.com",
+    description="Package to fetch data from Nordea",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/olundberg/nda_api",
+    download_url="https://github.com/olundberg/nda_api/archive/refs/tags/v_0.0.2.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.9",
+    install_requires=["webdriver-manager",
+                      "selenium>=3.141.0",
+                      "pandas",
+                      "lxml",
+                      "html5lib",
+                      "bs4",
+                      ],
+)
