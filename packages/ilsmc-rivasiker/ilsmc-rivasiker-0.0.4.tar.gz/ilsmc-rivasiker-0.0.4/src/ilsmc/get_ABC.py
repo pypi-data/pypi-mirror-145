413305230f64b0a56965574f68009323233f5a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:26 +0000

    Add package ilsmc-rivasiker/0.0.4/ilsmc-rivasiker-0.0.4.tar.gz

---
+++ packages/ilsmc-rivasiker/ilsmc-rivasiker-0.0.4.tar.gz/ilsmc-rivasiker-0.0.4/src/ilsmc/get_ABC.py
@@ -0,0 +1,25 @@
+from scipy.linalg import expm
+
+
+def get_ABC(trans_mat, times, omegas):
+    """
+    This function calculates the joint probabilities
+    for the two-sequence CTMC.
+    
+    Parameters
+    ----------
+    trans_mat : numpy array
+        The transition rate matrix of the two-sequence CTMC
+    times : list of numbers
+        Time intervals for each matrix multiplication
+    omegas : list of lists
+        Sets of states for each matrix multiplication
+    """
+    # Calculate first multiplication
+    g = expm(trans_mat*times[0])[omegas[0]][:,omegas[1]]
+    # For each of the remaining omegas
+    for i in range(1, len(times)):
+        # Perform multiplication
+        g = g @ expm(trans_mat*times[i])[omegas[i]][:,omegas[i+1]]
+    # Return a numpy array that contains the probabilities in the right order.
+    return g
