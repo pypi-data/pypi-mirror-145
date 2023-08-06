48d80893846f77223c8050e23d6e339dda11b2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:57:18 +0000

    Add package recommendation-library/0.0/recommendation_library-0.0.tar.gz

---
+++ packages/recommendation-library/recommendation_library-0.0.tar.gz/recommendation_library-0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='recommendation_library',
+	version='0.0',
+	packages=[''],
+	url='https://github.com/h0tmi/recomendation_library/blob/main/klasters.py',
+	#download_url='https://github.com/h0tmi/recomendation_library/blob/main/klasters.py'
+    license='',
+    install_requires=['sklearn', 'pandas'],
+	author='Y&T',
+	author_email='yaroslav.akhramenko@gmail.com',
+	description='My/Our Project'
+)
