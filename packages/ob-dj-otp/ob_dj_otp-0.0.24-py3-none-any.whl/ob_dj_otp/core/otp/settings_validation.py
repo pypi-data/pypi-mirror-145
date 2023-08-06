9d890e09ba7f4081b07cafcb2f522978e5c7c471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:53 +0000

    Add package ob-dj-otp/0.0.24/ob_dj_otp-0.0.24-py3-none-any.whl

---
+++ packages/ob-dj-otp/ob_dj_otp-0.0.24-py3-none-any.whl/ob_dj_otp/core/otp/settings_validation.py
@@ -0,0 +1,15 @@
+# TODO: Deprecate settings_validation in favor of using app_settings to run necessary validations
+from django.apps import apps
+from django.core.checks import Error
+
+REQUIRED_INSTALLED_APPS = [
+    "rest_framework",
+]
+
+
+def required_installed_apps(app_configs, **kwargs):
+    return [
+        Error(f"{app} is required in INSTALLED_APPS")
+        for app in REQUIRED_INSTALLED_APPS
+        if not apps.is_installed(app)
+    ]
