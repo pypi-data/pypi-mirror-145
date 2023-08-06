9f7f03bdc17376d34e6dd9ba63c48f07ec885949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package gcloud-aio-bigquery/6.0.0/gcloud_aio_bigquery-6.0.0-py3-none-any.whl

---
+++ packages/gcloud-aio-bigquery/gcloud_aio_bigquery-6.0.0-py3-none-any.whl/gcloud/aio/bigquery/__init__.py
@@ -0,0 +1,14 @@
+from pkg_resources import get_distribution
+__version__ = get_distribution('gcloud-aio-bigquery').version
+
+from gcloud.aio.bigquery.bigquery import Disposition
+from gcloud.aio.bigquery.bigquery import SCOPES
+from gcloud.aio.bigquery.bigquery import SchemaUpdateOption
+from gcloud.aio.bigquery.bigquery import SourceFormat
+from gcloud.aio.bigquery.dataset import Dataset
+from gcloud.aio.bigquery.job import Job
+from gcloud.aio.bigquery.table import Table
+
+
+__all__ = ['__version__', 'Dataset', 'Disposition', 'Job', 'SCOPES',
+           'SchemaUpdateOption', 'SourceFormat', 'Table']
