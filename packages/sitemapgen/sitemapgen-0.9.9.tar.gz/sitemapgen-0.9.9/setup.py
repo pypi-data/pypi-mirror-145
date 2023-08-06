21321f540f36245286652a026ee67a38846ee92e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:19:39 +0000

    Add package sitemapgen/0.9.9/sitemapgen-0.9.9.tar.gz

---
+++ packages/sitemapgen/sitemapgen-0.9.9.tar.gz/sitemapgen-0.9.9/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("requirements.txt", "r") as f:
+	requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="sitemapgen",
+    version="0.9.9",
+    author="Nalin Angrish",
+    author_email="nalin@nalinangrish.me",
+    description="A package to generate Sitemaps from a URL. Also provides a CLI for non programmatical use.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.nalinangrish.me/apps/sitemapgen",
+    entry_points='''
+        [console_scripts]
+        sitemapgen=sitemapgen.cli:run
+        smg=sitemapgen.cli:run
+    ''',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    python_requires='>=3.0',
+)
