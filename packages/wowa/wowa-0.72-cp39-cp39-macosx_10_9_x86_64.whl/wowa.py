adce8db22b5477e1c3dda56dd903c47baee97510
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:09 +0000

    Add package wowa/0.72/wowa-0.72-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/wowa/wowa-0.72-cp39-cp39-macosx_10_9_x86_64.whl/wowa.py
@@ -0,0 +1,56 @@
+# Funtions to combine NumPy arrays in C++ 
+import numpy as np
+import types
+from _wowa import ffi, lib
+
+
+# Callback funtion when no sorting is needed when used in the tree
+# Input parameters:
+#   n: size of arrays
+#   double x[]: array of size n
+#   double w[]: array of size n
+# Output parameters:
+#   double y: sum of x[i] * w[i]
+@ffi.def_extern()
+def py_owa( n, x, w):
+    # Call C++ function
+    return lib.OWA( n, x, w)
+    
+
+# Function F is the symmetric base aggregator.
+# Input parameters:
+#   double x[] = inputs
+#   double p[] = array of weights of inputs x[],
+#   double w[] = array of weights for OWA, 
+#   n = the dimension of x, p, w.
+#   the weights must add to one and be non-negative.
+#   double(*cb)(int, double[],double[]) = callback function 
+#   L = number of binary tree levels. Run time = O[(n-1)L] 
+# Output parameters:
+#    y = weightedf
+def weightedf( x, p, w, cb, L):
+    try:
+        # check for size
+        if not( x.size == p.size == w.size): raise ValueError( "arrays must have same size")
+        # check type of function
+        if not( isinstance(cb, types.FunctionType)): raise ValueError( "no callback function")
+
+        # check types of arrays
+        if x.dtype != "float64": x = x.astype(float)
+        if p.dtype != "float64": p = p.astype(float)
+        if w.dtype != "float64": w = w.astype(float)
+    
+        # Use CFFI type conversion
+        px = ffi.cast( "double *", x.ctypes.data)
+        pp = ffi.cast( "double *", p.ctypes.data)
+        pw = ffi.cast( "double *", w.ctypes.data)
+
+        # Call C++ function
+        if( cb == py_owa): pcb = lib.py_owa
+        else: raise ValueError( "undefined callback function")
+
+        y = lib.weightedf( px, pp, pw, x.size, pcb, L)
+        return y
+
+    except ValueError:
+        raise
