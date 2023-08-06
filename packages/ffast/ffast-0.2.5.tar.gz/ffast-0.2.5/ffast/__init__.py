955c58cb93998310c907a5552fbe1d312f189b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:42 +0000

    Add package ffast/0.2.5/ffast-0.2.5.tar.gz

---
+++ packages/ffast/ffast-0.2.5.tar.gz/ffast-0.2.5/ffast/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.2.5"
+WORDNET = "wordnet"
+POINCARE = "poincare"
+
+def load(vectors:str=WORDNET):
+    if vectors==WORDNET:
+        from ffast.wordnet.tokeniser import Tokeniser
+        from nltk import download
+        download(WORDNET)
+        download('stopwords')
+        return Tokeniser()
+    if vectors==POINCARE:
+        from ffast.poincare.tokeniser import Tokeniser
+        return Tokeniser()    
+    raise TypeError(f"{vectors} is an unrecognised choice. Valid choices are: '{WORDNET}' or '{POINCARE}'")
