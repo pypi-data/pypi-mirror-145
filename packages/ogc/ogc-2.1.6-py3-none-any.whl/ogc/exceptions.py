f99af378245a43a603d544f8516df5dc5f8c4fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:36 +0000

    Add package ogc/2.1.6/ogc-2.1.6-py3-none-any.whl

---
+++ packages/ogc/ogc-2.1.6-py3-none-any.whl/ogc/exceptions.py
@@ -0,0 +1,14 @@
+class SpecLoaderException(Exception):
+    pass
+
+
+class SpecProcessException(Exception):
+    """Raise when process fails"""
+
+
+class ProvisionException(Exception):
+    """Raise when process fails"""
+
+
+class ProvisionDeployerException(Exception):
+    """Raise when deployer fails"""
