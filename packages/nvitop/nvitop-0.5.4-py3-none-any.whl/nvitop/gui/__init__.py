7fc911fa537741f5c59e9902bfcf007c0a432053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:58 +0000

    Add package nvitop/0.5.4/nvitop-0.5.4-py3-none-any.whl

---
+++ packages/nvitop/nvitop-0.5.4-py3-none-any.whl/nvitop/gui/__init__.py
@@ -0,0 +1,7 @@
+# This file is part of nvitop, the interactive NVIDIA-GPU process viewer.
+# License: GNU GPL version 3.
+
+# pylint: disable=missing-module-docstring
+
+from nvitop.gui.library import Device, libcurses, colored, set_color, USERNAME, SUPERUSER
+from nvitop.gui.top import Top
