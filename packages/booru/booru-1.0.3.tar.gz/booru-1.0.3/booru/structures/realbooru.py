033cd8cb8bfc7a2d9ced157f4b22ea3a98563c0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:26:48 +0000

    Add package booru/1.0.3/booru-1.0.3.tar.gz

---
+++ packages/booru/booru-1.0.3.tar.gz/booru-1.0.3/booru/structures/realbooru.py
@@ -0,0 +1,185 @@
+import requests
+import json
+import re
+from random import shuffle
+from ..utils.parser import Api, better_object, parse_image, get_hostname
+
+Booru = Api()
+
+
+class Realbooru(object):
+    """ Realbooru wrapper
+
+    Methods
+    -------
+    search : function
+        Search and gets images from realbooru.
+
+    get_image_only : function
+        Gets images, image urls only from realbooru.
+
+    """
+    @staticmethod
+    def append_obj(raw_object: dict):
+        """ Extends new object to the raw dict
+
+        Parameters
+        ----------
+        raw_object : dict
+            The raw object returned by realbooru.
+
+        Returns
+        -------
+        str
+            The image url.
+        """
+        for i in range(len(raw_object)):
+            if raw_object[i]['directory']:
+                raw_object[i]['file_url'] = f"{get_hostname(Booru.realbooru)}/images/{raw_object[i]['directory']}/{raw_object[i]['image']}"
+                raw_object[i]['post_url'] = f"{get_hostname(Booru.realbooru)}/index.php?page=post&s=view&id={raw_object[i]['id']}"
+
+            else:
+                raw_object[i]['file_url'] = Booru.error_handling_cantparse
+                raw_object[i]['post_url'] = f"{get_hostname(Booru.realbooru)}/index.php?page=post&s=view&id={raw_object[i]['id']}"
+
+        return raw_object
+
+    def __init__(self, api_key: str = '', user_id: str = ''):
+        """ Initializes realbooru.
+
+        Parameters
+        ----------
+        api_key : str
+            Your API Key which is accessible within your account options page
+
+        user_id : str
+            Your user ID, which is accessible on the account options/profile page.
+        """
+
+        if api_key and user_id == '':
+            self.api_key = None
+            self.user_id = None
+        else:
+            self.api_key = api_key
+            self.user_id = user_id
+
+        self.specs = {'api_key': self.api_key, 'user_id': self.user_id}
+
+    async def search(self, query: str,  block: str = '', limit: int = 100, page: int = 1, random: bool = True):
+
+        """ Search and gets images with raw data from realbooru.
+
+        Parameters
+        ----------
+        query : str
+            The query to search for.
+
+        block : str
+            The disgusting query you want to block, 
+            e.g: you want to search 'erza_scarlet' but dont want to gets furry, fill in 'furry'
+
+        limit : int
+            The limit of images to return.
+
+        page : int
+            The number of desired page
+
+        random : bool
+            Shuffle the whole dict, default is True.
+
+        Returns
+        -------
+        dict
+            The json object returned by realbooru.
+
+        """
+
+        if limit > 100:
+            raise ValueError(Booru.error_handling_limit)
+
+        if re.findall(block, query):
+            raise ValueError(Booru.error_handling_sameval)
+
+        if block != '':
+            self.query = f"{query} -{block}*"
+
+        else:
+            self.query = query
+
+        self.specs['tags'] = str(self.query)
+        self.specs['limit'] = str(limit)
+        self.specs['pid'] = str(page)
+        self.specs['json'] = '1'
+
+        self.data = requests.get(Booru.realbooru, params=self.specs)
+        self.final = json.loads(better_object(
+            self.data.json()), encoding="utf-8")
+
+        try:
+            if random:
+                self.not_random = Realbooru.append_obj(self.final)
+                shuffle(self.not_random)
+                return better_object(self.not_random)
+
+            else:
+                return better_object(Realbooru.append_obj(self.final))
+
+        except Exception as e:
+            raise ValueError(f'Failed to get data: {e}')
+
+    async def get_image_only(self, query: str,  block: str = '', limit: int = 100, page: int = 1):
+
+        """Gets images, meant just image urls from realbooru.
+
+        Parameters
+        ----------
+        query : str
+            The query to search for.
+
+        block : str
+            The disgusting query you want to block, 
+            e.g: you want to search 'erza_scarlet' but dont want to gets furry, fill in 'furry'
+
+        limit : int
+            The limit of images to return.
+
+        page : int
+            The number of desired page
+
+        Returns
+        -------
+        list
+            The list of image urls.
+
+        """
+
+        if limit > 100:
+            raise ValueError(Booru.error_handling_limit)
+
+        if re.findall(block, query):
+            raise ValueError(Booru.error_handling_sameval)
+
+        if block != '':
+            self.query = f"{query} -{block}*"
+
+        else:
+            self.query = query
+
+        self.specs['tags'] = str(self.query)
+        self.specs['limit'] = str(limit)
+        self.specs['pid'] = str(page)
+        self.specs['json'] = '1'
+
+        try:
+            self.data = requests.get(Booru.realbooru, params=self.specs)
+            self.final = json.loads(better_object(
+                self.data.json()), encoding="utf-8")
+
+            self.not_random = parse_image(Realbooru.append_obj(self.final))
+            remove_bad_array = [
+                x for x in self.not_random if x != Booru.error_handling_cantparse]
+            shuffle(remove_bad_array)
+            return better_object(remove_bad_array)
+
+        except Exception as e:
+            print(f'Failed to get data: {e}')
