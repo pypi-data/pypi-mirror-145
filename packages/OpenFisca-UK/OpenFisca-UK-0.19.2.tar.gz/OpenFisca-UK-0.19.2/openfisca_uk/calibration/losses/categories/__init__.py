f2b5ca3db1003569321353c943ad18b79ca9b008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:26 +0000

    Add package OpenFisca-UK/0.19.2/OpenFisca-UK-0.19.2.tar.gz

---
+++ packages/OpenFisca-UK/OpenFisca-UK-0.19.2.tar.gz/OpenFisca-UK-0.19.2/openfisca_uk/calibration/losses/categories/__init__.py
@@ -0,0 +1,12 @@
+from openfisca_uk.calibration.losses.categories.budgetary_impact import (
+    BudgetaryImpact,
+)
+from openfisca_uk.calibration.losses.categories.families import (
+    Families,
+)
+from openfisca_uk.calibration.losses.categories.households import (
+    Households,
+)
+from openfisca_uk.calibration.losses.categories.populations import (
+    Populations,
+)
