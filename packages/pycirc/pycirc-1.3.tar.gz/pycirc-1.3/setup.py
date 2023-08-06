692c0bf3714419f86f9e7e4bf69d088aeb2dd43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:24:53 +0000

    Add package pycirc/1.3/pycirc-1.3.tar.gz

---
+++ packages/pycirc/pycirc-1.3.tar.gz/pycirc-1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='pycirc',
+    version='1.3',
+    description='Python logic circuit modeling and simulation',
+    keywords='digital circuit logic modeling simulation',
+    author='Samy Zafrany',
+    url='https://www.samyzaf.com/pycirc/pycirc.html',
+    author_email='samyz@technion.ac.il',
+    license='MIT',
+    packages=['pycirc'],
+    install_requires=['networkx'],
+    zip_safe=False,
+)
+
