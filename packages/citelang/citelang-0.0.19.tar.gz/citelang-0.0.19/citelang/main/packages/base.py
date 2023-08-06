ffbf05ac76dabf997e1a1a407f8489d9a76827f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:43 +0000

    Add package citelang/0.0.19/citelang-0.0.19.tar.gz

---
+++ packages/citelang/citelang-0.0.19.tar.gz/citelang-0.0.19/citelang/main/packages/base.py
@@ -0,0 +1,59 @@
+__author__ = "Vanessa Sochat"
+__copyright__ = "Copyright 2022, Vanessa Sochat"
+__license__ = "MPL 2.0"
+
+# Custom package managers not in libraries IO
+
+import sys
+from citelang.logger import logger
+import citelang.main.cache as cache
+import requests
+
+
+class PackageManager:
+    """
+    A package manager is a custom class (not supported by libraries io)
+    """
+
+    underlying_manager = None
+
+    def __init__(self, *args, **kwargs):
+        self.cache = cache.cache
+        self.data = {}
+        for attr in [
+            "name",
+            "project_count",
+            "homepage",
+            "color",
+            "default_language",
+            "default_versions",
+        ]:
+            if not hasattr(self, attr):
+                sys.exit(
+                    "Misconfigured package manager %s missing %s attribute"
+                    % (self.name, attr)
+                )
+
+    # If needed, make sure endpoint data is sorted
+    def info(self):
+        return {
+            "name": self.underlying_manager or self.name,
+            "project_count": self.project_count,
+            "homepage": self.homepage,
+            "color": self.color,
+            "default_language": self.default_language,
+        }
+
+    def get_or_fail(self, url, headers=None, return_text=False):
+        """
+        Shared endpoint to get a url or fail.
+        """
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            logger.exit("Cannot retrieve %s: %s" % (url, response.json()))
+        if return_text:
+            return response.text
+        return response.json()
+
+    def package(self, name, **kwargs):
+        raise NotImplementedError
---
+++ packages/citelang/citelang-0.0.19.tar.gz/citelang-0.0.19/citelang/utils/__init__.py
@@ -0,0 +1,18 @@
+from .terminal import (
+    clone,
+    run_command,
+    get_installdir,
+    get_time_now,
+    confirm_action,
+    confirm_uninstall,
+)
+from .fileio import (
+    get_tmpfile,
+    get_tmpdir,
+    mkdir_p,
+    print_json,
+    read_file,
+    read_json,
+    write_file,
+    write_json,
+)
