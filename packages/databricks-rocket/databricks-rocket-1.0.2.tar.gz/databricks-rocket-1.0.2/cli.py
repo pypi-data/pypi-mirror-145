8accd62b8538a3f6a1c71fd6f401993ba5e2b106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:53 +0000

    Add package databricks-rocket/1.0.2/databricks-rocket-1.0.2.tar.gz

---
+++ packages/databricks-rocket/databricks-rocket-1.0.2.tar.gz/databricks-rocket-1.0.2/cli.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import fire
+
+from rocket.development.develoment_cli import Cli
+
+if __name__ == "__main__":
+    fire.Fire(Cli)
---
+++ packages/databricks-rocket/databricks-rocket-1.0.2.tar.gz/databricks-rocket-1.0.2/docs/development.md
@@ -0,0 +1,8 @@
+# Developing on local machine
+
+## Build the package and upload it to PyPi
+
+
+```sh
+./cli.py self_release release
+```
