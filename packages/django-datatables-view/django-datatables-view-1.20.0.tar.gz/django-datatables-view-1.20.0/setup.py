9adcfcb9918af452bc0be7a193d197cb9f227cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:37 +0000

    Add package django-datatables-view/1.20.0/django-datatables-view-1.20.0.tar.gz

---
+++ packages/django-datatables-view/django-datatables-view-1.20.0.tar.gz/django-datatables-view-1.20.0/setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+# minimal setup.py for editable installations:
+#   https://pip.pypa.io/en/stable/reference/build-system/pyproject-toml/#editable-installation
+if __name__ == "__main__":
+    setuptools.setup()
