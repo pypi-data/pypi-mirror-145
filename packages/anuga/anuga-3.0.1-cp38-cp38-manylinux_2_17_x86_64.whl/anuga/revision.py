4fab6fca9da07de9486f5e9455a7fa004e0cd6af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:08 +0000

    Add package anuga/3.0.1/anuga-3.0.1-cp38-cp38-manylinux_2_17_x86_64.whl

---
+++ packages/anuga/anuga-3.0.1-cp38-cp38-manylinux_2_17_x86_64.whl/anuga/revision.py
@@ -0,0 +1,8 @@
+"""Stored git revision info.
+
+This file provides the git sha id and commit date for the installed revision of ANUGA.
+The file is automatically generated and should not be modified manually.
+"""
+
+__git_sha__ = "a4cc79b0efafaa54a12241272c1b23014434fda6"
+__git_committed_datetime__ = "2022-03-31 19:38:39+10:18"
