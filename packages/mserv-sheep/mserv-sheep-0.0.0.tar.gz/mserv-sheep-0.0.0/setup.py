22d470eefec553f2ab4f1a6ff551277cc8f77e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:18 +0000

    Add package mserv-sheep/0.0.0/mserv-sheep-0.0.0.tar.gz

---
+++ packages/mserv-sheep/mserv-sheep-0.0.0.tar.gz/mserv-sheep-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="mserv-sheep",
+    version="0.0.0",
+    description=(
+        "This package stands as a placeholder for Ampio packages maintained in private pypi "
+        "repositories. The purpose of such action is to prevent supply chain attacks."
+    ),
+    python_requires=">=99.0",
+    author="Michał Getka",
+    url="https://ampio.com",
+    packages=["placeholder"],
+)
