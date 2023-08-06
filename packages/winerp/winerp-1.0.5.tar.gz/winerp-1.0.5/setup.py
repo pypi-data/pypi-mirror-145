e2c1105c7819310f85853021fe3b359f64ca4183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:13 +0000

    Add package winerp/1.0.5/winerp-1.0.5.tar.gz

---
+++ packages/winerp/winerp-1.0.5.tar.gz/winerp-1.0.5/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="winerp",
+    version="1.0.5",
+    description="Websocket based IPC for discord.py bots",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BlackThunder01001/winerp",
+    author="BlackThunder",
+    author_email="nouman0103@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["winerp"],
+    package_data={
+     'winerp.lib': ['*'],
+    },
+    include_package_data=True,
+    install_requires=["websockets", "websocket-server"],
+)
