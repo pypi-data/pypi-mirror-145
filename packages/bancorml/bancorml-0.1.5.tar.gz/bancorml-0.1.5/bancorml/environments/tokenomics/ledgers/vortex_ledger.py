ced6420fe15f35cbdf3e790af71c59083a194b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:49 +0000

    Add package bancorml/0.1.5/bancorml-0.1.5.tar.gz

---
+++ packages/bancorml/bancorml-0.1.5.tar.gz/bancorml-0.1.5/bancorml/environments/tokenomics/ledgers/vortex_ledger.py
@@ -0,0 +1,9 @@
+"""Component that emulates a Vortex Ledger Ledger."""
+from .ledger_base import LedgerBase
+
+class VortexLedger(LedgerBase):
+    def __init__(self):
+        super().__init__()
+        self._ledger = {'block_num': [0], 'BNT':[0]}
+
+
