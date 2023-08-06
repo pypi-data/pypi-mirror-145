7316daebfe6223184ddc6a72e62895cc1360e366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:37 +0000

    Add package napari-skimage-regionprops/0.4.1/napari-skimage-regionprops-0.4.1.tar.gz

---
+++ packages/napari-skimage-regionprops/napari-skimage-regionprops-0.4.1.tar.gz/napari-skimage-regionprops-0.4.1/napari_skimage_regionprops/__init__.py
@@ -0,0 +1,15 @@
+from ._table import add_table, get_table, TableWidget
+from ._regionprops import regionprops, regionprops_table, regionprops_table_all_frames
+from ._parametric_images import visualize_measurement_on_labels
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.4.1"
+
+from napari_plugin_engine import napari_hook_implementation
+
+
+@napari_hook_implementation
+def napari_experimental_provide_function():
+    return [regionprops_table, visualize_measurement_on_labels]
+
