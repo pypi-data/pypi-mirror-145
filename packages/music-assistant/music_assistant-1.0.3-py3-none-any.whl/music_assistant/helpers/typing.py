b1076b449fc1dd4fbaeb99ae6f0ccf43316a57fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:55 +0000

    Add package music-assistant/1.0.3/music_assistant-1.0.3-py3-none-any.whl

---
+++ packages/music-assistant/music_assistant-1.0.3-py3-none-any.whl/music_assistant/helpers/typing.py
@@ -0,0 +1,36 @@
+"""Typing helper."""
+
+from typing import TYPE_CHECKING, Any, List, Optional
+
+# pylint: disable=invalid-name
+if TYPE_CHECKING:
+    from music_assistant.mass import (
+        MusicAssistant,
+        EventDetails,
+        EventCallBackType,
+        EventSubscriptionType,
+    )
+    from music_assistant.models.media_items import MediaType
+    from music_assistant.models.player import (
+        PlayerQueue,
+        QueueItem,
+    )
+    from music_assistant.models.player import Player
+
+else:
+    MusicAssistant = "MusicAssistant"
+    QueueItem = "QueueItem"
+    PlayerQueue = "PlayerQueue"
+    StreamDetails = "StreamDetails"
+    Player = "Player"
+    MediaType = "MediaType"
+    EventDetails = Any | None
+    EventCallBackType = "EventCallBackType"
+    EventSubscriptionType = "EventSubscriptionType"
+
+
+QueueItems = List[QueueItem]
+Players = List[Player]
+
+OptionalInt = Optional[int]
+OptionalStr = Optional[str]
