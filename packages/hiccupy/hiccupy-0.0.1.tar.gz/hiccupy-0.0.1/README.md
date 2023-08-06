5fbadc4618ad322052f438bcef6564ce5bddee96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:36 +0000

    Add package hiccupy/0.0.1/hiccupy-0.0.1.tar.gz

---
+++ packages/hiccupy/hiccupy-0.0.1.tar.gz/hiccupy-0.0.1/README.md
@@ -0,0 +1,3 @@
+# hiccupy
+
+Rendering HTML from [hiccup-style lists](https://github.com/weavejester/hiccup) in python.
---
+++ packages/hiccupy/hiccupy-0.0.1.tar.gz/hiccupy-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="hiccupy",
+    py_modules=["hiccupy"],
+    version="0.0.1",
+    description="",
+    author="Rebecca Jackson",
+    author_email="rbca.jackson@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: Text Processing :: Markup :: HTML",
+    ],
+)
