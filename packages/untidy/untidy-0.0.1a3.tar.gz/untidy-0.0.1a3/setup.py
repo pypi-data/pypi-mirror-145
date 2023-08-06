dfd54bbb22a1c1a7d2cb9478d5c23d34f6a1444a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:30 +0000

    Add package untidy/0.0.1a3/untidy-0.0.1a3.tar.gz

---
+++ packages/untidy/untidy-0.0.1a3.tar.gz/untidy-0.0.1a3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+# Use requirements.txt to set the install_requires
+with open("requirements.txt") as f:
+    install_requires = [line.strip() for line in f]
+
+# Use requirements-dev.txt and requirements-examples.txt to set the extras_require
+extras_require = {}
+with open("requirements-dev.txt") as f:
+    extras_require["dev"] = [line.strip() for line in f]
+
+with open("requirements-examples.txt") as f:
+    extras_require["examples"] = [line.strip() for line in f]
+
+
+setup(
+    name="untidy",
+    packages=["untidy"],
+    version="0.0.1a3",
+    license="MIT",
+    description="Python package for creating messy data.",
+    author="Paolo Fantinel, Sinem Unal Nazaroglu, Mate Varadi",
+    author_email="paolo.fantinel@dainstudios.com, sinem.nazaroglu@dainstudios.com, mate.varadi@dainstudios.com",
+    python_requires=">=3.8",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dainstudios/untidy",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
