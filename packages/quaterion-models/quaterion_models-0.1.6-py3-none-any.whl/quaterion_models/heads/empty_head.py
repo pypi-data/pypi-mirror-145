c1e06fb9e6598869d286ff0bc25833b6f7038365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:55 +0000

    Add package quaterion-models/0.1.6/quaterion_models-0.1.6-py3-none-any.whl

---
+++ packages/quaterion-models/quaterion_models-0.1.6-py3-none-any.whl/quaterion_models/heads/empty_head.py
@@ -0,0 +1,15 @@
+import torch
+
+from quaterion_models.heads import EncoderHead
+
+
+class EmptyHead(EncoderHead):
+    def __init__(self, input_embedding_size: int):
+        super(EmptyHead, self).__init__(input_embedding_size)
+
+    @property
+    def output_size(self) -> int:
+        return self.input_embedding_size
+
+    def forward(self, input_vectors: torch.Tensor) -> torch.Tensor:
+        return input_vectors
---
+++ packages/quaterion-models/quaterion_models-0.1.6-py3-none-any.whl/quaterion_models/heads/encoder_head.py
@@ -0,0 +1,41 @@
+import json
+import os
+from typing import Dict, Any
+
+import torch
+from torch import nn
+
+
+class EncoderHead(nn.Module):
+    def __init__(self, input_embedding_size: int, **kwargs):
+        super(EncoderHead, self).__init__()
+        self.input_embedding_size = input_embedding_size
+
+    @property
+    def output_size(self) -> int:
+        raise NotImplementedError()
+
+    def forward(self, input_vectors: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError()
+
+    def get_config_dict(self) -> Dict[str, Any]:
+        """Constructs savable params dict
+
+        Returns:
+            Serializable parameters for __init__ of the Module
+        """
+        return {"input_embedding_size": self.input_embedding_size}
+
+    def save(self, output_path):
+        torch.save(self.state_dict(), os.path.join(output_path, "weights.bin"))
+
+        with open(os.path.join(output_path, "config.json"), "w") as f_out:
+            json.dump(self.get_config_dict(), f_out, indent=2)
+
+    @classmethod
+    def load(cls, input_path: str) -> "EncoderHead":
+        with open(os.path.join(input_path, "config.json")) as f_in:
+            config = json.load(f_in)
+        model = cls(**config)
+        model.load_state_dict(torch.load(os.path.join(input_path, "weights.bin")))
+        return model
