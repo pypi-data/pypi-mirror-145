c49142e20527038340fad3613c801713956ab360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:41 +0000

    Add package dkist-processing-test/0.0.53/dkist-processing-test-0.0.53.tar.gz

---
+++ packages/dkist-processing-test/dkist-processing-test-0.0.53.tar.gz/dkist-processing-test-0.0.53/dkist_processing_test/workflows/fail.py
@@ -0,0 +1,10 @@
+"""
+Workflow which is designed to fail
+"""
+from dkist_processing_core import Workflow
+
+from dkist_processing_test.tasks.fail import FailTask
+
+
+fail = Workflow(process_category="test", process_name="fail", workflow_package=__package__)
+fail.add_node(task=FailTask, upstreams=None)
---
+++ packages/dkist-processing-test/dkist-processing-test-0.0.53.tar.gz/dkist-processing-test-0.0.53/dkist_processing_test/workflows/noop.py
@@ -0,0 +1,18 @@
+"""
+Workflow which exercises the api's but doesn't perform an action
+"""
+from dkist_processing_core import Workflow
+
+from dkist_processing_test.tasks.noop import NoOpTask
+from dkist_processing_test.tasks.noop import NoOpTask2
+
+
+noop = Workflow(process_category="test", process_name="noop", workflow_package=__package__)
+noop.add_node(task=NoOpTask, upstreams=None)
+
+
+noop_flow = Workflow(
+    process_category="test", process_name="noop_flow", workflow_package=__package__
+)
+noop_flow.add_node(task=NoOpTask, upstreams=None)
+noop_flow.add_node(task=NoOpTask2, upstreams=NoOpTask)
