5a45eae4c19b4c108cf08e0300d18a202fe30652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:55 +0000

    Add package Jux/0.0.3/Jux-0.0.3.tar.gz

---
+++ packages/Jux/Jux-0.0.3.tar.gz/Jux-0.0.3/setup.py
@@ -0,0 +1,37 @@
+from subprocess import list2cmdline
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name="Jux",
+    version="0.0.3",
+    description="X-RAY Flare Burst Deterministic Detection",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Team 10",
+    packages=[".jux"],
+    install_requires=[
+        "astropy",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "scipy",
+        "plotly",
+        "sklearn",
+    ],
+    liscense="MIT",
+    py_modules=[
+        "file_handler",
+        "create_df_minmax",
+        "denoise",
+        "false_positive_detection",
+        "flare_detect_minmax",
+        "flare_detect_thresh",
+        "helper",
+        "param",
+        "version",
+        "jux",
+    ],
+)
