a28335e582411510f5bdfcbf2119def5fe24aa06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:49 +0000

    Add package yellowbox/0.7.1/yellowbox-0.7.1.tar.gz

---
+++ packages/yellowbox/yellowbox-0.7.1.tar.gz/yellowbox-0.7.1/README.md
@@ -0,0 +1,105 @@
+# Yellowbox
+![Test YellowBox](https://github.com/biocatchltd/yellowbox/workflows/Test%20YellowBox/badge.svg?branch=master)
+[![Coverage](https://codecov.io/github/biocatchltd/yellowbox/coverage.svg?branch=master)](https://codecov.io/github/biocatchltd/yellowbox?branch=master)
+
+
+Yellowbox makes it easy to run docker containers as part of black box tests.
+
+**Documentation:** https://yellowbox.readthedocs.io/
+
+---
+## Examples
+Say you want to run a blackbox test on a service that depends on a redis server.
+
+```python
+from yellowbox.clients import docker_client
+from yellowbox.extras import RedisService
+
+
+def test_black_box():
+  with docker_client() as docker_client, RedisService.run(docker_client) as redis:
+    redis_port = redis.client_port()  # this the host port the redis
+    ...  # run your black box test here
+  # yellowbox will automatically close the service when exiting the scope
+
+
+def test_black_box_with_initial_data():
+  # you can use the service's built-in utility functions to
+  # easily interoperate the service
+  with docker_client() as docker_client,
+          RedisService.run(docker_client) as redis,
+          redis.client() as client:
+    client.set("foo", "bar")
+  ...
+```
+## Supported Extras
+The currently supported built-in services are:
+* Kafka: `from yellowbox.extras import KafkaService`
+    * currently, the kafka service supports only plaintext protocol, and always binds to the host port 9092
+* Logstash: `from yellowbox.extras import LogstashService`
+* RabbitMQ: `from yellowbox.extras import RabbitMQService`
+* Redis: `from yellowbox.extras import RedisService`
+
+Note: all these extras require additional dependencies as specified in the project's `extras`.
+## Networks
+Yellowbox also makes it easy to set up temporary docker networks, so that different containers and services can
+communicate directly.
+
+```python
+from yellowbox.clients import docker_client
+from yellowbox import temp_network, connect
+from yellowbox.extras import RabbitMQService
+
+
+def test_network():
+  with docker_client() as docker_client,
+          RabbitMQService.run(docker_client) as rabbit,
+          temp_network(docker_client) as network,
+          connect(network, rabbit) as alias:
+    # yellow's "connect" function connects between a network and a
+    # Container/YellowService, retrieves the container's alias(es) on 
+    # the network, and disconnects the two when done
+    another_container = docker_client.containers.create("my-image",
+                                                        environment={"RABBITMQ_HOSTNAME": alias[0]}
+                                                        )
+    with connect(network, another_container):
+      another_container.start()
+      another_container.wait()
+```
+## As Pytest Fixtures
+Both yellow services and networks can be used fluently with `pytest` fixures
+
+```python
+from yellowbox.clients import docker_client
+from pytest import fixture
+
+from yellowbox.extras import RedisService
+
+
+@fixture
+def docker_client():
+  with docker_client() as ret:
+    yield ret
+
+
+@fixture
+def redis_service(docker_client):
+  with RedisService.run(docker_client) as service:
+    yield service
+
+
+def black_box(redis_service):
+  # run your test with the redis service provided
+  ...
+```
+since docker container may take some time to set up, it's advisable to set their scope to at least `"module"`
+
+---
+## Extending Yellow
+Users can create their own Yellow Service classes by implementing the `YellowService` abstract class.
+If the service encapsulates only a single container, the `SingleContainerService` class already implements
+the necessary methods.
+
+---
+## License
+Yellowbox is registered under the MIT public license
---
+++ packages/yellowbox/yellowbox-0.7.1.tar.gz/yellowbox-0.7.1/yellowbox/extras/postgresql.py
@@ -0,0 +1,128 @@
+from typing import Optional
+
+from docker import DockerClient
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+from yellowbox import RetrySpec, RunMixin
+from yellowbox.containers import create_and_pull, get_ports
+from yellowbox.subclasses import AsyncRunMixin, SingleContainerService
+
+__all__ = ['PostgreSQLService', 'POSTGRES_INTERNAL_PORT']
+
+from yellowbox.utils import docker_host_name
+
+POSTGRES_INTERNAL_PORT = 5432
+
+
+class PostgreSQLService(SingleContainerService, RunMixin, AsyncRunMixin):
+    """
+    A postgresSQL service
+    """
+
+    def __init__(self, docker_client: DockerClient, image='postgres:latest', *, user='postgres',
+                 password='guest', default_db: str = None, **kwargs):
+        """
+        Args:
+            user: the Name of the default user for the database
+            password: The password of the default user for the database
+            default_db: The name of the default database. Defaults to the user name.
+        """
+        if default_db is None:
+            default_db = user
+
+        self.user = user
+        self.password = password
+        self.default_db = default_db
+        super().__init__(create_and_pull(
+            docker_client, image, publish_all_ports=True, detach=True, environment={
+                'POSTGRES_USER': user,
+                'POSTGRES_PASSWORD': password,
+                'POSTGRES_DB': default_db
+            }
+        ), **kwargs)
+
+    def external_port(self):
+        return get_ports(self.container)[POSTGRES_INTERNAL_PORT]
+
+    def local_connection_string(self, dialect: str = 'postgresql', driver: str = None, database: str = None):
+        """
+        Generate an sqlalchemy-style connection string to the database in the service from the docker host.
+        Args:
+            dialect: The dialect of the sql server.
+            driver: additional driver for sqlalchemy to use.
+            database: the name of the database to connect to. Defaults to the service's default database.
+        """
+        database = database or self.default_db
+
+        if driver is not None:
+            dialect += '+' + driver
+
+        return f'{dialect}://{self.user}:{self.password}@localhost:{self.external_port()}/{database}'
+
+    def container_connection_string(self, hostname: str, dialect: str = 'postgresql', driver: str = None,
+                                    database: str = None):
+        """
+        Generate an sqlalchemy-style connection string to the database in the service from another container on a
+         common network.
+        Args:
+            hostname: the alias of the container.
+            dialect: The dialect of the sql server.
+            driver: additional driver for sqlalchemy to use.
+            database: the name of the database to connect to. Defaults to the service's default database.
+        """
+        database = database or self.default_db
+
+        if driver is not None:
+            dialect += '+' + driver
+
+        return f'{dialect}://{self.user}:{self.password}@{hostname}:{POSTGRES_INTERNAL_PORT}/{database}'
+
+    def host_connection_string(self, dialect: str = 'postgresql', driver: str = None, database: str = None):
+        """
+        Generate an sqlalchemy-style connection string to the database in the service from another container.
+        Args:
+            dialect: The dialect of the sql server.
+            driver: additional driver for sqlalchemy to use.
+            database: the name of the database to connect to. Defaults to the service's default database.
+        """
+        database = database or self.default_db
+
+        if driver is not None:
+            dialect += '+' + driver
+
+        return f'{dialect}://{self.user}:{self.password}@{docker_host_name}:{self.external_port()}/{database}'
+
+    def engine(self, **kwargs):
+        """
+        Create an sqlalchemy Engine connected to the service's default db.
+        """
+        cs = self.local_connection_string()
+        return create_engine(cs, **kwargs)
+
+    def connection(self, **kwargs):
+        """
+        Create an sqlalchemy Connection connected to the service's default db.
+        """
+        return self.engine().connect(**kwargs)
+
+    def _connect(self):
+        with self.connection():
+            return
+
+    def start(self, retry_spec: Optional[RetrySpec] = None):
+        super().start(retry_spec)
+        retry_spec = retry_spec or RetrySpec(attempts=20)
+
+        retry_spec.retry(self._connect, OperationalError)
+        return self
+
+    async def astart(self, retry_spec: Optional[RetrySpec] = None) -> None:
+        super().start(retry_spec)
+        retry_spec = retry_spec or RetrySpec(attempts=20)
+
+        await retry_spec.aretry(self._connect, OperationalError)
+
+    def stop(self, signal='SIGINT'):
+        # change in default
+        return super().stop(signal)
