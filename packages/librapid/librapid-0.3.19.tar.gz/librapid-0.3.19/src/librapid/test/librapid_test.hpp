bdb2666247a2989a1686cf591328dd665ed1ab0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:57 +0000

    Add package librapid/0.3.19/librapid-0.3.19.tar.gz

---
+++ packages/librapid/librapid-0.3.19.tar.gz/librapid-0.3.19/src/librapid/test/librapid_test.hpp
@@ -0,0 +1,12 @@
+#ifndef LIBRAPID_TEST
+#define LIBRAPID_TEST
+
+#include <librapid/config.hpp>
+
+namespace librapid::test {
+	int testLibrapid(int x);
+
+	void streamTest();
+} // namespace librapid::test
+
+#endif // LIBRAPID_TEST
