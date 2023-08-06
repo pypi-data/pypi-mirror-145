3635b0e5236eeaf12ea2bf7788b69b2d7f7870f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:54 +0000

    Add package fnshelp/0.2/fnshelp-0.2.tar.gz

---
+++ packages/fnshelp/fnshelp-0.2.tar.gz/fnshelp-0.2/fnshelp/__init__.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('text')
+
+from ner import convert
+from ner import get_entities
+
+def callme():
+    return "Welcome to functions help library. \n fns-Functions && help - duh!!"
