efff905d68c2ee7bad5464191daede1706804135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:45 +0000

    Add package ryayoung-util/0.1.2/ryayoung_util-0.1.2.tar.gz

---
+++ packages/ryayoung-util/ryayoung_util-0.1.2.tar.gz/ryayoung_util-0.1.2/src/lib.rs
@@ -0,0 +1,39 @@
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn count_max_char_repeats(val: &str) -> PyResult<u64> {
+    let mut max = 0u64;
+    let mut total = 0u64;
+
+    let mut chars = val.chars();
+    if let Some(mut c1) = chars.next() {
+        for c2 in chars {
+            if c1 == c2 {
+                total += 1;
+                if total > max {
+                    max += 1;
+                }
+            } else {
+                total = 0;
+            }
+            c1 = c2;
+        }
+    }
+
+    Ok(max + 1)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn ryayoung_util(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(count_max_char_repeats, m)?)?;
+    Ok(())
+}
