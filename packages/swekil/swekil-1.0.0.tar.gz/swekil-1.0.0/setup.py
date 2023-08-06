b642aa3fde73f3cb50b9ad8cf29751d54c844263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:49 +0000

    Add package swekil/1.0.0/swekil-1.0.0.tar.gz

---
+++ packages/swekil/swekil-1.0.0.tar.gz/swekil-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='swekil',
+    packages=find_packages(include=['swekil']),
+    version='1.0.0',
+    description='A simple webkit library',
+    author='Kai Lyons',
+    url = "https://github.com/loralighte/py38-swekil",
+    license='MIT',
+)
