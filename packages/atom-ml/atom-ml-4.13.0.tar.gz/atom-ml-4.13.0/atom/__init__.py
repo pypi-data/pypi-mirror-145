640808035e8fa4feeac2d626e3dc59137ce1eab7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:55 +0000

    Add package atom-ml/4.13.0/atom-ml-4.13.0.tar.gz

---
+++ packages/atom-ml/atom-ml-4.13.0.tar.gz/atom-ml-4.13.0/atom/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+Automated Tool for Optimized Modelling (ATOM)
+Author: Mavs
+Description: Import API and declare version.
+
+"""
+
+from atom.api import ATOMClassifier, ATOMLoader, ATOMModel, ATOMRegressor
+
+
+__version__ = "4.13.0"
