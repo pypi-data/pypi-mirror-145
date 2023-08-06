d678005b1e88200401f138a8f89501a619718c3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:10 +0000

    Add package libvcs/0.12.0b14/libvcs-0.12.0b14.tar.gz

---
+++ packages/libvcs/libvcs-0.12.0b14.tar.gz/libvcs-0.12.0b14/tests/states/test_base.py
@@ -0,0 +1,39 @@
+"""tests for libvcs repo abstract base class."""
+import pathlib
+
+from libvcs.shortcuts import create_repo
+from libvcs.states.base import BaseRepo, convert_pip_url
+
+
+def test_repr():
+    repo = create_repo(url="file://path/to/myrepo", repo_dir="/hello/", vcs="git")
+
+    str_repo = str(repo)
+    assert "GitRepo" in str_repo
+    assert "hello" in str_repo
+    assert "<GitRepo hello>" == str_repo
+
+
+def test_repr_base():
+    repo = BaseRepo(url="file://path/to/myrepo", repo_dir="/hello/")
+
+    str_repo = str(repo)
+    assert "Repo" in str_repo
+    assert "hello" in str_repo
+    assert "<BaseRepo hello>" == str_repo
+
+
+def test_ensure_dir_creates_parent_if_not_exist(tmp_path: pathlib.Path):
+    repos_path = tmp_path / "repos_path"  # doesn't exist yet
+    repo_dir = repos_path / "myrepo"
+    repo = BaseRepo(url="file://path/to/myrepo", repo_dir=repo_dir)
+
+    repo.ensure_dir()
+    assert repos_path.is_dir()
+
+
+def test_convert_pip_url():
+    url, rev = convert_pip_url(pip_url="git+file://path/to/myrepo@therev")
+
+    assert url, rev == "therev"
+    assert url, rev == "file://path/to/myrepo"
