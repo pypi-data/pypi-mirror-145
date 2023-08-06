c3483239c44b19957bef0e714b38bdf1ebfeb1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:57 +0000

    Add package hermessplitter/0.2.6/hermessplitter-0.2.6-py3-none-any.whl

---
+++ packages/hermessplitter/hermessplitter-0.2.6-py3-none-any.whl/hermessplitter/db/init_db.py
@@ -0,0 +1,25 @@
+import sqlalchemy.exc
+from sqlalchemy import create_engine
+from hermessplitter.db import tables
+
+engine = create_engine('sqlite:///hdb.db')
+engine.connect()
+tables.metadata.create_all(engine)
+try:
+    ins = tables.settings.insert().values(
+        key='active',
+        value=True
+    )
+    engine.execute(ins)
+except sqlalchemy.exc.IntegrityError:
+    pass
+
+try:
+    ins = tables.settings.insert().values(
+        key='test_mode',
+        value=True
+    )
+    engine.execute(ins)
+except sqlalchemy.exc.IntegrityError:
+    pass
+
