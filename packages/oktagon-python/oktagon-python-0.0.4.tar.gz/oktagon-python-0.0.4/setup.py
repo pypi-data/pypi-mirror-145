611e9a2446d411caf6af4fe7037f142f9d3a4a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:58 +0000

    Add package oktagon-python/0.0.4/oktagon-python-0.0.4.tar.gz

---
+++ packages/oktagon-python/oktagon-python-0.0.4.tar.gz/oktagon-python-0.0.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="oktagon-python",
+    version="0.0.4",
+    author="Made.com Tech Team",
+    author_email="andrii.piratovskyi@made.com",
+    description="Python utility package for verifying & decoding OKTA tokens",
+    url="https://github.com/madedotcom/oktagon-python",
+    install_requires=[
+        "okta_jwt_verifier",
+        "starlette",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
