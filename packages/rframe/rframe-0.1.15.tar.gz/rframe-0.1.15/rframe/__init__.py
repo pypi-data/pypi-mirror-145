3c3d889947dedec56675bab7469714c3cc98cecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:47 +0000

    Add package rframe/0.1.15/rframe-0.1.15.tar.gz

---
+++ packages/rframe/rframe-0.1.15.tar.gz/rframe-0.1.15/rframe/__init__.py
@@ -0,0 +1,18 @@
+"""Top-level package for rframe."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = "joe.mosbacher@gmail.com"
+__version__ = "0.1.15"
+
+from . import schema, indexes, utils
+
+from .indexes import Index, InterpolatingIndex, IntervalIndex
+from .types import Interval, IntegerInterval, TimeInterval
+from .rframe import RemoteFrame
+from .schema import BaseSchema
+from .rest_client import BaseRestClient, RestClient
+from .rest_server import SchemaRouter
+from .utils import jsonable
+from loguru import logger
+
+logger.disable("rframe")
