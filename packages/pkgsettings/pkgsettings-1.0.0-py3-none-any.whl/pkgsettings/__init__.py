b961e5b4f0c0d88efab6baaa7b4f00d27b5b5927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:19:05 +0000

    Add package pkgsettings/1.0.0/pkgsettings-1.0.0-py3-none-any.whl

---
+++ packages/pkgsettings/pkgsettings-1.0.0-py3-none-any.whl/pkgsettings/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from .pkgsettings import DuplicateConfigureWarning, PrefixedSettings, Settings, SimpleSettings
+
+__all__ = ["DuplicateConfigureWarning", "PrefixedSettings", "Settings", "SimpleSettings"]
