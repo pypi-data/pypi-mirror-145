6b5f942dff98dfde4c589262d5aa6efe9b1ce368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:00 +0000

    Add package siyuan/0.1.2/SiYuan-0.1.2.tar.gz

---
+++ packages/siyuan/SiYuan-0.1.2.tar.gz/SiYuan-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['siyuan']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['ToXmind = SiYuan.export_to_xmind:cli']}
+
+setup_kwargs = {
+    'name': 'siyuan',
+    'version': '0.1.2',
+    'description': 'SiYuan Api Implement',
+    'long_description': None,
+    'author': 'InEase',
+    'author_email': 'InEase28@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
