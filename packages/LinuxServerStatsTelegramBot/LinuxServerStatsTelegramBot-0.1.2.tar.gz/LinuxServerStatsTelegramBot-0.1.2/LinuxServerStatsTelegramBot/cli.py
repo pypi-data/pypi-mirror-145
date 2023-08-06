5cbf2585e22222f3ee01fb621c7e5396f73931db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:35:12 +0000

    Add package LinuxServerStatsTelegramBot/0.1.2/LinuxServerStatsTelegramBot-0.1.2.tar.gz

---
+++ packages/LinuxServerStatsTelegramBot/LinuxServerStatsTelegramBot-0.1.2.tar.gz/LinuxServerStatsTelegramBot-0.1.2/LinuxServerStatsTelegramBot/cli.py
@@ -0,0 +1,11 @@
+import click
+
+from LinuxServerStatsTelegramBot.DI import DI
+from LinuxServerStatsTelegramBot.core.Invoker import Invoker
+from LinuxServerStatsTelegramBot.sender.TelegramSender import TelegramSender
+
+
+@click.command()
+def notify_server_stats():
+    DI.set_sender(TelegramSender())
+    Invoker.send_server_stats()
