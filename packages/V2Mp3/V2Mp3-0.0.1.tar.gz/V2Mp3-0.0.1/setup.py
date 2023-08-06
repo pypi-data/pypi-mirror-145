7a3ce33c06726129a8a996885aeac930fdfae19c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:23 +0000

    Add package V2Mp3/0.0.1/V2Mp3-0.0.1.tar.gz

---
+++ packages/V2Mp3/V2Mp3-0.0.1.tar.gz/V2Mp3-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+with open('requirements.txt', 'r') as fh2:
+    reqs = fh2.read()
+
+    setup(
+        name='V2Mp3',
+        version='0.0.1',
+        description=
+        'Compact video-to-audio convertion tool built with YouTube video/audio download functionality.',
+        url='https://github.com/schlopp96/V2Mp3',
+        author='schlopp96',
+        author_email='schloppdaddy@gmail.com',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=[reqs],
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: End Users/Desktop",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        ],
+        keywords=
+        'youtube to mp3 video file convert converter download python py python3 audio'
+    )
