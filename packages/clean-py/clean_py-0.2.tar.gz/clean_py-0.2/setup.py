5e819b919a6ab045d13cb118d817c4881d61c561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:42 +0000

    Add package clean-py/0.2/clean_py-0.2.tar.gz

---
+++ packages/clean-py/clean_py-0.2.tar.gz/clean_py-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+with open("./requirements.txt") as f:
+    REQUIREMENTS = f.readlines()
+
+setup(
+    name="clean_py",
+    version="0.2",
+    url="https://github.com/samhardyhey/clean-py",
+    author="Sam Hardy",
+    author_email="samhardyhey@gmail.com",
+    packages=find_packages(),
+    # entry_points={"console_scripts": ["clean_py=clean_py.cli"]},
+    entry_points={"console_scripts": ["clean_py=clean_py.cli:main"]},
+    python_requires=">=3.6",
+    install_requires=REQUIREMENTS,
+)
