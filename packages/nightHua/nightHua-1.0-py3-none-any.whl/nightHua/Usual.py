9791a38b515541bc517304ef78013866b5c2e8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:51 +0000

    Add package nightHua/1.0/nightHua-1.0-py3-none-any.whl

---
+++ packages/nightHua/nightHua-1.0-py3-none-any.whl/nightHua/Usual.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/4/2 21:16
+# @Author : NightHua
+# @Email : wayne_lau@aliyun.com
+# @File : Usual.py
+# @Project : 我的pip模块
+
+
+def print1(x):
+    print(x+1)
---
+++ packages/nightHua/nightHua-1.0-py3-none-any.whl/nightHua/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2022/4/2 21:13
+# @Author : NightHua
+# @Email : wayne_lau@aliyun.com
+# @File : __init__.py.py
+# @Project : 我的pip模块
