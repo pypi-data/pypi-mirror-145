caaf80de2b8296901748bae9c25b89bc2c8bb175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:21 +0000

    Add package silvera/0.3.0/silvera-0.3.0-py3-none-any.whl

---
+++ packages/silvera/silvera-0.3.0-py3-none-any.whl/silvera/const.py
@@ -0,0 +1,15 @@
+
+REST = "REST"
+MVN_PACKAGE = "mvn package"
+
+HOST_PC = "PC"
+HOST_CONTAINER = "container"
+
+# HTTP verbs
+HTTP_POST = "POST"
+HTTP_PUT = "PUT"
+HTTP_GET = "GET"
+HTTP_DELETE = "DELETE"
+
+BASIC_TYPES = {"date", "i16", "i32", "i64", "bool", "int", "void", "str",
+               "double", "pwd"}
