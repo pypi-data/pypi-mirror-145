303633feeb3d2ede2d9bdb2a83266bfe24cf2027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:36:00 +0000

    Add package orq/0.0.1/orq-0.0.1.tar.gz

---
+++ packages/orq/orq-0.0.1.tar.gz/orq-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="orq",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="",
+    author="Michał Stęchły",
+    author_email="michal.stechly@zapatacomputing.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
