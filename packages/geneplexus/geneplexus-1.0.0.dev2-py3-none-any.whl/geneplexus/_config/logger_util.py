5eb4f6bcc420aa90d807f162637df66371fc651e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:24 +0000

    Add package geneplexus/1.0.0.dev2/geneplexus-1.0.0.dev2-py3-none-any.whl

---
+++ packages/geneplexus/geneplexus-1.0.0.dev2-py3-none-any.whl/geneplexus/_config/logger_util.py
@@ -0,0 +1,54 @@
+"""Logger utilities."""
+import logging
+from contextlib import contextmanager
+from typing import Optional
+
+from .config import LOG_LEVEL_TYPE
+
+
+def make_logger(
+    name: Optional[str] = None,
+    log_level: LOG_LEVEL_TYPE = "INFO",
+) -> logging.Logger:
+    """Make a basic logger used by GenePlexus.
+
+    name: Name of the logger.
+    log_level: Level of details to log
+
+    """
+    formatter = logging.Formatter("%(name)s:%(funcName)s:%(levelname)s:%(message)s")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+
+    logger_name = "geneplexus" if name is None else f"geneplexus.{name}"
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(stream_handler)
+    logger.setLevel(logging.getLevelName(log_level))
+
+    return logger
+
+
+def attach_file_handler(
+    logger: logging.Logger,
+    log_path: str,
+    log_level: LOG_LEVEL_TYPE = "INFO",
+) -> logging.FileHandler:
+    """Attach a file handler to a logger."""
+    formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    file_handler = logging.FileHandler(log_path)
+    file_handler.setLevel(logging.getLevelName(log_level))
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return file_handler
+
+
+@contextmanager
+def log_level_context(logger: logging.Logger, log_level: LOG_LEVEL_TYPE):
+    """Temporarily set a log level."""
+    prev_log_level = logger.level
+    logger.setLevel(logging.getLevelName(log_level))
+    try:
+        yield
+    finally:
+        logger.setLevel(prev_log_level)
