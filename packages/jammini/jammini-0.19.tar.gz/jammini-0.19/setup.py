d3fe3af00e0850ce7959730bfba6e3d7da8afe29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:39 +0000

    Add package jammini/0.19/jammini-0.19.tar.gz

---
+++ packages/jammini/jammini-0.19.tar.gz/jammini-0.19/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="jammini", # Replace with your own username
+    version="0.19",
+    author="jammini",
+    author_email="iddiid01@gmail.com",
+    description="jamminimodule",
+    url="https://jaemincoding.wixsite.com/jamminimodule",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
