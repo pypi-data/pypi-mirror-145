8c9d173ffd399e3093864e934e76bb66cbb88543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:12 +0000

    Add package thetagang/0.1.31/thetagang-0.1.31-py3-none-any.whl

---
+++ packages/thetagang/thetagang-0.1.31-py3-none-any.whl/thetagang/config_defaults.py
@@ -0,0 +1,11 @@
+DEFAULT_CONFIG = {
+    "target": {
+        "maximum_new_contracts_percent": 0.05,
+        "delta": 0.3,
+    },
+    "roll_when": {
+        "min_pnl": 0.0,
+        "calls": {"itm": True},
+        "puts": {"itm": False},
+    },
+}
