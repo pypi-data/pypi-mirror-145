8433738603fbd75ae473369a295eb5568adc21b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:34 +0000

    Add package ucheck/0.0.6/ucheck-0.0.6.tar.gz

---
+++ packages/ucheck/ucheck-0.0.6.tar.gz/ucheck-0.0.6/ucheck/__init__.py
@@ -0,0 +1,12 @@
+"""
+ucheck library
+~~~~~~~~~~~~~~
+
+ucheck is a simple Python library to automatically fill and
+submit your UCheck COVID-19 self-assessment form.
+"""
+
+from ucheck.ucheck import UCheck
+
+__all__ = ["UCheck"]
+__version__ = "0.0.6"
