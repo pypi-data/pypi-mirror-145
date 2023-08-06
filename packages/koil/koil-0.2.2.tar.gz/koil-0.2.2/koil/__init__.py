67980661a59c9580e99f21606a2ed455a805c3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:14 +0000

    Add package koil/0.2.2/koil-0.2.2.tar.gz

---
+++ packages/koil/koil-0.2.2.tar.gz/koil-0.2.2/koil/__init__.py
@@ -0,0 +1,6 @@
+from .helpers import unkoil, unkoil_gen
+from .decorators import koilable, unkoilable
+from .errors import CancelledError
+from .helpers import create_task
+from .types import Contextual
+from .koil import Koil
