12e7333f688eff065a84da799b27866bdb1be19f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:14 +0000

    Add package localtileserver/0.5.0/localtileserver-0.5.0.tar.gz

---
+++ packages/localtileserver/localtileserver-0.5.0.tar.gz/localtileserver-0.5.0/tests/test_app.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def test_home_page_with_file(bahamas):
+    r = requests.get(bahamas.server_base_url)
+    r.raise_for_status()
+
+
+def test_home_page(flask_client):
+    r = flask_client.get("/")
+    assert r
+    r = flask_client.get("/roi/")
+    assert r
