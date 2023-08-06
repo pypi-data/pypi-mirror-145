f3248e96379a9f65b38af7a92d092ddefe20853e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:57 +0000

    Add package ibm-watson-machine-learning/1.0.204/ibm_watson_machine_learning-1.0.204-py3-none-any.whl

---
+++ packages/ibm-watson-machine-learning/ibm_watson_machine_learning-1.0.204-py3-none-any.whl/ibm_watson_machine_learning/tests/autoai/fvt/test_autoai_async_project_forecasting_twitter_multivaried_ca_default_WebService.py
@@ -0,0 +1,98 @@
+#  (C) Copyright IBM Corp. 2021.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import unittest
+
+import ibm_boto3
+from ibm_watson_machine_learning.experiment import AutoAI
+from ibm_watson_machine_learning.helpers.connections import DataConnection, S3Location
+from ibm_watson_machine_learning.utils.autoai.errors import WMLClientError
+from ibm_watson_machine_learning.tests.utils import create_connection_to_cos
+from ibm_watson_machine_learning.tests.autoai.abstract_tests_classes import AbstractTestTSAsync, \
+    AbstractTestWebservice
+
+from ibm_watson_machine_learning.utils.autoai.enums import PredictionType, Metrics
+
+
+class TestAutoAIRemote(AbstractTestTSAsync, unittest.TestCase):
+    """
+    The test can be run on Cloud only
+    """
+
+    cos_resource = None
+    data_location = './autoai/data/Twitter_volume_AMZN_multivariate.csv'
+
+    data_cos_path = 'Twitter_volume_AMZN_multivariate.csv'
+
+    batch_payload_location = data_location
+    batch_payload_cos_location = data_cos_path
+
+    SPACE_ONLY = False
+    BATCH_DEPLOYMENT = False
+
+    OPTIMIZER_NAME = "Twitter_volume_AMZN_multivariate test sdk"
+    DEPLOYMENT_NAME = OPTIMIZER_NAME + "Deployment"
+
+    target_space_id = None
+
+    experiment_info = dict(
+        name=OPTIMIZER_NAME,
+        prediction_type=PredictionType.FORECASTING,
+        prediction_columns=['value1', 'value2'],
+        timestamp_column_name='day',
+        backtest_num=4,
+        backtest_gap_length=5,
+        lookback_window=10,
+        forecast_window=5,
+        max_number_of_estimators=2,
+        notebooks=True
+    )
+
+    def test_00b_prepare_COS_instance_and_connection(self):
+        TestAutoAIRemote.connection_id, TestAutoAIRemote.bucket_name = create_connection_to_cos(
+            wml_client=self.wml_client,
+            cos_credentials=self.cos_credentials,
+            cos_endpoint=self.cos_endpoint,
+            bucket_name=self.bucket_name,
+            save_data=True,
+            data_path=self.data_location,
+            data_cos_path=self.data_cos_path)
+
+        self.assertIsInstance(self.connection_id, str)
+
+    def test_02_data_reference_setup(self):
+        TestAutoAIRemote.data_connection = DataConnection(
+            connection_asset_id=self.connection_id,
+            location=S3Location(
+                bucket=self.bucket_name,
+                path=self.data_cos_path
+            )
+        )
+        TestAutoAIRemote.results_connection = None
+
+        self.assertIsNotNone(obj=TestAutoAIRemote.data_connection)
+        self.assertIsNone(obj=TestAutoAIRemote.results_connection)
+
+    def test_99_delete_connection(self):
+        if not self.SPACE_ONLY:
+            self.wml_client.set.default_project(self.project_id)
+
+        self.wml_client.connections.delete(self.connection_id)
+
+        with self.assertRaises(WMLClientError):
+            self.wml_client.connections.get_details(self.connection_id)
+
+
+if __name__ == '__main__':
+    unittest.main()
