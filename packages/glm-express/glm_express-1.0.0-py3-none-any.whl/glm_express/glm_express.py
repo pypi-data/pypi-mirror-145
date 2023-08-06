4352705bdf5ff4ed1194c8ba80a2c5e2c28072f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:48 +0000

    Add package glm-express/1.0.0/glm_express-1.0.0-py3-none-any.whl

---
+++ packages/glm-express/glm_express-1.0.0-py3-none-any.whl/glm_express/glm_express.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+"""
+About this Class
+"""
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=UserWarning)
+
+from subject.subject import Subject
+from group_level.group import GroupLevel
+from aggregator.aggregator import Aggregator
