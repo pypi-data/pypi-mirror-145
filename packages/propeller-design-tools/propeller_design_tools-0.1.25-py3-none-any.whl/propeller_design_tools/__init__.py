0bfd8e2882a60f5be4d749e0bd8f424f03c2cdff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:14 +0000

    Add package propeller-design-tools/0.1.25/propeller_design_tools-0.1.25-py3-none-any.whl

---
+++ packages/propeller-design-tools/propeller_design_tools-0.1.25-py3-none-any.whl/propeller_design_tools/__init__.py
@@ -0,0 +1,6 @@
+from propeller_design_tools.funcs import *
+from propeller_design_tools.settings import set_airfoil_database, set_propeller_database
+from propeller_design_tools.airfoil import Airfoil
+from propeller_design_tools.radialstation import RadialStation
+from propeller_design_tools.propeller import Propeller
+from propeller_design_tools.user_interface import InterfaceMainWindow
