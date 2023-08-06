0dd2b11e381e1d310ca883baa79dbeadc613bc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:45 +0000

    Add package mypythontools/1.0.8/mypythontools-1.0.8-py3-none-any.whl

---
+++ packages/mypythontools/mypythontools-1.0.8-py3-none-any.whl/mypythontools/paths/__init__.py
@@ -0,0 +1,13 @@
+"""Some functions around paths. You can find here `find_path()` to find some path efficiently in
+some folder, excluding some other inner folders (like venv, node_modules etc.). There is also function
+to get desktop path in posix way.
+"""
+
+from mypythontools.paths.paths_internal import (
+    find_path,
+    get_desktop_path,
+    validate_path,
+    PathLike,
+)
+
+__all__ = ["find_path", "get_desktop_path", "validate_path", "PathLike"]
