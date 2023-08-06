113e4e08e5464b4d9f9a05ec3876833a210d2de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:47 +0000

    Add package quo/2022.4/quo-2022.4-py3-none-any.whl

---
+++ packages/quo/quo-2022.4-py3-none-any.whl/quo/i_o/__init__.py
@@ -0,0 +1,9 @@
+from .termui import (
+        confirm,
+        echo,
+        edit,
+        raw_terminal,
+        flair,
+        unstyle,
+        )
+
---
+++ packages/quo/quo-2022.4-py3-none-any.whl/quo/width/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+
+
+"""
+from .width import wcwidth, wcswidth  # noqa
+
+__all__ = ('wcwidth', 'wcswidth',)
+
