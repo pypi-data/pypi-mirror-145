029bd25bc0da21872710a8d5e6e545312a7c6b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:51:18 +0000

    Add package codeforlife-portal/6.2.2/codeforlife-portal-6.2.2.tar.gz

---
+++ packages/codeforlife-portal/codeforlife-portal-6.2.2.tar.gz/codeforlife-portal-6.2.2/portal/strings/teacher_resources.py
@@ -0,0 +1,15 @@
+RAPID_ROUTER_RESOURCES_BANNER = {
+    "title": "Rapid Router Resources",
+    "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
+    "teach students Computing.",
+    "text": "",
+    "image_class": "banner--picture--play",
+}
+
+KURONO_RESOURCES_BANNER = {
+    "title": "Kurono Resources",
+    "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
+    "teach students the UK National Computing Curriculum.",
+    "text": "",
+    "image_class": "banner--picture--aimmo-resources",
+}
