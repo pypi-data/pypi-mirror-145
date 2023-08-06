fd2544fdba6fc69e95ef2eabcf2d1cfada30f587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:01:28 +0000

    Add package BioSAK/1.42.0/BioSAK-1.42.0.tar.gz

---
+++ packages/BioSAK/BioSAK-1.42.0.tar.gz/BioSAK-1.42.0/BioSAK/get_genome_GTDB.py
@@ -0,0 +1,7 @@
+
+# gtdb_op_txt                 = '/Users/songweizhi/Desktop/retrieve_GTDB_genomes/Sponge_MAGs_1677.ar122.summary.tsv'
+# gtdb_ref_gnm_folder         = '/srv/scratch/z5039045/DB/GTDB_r202/release202/fastani'
+
+gtdb_gnm_metadata           = '/Users/songweizhi/Desktop/retrieve_GTDB_genomes/ar122_metadata_r202.tsv'
+gtdb_ref_gnm_path_txt       = '/Users/songweizhi/Desktop/retrieve_GTDB_genomes/GTDB_r202_genome_paths.tsv'
+taxons_to_retrieve_txt      = '/Users/songweizhi/Desktop/retrieve_GTDB_genomes/taxons_to_retrieve.txt'
