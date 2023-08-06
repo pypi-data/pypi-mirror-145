7226d2b25ccea251f6aee7fe13e740e611dbbb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:34 +0000

    Add package django-nested-inline/0.4.5/django_nested_inline-0.4.5-py3-none-any.whl

---
+++ packages/django-nested-inline/django_nested_inline-0.4.5-py3-none-any.whl/example/app/tests/test_admin.py
@@ -0,0 +1,31 @@
+from django import VERSION
+from django.contrib.auth.models import User
+from django.template.response import TemplateResponse
+from django.test import TestCase
+
+if VERSION < (2, 0, 0):
+    from django.core.urlresolvers import reverse
+else:
+    from django.urls import reverse
+
+
+class TopLevelAdminTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.superuser = User.objects.create_superuser(username='super', password='secret', email='super@example.com')
+
+    def setUp(self):
+        try:
+            self.client.force_login(self.superuser)
+        except AttributeError:
+            self.client.login(username=self.superuser.username, password='secret')
+
+    def test_changelist(self):
+        response = self.client.get(reverse('admin:app_toplevel_changelist'))
+        self.assertIsInstance(response, TemplateResponse)
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_view(self):
+        response = self.client.get(reverse('admin:app_toplevel_add'))
+        self.assertIsInstance(response, TemplateResponse)
+        self.assertEqual(response.status_code, 200)
