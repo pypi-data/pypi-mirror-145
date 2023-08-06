1621a6cc6158ab80464c434ac59b589cadb0d402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:16:31 +0000

    Add package cmind/0.5.4/cmind-0.5.4.tar.gz

---
+++ packages/cmind/cmind-0.5.4.tar.gz/cmind-0.5.4/cmind/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.5.4"
+
+from cmind.kernel import access
+from cmind.kernel import error
+from cmind.kernel import halt
+from cmind.kernel import CM
---
+++ packages/cmind/cmind-0.5.4.tar.gz/cmind-0.5.4/cmind/cli.py
@@ -0,0 +1,45 @@
+# Collective Mind command line wrapper
+
+import sys
+
+def run(argv = None):
+    """
+    Run CM action from the command line.
+
+    Args:
+        argv (str | list): CM input
+    
+    Returns: 
+        dict: CM output:
+          - return (int): return code = 0 if successful 
+                                      > 0 if error
+
+          - (error) (str): error text if return > 0
+
+          - ...: Output from a given CM automation
+    
+    """
+
+    # Access CM
+    from cmind.kernel import CM
+
+    cm = CM(out='con')
+
+    if argv is None:
+        argv = sys.argv[1:]
+
+    r = cm.access(argv)
+
+    ret = r['return']
+
+    # Print error only in CLI
+    if ret > 0:
+       # Process error: either raise or just print to stderr
+       # depending on settings
+       cm.error(r)
+
+    sys.exit(ret)
+
+
+if __name__ == "__main__":
+    run()
