ea4ef416b3096c46c8ae28531cdcdc1490b1524f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:02 +0000

    Add package sw-aqueduct/0.1.2/sw-aqueduct-0.1.2.tar.gz

---
+++ packages/sw-aqueduct/sw-aqueduct-0.1.2.tar.gz/sw-aqueduct-0.1.2/aqueduct/utils/exceptions.py
@@ -0,0 +1,30 @@
+# Copyright: (c) 2022, Swimlane <info@swimlane.com>
+# MIT License (see LICENSE or https://opensource.org/licenses/MIT)
+
+class IncorrectParameters(Exception):
+
+    """
+    Raised when the incorrect configuration of parameters is passed into a Class
+    """
+    pass
+
+
+class MissingDefinitionFile(Exception):
+
+    """
+    Raised when a definition file cannot be find
+    """
+    pass
+
+
+class AtomicsFolderNotFound(Exception):
+
+    """Raised when unable to find a folder containing Atomics
+    """
+    pass
+
+class MalformedFile(Exception):
+
+    """Raised when a file does not meet an expected and defined format structure
+    """
+    pass
