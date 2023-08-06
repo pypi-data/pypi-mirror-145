bc2976bf5d3f29a53c95f60d46f1b24c06d3b006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:27 +0000

    Add package aws-profiles/0.1.0/aws-profiles-0.1.0.tar.gz

---
+++ packages/aws-profiles/aws-profiles-0.1.0.tar.gz/aws-profiles-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aws_profile']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['aws_profile = aws_profile.aws_profile:main']}
+
+setup_kwargs = {
+    'name': 'aws-profiles',
+    'version': '0.1.0',
+    'description': 'Simple Script to list all named profiles in ~/.aws/config',
+    'long_description': None,
+    'author': 'Brian Peterson',
+    'author_email': 'brian.peterson@cloudshift.cc',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/cloudshiftstrategies/aws-profiles',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
