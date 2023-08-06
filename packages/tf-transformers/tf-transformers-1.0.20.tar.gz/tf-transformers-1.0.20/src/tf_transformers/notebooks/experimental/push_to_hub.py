abddb71f0b2462d814d11009d9e7ad85a950d560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:39 +0000

    Add package tf-transformers/1.0.20/tf-transformers-1.0.20.tar.gz

---
+++ packages/tf-transformers/tf-transformers-1.0.20.tar.gz/tf-transformers-1.0.20/src/tf_transformers/notebooks/experimental/push_to_hub.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+import time
+from distutils.dir_util import copy_tree
+
+from absl import logging
+
+from tf_transformers.models import ViTModel
+
+logging.set_verbosity("INFO")
+
+# Load Model
+model_name = 'vit-large-patch32-384'
+model = ViTModel.from_pretrained(model_name)
+
+model = ViTModel.from_pretrained(model_name, classification_labels=1000)
+
+
+models_list = [
+    'vit-base-patch16-224',
+    'vit-base-patch32-384',
+    'vit-base-patch32-224-in21k',
+    'vit-large-patch16-224',
+    'vit-large-patch32-384',
+]
+cwd = os.getcwd()
+MODEL_DIR = '/home/sarathrnair/MODELS/'
+
+for model_name in models_list:
+
+    subprocess.run(
+        ['huggingface-cli', 'repo', 'create', '{}'.format(model_name), '--yes', '--organization', 'tftransformers']
+    )
+
+    subprocess.run(["git", "clone", "https://huggingface.co/tftransformers/{}".format(model_name)])
+    new_working_dir = os.path.join(cwd, model_name)
+    os.chdir("{}".format(new_working_dir))
+    cached_model_dir = os.path.join(MODEL_DIR, "tf_transformers_cache/{}/".format(model_name))
+    copy_tree(cached_model_dir, new_working_dir)
+
+    subprocess.run(["git-lfs", "track", "*"])
+    subprocess.run(["git", "add", "."])
+    subprocess.run(["git", "commit", "-m", "New Model"])
+    subprocess.run(["git", "push"])
+
+    os.chdir("{}".format(cwd))
+    time.sleep(2)
+    logging.info("Completed {}".format(model_name))
+    print("------------------------------------------------------------------")
