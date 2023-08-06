a1a2f0f9bb4562d0c1a46d6161a50e7d567160f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:07 +0000

    Add package pybgpkit/0.0.3a1/pybgpkit-0.0.3a1.tar.gz

---
+++ packages/pybgpkit/pybgpkit-0.0.3a1.tar.gz/pybgpkit-0.0.3a1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name='pybgpkit',
+    version='0.0.3-alpha.1',
+    description='BGPKIT tools Python bindings',
+    url='https://github.com/bgpkit/pybgpkit',
+    author='Mingwei Zhang',
+    author_email='mingwei@bgpkit.com',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        # available on pip
+        'pybgpkit-parser==0.1.0',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            "pybgpkit=bgpkit.cli:main"
+        ]
+    }
+)
