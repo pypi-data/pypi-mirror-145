c86731fc00dcd67b7ba8ca776893313380a30d36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package lully/0.0.1/lully-0.0.1-py3-none-any.whl

---
+++ packages/lully/lully-0.0.1-py3-none-any.whl/lully/__init__.py
@@ -0,0 +1,7 @@
+
+__version__ = '0.0.1'
+
+# all functions are accessible from upper level,
+#  since their dissociation into files are often not relevant for user.
+from .kotlin import *
+from .itermore import *
