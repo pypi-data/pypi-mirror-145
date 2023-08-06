1236994add4c3c88175d1c336b7fe6b48f85ddfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:26 +0000

    Add package cosecurity-amqp-lib/0.0.18/cosecurity-amqp-lib-0.0.18.tar.gz

---
+++ packages/cosecurity-amqp-lib/cosecurity-amqp-lib-0.0.18.tar.gz/cosecurity-amqp-lib-0.0.18/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cosecurity-amqp-lib',
+    version='0.0.18',
+    description='This is a package that includes all AMQP handled messages by the CoSecurity infrastructure',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
