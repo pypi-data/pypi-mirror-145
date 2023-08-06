e8cce84e6e261d758d714bc74c0815053f44f177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:04 +0000

    Add package pysigma/0.4.5/pySigma-0.4.5-py3-none-any.whl

---
+++ packages/pysigma/pySigma-0.4.5-py3-none-any.whl/sigma/pipelines/common.py
@@ -0,0 +1,43 @@
+from sigma.processing.conditions import LogsourceCondition, RuleContainsDetectionItemCondition
+
+def logsource_windows(service : str) -> LogsourceCondition:
+    return LogsourceCondition(
+        product="windows",
+        service=service,
+    )
+
+def logsource_windows_process_creation() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="process_creation",
+        product="windows",
+    )
+
+def logsource_windows_file_change() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="file_change",
+        product="windows",
+    )
+
+def logsource_windows_network_connection() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="network_connection",
+        product="windows",
+    )
+
+def logsource_windows_network_connection_initiated(initiated : bool) -> RuleContainsDetectionItemCondition:
+    return RuleContainsDetectionItemCondition(
+        field="Initiated",
+        value="true" if initiated else "false",
+    )
+
+def logsource_linux_network_connection() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="network_connection",
+        product="linux",
+    )
+
+def logsource_windows_dns_query() -> LogsourceCondition:
+    return LogsourceCondition(
+        category="dns_query",
+        product="windows",
+    )
