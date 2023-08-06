6780c02ca3c013555f2618638ddbdf919c0af7a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:55 +0000

    Add package novobibliotecas/0.0.1/novobibliotecas-0.0.1.tar.gz

---
+++ packages/novobibliotecas/novobibliotecas-0.0.1.tar.gz/novobibliotecas-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='novobibliotecas',
+    version='0.0.1',
+    description='Calculator to test',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type = 'text/markdown',
+    url='',
+    author='Matheus Aranha Lopes',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)
