5f28abc2769a661ea5dbd268bf4cea09f61b8495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:37 +0000

    Add package logger-bro/0.0.4/logger_bro-0.0.4.tar.gz

---
+++ packages/logger-bro/logger_bro-0.0.4.tar.gz/logger_bro-0.0.4/logger_bro/logger.py
@@ -0,0 +1,34 @@
+import logging
+from typing import Union
+import logging_loki
+from multiprocessing import Queue
+from logger_bro.settings import get_settings
+
+settings = get_settings()
+
+handler = logging_loki.LokiQueueHandler(
+    Queue(-1),
+    url=settings.LOKI,
+    # TODO: Create versioning for the logger. We need different logs
+    # for v1 and v2
+    tags={"application": settings.APP_NAME, "version": "v1"},
+    # Version is used internally by the client. It is irrelevant
+    # to the api version
+    version="1",
+)
+
+
+logger = logging.getLogger("loki")
+logger.setLevel(level=logging.INFO)
+logger.addHandler(handler)
+
+
+def get_loki_logger():
+    return logging.getLogger("loki")
+
+
+def log_exception(exc: Exception):
+    logger = get_loki_logger()
+    logger.exception(
+        exc, extra={"tags": {"level": logging.getLevelName(logging.ERROR)},},
+    )
