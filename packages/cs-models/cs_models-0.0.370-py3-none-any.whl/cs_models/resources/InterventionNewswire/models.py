a99b23806455d15f9bf91f159522e920ee988c9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:48 +0000

    Add package cs-models/0.0.370/cs_models-0.0.370-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.370-py3-none-any.whl/cs_models/resources/InterventionNewswire/models.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from sqlalchemy import (
+    Column,
+    Integer,
+    DateTime,
+    ForeignKey,
+    Float,
+)
+
+from ...database import Base
+
+
+class InterventionNewswireModel(Base):
+    __tablename__ = "intervention_newswires"
+
+    id = Column(Integer, primary_key=True)
+    news_id = Column(
+        Integer,
+        ForeignKey('newswires.id'),
+        nullable=False,
+    )
+    intervention_id = Column(
+        Integer,
+        ForeignKey('interventions.id'),
+        nullable=False,
+    )
+    score = Column(
+        Float,
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        # https://stackoverflow.com/questions/58776476/why-doesnt-freezegun-work-with-sqlalchemy-default-values
+        default=lambda: datetime.utcnow(),
+        onupdate=lambda: datetime.utcnow(),
+    )
