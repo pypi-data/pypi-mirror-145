176ad5bae653f52be3204145772d8fd09383f141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:57 +0000

    Add package mpl-data-cast/0.2.3/mpl_data_cast-0.2.3.tar.gz

---
+++ packages/mpl-data-cast/mpl_data_cast-0.2.3.tar.gz/mpl_data_cast-0.2.3/tests/test_rcp_rtdc.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from helper import retrieve_data
+
+from mpl_data_cast.mod_recipes import RTDCRecipe
+
+import dclab
+
+
+def test_rcp_rtdc_base(tmp_path):
+    path_in = retrieve_data("rcp_rtdc_mask-contour_2018.zip")
+
+    rcp = RTDCRecipe(path_raw=path_in, path_tar=tmp_path)
+    rcp.cast()
+
+    assert (path_in / "M001_SoftwareSettings.ini").exists()
+    assert not (tmp_path / "M001_SoftwareSettings.ini").exists()
+
+    pin = path_in / "M001_data.rtdc"
+    pout = tmp_path / "M001_data.rtdc"
+
+    with dclab.new_dataset(pin) as ds1, dclab.new_dataset(pout) as ds2:
+        assert "M001_SoftwareSettings.ini" not in ds1.logs
+        assert "M001_SoftwareSettings.ini" in ds2.logs
+        assert np.all(ds1["deform"] == ds2["deform"])
+
+    # Make sure compression worked (even with the ini file in it)
+    assert pin.stat().st_size > pout.stat().st_size
+
+
+def test_rcp_rtdc_with_other_files(tmp_path):
+    path_in = retrieve_data("rcp_rtdc_mask-contour_2018.zip")
+    # an additional file that should be copied
+    (path_in / "additional.docx").touch()
+
+    rcp = RTDCRecipe(path_raw=path_in, path_tar=tmp_path)
+    rcp.cast()
+
+    assert (tmp_path / "additional.docx").exists()
