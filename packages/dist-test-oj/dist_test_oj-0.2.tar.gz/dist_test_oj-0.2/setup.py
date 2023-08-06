0cd472998a8c9ee8f08f2fd4f4539fe76e087c02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:28 +0000

    Add package dist-test-oj/0.2/dist_test_oj-0.2.tar.gz

---
+++ packages/dist-test-oj/dist_test_oj-0.2.tar.gz/dist_test_oj-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dist_test_oj',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['dist_test_oj'],
+      author = 'OJ Liseth',
+      author_email = 'myemail@something.com',
+      zip_safe=False)
