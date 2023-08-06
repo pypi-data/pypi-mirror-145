99dfcbdf259e4c5a5c7e79b1d9fc371ac71df039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:31 +0000

    Add package costeopy-package/0.0.2/costeopy_package-0.0.2.tar.gz

---
+++ packages/costeopy-package/costeopy_package-0.0.2.tar.gz/costeopy_package-0.0.2/micospy/costeopy_package/costeopy.py
@@ -0,0 +1,18 @@
+import math
+
+def mancha(num1,num2):
+    return num1+num2
+
+def leo(num1,num2):
+    return num1-num2
+
+def vaca(num1,num2):
+    return num1*num2
+
+def mila(num1,num2):
+    return num1/num2
+
+def tope(UIT_porc,UIT):
+    x=math.ceil(0.09*UIT_porc*UIT)
+    return x
+
