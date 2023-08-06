c6aa6606c4f15d135fa2ca7211b8e5547170cf54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:52 +0000

    Add package HuTao-agent/1.0.1/HuTao%20agent-1.0.1.tar.gz

---
+++ packages/HuTao-agent/HuTao%20agent-1.0.1.tar.gz/HuTao agent-1.0.1/test/test_wda.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @File    : test_wda.py
+# @Date    : 2022-01-22
+# @Author  : chenbo
+
+__author__ = 'chenbo'
+
+import time
---
+++ packages/HuTao-agent/HuTao%20agent-1.0.1.tar.gz/HuTao agent-1.0.1/test/test_ws.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# @File    : test_ws.py
+# @Date    : 2022-01-26
+# @Author  : chenbo
+
+__author__ = 'chenbo'
+
+
