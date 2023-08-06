27a1b107f445d52c7983a015d8bae9798ddf9d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:44 +0000

    Add package ihatemoney/5.2.0/ihatemoney-5.2.0.tar.gz

---
+++ packages/ihatemoney/ihatemoney-5.2.0.tar.gz/ihatemoney-5.2.0/ihatemoney/default_settings.py
@@ -0,0 +1,37 @@
+# Verbose and documented settings are in conf-templates/ihatemoney.cfg.j2
+DEBUG = SQLACHEMY_ECHO = False
+SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/ihatemoney.db"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = "tralala"
+MAIL_DEFAULT_SENDER = "Budget manager <admin@example.com>"
+SHOW_ADMIN_EMAIL = True
+ACTIVATE_DEMO_PROJECT = True
+ADMIN_PASSWORD = ""
+ALLOW_PUBLIC_PROJECT_CREATION = True
+ACTIVATE_ADMIN_DASHBOARD = False
+SESSION_COOKIE_SECURE = True
+SUPPORTED_LANGUAGES = [
+    "de",
+    "el",
+    "en",
+    "eo",
+    "es_419",
+    "fr",
+    "hi",
+    "id",
+    "it",
+    "ja",
+    "nb_NO",
+    "nl",
+    "pl",
+    "pt",
+    "pt_BR",
+    "ru",
+    "sv",
+    "ta",
+    "tr",
+    "uk",
+    "zh_Hans",
+]
+ENABLE_CAPTCHA = False
+LEGAL_LINK = ""
