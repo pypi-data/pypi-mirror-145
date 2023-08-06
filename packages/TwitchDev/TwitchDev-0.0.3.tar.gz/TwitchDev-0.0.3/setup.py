f4219c90de05ddb57dfc1331200ab373c5630cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:59 +0000

    Add package TwitchDev/0.0.3/TwitchDev-0.0.3.tar.gz

---
+++ packages/TwitchDev/TwitchDev-0.0.3.tar.gz/TwitchDev-0.0.3/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+  name="TwitchDev",
+  version="0.0.3",
+  author="dndbxkwh",
+  author_email="dndbxkwh@gmail.com",
+  description="TwitchDev",
+  long_description='*TwitchDev*',
+  long_description_content_type="text/markdown",
+  url="https://github.com/dndbxkwh/TwitchDev",
+  packages=setuptools.find_packages(),
+  classifiers=["Programming Language :: Python :: 3"],
+  python_requires='>=3.6',
+)
