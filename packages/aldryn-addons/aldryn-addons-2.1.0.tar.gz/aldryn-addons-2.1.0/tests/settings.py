211123580813fe11857200032ace34479e9ea17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:13 +0000

    Add package aldryn-addons/2.1.0/aldryn-addons-2.1.0.tar.gz

---
+++ packages/aldryn-addons/aldryn-addons-2.1.0.tar.gz/aldryn-addons-2.1.0/tests/settings.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+HELPER_SETTINGS = {
+    "INSTALLED_APPS": [],
+    "SECRET_KEY": "some-secret",
+    "ALLOWED_HOSTS": ["localhost"],
+    "CMS_LANGUAGES": {
+        1: [
+            {
+                "code": "en",
+                "name": "English",
+            }
+        ]
+    },
+    "LANGUAGE_CODE": "en",
+}
+
+
+def run():
+    from app_helper import runner
+
+    try:
+        runner.cms("aldryn_addons")
+    except ImportError:
+        runner.run("aldryn_addons")
+
+
+if __name__ == "__main__":
+    run()
