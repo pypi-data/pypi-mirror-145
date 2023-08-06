589ef50bb6a2a8a842389b010da3aae6e2aa2384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:39 +0000

    Add package example4bim2/0.0.4/example4bim2-0.0.4.tar.gz

---
+++ packages/example4bim2/example4bim2-0.0.4.tar.gz/example4bim2-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example4bim2",
+    version="0.0.4",
+    author="Team2",
+    author_email="seungyun.shin@insa-lyon.fr",
+    description="A example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ruthh1/Projet-4bim",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+            ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires= ">=3.6",
+
+)
