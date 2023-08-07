ff76d7b6725d4c784f0499237a8cb6b66a865ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:56:01 +0000

    Add package amk.kakashi/0.1.0/amk_kakashi-0.1.0-py3-none-any.whl

---
+++ packages/amk.kakashi/amk_kakashi-0.1.0-py3-none-any.whl/kakashi/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+from kakashi.arg_parse import *
+from kakashi.logger import *
+from kakashi.kakashi import Kakashi
+
+
+def main():
+    Kakashi()
+
+
+if __name__ == '__main__':
+    main()
